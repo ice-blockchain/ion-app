f870c097eaf8bd76f30c2aba9daaf9a68b7c501b
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-10-06 12:55:38 +0400

    refactor: simplify the DropDownMenu widget

--- lib/app/shared/widgets/drop_down_menu/drop_down_menu.dart
+++ lib/app/shared/widgets/drop_down_menu/drop_down_menu.dart
@@ -2,52 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:ice/app/features/core/providers/template_provider.dart';
 
 /// Basically just a MenuAnchor with some predefined values from the Template
-class DropDownMenu extends StatelessWidget {
-  const DropDownMenu({
+class DropDownMenu extends MenuAnchor {
+  DropDownMenu({
     super.key,
-    this.controller,
-    this.childFocusNode,
-    this.style,
-    this.alignmentOffset,
-    this.clipBehavior = Clip.hardEdge,
-    this.anchorTapClosesMenu = false,
-    this.onOpen,
-    this.onClose,
-    this.crossAxisUnconstrained = true,
-    required this.menuChildren,
-    this.builder,
-    this.child,
-  });
-  final MenuController? controller;
-  final FocusNode? childFocusNode;
-  final MenuStyle? style;
-  final Offset? alignmentOffset;
-  final Clip clipBehavior;
-  final bool anchorTapClosesMenu;
-  final void Function()? onOpen;
-  final void Function()? onClose;
-  final bool crossAxisUnconstrained;
-  final List<Widget> menuChildren;
-  final Widget Function(BuildContext, MenuController, Widget?)? builder;
-  final Widget? child;
-
-  @override
-  Widget build(BuildContext context) {
-    return MenuAnchor(
-      key: key,
-      alignmentOffset:
-          alignmentOffset ?? Offset(0, appTemplate.menuAnchor.alignmentOffsetY),
-      controller: controller,
-      childFocusNode: childFocusNode,
-      style: style,
-      clipBehavior: clipBehavior,
-      anchorTapClosesMenu: anchorTapClosesMenu,
-      onOpen: onOpen,
-      onClose: onClose,
-      crossAxisUnconstrained: crossAxisUnconstrained,
-      menuChildren: menuChildren,
-      builder: builder,
-      child: child,
-    );
-  }
+    super.controller,
+    super.childFocusNode,
+    super.style,
+    Offset? alignmentOffset,
+    super.clipBehavior = Clip.hardEdge,
+    super.anchorTapClosesMenu = false,
+    super.onOpen,
+    super.onClose,
+    super.crossAxisUnconstrained = true,
+    required super.menuChildren,
+    super.builder,
+    super.child,
+  }) : super(
+          alignmentOffset: alignmentOffset ??
+              Offset(0, appTemplate.menuAnchor.alignmentOffsetY),
+        );
 }
