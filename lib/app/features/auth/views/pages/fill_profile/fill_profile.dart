9d3be6b5f60c8dc4788d78459d614f675955a538
ice-hector <96414297+ice-hector@users.noreply.github.com>
2023-11-29 15:26:52 +0200

    chore: add keyboard avoiding for active textfields

--- lib/app/features/auth/views/pages/fill_profile/fill_profile.dart
+++ lib/app/features/auth/views/pages/fill_profile/fill_profile.dart
@@ -50,7 +50,6 @@ class FillProfile extends HookConsumerWidget {
       nameFieldKey.currentState!.validateText();
       nicknameFieldKey.currentState!.validateText();
       inviterFieldKey.currentState!.validateText();
-      // final CroppedFile? croppedFile = ref.read(croppedFileProvider);
     }
 
     final Widget profileImage = ref.watch(croppedFileProvider) != null
@@ -75,101 +74,101 @@ class FillProfile extends HookConsumerWidget {
           );
 
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.symmetric(horizontal: defaultEdgeInset),
-        child: Column(
-          children: <Widget>[
-            const SizedBox(
-              height: 65,
-            ),
-            Image.asset(
-              Assets.images.iceRound.path,
-            ),
-            const SizedBox(
-              height: 19,
-            ),
-            Text(
-              'Your profile',
-              style: context.theme.appTextThemes.headline1,
-            ),
-            Text(
-              'Customize your account',
-              style: context.theme.appTextThemes.body2.copyWith(
-                color: context.theme.appColors.tertararyText,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: defaultEdgeInset),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 65,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Stack(
-              children: <Widget>[
-                profileImage,
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: GestureDetector(
-                    onTap: addPhoto,
-                    child: Container(
-                      width: 36,
-                      height: 36,
-                      decoration: const BoxDecoration(),
-                      child: Image.asset(
-                        Assets.images.profileCamera.path,
+              Image.asset(
+                Assets.images.iceRound.path,
+              ),
+              const SizedBox(
+                height: 19,
+              ),
+              Text(
+                'Your profile',
+                style: context.theme.appTextThemes.headline1,
+              ),
+              Text(
+                'Customize your account',
+                style: context.theme.appTextThemes.body2.copyWith(
+                  color: context.theme.appColors.tertararyText,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Stack(
+                children: <Widget>[
+                  profileImage,
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: GestureDetector(
+                      onTap: addPhoto,
+                      child: Container(
                         width: 36,
                         height: 36,
+                        decoration: const BoxDecoration(),
+                        child: Image.asset(
+                          Assets.images.profileCamera.path,
+                          width: 36,
+                          height: 36,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 28,
-            ),
-            TextFieldWrapper(
-              defaultIcon: AssetImage(Assets.images.fieldName.path),
-              onTextChanged: (String text) {},
-              placeholder: 'Name',
-              validator: validateName,
-              key: nameFieldKey,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            TextFieldWrapper(
-              defaultIcon: AssetImage(Assets.images.fieldNickname.path),
-              onTextChanged: (String text) {},
-              placeholder: 'Nickname',
-              validator: validateNickname,
-              key: nicknameFieldKey,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            TextFieldWrapper(
-              defaultIcon: AssetImage(Assets.images.fieldInviter.path),
-              onTextChanged: (String text) {},
-              placeholder: 'Who invited you',
-              validator: validateWhoInvited,
-              key: inviterFieldKey,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: Button(
-                leadingIcon: ImageIcon(
-                  AssetImage(Assets.images.profileSave.path),
-                  size: 24,
-                ),
-                onPressed: () {
-                  onSave();
-                },
-                label: const Text('Save'),
-                mainAxisSize: MainAxisSize.max,
+                ],
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 28,
+              ),
+              TextFieldWrapper(
+                defaultIcon: AssetImage(Assets.images.fieldName.path),
+                onTextChanged: (String text) {},
+                placeholder: 'Name',
+                validator: validateName,
+                key: nameFieldKey,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFieldWrapper(
+                defaultIcon: AssetImage(Assets.images.fieldNickname.path),
+                onTextChanged: (String text) {},
+                placeholder: 'Nickname',
+                validator: validateNickname,
+                key: nicknameFieldKey,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFieldWrapper(
+                defaultIcon: AssetImage(Assets.images.fieldInviter.path),
+                onTextChanged: (String text) {},
+                placeholder: 'Who invited you',
+                validator: validateWhoInvited,
+                key: inviterFieldKey,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Button(
+                  leadingIcon: ImageIcon(
+                    AssetImage(Assets.images.profileSave.path),
+                    size: 24,
+                  ),
+                  onPressed: onSave,
+                  label: const Text('Save'),
+                  mainAxisSize: MainAxisSize.max,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
