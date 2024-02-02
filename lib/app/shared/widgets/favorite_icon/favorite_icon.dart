6846fab1d40072021e2ba73382d90a4323936330
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-02-02 14:05:09 +0200

    chore: add favourite icon

---
+++ lib/app/shared/widgets/favorite_icon/favorite_icon.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:ice/app/extensions/build_context.dart';
+import 'package:ice/app/extensions/theme_data.dart';
+import 'package:ice/generated/assets.gen.dart';
+
+const double iconSideDimension = 36.0;
+
+class FavoriteIcon extends StatelessWidget {
+  const FavoriteIcon({
+    super.key,
+    this.isFavorite = false,
+    this.backgroundColor,
+  });
+
+  final bool isFavorite;
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final String iconPath = isFavorite
+        ? Assets.images.bookmarksOn.path
+        : Assets.images.bookmarks.path;
+
+    final Color iconBackgroundColor =
+        backgroundColor ?? context.theme.appColors.tertararyBackground;
+    return Container(
+      width: iconSideDimension,
+      height: iconSideDimension,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: iconBackgroundColor,
+        border: Border.all(
+          color: context.theme.appColors.onTerararyFill,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            iconPath,
+            width: 24,
+          ),
+        ],
+      ),
+    );
+  }
+}
