457557a7fcbb785df7ef976228b062ce4e352425
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-02-08 19:18:42 +0200

    refactor: shadow approach

--- lib/app/constants/ui.dart
+++ lib/app/constants/ui.dart
@@ -1,14 +1,3 @@
-import 'package:flutter/material.dart';
-
 const double kDefaultPadding = 44.0;
 const double kDefaultIconButtonSide = 44.0;
 const double kDefaultNavHeaderTopPadding = 50.0;
-
-const TextStyle shadowStyle = TextStyle(
-  shadows: <Shadow>[
-    Shadow(
-      offset: Offset(0, 0.5),
-      color: Color.fromRGBO(0, 0, 0, 0.4),
-    ),
-  ],
-);
