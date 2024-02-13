7f6c9572aa4423b8b71f0631cbbe8a176dc86217
ice-pallas <106649717+ice-pallas@users.noreply.github.com>
2024-02-13 12:21:25 -0500

    feat: better dart formatter to avoid generated files

--- scripts/format_code.sh
+++ scripts/format_code.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-dart format --set-exit-if-changed .
+FILES=$(find lib test -type f -name '*.dart' -not \( -path 'lib/generated*' -o -path 'lib/l10n*' \) -not \( -name '*.freezed.dart' -o -name '*.g.dart' \))
+dart format --set-exit-if-changed $FILES
