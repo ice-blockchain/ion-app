b6ae49722436468279f03b474c38505b581c1065
ice-pallas <106649717+ice-pallas@users.noreply.github.com>
2024-02-13 01:39:50 -0500

    refactor: moved routing_utils.dart to routing directory

--- lib/app/router/hooks/use_app_router.dart
+++ lib/app/router/hooks/use_app_router.dart
@@ -3,7 +3,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ice/app/router/app_router_listenable.dart';
-import 'package:ice/app/utils/routing_utils.dart';
+import 'package:ice/app/router/routing_utils.dart';
 
 final GlobalKey<NavigatorState> _rootNavigatorKey = GlobalKey<NavigatorState>();
 
