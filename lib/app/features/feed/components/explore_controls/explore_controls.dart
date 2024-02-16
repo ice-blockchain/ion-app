ab781f0ca57aa10b7eff4b1f1478368a290dd358
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-02-16 12:34:44 +0400

    chore: add icon_home_notification, style explore controls

--- lib/app/features/feed/components/explore_controls/explore_controls.dart
+++ lib/app/features/feed/components/explore_controls/explore_controls.dart
@@ -1,37 +1,68 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:ice/app/components/button/button.dart';
 import 'package:ice/app/components/search_input/search_input.dart';
+import 'package:ice/app/extensions/build_context.dart';
 import 'package:ice/app/extensions/num.dart';
+import 'package:ice/app/extensions/theme_data.dart';
 import 'package:ice/generated/assets.gen.dart';
 
 class ExploreControls extends HookWidget {
   const ExploreControls({super.key});
   @override
   Widget build(BuildContext context) {
-    final ValueNotifier<bool> focused = useState(false);
+    return Padding(
+      padding: EdgeInsets.only(top: 9.0.s),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: SearchInput(
+              onTextChanged: (String st) {},
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 12.0.s),
+            child: ExploreButton(
+              onPressed: () {},
+              iconPath: Assets.images.icons.iconHomeNotification.path,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 12.0.s),
+            child: ExploreButton(
+              onPressed: () {},
+              iconPath: Assets.images.icons.iconHeaderMenu.path,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: SearchInput(
-            onTextChanged: (String st) {},
-          ),
-        ),
-        if (!focused.value)
-          Button.icon(
-            type: ButtonType.outlined,
-            size: 40.0.s,
-            onPressed: () {},
-            icon: ButtonIcon(Assets.images.icons.iconLoginApplelogo.path),
-          ),
-        Button.icon(
-          type: ButtonType.outlined,
-          size: 40.0.s,
-          onPressed: () {},
-          icon: ButtonIcon(Assets.images.icons.iconLoginApplelogo.path),
-        ),
-      ],
+class ExploreButton extends StatelessWidget {
+  const ExploreButton({
+    super.key,
+    required this.iconPath,
+    required this.onPressed,
+  });
+
+  final String iconPath;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Button.icon(
+      tintColor: context.theme.appColors.onTerararyFill,
+      size: 40.0.s,
+      onPressed: onPressed,
+      icon: ButtonIcon(
+        iconPath,
+        color: context.theme.appColors.primaryText,
+      ),
+      style: OutlinedButton.styleFrom(
+        backgroundColor: context.theme.appColors.tertararyBackground,
+      ),
     );
   }
 }
