1019dd1a651ae67ee78aeae3cc4ba97bbef01e0e
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-02-20 15:27:37 +0400

    chore: add compact num format

--- lib/app/features/feed/views/pages/feed_page/components/trending_videos/trending_videos.dart
+++ lib/app/features/feed/views/pages/feed_page/components/trending_videos/trending_videos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ice/app/extensions/num.dart';
 import 'package:ice/app/features/feed/views/pages/feed_page/components/trending_videos/components/trending_videos_header.dart';
 import 'package:ice/app/features/feed/views/pages/feed_page/components/trending_videos/components/trending_videos_list.dart';
 
@@ -7,11 +8,14 @@ class TrendingVideos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: <Widget>[
-        TrendingVideosHeader(),
-        TrendingVideosList(),
-      ],
+    return Padding(
+      padding: EdgeInsets.only(bottom: 18.0.s),
+      child: const Column(
+        children: <Widget>[
+          TrendingVideosHeader(),
+          TrendingVideosList(),
+        ],
+      ),
     );
   }
 }
--- lib/app/utils/num.dart
+++ lib/app/utils/num.dart
@@ -4,3 +4,7 @@ String formatDouble(double value) {
   final NumberFormat formatter = NumberFormat('#,##0.00', 'en_US');
   return formatter.format(value);
 }
+
+String formatDoubleCompact(num value) {
+  return NumberFormat.compact().format(value);
+}
