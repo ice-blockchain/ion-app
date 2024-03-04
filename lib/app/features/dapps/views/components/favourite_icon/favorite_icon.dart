f12a90668e33e73cd0a56b92488a2bf0a388c4b1
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-03-04 20:07:31 +0200

    fix: favourite button

--- lib/app/features/dapps/views/components/favourite_icon/favorite_icon.dart
+++ lib/app/features/dapps/views/components/favourite_icon/favorite_icon.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:ice/app/extensions/build_context.dart';
+import 'package:ice/app/components/button/button.dart';
 import 'package:ice/app/extensions/num.dart';
-import 'package:ice/app/extensions/theme_data.dart';
 import 'package:ice/generated/assets.gen.dart';
 
-final double iconSideDimension = 36.0.s;
+final double iconSideDimension = 40.0.s;
 
 class FavouriteIcon extends StatelessWidget {
   const FavouriteIcon({
     super.key,
     this.isFavourite = false,
-    this.backgroundColor,
     this.onTap,
   });
 
   final bool isFavourite;
-  final Color? backgroundColor;
   final VoidCallback? onTap;
 
   @override
@@ -24,30 +21,16 @@ class FavouriteIcon extends StatelessWidget {
         ? Assets.images.icons.iconBookmarksOn.path
         : Assets.images.icons.iconBookmarks.path;
 
-    final Color iconBackgroundColor =
-        backgroundColor ?? context.theme.appColors.tertararyBackground;
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        width: iconSideDimension,
-        height: iconSideDimension,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12.0.s),
-          color: iconBackgroundColor,
-          border: Border.all(
-            color: context.theme.appColors.onTerararyFill,
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              iconPath,
-              width: 24.0.s,
-            ),
-          ],
-        ),
+    return Button.icon(
+      onPressed: () {
+        onTap?.call();
+      },
+      icon: Image.asset(
+        iconPath,
+        width: 24.0.s,
       ),
+      type: ButtonType.outlined,
+      size: iconSideDimension,
     );
   }
 }
