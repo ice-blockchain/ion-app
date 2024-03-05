255b6995520d823483f7df8cf9e04a8edd6adb5f
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-03-05 21:56:06 +0400

    chore: use SizedBox.square over SizedBox with width and height

--- lib/app/extensions/asset_gen_image.dart
+++ lib/app/extensions/asset_gen_image.dart
@@ -4,9 +4,8 @@ import 'package:ice/generated/assets.gen.dart';
 
 extension IconExtension on AssetGenImage {
   Widget icon({Color? color, double? size}) {
-    return SizedBox(
-      width: size ?? 24.0.s,
-      height: size ?? 24.0.s,
+    return SizedBox.square(
+      dimension: size ?? 24.0.s,
       child: FittedBox(
         child: ImageIcon(AssetImage(path), color: color),
       ),
