b4b57e6a1fdd85eaecc81535d0513ed2f9968eaf
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-03-11 15:07:16 +0400

    feat: add floating screen header

--- lib/app/features/feed/views/pages/feed_page/feed_page.dart
+++ lib/app/features/feed/views/pages/feed_page/feed_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ice/app/components/screen_offset/screen_top_offset.dart';
 import 'package:ice/app/components/template/ice_page.dart';
 import 'package:ice/app/extensions/num.dart';
 import 'package:ice/app/features/feed/components/post/post.dart';
@@ -10,6 +9,7 @@ import 'package:ice/app/features/feed/views/pages/feed_page/components/list_sepa
 import 'package:ice/app/features/feed/views/pages/feed_page/components/stories/stories.dart';
 import 'package:ice/app/features/feed/views/pages/feed_page/components/trending_videos/trending_videos.dart';
 import 'package:ice/app/features/user/pages/pull_right_menu_page/pull_right_menu_handler.dart';
+import 'package:ice/app/router/components/floating_app_bar/floating_app_bar.dart';
 
 enum FeedType { feed, videos, stories }
 
@@ -25,24 +25,23 @@ class FeedPage extends IceSimplePage {
         body: CustomScrollView(
           controller: scrollController,
           slivers: <Widget>[
+            FloatingAppBar(
+              height: FeedControls.height,
+              child: FeedControls(
+                pageScrollController: scrollController,
+              ),
+            ),
             SliverToBoxAdapter(
-              child: ScreenTopOffset(
-                child: Column(
-                  children: <Widget>[
-                    FeedControls(
-                      pageScrollController: scrollController,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 16.0.s),
-                      child: const Stories(),
-                    ),
-                    FeedListSeparator(
-                      height: 16.0.s,
-                    ),
-                    const TrendingVideos(),
-                    FeedListSeparator(),
-                  ],
-                ),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 16.0.s, top: 3.0.s),
+                    child: const Stories(),
+                  ),
+                  FeedListSeparator(height: 16.0.s),
+                  const TrendingVideos(),
+                  FeedListSeparator(),
+                ],
               ),
             ),
             SliverList.separated(
---
+++ lib/app/router/components/floating_app_bar/floating_app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:ice/app/components/screen_offset/screen_top_offset.dart';
+import 'package:ice/app/extensions/build_context.dart';
+import 'package:ice/app/extensions/num.dart';
+import 'package:ice/app/extensions/theme_data.dart';
+
+class FloatingAppBar extends StatelessWidget {
+  FloatingAppBar({
+    super.key,
+    required this.child,
+    required this.height,
+    double? bottomOffset,
+    double? topOffset,
+  })  : bottomOffset = bottomOffset ?? 10.0.s,
+        topOffset = topOffset ?? ScreenTopOffset.defaultMargin;
+
+  final Widget child;
+  final double height;
+  final double bottomOffset;
+  final double topOffset;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      elevation: 50,
+      floating: true,
+      shadowColor:
+          context.theme.appColors.onTertararyBackground.withOpacity(0.05),
+      toolbarHeight: height + topOffset + bottomOffset,
+      backgroundColor: context.theme.appColors.onPrimaryAccent,
+      surfaceTintColor: context.theme.appColors.onPrimaryAccent,
+      flexibleSpace: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return ScreenTopOffset(margin: topOffset, child: child);
+        },
+      ),
+    );
+  }
+}
