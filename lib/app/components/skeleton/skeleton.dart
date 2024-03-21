b98dca0a66b3536b788ad37138a3891e1c0699e7
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-03-21 13:45:18 +0400

    feat: add skeleton wrapper, stories loading state

---
+++ lib/app/components/skeleton/skeleton.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ice/app/extensions/build_context.dart';
+import 'package:ice/app/extensions/theme_data.dart';
+import 'package:shimmer/shimmer.dart';
+
+class Skeleton extends StatelessWidget {
+  const Skeleton({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: context.theme.appColors.primaryBackground,
+      highlightColor: context.theme.appColors.tertararyBackground,
+      child: child,
+    );
+  }
+}
---
+++ lib/app/features/feed/views/pages/feed_page/components/stories/components/story_list_item_skeleton.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:ice/app/extensions/build_context.dart';
+import 'package:ice/app/extensions/num.dart';
+import 'package:ice/app/extensions/theme_data.dart';
+import 'package:ice/app/features/feed/views/pages/feed_page/components/stories/components/story_list_item.dart';
+
+class StoryListItemSkeleton extends StatelessWidget {
+  const StoryListItemSkeleton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle = context.theme.appTextThemes.caption3;
+
+    return SizedBox(
+      width: StoryListItem.width,
+      height: StoryListItem.height,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            width: StoryListItem.width,
+            height: StoryListItem.width,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(19.5.s),
+            ),
+          ),
+          Container(
+            height: textStyle.fontSize! * textStyle.height!,
+            alignment: Alignment.center,
+            child: Container(
+              height: textStyle.fontSize,
+              width: StoryListItem.width * 0.8,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(textStyle.fontSize! / 2),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
