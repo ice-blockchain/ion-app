029aae536d134215ba8604f151f8f6cb2d2c74d2
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-03-22 16:17:13 +0400

    fix: set colors to post icons

--- lib/app/features/feed/components/post/components/post_footer/post_footer.dart
+++ lib/app/features/feed/components/post/components/post_footer/post_footer.dart
@@ -3,7 +3,9 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:ice/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ice/app/extensions/asset_gen_image.dart';
+import 'package:ice/app/extensions/build_context.dart';
 import 'package:ice/app/extensions/num.dart';
+import 'package:ice/app/extensions/theme_data.dart';
 import 'package:ice/app/features/feed/components/post/components/post_footer/post_engagement_metric.dart';
 import 'package:ice/app/features/feed/components/post/components/post_footer/post_metric_space.dart';
 import 'package:ice/generated/assets.gen.dart';
@@ -31,34 +33,49 @@ class PostFooter extends StatelessWidget {
           PostMetricSpace(
             child: PostEngagementMetric(
               onPressed: () {},
-              icon: Assets.images.icons.iconBlockComment.icon(size: 16.0.s),
+              icon: Assets.images.icons.iconBlockComment.icon(
+                size: 16.0.s,
+                color: context.theme.appColors.onTertararyBackground,
+              ),
               value: '121k',
             ),
           ),
           PostMetricSpace(
             child: PostEngagementMetric(
               onPressed: () {},
-              icon: Assets.images.icons.iconBlockRepost.icon(size: 16.0.s),
+              icon: Assets.images.icons.iconBlockRepost.icon(
+                size: 16.0.s,
+                color: context.theme.appColors.onTertararyBackground,
+              ),
               value: '442k',
             ),
           ),
           PostMetricSpace(
             child: PostEngagementMetric(
               onPressed: () {},
-              icon: Assets.images.icons.iconVideoLikeOff.icon(size: 16.0.s),
+              icon: Assets.images.icons.iconVideoLikeOff.icon(
+                size: 16.0.s,
+                color: context.theme.appColors.onTertararyBackground,
+              ),
               value: '121k',
             ),
           ),
           PostMetricSpace(
             child: PostEngagementMetric(
               onPressed: () {},
-              icon: Assets.images.icons.iconButtonIceStroke.icon(size: 16.0.s),
+              icon: Assets.images.icons.iconButtonIceStroke.icon(
+                size: 16.0.s,
+                color: context.theme.appColors.onTertararyBackground,
+              ),
               value: '7',
             ),
           ),
           PostEngagementMetric(
             onPressed: () {},
-            icon: Assets.images.icons.iconBlockShare.icon(size: 16.0.s),
+            icon: Assets.images.icons.iconBlockShare.icon(
+              size: 16.0.s,
+              color: context.theme.appColors.onTertararyBackground,
+            ),
           ),
         ],
       ),
