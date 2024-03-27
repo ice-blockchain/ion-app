0bc5693eba6722283fcb40222cdffb51fd04f751
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-03-27 12:34:21 +0400

    refactor: select lang page

---
+++ lib/app/features/auth/views/pages/select_languages/language_list_item.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:ice/app/extensions/asset_gen_image.dart';
+import 'package:ice/app/extensions/build_context.dart';
+import 'package:ice/app/extensions/num.dart';
+import 'package:ice/app/extensions/theme_data.dart';
+import 'package:ice/app/features/auth/views/pages/select_languages/languages.dart';
+import 'package:ice/generated/assets.gen.dart';
+
+class LanguageListItem extends StatelessWidget {
+  const LanguageListItem({
+    super.key,
+    required this.language,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  final Language language;
+
+  final bool isSelected;
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 44.0.s,
+        decoration: BoxDecoration(
+          color: context.theme.appColors.tertararyBackground,
+          borderRadius: BorderRadius.circular(12.0.s),
+        ),
+        margin: EdgeInsets.only(
+          bottom: 12.0.s,
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: 16.0.s,
+        ),
+        child: Row(
+          children: <Widget>[
+            Text(
+              language.flag,
+              style: context.theme.appTextThemes.subtitle2.copyWith(
+                color: context.theme.appColors.primaryText,
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(
+              width: 16.0.s,
+            ),
+            Expanded(
+              child: Text(
+                language.name,
+                style: context.theme.appTextThemes.subtitle2.copyWith(
+                  color: context.theme.appColors.primaryText,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 30.0.s,
+              child: isSelected
+                  ? Assets.images.icons.iconBlockCheckboxOn.icon()
+                  : Assets.images.icons.iconBlockCheckboxOff.icon(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/app/features/auth/views/pages/select_languages/select_languages.dart
+++ lib/app/features/auth/views/pages/select_languages/select_languages.dart
@@ -4,15 +4,15 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ice/app/components/inputs/search_input/search_input.dart';
 import 'package:ice/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ice/app/components/template/ice_page.dart';
-import 'package:ice/app/extensions/asset_gen_image.dart';
 import 'package:ice/app/extensions/build_context.dart';
 import 'package:ice/app/extensions/num.dart';
 import 'package:ice/app/extensions/theme_data.dart';
 import 'package:ice/app/features/auth/views/components/title_description_header/title_description_header.dart';
+import 'package:ice/app/features/auth/views/pages/select_languages/language_list_item.dart';
 import 'package:ice/app/features/auth/views/pages/select_languages/languages.dart';
+import 'package:ice/app/router/components/floating_app_bar/floating_app_bar.dart';
 import 'package:ice/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ice/app/router/components/sheet_content/sheet_content.dart';
-import 'package:ice/generated/assets.gen.dart';
 
 class SelectLanguages extends IceSimplePage {
   const SelectLanguages(super._route, super.payload);
@@ -21,9 +21,8 @@ class SelectLanguages extends IceSimplePage {
   Widget buildPage(BuildContext context, WidgetRef ref, __) {
     final ValueNotifier<String> searchText = useState('');
 
-    final ValueNotifier<Set<Language>> selectedLanguagesNotifier =
+    final ValueNotifier<Set<Language>> selectedLanguages =
         useState<Set<Language>>(<Language>{});
-    final Set<Language> selectedLanguages = selectedLanguagesNotifier.value;
 
     final List<Language> filteredLanguages = searchText.value.isEmpty
         ? languages
@@ -34,111 +33,52 @@ class SelectLanguages extends IceSimplePage {
           }).toList();
 
     return SheetContent(
-      body: Container(
-        width: double.infinity,
-        color: context.theme.appColors.secondaryBackground,
-        child: Stack(
-          children: <Widget>[
-            NavigationAppBar.modal(),
-            ScreenSideOffset.small(
-              child: Padding(
-                padding: EdgeInsets.only(
-                  top: NavigationAppBar.modalHeaderHeight,
-                ),
-                child: Column(
-                  children: <Widget>[
-                    TitleDescription(
-                      title: context.i18n.select_languages_title,
-                      description: context.i18n.select_languages_description,
+      backgroundColor: context.theme.appColors.secondaryBackground,
+      body: Column(
+        children: <Widget>[
+          NavigationAppBar.modal(),
+          TitleDescription(
+            title: context.i18n.select_languages_title,
+            description: context.i18n.select_languages_description,
+            bottomPadding: 6.0.s,
+          ),
+          Expanded(
+            child: ScreenSideOffset.small(
+              child: CustomScrollView(
+                slivers: <Widget>[
+                  FloatingAppBar(
+                    height: SearchInput.height,
+                    child: SearchInput(
+                      onTextChanged: (String value) => searchText.value = value,
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 12.0.s),
-                      child: SearchInput(
-                        onTextChanged: (String value) =>
-                            searchText.value = value,
-                      ),
-                    ),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: filteredLanguages.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          final Language country = filteredLanguages[index];
-                          final bool isSelected =
-                              selectedLanguages.contains(country);
-
-                          return InkWell(
-                            onTap: () {
-                              final Set<Language> newSelectedLanguages =
-                                  Set<Language>.from(selectedLanguages);
-                              if (isSelected) {
-                                newSelectedLanguages.remove(country);
-                              } else {
-                                newSelectedLanguages.add(country);
-                              }
-                              selectedLanguagesNotifier.value =
-                                  newSelectedLanguages;
-                            },
-                            child: Container(
-                              height: 44.0.s,
-                              decoration: BoxDecoration(
-                                color:
-                                    context.theme.appColors.tertararyBackground,
-                                borderRadius: BorderRadius.circular(12.0.s),
-                              ),
-                              margin: EdgeInsets.only(
-                                bottom: 12.0.s,
-                              ),
-                              padding: EdgeInsets.symmetric(
-                                horizontal: 16.0.s,
-                              ),
-                              child: Row(
-                                children: <Widget>[
-                                  Text(
-                                    country.flag,
-                                    style: context.theme.appTextThemes.subtitle2
-                                        .copyWith(
-                                      color:
-                                          context.theme.appColors.primaryText,
-                                      fontSize: 24,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: 16.0.s,
-                                  ),
-                                  Expanded(
-                                    child: Text(
-                                      country.name,
-                                      style: context
-                                          .theme.appTextThemes.subtitle2
-                                          .copyWith(
-                                        color:
-                                            context.theme.appColors.primaryText,
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: 30.0.s,
-                                    child: isSelected
-                                        ? Assets
-                                            .images.icons.iconBlockCheckboxOn
-                                            .icon()
-                                        : Assets
-                                            .images.icons.iconBlockCheckboxOff
-                                            .icon(),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
+                  ),
+                  SliverList.builder(
+                    itemCount: filteredLanguages.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Language language = filteredLanguages[index];
+                      final bool isSelected =
+                          selectedLanguages.value.contains(language);
+                      return LanguageListItem(
+                        language: language,
+                        isSelected: isSelected,
+                        onTap: () {
+                          final Set<Language> newSelectedLanguages =
+                              Set<Language>.from(selectedLanguages.value);
+                          if (isSelected) {
+                            newSelectedLanguages.remove(language);
+                          } else {
+                            newSelectedLanguages.add(language);
+                          }
+                          selectedLanguages.value = newSelectedLanguages;
                         },
-                      ),
-                    ),
-                  ],
-                ),
+                      );
+                    },
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
