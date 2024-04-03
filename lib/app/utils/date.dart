dd1952c0764c2e85aa42c10c362f3659c87ffbda
Ice Hades <119406114+ice-hades@users.noreply.github.com>
2024-04-03 10:24:53 +0300

    chore: more comments

--- lib/app/utils/date.dart
+++ lib/app/utils/date.dart
@@ -2,10 +2,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:ice/app/extensions/build_context.dart';
 import 'package:intl/intl.dart';
 
-/*
-  For the given timestamp returns a data in format 'MMM d yyyy'.
-  But has 2 special cases: if a date is Yesterday and Today then returns a corresponding translation.
- */
+/// Converts a timestamp to a more readable date string, with special handling
+/// for dates that represent "today" and "yesterday".
+///
+/// The function checks if the given timestamp falls into today, yesterday, or
+/// any other day. If it's today or yesterday, it returns localized strings for
+/// these values. For other dates, it formats the date as 'MMM d yyyy'.
+///
+/// Parameters:
+///   - `timestamp`: The Unix timestamp (in milliseconds) that will be converted
+///     into a date string.
+///   - `context`: The BuildContext used for accessing localized strings for
+///     today and yesterday through an extension on BuildContext.
+///
+/// Returns: A string representing the human-readable date. This will be either
+/// "today", "yesterday", or a date formatted as 'MMM d yyyy'.
 String toPastDateDisplayValue(int timestamp, BuildContext context) {
   final DateTime date = DateTime.fromMillisecondsSinceEpoch(timestamp);
   final DateTime now = DateTime.now();
@@ -25,9 +36,16 @@ String toPastDateDisplayValue(int timestamp, BuildContext context) {
   return DateFormat('MMM d yyyy').format(date);
 }
 
-/*
-  For the given timestamp returns a time in format 'HH:mm'
- */
+/// Converts a timestamp to a time string in the format 'HH:mm'.
+///
+/// This method takes a Unix timestamp (in milliseconds) and formats it into
+/// a string showing only the hours and minutes of the time.
+///
+/// Parameters:
+///   - `timestamp`: The Unix timestamp (in milliseconds) that will be converted
+///     into a time string.
+///
+/// Returns: A string representing the time in 'HH:mm' format.
 String toTimeDisplayValue(int timestamp) {
   final DateTime date = DateTime.fromMillisecondsSinceEpoch(timestamp);
   return DateFormat('HH:mm').format(date);
