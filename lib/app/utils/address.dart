811d0701d68d2569fd52fbdcaf32d980e7b8c245
Ice Hades <119406114+ice-hades@users.noreply.github.com>
2024-04-05 16:09:33 +0300

    chore: fixed substring exclusive second parameter

--- lib/app/utils/address.dart
+++ lib/app/utils/address.dart
@@ -1,6 +1,5 @@
 String shortenAddress(String address) {
   assert(address.length == 42);
 
-  final int lastIndex = address.length - 1;
-  return '${address.substring(0, 12)}...${address.substring(lastIndex - 14, lastIndex)}';
+  return '${address.substring(0, 12)}...${address.substring(address.length - 14, address.length)}';
 }
