ef993292a13006208931361e8c8c373c7578f392
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-06-06 12:53:54 +0300

    feat: handle "Send" coins button, showing of the coins list (#59)
    ![simulator_screenshot_6AFB04CC-B6D5-4EC7-9A9B-A1975165FCA3](https://github.com/ice-blockchain/flutter-app/assets/171546305/4321d202-2184-4c05-873a-2786d40c4bf5)

---
+++ lib/app/features/wallet/views/pages/send_coins/send_coin_modal_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ice/app/components/template/ice_page.dart';
+import 'package:ice/app/extensions/build_context.dart';
+import 'package:ice/app/extensions/theme_data.dart';
+import 'package:ice/app/features/wallet/views/pages/send_coins/components/coins_list_view.dart';
+import 'package:ice/app/router/components/sheet_content/sheet_content.dart';
+
+class SendCoinModalPage extends IceSimplePage {
+  const SendCoinModalPage(super.route, super.payload);
+
+  @override
+  Widget buildPage(BuildContext context, WidgetRef ref, __) {
+    return SheetContent(
+      body: const CoinsListView(),
+      backgroundColor: context.theme.appColors.secondaryBackground,
+    );
+  }
+}
