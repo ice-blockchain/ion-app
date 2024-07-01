67048da35086362a5c0504e87aa80602cfcd58d4
ice-herakles <172259714+ice-herakles@users.noreply.github.com>
2024-07-01 18:07:42 +0200

    fix: resolve icon size

--- lib/app/features/feed/views/pages/quote_post_modal_page/components/bottom_action_bar.dart
+++ lib/app/features/feed/views/pages/quote_post_modal_page/components/bottom_action_bar.dart
@@ -12,29 +12,40 @@ class BottomActionBar extends StatelessWidget {
       height: 40.0.s,
       padding: EdgeInsets.symmetric(horizontal: 16.0.s),
       decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: context.theme.appColors.strokeElements.withOpacity(0.5),
+            offset: Offset(0.0.s, -1.0.s),
+            blurRadius: 5.0.s,
+          ),
+        ],
         color: context.theme.appColors.secondaryBackground,
       ),
       child: Row(
         children: [
-          IconButton(
-            icon: Assets.images.icons.iconGalleryOpen.icon(),
-            onPressed: () {},
+          GestureDetector(
+            onTap: () {},
+            child: Assets.images.icons.iconGalleryOpen.icon(),
           ),
           SizedBox(width: 16.0.s),
-          IconButton(
-            icon: Assets.images.icons.iconCameraOpen.icon(),
-            onPressed: () {},
+          GestureDetector(
+            onTap: () {},
+            child: Assets.images.icons.iconCameraOpen.icon(),
           ),
           SizedBox(width: 16.0.s),
-          IconButton(
-            icon: Assets.images.icons.iconFeedAddfile.icon(),
-            onPressed: () {},
+          GestureDetector(
+            onTap: () {},
+            child: Assets.images.icons.iconFeedAddfile.icon(),
           ),
           const Spacer(),
-          Button.icon(
+          Button(
+            minimumSize: Size(48.0.s, 28.0.s),
             borderRadius: BorderRadius.circular(100.0.s),
             onPressed: () {},
-            icon: Assets.images.icons.iconFeedSendbutton.icon(),
+            leadingIcon: Padding(
+              padding: EdgeInsets.symmetric(vertical: 4.0.s),
+              child: Assets.images.icons.iconFeedSendbutton.icon(size: 20.0.s),
+            ),
           ),
         ],
       ),
