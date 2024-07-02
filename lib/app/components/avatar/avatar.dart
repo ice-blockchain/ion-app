2d0214b15da61fa1535879b590a0677c06bbe5b3
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-07-02 12:14:19 +0300

    fix: avatar

--- lib/app/components/avatar/avatar.dart
+++ lib/app/components/avatar/avatar.dart
@@ -33,25 +33,56 @@ class Avatar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (imageWidget != null) {
-      return _buildImageWidgetWithBadge(context);
+      return _ImageWidgetWithBadge(
+        imageWidget: imageWidget!,
+        iceBadge: iceBadge,
+        size: size,
+      );
     } else if (imageUrl != null) {
-      return _buildNetworkImage();
+      return _NetworkImage(
+        imageUrl: imageUrl!,
+        size: size,
+        borderRadius: borderRadius,
+        fit: fit,
+      );
     } else {
-      return const SizedBox();
+      return const SizedBox.shrink();
     }
   }
+}
 
-  Widget _buildImageWidgetWithBadge(BuildContext context) {
+class _ImageWidgetWithBadge extends StatelessWidget {
+  const _ImageWidgetWithBadge({
+    required this.imageWidget,
+    required this.iceBadge,
+    required this.size,
+  });
+
+  final Widget imageWidget;
+  final bool iceBadge;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
     return Stack(
       clipBehavior: Clip.none,
       children: [
-        imageWidget!,
-        if (iceBadge) _buildIceBadge(context),
+        imageWidget,
+        if (iceBadge) _IceBadge(size: size),
       ],
     );
   }
+}
 
-  Widget _buildIceBadge(BuildContext context) {
+class _IceBadge extends StatelessWidget {
+  const _IceBadge({
+    required this.size,
+  });
+
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
     return Positioned(
       bottom: -2.0.s,
       right: -2.0.s,
@@ -79,12 +110,27 @@ class Avatar extends StatelessWidget {
       ),
     );
   }
+}
 
-  Widget _buildNetworkImage() {
+class _NetworkImage extends StatelessWidget {
+  const _NetworkImage({
+    required this.imageUrl,
+    required this.size,
+    required this.borderRadius,
+    required this.fit,
+  });
+
+  final String imageUrl;
+  final double size;
+  final BorderRadiusGeometry borderRadius;
+  final BoxFit fit;
+
+  @override
+  Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: borderRadius,
       child: CachedNetworkImage(
-        imageUrl: getAdaptiveImageUrl(imageUrl!, size * 2),
+        imageUrl: getAdaptiveImageUrl(imageUrl, size * 2),
         width: size,
         height: size,
         fit: fit,
