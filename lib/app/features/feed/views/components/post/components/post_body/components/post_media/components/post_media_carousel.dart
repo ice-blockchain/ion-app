aabb05f83d1375c62eb9645754d628eca8486f18
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-07-03 17:28:38 +0300

    chore: remove useless HookConsumerWidget

--- lib/app/features/feed/views/components/post/components/post_body/components/post_media/components/post_media_carousel.dart
+++ lib/app/features/feed/views/components/post/components/post_body/components/post_media/components/post_media_carousel.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ice/app/features/feed/model/post_media_data.dart';
 import 'package:ice/app/features/feed/views/components/post/components/post_body/components/post_media/components/post_media_carousel_horizontal.dart';
 import 'package:ice/app/features/feed/views/components/post/components/post_body/components/post_media/components/post_media_carousel_vertical.dart';
 
-class PostMediaCarousel extends HookConsumerWidget {
+class PostMediaCarousel extends StatelessWidget {
   const PostMediaCarousel({
     required this.media,
     required this.aspectRatio,
@@ -16,7 +15,7 @@ class PostMediaCarousel extends HookConsumerWidget {
   final double aspectRatio;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     if (aspectRatio < 1) {
       return PostMediaCarouselVertical(media: media, aspectRatio: aspectRatio);
     }
