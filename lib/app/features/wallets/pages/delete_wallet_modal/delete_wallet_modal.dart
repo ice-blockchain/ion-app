e77002246f5291b2eb335b211f093b5366a726b5
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-07-03 15:18:03 +0300

    fix: delete wallet popup height

--- lib/app/features/wallets/pages/delete_wallet_modal/delete_wallet_modal.dart
+++ lib/app/features/wallets/pages/delete_wallet_modal/delete_wallet_modal.dart
@@ -34,71 +34,73 @@ class DeleteWalletModal extends IcePage<WalletData> {
     return SheetContent(
       backgroundColor: context.theme.appColors.secondaryBackground,
       body: ScreenSideOffset.small(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 31.0.s, bottom: 4.0.s),
-              child: Assets.images.misc.actionDeletewallet.icon(size: 80.0.s),
-            ),
-            Text(
-              context.i18n.wallet_delete_q,
-              style: context.theme.appTextThemes.title.copyWith(
-                color: context.theme.appColors.primaryText,
+        child: IntrinsicHeight(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 31.0.s, bottom: 4.0.s),
+                child: Assets.images.misc.actionDeletewallet.icon(size: 80.0.s),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 8.0.s,
-                bottom: 30.0.s,
-                left: 36.0.s,
-                right: 36.0.s,
-              ),
-              child: Text(
-                context.i18n.wallet_delete_message,
-                style: context.theme.appTextThemes.body2.copyWith(
-                  color: context.theme.appColors.secondaryText,
+              Text(
+                context.i18n.wallet_delete_q,
+                style: context.theme.appTextThemes.title.copyWith(
+                  color: context.theme.appColors.primaryText,
                 ),
-                textAlign: TextAlign.center,
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Expanded(
-                  child: Button.compact(
-                    type: ButtonType.outlined,
-                    label: Text(
-                      context.i18n.button_cancel,
-                    ),
-                    onPressed: () {
-                      context.pop();
-                    },
-                    minimumSize: buttonMinimalSize,
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 8.0.s,
+                  bottom: 30.0.s,
+                  left: 36.0.s,
+                  right: 36.0.s,
+                ),
+                child: Text(
+                  context.i18n.wallet_delete_message,
+                  style: context.theme.appTextThemes.body2.copyWith(
+                    color: context.theme.appColors.secondaryText,
                   ),
+                  textAlign: TextAlign.center,
                 ),
-                SizedBox(
-                  width: 15.0.s,
-                ),
-                Expanded(
-                  child: Button.compact(
-                    label: Text(
-                      context.i18n.button_delete,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Expanded(
+                    child: Button.compact(
+                      type: ButtonType.outlined,
+                      label: Text(
+                        context.i18n.button_cancel,
+                      ),
+                      onPressed: () {
+                        context.pop();
+                      },
+                      minimumSize: buttonMinimalSize,
                     ),
-                    onPressed: () {
-                      ref
-                          .read(walletsDataNotifierProvider.notifier)
-                          .deleteWallet(payload.id);
-                      context.pop();
-                    },
-                    minimumSize: buttonMinimalSize,
-                    backgroundColor: context.theme.appColors.attentionRed,
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: MediaQuery.paddingOf(context).bottom + 20.0.s),
-          ],
+                  SizedBox(
+                    width: 15.0.s,
+                  ),
+                  Expanded(
+                    child: Button.compact(
+                      label: Text(
+                        context.i18n.button_delete,
+                      ),
+                      onPressed: () {
+                        ref
+                            .read(walletsDataNotifierProvider.notifier)
+                            .deleteWallet(payload.id);
+                        context.pop();
+                      },
+                      minimumSize: buttonMinimalSize,
+                      backgroundColor: context.theme.appColors.attentionRed,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: MediaQuery.paddingOf(context).bottom + 20.0.s),
+            ],
+          ),
         ),
       ),
     );
