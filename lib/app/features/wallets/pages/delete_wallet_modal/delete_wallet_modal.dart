a27445948e81823a3e29d0c21150f366025376f9
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-07-04 11:34:43 +0300

    fix: wallet and dapps (#79)
    fix: dapp details on go back navigation (before it resets to tab, now go
    back to category if opened from there)
    fix: delete wallet popup height
    fix: receive/send coin titles
    
    <img width="472" alt="Screenshot 2024-07-03 at 16 41 24"
    src="https://github.com/ice-blockchain/flutter-app/assets/96414297/57f00d48-d63f-43e1-a38b-7f13eb2733b8">
    <img width="472" alt="Screenshot 2024-07-03 at 16 41 10"
    src="https://github.com/ice-blockchain/flutter-app/assets/96414297/ebcef47a-e232-4fae-86ed-0a841181b2b1">
    <img width="472" alt="Screenshot 2024-07-03 at 16 41 01"
    src="https://github.com/ice-blockchain/flutter-app/assets/96414297/9ea9d936-f90a-429b-869c-812a8e78be10">

--- lib/app/features/wallets/pages/delete_wallet_modal/delete_wallet_modal.dart
+++ lib/app/features/wallets/pages/delete_wallet_modal/delete_wallet_modal.dart
@@ -11,7 +11,6 @@ import 'package:ice/app/extensions/num.dart';
 import 'package:ice/app/extensions/theme_data.dart';
 import 'package:ice/app/features/wallet/model/wallet_data.dart';
 import 'package:ice/app/features/wallets/providers/wallets_data_provider.dart';
-import 'package:ice/app/router/components/sheet_content/sheet_content.dart';
 import 'package:ice/generated/assets.gen.dart';
 
 class DeleteWalletModal extends IcePage<WalletData> {
@@ -31,75 +30,72 @@ class DeleteWalletModal extends IcePage<WalletData> {
 
     final buttonMinimalSize = Size(buttonsSize, buttonsSize);
 
-    return SheetContent(
-      backgroundColor: context.theme.appColors.secondaryBackground,
-      body: ScreenSideOffset.small(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 31.0.s, bottom: 4.0.s),
-              child: Assets.images.misc.actionDeletewallet.icon(size: 80.0.s),
+    return ScreenSideOffset.small(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 31.0.s, bottom: 4.0.s),
+            child: Assets.images.misc.actionDeletewallet.icon(size: 80.0.s),
+          ),
+          Text(
+            context.i18n.wallet_delete_q,
+            style: context.theme.appTextThemes.title.copyWith(
+              color: context.theme.appColors.primaryText,
             ),
-            Text(
-              context.i18n.wallet_delete_q,
-              style: context.theme.appTextThemes.title.copyWith(
-                color: context.theme.appColors.primaryText,
-              ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 8.0.s,
+              bottom: 30.0.s,
+              left: 36.0.s,
+              right: 36.0.s,
             ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 8.0.s,
-                bottom: 30.0.s,
-                left: 36.0.s,
-                right: 36.0.s,
+            child: Text(
+              context.i18n.wallet_delete_message,
+              style: context.theme.appTextThemes.body2.copyWith(
+                color: context.theme.appColors.secondaryText,
               ),
-              child: Text(
-                context.i18n.wallet_delete_message,
-                style: context.theme.appTextThemes.body2.copyWith(
-                  color: context.theme.appColors.secondaryText,
-                ),
-                textAlign: TextAlign.center,
-              ),
+              textAlign: TextAlign.center,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Expanded(
-                  child: Button.compact(
-                    type: ButtonType.outlined,
-                    label: Text(
-                      context.i18n.button_cancel,
-                    ),
-                    onPressed: () {
-                      context.pop();
-                    },
-                    minimumSize: buttonMinimalSize,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                child: Button.compact(
+                  type: ButtonType.outlined,
+                  label: Text(
+                    context.i18n.button_cancel,
                   ),
+                  onPressed: () {
+                    context.pop();
+                  },
+                  minimumSize: buttonMinimalSize,
                 ),
-                SizedBox(
-                  width: 15.0.s,
-                ),
-                Expanded(
-                  child: Button.compact(
-                    label: Text(
-                      context.i18n.button_delete,
-                    ),
-                    onPressed: () {
-                      ref
-                          .read(walletsDataNotifierProvider.notifier)
-                          .deleteWallet(payload.id);
-                      context.pop();
-                    },
-                    minimumSize: buttonMinimalSize,
-                    backgroundColor: context.theme.appColors.attentionRed,
+              ),
+              SizedBox(
+                width: 15.0.s,
+              ),
+              Expanded(
+                child: Button.compact(
+                  label: Text(
+                    context.i18n.button_delete,
                   ),
+                  onPressed: () {
+                    ref
+                        .read(walletsDataNotifierProvider.notifier)
+                        .deleteWallet(payload.id);
+                    context.pop();
+                  },
+                  minimumSize: buttonMinimalSize,
+                  backgroundColor: context.theme.appColors.attentionRed,
                 ),
-              ],
-            ),
-            SizedBox(height: MediaQuery.paddingOf(context).bottom + 20.0.s),
-          ],
-        ),
+              ),
+            ],
+          ),
+          SizedBox(height: MediaQuery.paddingOf(context).bottom + 20.0.s),
+        ],
       ),
     );
   }
