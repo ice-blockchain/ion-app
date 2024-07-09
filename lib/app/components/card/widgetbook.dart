ef9a1ed2ca858c3699f07013fa81ba0e3aca61ec
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-07-09 18:39:00 +0300

    chore: update widgetbook

---
+++ lib/app/components/card/widgetbook.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:ice/app/components/card/rounded_card.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+@widgetbook.UseCase(
+  name: 'regular',
+  type: RoundedCard,
+)
+Widget regularRoundedCardUseCase(BuildContext context) {
+  return const Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        RoundedCard(
+          child: Text('Simple Rounded Card'),
+        ),
+      ],
+    ),
+  );
+}
