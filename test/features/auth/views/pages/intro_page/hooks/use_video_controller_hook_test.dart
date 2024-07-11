53e801d3840db422bdf555dde7ba24b304f47d42
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-07-11 17:15:40 +0300

    chore: update test

--- test/features/auth/views/pages/intro_page/hooks/use_video_controller_hook_test.dart
+++ test/features/auth/views/pages/intro_page/hooks/use_video_controller_hook_test.dart
@@ -14,10 +14,23 @@ void main() {
       when(() => mockController.initialize()).thenAnswer((_) async => {});
       when(() => mockController.setLooping(any())).thenAnswer((_) async => {});
       when(() => mockController.play()).thenAnswer((_) async => {});
+      when(() => mockController.value).thenReturn(
+        const VideoPlayerValue(
+          duration: Duration.zero,
+          isInitialized: true,
+          isPlaying: true,
+          isLooping: true,
+        ),
+      );
     });
 
     test('initializes, sets looping, and plays video', () async {
       await initializeVideoController(mockController);
+
+      expect(mockController, isA<VideoPlayerController>());
+      expect(mockController.value.isInitialized, isTrue);
+      expect(mockController.value.isLooping, isTrue);
+      expect(mockController.value.isPlaying, isTrue);
 
       verify(() => mockController.initialize()).called(1);
       verify(() => mockController.setLooping(true)).called(1);
