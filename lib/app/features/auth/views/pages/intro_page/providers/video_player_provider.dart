6c68d25aeda066777613ede5fb544c3d230b87cb
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-07-12 11:27:42 +0300

    chore: use generated Assets class for video, .env, move launch icon out of assets folder

--- lib/app/features/auth/views/pages/intro_page/providers/video_player_provider.dart
+++ lib/app/features/auth/views/pages/intro_page/providers/video_player_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:ice/generated/assets.gen.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:video_player/video_player.dart';
 
@@ -9,7 +10,7 @@ part 'video_player_provider.g.dart';
 Future<Raw<VideoPlayerController>> videoController(
   VideoControllerRef ref,
 ) async {
-  final controller = VideoPlayerController.asset('assets/videos/intro.mp4');
+  final controller = VideoPlayerController.asset(Assets.videos.intro);
 
   await controller.initialize();
   await controller.setLooping(true);
