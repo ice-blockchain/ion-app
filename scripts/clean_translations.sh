908f1e51b410ae2221d08179f90c01996d5341c2
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-07-12 11:10:09 +0300

    chore: remove unused translations, improve clean_translations script

--- scripts/clean_translations.sh
+++ scripts/clean_translations.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
 # This script processes ARB files by checking if the keys from app_en.arb (or specified keys)
-# are used in a given library folder. If a key is not found, the script outputs the key. If the --dryRun flag is not provided,
+# are used in a given app folder. If a key is not found, the script outputs the key. If the --dryRun flag is not provided,
 # the script will also remove the key from all ARB files in the specified ARB folder.
 
 # Usage:
-# ./process_arb.sh <path_to_arb_folder> <path_to_lib_folder> [--dryRun] [--translations keys]
+# ./process_arb.sh <path_to_arb_folder> <path_to_app_folder> [--dryRun] [--translations keys]
 #
 # Parameters:
 # <path_to_arb_folder>   - Path to the folder containing ARB files.
-# <path_to_lib_folder>   - Path to the library folder to search for keys.
+# <path_to_app_folder>   - Path to the app folder to search for keys.
 # --dryRun               - (Optional) Perform a dry run without modifying the ARB files.
 # --translations keys    - (Optional) Comma-separated list of translation keys to check. If not provided, keys from app_en.arb will be used.
 #
 # Examples:
-# ./process_arb.sh /path/to/arb/folder /path/to/lib/folder
-# ./process_arb.sh /path/to/arb/folder /path/to/lib/folder --dryRun
-# ./process_arb.sh /path/to/arb/folder /path/to/lib/folder --translations key1,key2,key3
-# ./process_arb.sh /path/to/arb/folder /path/to/lib/folder --dryRun --translations key1,key2,key3
+# ./scripts/process_arb.sh ./lib/l10n ./lib/app
+# ./scripts/process_arb.sh ./lib/l10n ./lib/app --dryRun
+# ./scripts/process_arb.sh ./lib/l10n ./lib/app --translations key1,key2,key3
+# ./scripts/process_arb.sh ./lib/l10n ./lib/app --dryRun --translations key1,key2,key3
 
 # Function to show usage
 usage() {
-  echo "Usage: $0 <path_to_arb_folder> <path_to_lib_folder> [--dryRun] [--translations keys]"
+  echo "Usage: $0 <path_to_arb_folder> <path_to_app_folder> [--dryRun] [--translations keys]"
   echo "  --dryRun              Perform a dry run without modifying the ARB files"
   echo "  --translations keys   Comma-separated list of translation keys to check"
   exit 1
 }
 
-# Check if at least two arguments are passed (path to ARB files and lib folder)
+# Check if at least two arguments are passed (path to ARB files and app folder)
 if [ "$#" -lt 2 ]; then
   usage
 fi
 
 # Extract the arguments
 ARB_FOLDER=$1
-LIB_FOLDER=$2
+APP_FOLDER=$2
 DRY_RUN=false
 TRANSLATION_KEYS=()
 
@@ -65,6 +65,7 @@ done
 # Check if jq is installed
 if ! command -v jq &> /dev/null; then
   echo "jq is required but it's not installed. Please install jq and try again."
+  echo "brew install jq"
   exit 1
 fi
 
@@ -81,7 +82,7 @@ fi
 # Loop through each key
 for KEY in $TRANSLATION_KEYS; do
   # Search for the key in the lib folder recursively
-  FOUND=$(grep -r "\"$KEY\"" "$LIB_FOLDER" 2>/dev/null)
+  FOUND=$(grep -r "$KEY" "$APP_FOLDER" 2>/dev/null)
   
   # If the key is not found
   if [ -z "$FOUND" ]; then
