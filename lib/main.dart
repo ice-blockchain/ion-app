7cd5a1b0783423af316a30f4c04e55e639b75d9e
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-07-19 12:54:25 +0300

    chore: minor refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ice/app/features/core/providers/template_provider.dart';
 import 'package:ice/app/features/core/providers/theme_mode_provider.dart';
 import 'package:ice/app/features/core/views/components/content_scaler.dart';
+import 'package:ice/app/router/components/modal_wrapper/sheet_scope.dart';
 import 'package:ice/app/router/providers/go_router_provider.dart';
 import 'package:ice/app/services/logger/config.dart';
 import 'package:ice/app/services/riverpod/riverpod_logger.dart';
 import 'package:ice/app/templates/template.dart';
 import 'package:ice/app/theme/theme.dart';
 import 'package:ice/generated/app_localizations.dart';
-import 'package:smooth_sheets/smooth_sheets.dart';
 
 void main() async {
   runApp(
@@ -32,15 +32,7 @@ class IceApp extends HookConsumerWidget {
     final goRouter = ref.watch(goRouterProvider);
 
     return ContentScaler(
-      // Provides a SheetController to the descendant widgets
-      // to perform some sheet extent driven animations.
-      // The sheet will look up and use this controller unless
-      // another one is manually specified in the constructor.
-      // The descendant widgets can also get this controller by
-      // calling 'DefaultSheetController.of(context)'.
-      //
-      // See example in smooth_sheets package.
-      child: DefaultSheetController(
+      child: SheetScope(
         child: MaterialApp.router(
           localizationsDelegates: I18n.localizationsDelegates,
           supportedLocales: I18n.supportedLocales,
