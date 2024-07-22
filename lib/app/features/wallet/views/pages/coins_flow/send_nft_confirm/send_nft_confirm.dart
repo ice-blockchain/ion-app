28fcdfbcb9b1f8e632ddee2a502c174c8eb36799
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-07-22 18:05:16 +0300

    chore: add send to and wallet sections

--- lib/app/features/wallet/views/pages/coins_flow/send_nft_confirm/send_nft_confirm.dart
+++ lib/app/features/wallet/views/pages/coins_flow/send_nft_confirm/send_nft_confirm.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ice/app/components/button/button.dart';
 import 'package:ice/app/components/list_item/list_item.dart';
 import 'package:ice/app/components/screen_offset/screen_bottom_offset.dart';
 import 'package:ice/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ice/app/extensions/extensions.dart';
 import 'package:ice/app/features/wallet/model/nft_data.dart';
+import 'package:ice/app/features/wallet/views/pages/coins_flow/send_coins/providers/send_coins_form_provider.dart';
 import 'package:ice/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ice/app/router/components/navigation_app_bar/navigation_close_button.dart';
 import 'package:ice/app/router/components/sheet_content/sheet_content.dart';
 import 'package:ice/generated/assets.gen.dart';
 
-class SendNftConfirmPage extends StatelessWidget {
+class SendNftConfirmPage extends ConsumerWidget {
   const SendNftConfirmPage({required this.payload, super.key});
 
   final NftData payload;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final locale = context.i18n;
+    final formData = ref.watch(sendCoinsFormControllerProvider);
+
     return SheetContent(
       body: Column(
         children: [
@@ -31,6 +36,26 @@ class SendNftConfirmPage extends StatelessWidget {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
+                  SizedBox(height: 16.0.s),
+                  ListItem.text(
+                    title: Text(locale.wallet_send_to),
+                    value: formData.address,
+                  ),
+                  SizedBox(height: 16.0.s),
+                  ListItem.textWithIcon(
+                    title: Text(locale.wallet_title),
+                    value: formData.wallet.name,
+                    icon: Image.network(
+                      formData.wallet.icon,
+                      width: ScreenSideOffset.defaultSmallMargin,
+                      height: ScreenSideOffset.defaultSmallMargin,
+                    ),
+                    secondary: Text(
+                      '0xf59B7547F254854F3f17a594Fe97b0aB24gf3023',
+                      textAlign: TextAlign.right,
+                      style: context.theme.appTextThemes.caption3.copyWith(),
+                    ),
+                  ),
                   ListItem.text(
                     title: Text(context.i18n.send_nft_confirm_asset),
                     value: payload.collectionName.toString(),
