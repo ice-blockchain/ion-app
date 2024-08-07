335eb3de61d2894bb5a5ca0100b703f42be739da
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-08-07 10:51:43 +0300

    fix: build runner clean up

--- scripts/generate_code.sh
+++ scripts/generate_code.sh
@@ -4,4 +4,5 @@
 # https://pub.dev/packages/freezed
 # https://pub.dev/packages/json_serializable
 # https://pub.dev/packages/widgetbook
+dart run build_runner clean
 dart run build_runner build --delete-conflicting-outputs
