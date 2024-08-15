cbda1b114bab6e76066a63ecf970d007da2c1bab
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-08-15 12:46:45 +0300

    feat: add links constant

---
+++ lib/app/constants/links.dart
@@ -0,0 +1,9 @@
+class Links {
+  Links._();
+
+  static String terms = 'https://identity.io/terms';
+
+  static String privacy = 'https://identity.io/privacy';
+
+  static String identity = 'https://identity.io';
+}
