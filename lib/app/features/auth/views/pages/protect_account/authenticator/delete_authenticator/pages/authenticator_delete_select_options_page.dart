9c9f3d71147a0091c94117f8be773b6b8292876e
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-08-26 16:30:26 +0300

    chore: using provider for handling selecting values

--- lib/app/features/auth/views/pages/protect_account/authenticator/delete_authenticator/pages/authenticator_delete_select_options_page.dart
+++ lib/app/features/auth/views/pages/protect_account/authenticator/delete_authenticator/pages/authenticator_delete_select_options_page.dart
@@ -1,13 +1,11 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ice/app/components/button/button.dart';
 import 'package:ice/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ice/app/extensions/extensions.dart';
-import 'package:ice/app/features/auth/data/models/twofa_type.dart';
 import 'package:ice/app/features/auth/views/pages/protect_account/authenticator/model/authenticator_steps.dart';
+import 'package:ice/app/features/auth/views/pages/protect_account/providers/selected_two_fa_types_provider.dart';
 import 'package:ice/app/features/auth/views/pages/twofa_options/twofa_option_selector.dart';
 import 'package:ice/app/router/app_routes.dart';
 
@@ -16,13 +14,8 @@ class AuthenticatorDeleteSelectOptionsPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final optionsAmount = useState(Random().nextInt(2) + 1);
+    final optionsState = ref.watch(authenticatorDeleteOptionsProvider);
     final formKey = useRef(GlobalKey<FormState>());
-
-    final selectedValues = {
-      for (int i = 0; i < optionsAmount.value; i++) i: useState<TwoFaType?>(null)
-    };
-    final availableOptions = useState<Set<TwoFaType>>(TwoFaType.values.toSet());
 
     return ScreenSideOffset.large(
       child: Form(
@@ -31,24 +24,20 @@ class AuthenticatorDeleteSelectOptionsPage extends HookConsumerWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ...List.generate(
-              optionsAmount.value,
+              optionsState.optionsAmount,
               (option) {
+                final availableOptions = ref
+                    .read(authenticatorDeleteOptionsProvider.notifier)
+                    .getAvailableOptionsForIndex(option);
                 return Padding(
                   padding: EdgeInsets.only(bottom: 22.0.s),
                   child: TwoFaOptionSelector(
-                    availableOptions: selectedValues[option]?.value != null
-                        ? {selectedValues[option]!.value!, ...availableOptions.value}
-                        : availableOptions.value,
+                    availableOptions: availableOptions,
                     optionIndex: option + 1,
                     onSaved: (value) {
-                      if (selectedValues[option]?.value != null) {
-                        availableOptions.value = {
-                          ...availableOptions.value,
-                          selectedValues[option]!.value!
-                        };
-                      }
-                      selectedValues[option]?.value = value;
-                      availableOptions.value = {...availableOptions.value}..remove(value);
+                      ref
+                          .read(authenticatorDeleteOptionsProvider.notifier)
+                          .updateOption(option, value);
                     },
                   ),
                 );
@@ -58,13 +47,10 @@ class AuthenticatorDeleteSelectOptionsPage extends HookConsumerWidget {
               mainAxisSize: MainAxisSize.max,
               label: Text(context.i18n.button_confirm),
               onPressed: () {
+                formKey.value.currentState!.save();
                 if (formKey.value.currentState!.validate()) {
-                  final extra = selectedValues.values
-                      .map((selectedValue) => selectedValue.value)
-                      .where((TwoFaType? value) => value != null)
-                      .cast<TwoFaType>()
-                      .toSet();
-
+                  final extra =
+                      ref.read(authenticatorDeleteOptionsProvider.notifier).getSelectedOptions();
                   AuthenticatorDeleteRoute(
                     $extra: extra,
                     step: AuthenticatorDeleteSteps.input,
--- lib/app/features/auth/views/pages/protect_account/providers/selected_two_fa_types_provider.dart
+++ lib/app/features/auth/views/pages/protect_account/providers/selected_two_fa_types_provider.dart
@@ -1,14 +1,54 @@
+import 'dart:math';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:ice/app/features/auth/data/models/twofa_type.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+part 'selected_two_fa_types_provider.freezed.dart';
 part 'selected_two_fa_types_provider.g.dart';
 
-@riverpod
-class SelectedTwoFaTypesNotifier extends _$SelectedTwoFaTypesNotifier {
-  @override
-  List<TwoFaType> build() => [];
+@Freezed(copyWith: true)
+class AuthenticatorDeleteState with _$AuthenticatorDeleteState {
+  factory AuthenticatorDeleteState({
+    required int optionsAmount,
+    required Set<TwoFaType> availableOptions,
+    required Map<int, TwoFaType?> selectedValues,
+  }) = _AuthenticatorDeleteState;
+}
 
-  void select(TwoFaType type) {
-    state = [...state, type];
+@riverpod
+class AuthenticatorDeleteOptions extends _$AuthenticatorDeleteOptions {
+  @override
+  AuthenticatorDeleteState build() {
+    final optionsAmount = Random().nextInt(2) + 1;
+    return AuthenticatorDeleteState(
+      optionsAmount: optionsAmount,
+      availableOptions: TwoFaType.values.toSet(),
+      selectedValues: {for (int i = 0; i < optionsAmount; i++) i: null},
+    );
+  }
+
+  void updateOption(int index, TwoFaType? newValue) {
+    state = state.copyWith(
+      selectedValues: {...state.selectedValues, index: newValue},
+      availableOptions: _updateAvailableOptions(index, newValue),
+    );
+  }
+
+  Set<TwoFaType> _updateAvailableOptions(int index, TwoFaType? newValue) {
+    final newAvailableOptions = state.availableOptions.toSet();
+    final oldValue = state.selectedValues[index];
+    if (oldValue != null) newAvailableOptions.add(oldValue);
+    if (newValue != null) newAvailableOptions.remove(newValue);
+    return newAvailableOptions;
+  }
+
+  Set<TwoFaType> getAvailableOptionsForIndex(int index) {
+    final selectedValue = state.selectedValues[index];
+    return {if (selectedValue != null) selectedValue, ...state.availableOptions};
+  }
+
+  Set<TwoFaType> getSelectedOptions() {
+    return state.selectedValues.values.whereType<TwoFaType>().toSet();
   }
 }
