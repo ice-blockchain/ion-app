749a38943010a3c181af5221c0c627c875a049a0
ice-tychon <https://github.com/ice-blockchain/flutter-app/commits/master/>
2024-09-03 11:06:29 +0300

    feat(feed filters): save and load filters for each user

--- lib/app/features/feed/providers/feed_current_filter_provider.dart
+++ lib/app/features/feed/providers/feed_current_filter_provider.dart
@@ -1,6 +1,8 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:ice/app/features/feed/model/feed_category.dart';
 import 'package:ice/app/features/feed/model/feed_filter.dart';
+import 'package:ice/app/features/user/providers/user_data_provider.dart';
+import 'package:ice/app/services/storage/user_preferences_service.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'feed_current_filter_provider.freezed.dart';
@@ -15,19 +17,22 @@ class FeedFiltersState with _$FeedFiltersState {
     required FeedFilter filter,
   }) = _FeedFiltersState;
 
-  factory FeedFiltersState.initial() => const FeedFiltersState(
-        category: FeedCategory.feed,
-        filter: FeedFilter.forYou,
-      );
-
-  bool get isDefault => category == FeedCategory.feed && filter == FeedFilter.forYou;
+  static const FeedCategory defaultCategory = FeedCategory.feed;
+  static const FeedFilter defaultFilter = FeedFilter.forYou;
 }
 
 @riverpod
 class FeedCurrentFilter extends _$FeedCurrentFilter {
+  static const _feedFilterCategoryKey = 'feed_filter_category';
+  static const _feedFilterFilterKey = 'feed_filter_filter';
+
   @override
   FeedFiltersState build() {
-    return FeedFiltersState.initial();
+    _listenChanges();
+
+    final savedState = _loadSavedState();
+
+    return savedState;
   }
 
   set filter(FeedFilter filter) {
@@ -36,5 +41,30 @@ class FeedCurrentFilter extends _$FeedCurrentFilter {
 
   set category(FeedCategory category) {
     state = state.copyWith(category: category);
+  }
+
+  void _listenChanges() {
+    ref.listenSelf((_, next) => _saveState(next));
+  }
+
+  void _saveState(FeedFiltersState state) {
+    final userId = ref.read(userDataNotifierProvider).id;
+    final userPreferencesService = ref.read(userPreferencesServiceProvider(userId: userId));
+
+    userPreferencesService.setEnum(_feedFilterCategoryKey, state.category);
+    userPreferencesService.setEnum(_feedFilterFilterKey, state.filter);
+  }
+
+  FeedFiltersState _loadSavedState() {
+    final userId = ref.watch(userDataNotifierProvider).id;
+    final userPreferencesService = ref.watch(userPreferencesServiceProvider(userId: userId));
+
+    final category = userPreferencesService.getEnum(_feedFilterCategoryKey, FeedCategory.values);
+    final filter = userPreferencesService.getEnum(_feedFilterFilterKey, FeedFilter.values);
+
+    return FeedFiltersState(
+      category: category ?? FeedFiltersState.defaultCategory,
+      filter: filter ?? FeedFiltersState.defaultFilter,
+    );
   }
 }
