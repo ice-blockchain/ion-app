1e6ee8e90dbacf5308896be1f21b37de47fabdf8
ice-tychon <175587874+ice-tychon@users.noreply.github.com>
2024-09-05 11:43:48 +0300

    chore(feed): replace carousel_slider_plus with PageView (#177)

--- lib/app/features/feed/views/components/post/components/post_body/components/post_media/components/post_media_carousel_horizontal.dart
+++ lib/app/features/feed/views/components/post/components/post_body/components/post_media/components/post_media_carousel_horizontal.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider_plus/carousel_slider_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -23,22 +22,18 @@ class PostMediaCarouselHorizontal extends HookConsumerWidget {
 
     return Stack(
       children: [
-        CarouselSlider.builder(
-          options: CarouselOptions(
-            aspectRatio: aspectRatio,
-            enableInfiniteScroll: false,
-            viewportFraction: 1,
-            onPageChanged: (index, reason) {
-              currentIndex.value = index;
-            },
+        AspectRatio(
+          aspectRatio: aspectRatio,
+          child: PageView(
+            onPageChanged: (index) => currentIndex.value = index,
+            children: [
+              for (final m in media)
+                PostMediaItem(
+                  mediaItem: m,
+                  aspectRatio: aspectRatio,
+                ),
+            ],
           ),
-          itemCount: media.length,
-          itemBuilder: (context, index, realIndex) {
-            return PostMediaItem(
-              mediaItem: media[index],
-              aspectRatio: aspectRatio,
-            );
-          },
         ),
         Positioned(
           top: 8.0.s,
