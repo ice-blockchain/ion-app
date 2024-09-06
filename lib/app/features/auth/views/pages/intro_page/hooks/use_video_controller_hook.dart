1bd3e56dc3c2de8034c1d29dc10fc6d6f4ee9503
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-09-06 15:28:50 +0300

    feat: add splash-video

--- lib/app/features/auth/views/pages/intro_page/hooks/use_video_controller_hook.dart
+++ lib/app/features/auth/views/pages/intro_page/hooks/use_video_controller_hook.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:video_player/video_player.dart';
 
-VideoPlayerController useVideoController(String assetPath) {
+VideoPlayerController useVideoController(String assetPath, {bool isLooping = true}) {
   final controller = useMemoized(
     () => VideoPlayerController.asset(assetPath),
     [assetPath],
@@ -14,7 +14,7 @@ VideoPlayerController useVideoController(String assetPath) {
       Future.microtask(() async {
         await controller.initialize();
         isInitialized.value = true;
-        await controller.setLooping(true);
+        await controller.setLooping(isLooping);
         await controller.play();
       });
 
--- lib/app/features/core/views/pages/splash_page.dart
+++ lib/app/features/core/views/pages/splash_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ice/app/features/auth/views/pages/intro_page/hooks/use_video_controller_hook.dart';
 import 'package:ice/app/features/core/providers/splash_provider.dart';
 import 'package:ice/app/hooks/use_on_init.dart';
+import 'package:ice/generated/assets.gen.dart';
+import 'package:video_player/video_player.dart';
 
 class SplashPage extends HookConsumerWidget {
   const SplashPage({super.key});
@@ -13,13 +16,29 @@ class SplashPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     _setSystemChrome();
 
+    final videoController = useVideoController(
+      Assets.videos.logoStatic,
+      isLooping: false,
+    );
+
     useOnInit(() {
-      ref.read(splashProvider.notifier).animationCompleted = true;
+      videoController.addListener(() {
+        if (videoController.value.position >= videoController.value.duration) {
+          ref.read(splashProvider.notifier).animationCompleted = true;
+        }
+      });
     });
 
-    return const ColoredBox(
-      color: backgroundColor,
-      child: SizedBox.expand(),
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Center(
+        child: videoController.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: videoController.value.aspectRatio,
+                child: VideoPlayer(videoController),
+              )
+            : const SizedBox.shrink(),
+      ),
     );
   }
 
