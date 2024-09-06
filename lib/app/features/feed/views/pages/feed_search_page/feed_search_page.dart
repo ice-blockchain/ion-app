d87db6e9493f7df80bf79074862cd82c05a51663
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-09-06 14:37:30 +0300

    feat: initial search state

--- lib/app/features/feed/views/pages/feed_search_page/feed_search_page.dart
+++ lib/app/features/feed/views/pages/feed_search_page/feed_search_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ice/app/components/screen_offset/screen_top_offset.dart';
+import 'package:ice/app/extensions/extensions.dart';
 import 'package:ice/app/features/feed/views/pages/feed_search_page/components/feed_search_navigation.dart';
+import 'package:ice/generated/assets.gen.dart';
 
 class FeedSearchPage extends StatelessWidget {
   const FeedSearchPage({super.key});
@@ -8,7 +10,29 @@ class FeedSearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ScreenTopOffset(child: FeedSearchNavigation()),
+      body: ScreenTopOffset(
+        child: Column(
+          children: [
+            FeedSearchNavigation(),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Assets.images.icons.walletIconWalletSearching.icon(size: 48.0.s),
+                  SizedBox(height: 8.0.s),
+                  Text(
+                    context.i18n.feed_search_empty,
+                    style: context.theme.appTextThemes.body2.copyWith(
+                      color: context.theme.appColors.tertararyText,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
