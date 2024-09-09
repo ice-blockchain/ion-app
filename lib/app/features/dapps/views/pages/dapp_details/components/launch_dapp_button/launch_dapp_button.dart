f868890789c7ee6ca71518f9b2a072342b61a6d8
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-09-09 12:50:38 +0300

    chore: add launch button over UI

---
+++ lib/app/features/dapps/views/pages/dapp_details/components/launch_dapp_button/launch_dapp_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:ice/app/components/button/button.dart';
+import 'package:ice/app/components/screen_offset/screen_side_offset.dart';
+import 'package:ice/app/components/separated/separator.dart';
+import 'package:ice/app/extensions/build_context.dart';
+import 'package:ice/app/extensions/num.dart';
+import 'package:ice/app/extensions/theme_data.dart';
+
+double get launchButtonContainerHeight => 86.0.s;
+double get launchButtonHeight => 56.0.s;
+
+class LaunchDappButton extends StatelessWidget {
+  const LaunchDappButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      height: launchButtonContainerHeight,
+      child: Stack(
+        children: [
+          HorizontalSeparator(),
+          ScreenSideOffset.small(
+            child: Container(
+              color: context.theme.appColors.secondaryBackground,
+              padding: EdgeInsets.symmetric(
+                  vertical: (launchButtonContainerHeight - launchButtonHeight) / 2),
+              width: double.infinity,
+              child: SizedBox(
+                height: launchButtonHeight,
+                child: Button(
+                  label: Text(
+                    context.i18n.dapp_details_launch_dapp_button_title,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/app/features/dapps/views/pages/dapp_details/dapp_details.dart
+++ lib/app/features/dapps/views/pages/dapp_details/dapp_details.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ice/app/components/button/button.dart';
 import 'package:ice/app/components/read_more_text/read_more_text.dart';
 import 'package:ice/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ice/app/extensions/build_context.dart';
@@ -9,12 +8,15 @@ import 'package:ice/app/extensions/theme_data.dart';
 import 'package:ice/app/features/dapps/providers/dapps_provider.dart';
 import 'package:ice/app/features/dapps/views/components/grid_item/grid_item.dart';
 import 'package:ice/app/features/dapps/views/pages/dapp_details/components/dapp_details_info_block/dapp_details_info_block.dart';
+import 'package:ice/app/features/dapps/views/pages/dapp_details/components/launch_dapp_button/launch_dapp_button.dart';
 import 'package:ice/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ice/app/router/components/navigation_app_bar/navigation_close_button.dart';
 import 'package:ice/app/router/components/sheet_content/sheet_content.dart';
 import 'package:ice/app/services/browser/browser.dart';
 import 'package:ice/app/utils/num.dart';
 import 'package:ice/generated/assets.gen.dart';
+
+double get defaultLargeMargin => 44.0.s;
 
 class DAppDetails extends ConsumerWidget {
   DAppDetails({
@@ -33,140 +35,136 @@ class DAppDetails extends ConsumerWidget {
     }
 
     return SheetContent(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0.s),
-              child: NavigationAppBar.screen(
-                title: Text(app.title),
-                showBackButton: false,
-                actions: const [
-                  NavigationCloseButton(),
-                ],
-              ),
-            ),
-            ScreenSideOffset.small(
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    if (app.backgroundImage != null)
-                      AspectRatio(
-                        aspectRatio: 343 / 200,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(16.0.s),
-                          child: Image.asset(
-                            app.backgroundImage!,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 14.0.s, bottom: 18.0.s),
-                      child: GridItem(
-                        dAppData: app,
-                        showIsFavourite: true,
-                        showTips: false,
-                      ),
-                    ),
-                    if (app.fullDescription != null)
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 6.0.s),
-                        child: ReadMoreText(
-                          app.fullDescription!,
-                        ),
-                      ),
-                    if (app.link != null)
-                      DappDetailsInfoBlock(
-                        iconPath: Assets.svg.iconWalletLink,
-                        value: InkWell(
-                          onTap: () async {
-                            if (app.link != null) {
-                              await openUrl(app.link!);
-                            }
-                          },
-                          child: Text(
-                            app.link!,
-                            style: context.theme.appTextThemes.caption2.copyWith(
-                              color: context.theme.appColors.primaryAccent,
-                            ),
-                          ),
-                        ),
-                      ),
-                    if (app.value != null)
-                      DappDetailsInfoBlock(
-                        title: Text(
-                          context.i18n.dapp_details_tips,
-                          style: context.theme.appTextThemes.caption2.copyWith(
-                            color: context.theme.appColors.secondaryText,
-                          ),
-                        ),
-                        value: Text(
-                          formatDouble(app.value!),
-                          style: context.theme.appTextThemes.subtitle.copyWith(
-                            color: context.theme.appColors.primaryText,
-                          ),
-                        ),
-                        iconPath: Assets.svg.iconButtonIceStroke,
-                      ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: DappDetailsInfoBlock(
-                            title: Text(
-                              context.i18n.dapp_details_tips_games,
-                              style: context.theme.appTextThemes.caption2.copyWith(
-                                color: context.theme.appColors.secondaryText,
-                              ),
-                            ),
-                            value: Text(
-                              '№1',
-                              style: context.theme.appTextThemes.title.copyWith(
-                                color: context.theme.appColors.primaryText,
-                              ),
-                            ),
-                            iconPath: Assets.svg.iconDappGames,
-                          ),
-                        ),
-                        SizedBox(
-                          width: 10.0.s,
-                        ),
-                        Expanded(
-                          child: DappDetailsInfoBlock(
-                            title: Text(
-                              context.i18n.dapp_details_tips_global_rank,
-                              style: context.theme.appTextThemes.caption2.copyWith(
-                                color: context.theme.appColors.secondaryText,
-                              ),
-                            ),
-                            value: Text(
-                              '№3',
-                              style: context.theme.appTextThemes.title.copyWith(
-                                color: context.theme.appColors.primaryText,
-                              ),
-                            ),
-                            iconPath: Assets.svg.iconDappGames,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 16.0.s, bottom: 56.0.s),
-                      child: Button(
-                        label: Text(
-                          context.i18n.dapp_details_launch_dapp_button_title,
-                        ),
-                        onPressed: () {},
-                        mainAxisSize: MainAxisSize.max,
-                      ),
-                    ),
-                  ],
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0.s),
+                  child: NavigationAppBar.screen(
+                    title: Text(app.title),
+                    showBackButton: false,
+                    actions: const [
+                      NavigationCloseButton(),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
+                ScreenSideOffset.small(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      if (app.backgroundImage != null)
+                        AspectRatio(
+                          aspectRatio: 343 / 200,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(16.0.s),
+                            child: Image.asset(
+                              app.backgroundImage!,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 14.0.s, bottom: 18.0.s),
+                        child: GridItem(
+                          dAppData: app,
+                          showIsFavourite: true,
+                          showTips: false,
+                        ),
+                      ),
+                      if (app.fullDescription != null)
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 6.0.s),
+                          child: ReadMoreText(
+                            app.fullDescription!,
+                          ),
+                        ),
+                      if (app.link != null)
+                        DappDetailsInfoBlock(
+                          iconPath: Assets.svg.iconWalletLink,
+                          value: InkWell(
+                            onTap: () async {
+                              if (app.link != null) {
+                                await openUrl(app.link!);
+                              }
+                            },
+                            child: Text(
+                              app.link!,
+                              style: context.theme.appTextThemes.caption2.copyWith(
+                                color: context.theme.appColors.primaryAccent,
+                              ),
+                            ),
+                          ),
+                        ),
+                      if (app.value != null)
+                        DappDetailsInfoBlock(
+                          title: Text(
+                            context.i18n.dapp_details_tips,
+                            style: context.theme.appTextThemes.caption2.copyWith(
+                              color: context.theme.appColors.secondaryText,
+                            ),
+                          ),
+                          value: Text(
+                            formatDouble(app.value!),
+                            style: context.theme.appTextThemes.subtitle.copyWith(
+                              color: context.theme.appColors.primaryText,
+                            ),
+                          ),
+                          iconPath: Assets.svg.iconButtonIceStroke,
+                        ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: DappDetailsInfoBlock(
+                              title: Text(
+                                context.i18n.dapp_details_tips_games,
+                                style: context.theme.appTextThemes.caption2.copyWith(
+                                  color: context.theme.appColors.secondaryText,
+                                ),
+                              ),
+                              value: Text(
+                                '№1',
+                                style: context.theme.appTextThemes.title.copyWith(
+                                  color: context.theme.appColors.primaryText,
+                                ),
+                              ),
+                              iconPath: Assets.svg.iconDappGames,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10.0.s,
+                          ),
+                          Expanded(
+                            child: DappDetailsInfoBlock(
+                              title: Text(
+                                context.i18n.dapp_details_tips_global_rank,
+                                style: context.theme.appTextThemes.caption2.copyWith(
+                                  color: context.theme.appColors.secondaryText,
+                                ),
+                              ),
+                              value: Text(
+                                '№3',
+                                style: context.theme.appTextThemes.title.copyWith(
+                                  color: context.theme.appColors.primaryText,
+                                ),
+                              ),
+                              iconPath: Assets.svg.iconDappGames,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: launchButtonContainerHeight,
+                ),
+              ],
+            ),
+          ),
+          LaunchDappButton()
+        ],
       ),
     );
   }
