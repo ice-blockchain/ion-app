8af14f1d56766f7a7659ff864a5afa4166a11c5c
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-09-10 15:56:23 +0300

    fix: pr comments

--- lib/app/features/dapps/views/pages/dapp_details/components/launch_dapp_button/launch_dapp_button.dart
+++ lib/app/features/dapps/views/pages/dapp_details/components/launch_dapp_button/launch_dapp_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ice/app/components/button/button.dart';
+import 'package:ice/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ice/app/extensions/extensions.dart';
 
 class LaunchDappButton extends StatelessWidget {
@@ -9,18 +10,14 @@ class LaunchDappButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.topCenter,
+    return ScreenSideOffset.small(
       child: Padding(
         padding: EdgeInsets.only(
           top: 18.0.s,
-          left: 16.0.s,
-          right: 16.0.s,
           bottom: 36.0.s,
         ),
         child: Button(
           mainAxisSize: MainAxisSize.max,
-          minimumSize: Size(56.0.s, 56.0.s),
           label: Text(
             context.i18n.dapp_details_launch_dapp_button_title,
           ),
