ff0aebcdf78617ea2afa4a8dc4395e6ae37133e0
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-09-12 18:32:08 +0300

    chore: add textfield

--- lib/app/features/feed/create_article/views/pages/create_article_modal/create_article_modal.dart
+++ lib/app/features/feed/create_article/views/pages/create_article_modal/create_article_modal.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:ice/app/components/button/button.dart';
+import 'package:ice/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ice/app/extensions/extensions.dart';
 import 'package:ice/app/features/feed/views/components/toolbar/toolbar.dart';
+import 'package:ice/app/hooks/use_on_init.dart';
 import 'package:ice/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ice/app/router/components/sheet_content/sheet_content.dart';
 
-class CreateArticleModal extends StatelessWidget {
+class CreateArticleModal extends HookWidget {
   const CreateArticleModal({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final focusNode = useFocusNode();
+    useOnInit(focusNode.requestFocus);
+
     return SheetContent(
       body: Column(
         mainAxisSize: MainAxisSize.min,
@@ -31,7 +37,25 @@ class CreateArticleModal extends StatelessWidget {
               ),
             ],
           ),
-          Spacer(),
+          Expanded(
+            child: ScreenSideOffset.small(
+              child: TextField(
+                maxLines: null,
+                focusNode: focusNode,
+                keyboardType: TextInputType.multiline,
+                style: context.theme.appTextThemes.body2,
+                decoration: InputDecoration(
+                  hintStyle: context.theme.appTextThemes.body2.copyWith(
+                    color: context.theme.appColors.quaternaryText,
+                  ),
+                  hintText: context.i18n.create_post_modal_placeholder,
+                  border: InputBorder.none,
+                ),
+                cursorColor: context.theme.appColors.primaryAccent,
+                cursorHeight: 13.0.s,
+              ),
+            ),
+          ),
           Toolbar(),
         ],
       ),
