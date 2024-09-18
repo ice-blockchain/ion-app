95023c3757f17a4482dbef5bd950790248923704
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-09-18 10:26:50 +0300

    feat: add useAutomaticKeepAlive

--- lib/app/features/feed/feed_search/views/pages/feed_advanced_search_page/components/feed_advanced_search_users/feed_advanced_search_users.dart
+++ lib/app/features/feed/feed_search/views/pages/feed_advanced_search_page/components/feed_advanced_search_users/feed_advanced_search_users.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ice/app/features/feed/feed_search/providers/feed_search_users_provider.dart';
 import 'package:ice/app/features/feed/views/components/post_list/post_list.dart';
 import 'package:ice/app/features/feed/views/components/post_list/post_list_skeleton.dart';
 
-class FeedAdvancedSearchUsers extends ConsumerWidget {
+class FeedAdvancedSearchUsers extends HookConsumerWidget {
   const FeedAdvancedSearchUsers({super.key, required this.query});
 
   final String query;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    useAutomaticKeepAlive();
+
     final usersSearchResults = ref.watch(feedSearchUsersProvider(query));
+
     return CustomScrollView(
       slivers: [
         usersSearchResults.maybeWhen(
