bd6ee6730e6a4d0ce8fa5567e740baa751edb537
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-09-24 17:22:48 +0400

    chore: visibility toolbar ui

--- lib/app/features/feed/views/components/visibility_settings_toolbar/visibility_settings_toolbar.dart
+++ lib/app/features/feed/views/components/visibility_settings_toolbar/visibility_settings_toolbar.dart
@@ -3,6 +3,7 @@ import 'package:ice/app/components/list_item/list_item.dart';
 import 'package:ice/app/extensions/extensions.dart';
 import 'package:ice/app/features/feed/views/pages/visibility_settings_modal/data/model/visibility_settings_options.dart';
 import 'package:ice/app/router/app_routes.dart';
+import 'package:ice/generated/assets.gen.dart';
 
 class VisibilitySettingsToolbar extends StatelessWidget {
   const VisibilitySettingsToolbar({
@@ -16,16 +17,21 @@ class VisibilitySettingsToolbar extends StatelessWidget {
   Widget build(BuildContext context) {
     final option = VisibilitySettingsOptions.values[0];
 
-    return SizedBox(
-      height: 40.0.s,
-      child: ListItem(
-        title: Text(option.getTitle(context)),
-        backgroundColor: context.theme.appColors.onSecondaryBackground,
-        leading: option.getIcon(context),
-        onTap: () {
-          VisibilitySettingsRoute().go(context);
-        },
+    return ListItem(
+      title: Text(
+        option.getTitle(context),
+        style: context.theme.appTextThemes.caption.copyWith(
+          color: context.theme.appColors.primaryAccent,
+        ),
       ),
+      contentPadding: EdgeInsets.zero,
+      backgroundColor: context.theme.appColors.secondaryBackground,
+      leading: option.getIcon(context),
+      trailing: Assets.svg.iconArrowRight.icon(color: context.theme.appColors.primaryAccent),
+      constraints: BoxConstraints(minHeight: 40.0.s),
+      onTap: () {
+        VisibilitySettingsRoute().go(context);
+      },
     );
   }
 }
