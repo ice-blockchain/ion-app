d878e3374365db9b9461d400ecd8c0373cf922c7
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-09-25 16:03:10 +0300

    chore: add visibility options

---
+++ lib/app/features/feed/views/pages/visibility_settings_modal/data/model/visibility_settings_options.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:ice/app/extensions/extensions.dart';
+import 'package:ice/generated/assets.gen.dart';
+
+enum VisibilitySettingsOptions {
+  everyone,
+  followed_accounts,
+  verified_accounts,
+  mentioned_accounts,
+  ;
+
+  String getTitle(BuildContext context) {
+    return switch (this) {
+      VisibilitySettingsOptions.everyone => context.i18n.visibility_settings_everyone,
+      VisibilitySettingsOptions.followed_accounts =>
+        context.i18n.visibility_settings_followed_accounts,
+      VisibilitySettingsOptions.verified_accounts =>
+        context.i18n.visibility_settings_verified_accounts,
+      VisibilitySettingsOptions.mentioned_accounts =>
+        context.i18n.visibility_settings_mentioned_accounts,
+    };
+  }
+
+  Widget getIcon(BuildContext context) {
+    final icon = switch (this) {
+      VisibilitySettingsOptions.everyone => Assets.svg.iconPostEveryone,
+      VisibilitySettingsOptions.followed_accounts => Assets.svg.iconSearchFollow,
+      VisibilitySettingsOptions.verified_accounts => Assets.svg.iconBadgeVerify,
+      VisibilitySettingsOptions.mentioned_accounts => Assets.svg.iconFieldNickname,
+    };
+
+    return icon.icon(size: 24.0.s);
+  }
+}
---
+++ lib/app/features/feed/views/pages/visibility_settings_modal/visibility_settings_modal.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:ice/app/components/separated/separator.dart';
+import 'package:ice/app/extensions/extensions.dart';
+import 'package:ice/app/router/components/navigation_app_bar/navigation_app_bar.dart';
+import 'package:ice/app/router/components/sheet_content/sheet_content.dart';
+
+class VisibilitySettingsModal extends StatelessWidget {
+  const VisibilitySettingsModal({
+    required this.title,
+    super.key,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return SheetContent(
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          NavigationAppBar.modal(
+            showBackButton: false,
+            title: Text(title),
+          ),
+          SizedBox(height: 12.0.s),
+          HorizontalSeparator(),
+        ],
+      ),
+    );
+  }
+}
