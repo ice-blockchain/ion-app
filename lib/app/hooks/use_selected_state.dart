21fa761b01aaafb11a9cc140910c042034633d4c
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-09-26 17:22:52 +0300

    feat: add comments for the reusable hooks

--- lib/app/hooks/use_selected_state.dart
+++ lib/app/hooks/use_selected_state.dart
@@ -1,5 +1,9 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
 
+/// A hook that manages a selection state of items of type T.
+///
+/// Can be used in any UI component where users need to select/deselect
+/// multiple items, such as checkboxes, tags, categories, favorite etc.
 (List<T>, void Function(T)) useSelectedState<T>([List<T> initialState = const []]) {
   final selected = useState<Set<T>>(initialState.toSet());
 
