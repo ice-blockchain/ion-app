2361274b1bfe5cca615b93b811f0d8d819c4d6d8
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-10-08 14:28:57 +0300

    fix: popup pickers layout

--- lib/app/features/feed/views/pages/poll_length_time_modal/poll_length_time_modal.dart
+++ lib/app/features/feed/views/pages/poll_length_time_modal/poll_length_time_modal.dart
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ice/app/components/button/button.dart';
 import 'package:ice/app/components/screen_offset/screen_bottom_offset.dart';
@@ -9,15 +8,18 @@ import 'package:ice/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ice/app/extensions/extensions.dart';
 import 'package:ice/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 
-class PollLengthTimeModal extends HookWidget {
+const maxDaysCount = 8;
+const maxHoursCount = 24;
+
+class PollLengthTimeModal extends StatelessWidget {
   const PollLengthTimeModal({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    final selectedDay = useState(0);
-    final selectedHour = useState(0);
+    final dayScrollController = FixedExtentScrollController();
+    final hourScrollController = FixedExtentScrollController(initialItem: 3);
 
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -26,55 +28,96 @@ class PollLengthTimeModal extends HookWidget {
           showBackButton: false,
           title: Text(context.i18n.poll_length_modal_title),
         ),
-        const SizedBox(height: 16),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        Stack(
           children: [
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 178.0.s,
-                    child: CupertinoPicker(
-                      itemExtent: 32,
-                      onSelectedItemChanged: (int index) {
-                        selectedDay.value = index;
-                      },
-                      children: List<Widget>.generate(8, (int index) {
-                        return Center(
-                          child: Text(index.toString()),
-                        );
-                      }),
-                    ),
+            Positioned.fill(
+              child: Align(
+                child: Container(
+                  height: 34.0.s,
+                  margin: EdgeInsets.symmetric(horizontal: 9.0.s),
+                  decoration: BoxDecoration(
+                    color: context.theme.appColors.onSecondaryBackground,
+                    borderRadius: BorderRadius.circular(8.0.s),
                   ),
-                  const SizedBox(width: 8),
-                  const Text('day'),
-                ],
+                ),
               ),
             ),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 178.0.s,
-                    child: CupertinoPicker(
-                      itemExtent: 32,
-                      onSelectedItemChanged: (int index) {
-                        selectedHour.value = index;
-                      },
-                      children: List<Widget>.generate(24, (int index) {
-                        return Center(
-                          child: Text(index.toString()),
-                        );
-                      }),
-                    ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SizedBox(
+                        height: 178.0.s,
+                        width: 40.0.s,
+                        child: CupertinoPicker(
+                          itemExtent: 34.0.s,
+                          scrollController: dayScrollController,
+                          onSelectedItemChanged: (int index) {},
+                          selectionOverlay: Container(),
+                          children: List<Widget>.generate(maxDaysCount, (int index) {
+                            return Center(
+                              child: Text(
+                                index.toString(),
+                                style: context.theme.appTextThemes.body2.copyWith(
+                                  color: context.theme.appColors.primaryText,
+                                  fontSize: 23.0.s,
+                                ),
+                              ),
+                            );
+                          }),
+                        ),
+                      ),
+                      Text(
+                        'day',
+                        style: context.theme.appTextThemes.body2.copyWith(
+                          color: context.theme.appColors.primaryText,
+                          fontSize: 23.0.s,
+                        ),
+                      ),
+                    ],
                   ),
-                  const SizedBox(width: 8),
-                  const Text('hours'),
-                ],
-              ),
+                ),
+                SizedBox(
+                  width: 50.0.s,
+                ),
+                Flexible(
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        height: 178.0.s,
+                        width: 40.0.s,
+                        child: CupertinoPicker(
+                          itemExtent: 34.0.s,
+                          scrollController: hourScrollController,
+                          onSelectedItemChanged: (int index) {},
+                          selectionOverlay: Container(),
+                          children: List<Widget>.generate(maxHoursCount, (int index) {
+                            return Center(
+                              child: Text(
+                                index.toString(),
+                                style: context.theme.appTextThemes.body2.copyWith(
+                                  color: context.theme.appColors.primaryText,
+                                  fontSize: 23.0.s,
+                                ),
+                              ),
+                            );
+                          }),
+                        ),
+                      ),
+                      Text(
+                        'hours',
+                        style: context.theme.appTextThemes.body2.copyWith(
+                          color: context.theme.appColors.primaryText,
+                          fontSize: 23.0.s,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
