d93fe7391f049a6b4967b9d3149c64d7972b2d97
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-10-09 11:27:31 +0300

    feat: add relayProvider

--- lib/app/features/nostr/providers/relays_provider.dart
+++ lib/app/features/nostr/providers/relays_provider.dart
@@ -1,43 +1,14 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'package:ice/app/extensions/extensions.dart';
-import 'package:ice/app/features/auth/providers/auth_provider.dart';
 import 'package:nostr_dart/nostr_dart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'relays_provider.g.dart';
 
+typedef RelaysState = Map<String, NostrRelay>;
+
 @Riverpod(keepAlive: true)
-class Relays extends _$Relays {
-  static Map<String, NostrRelay> get initialState =>
-      Map<String, NostrRelay>.unmodifiable(<String, NostrRelay>{});
-
-  @override
-  Map<String, NostrRelay> build() {
-    ref.listen(authProvider, (previous, next) {
-      if ((next.valueOrNull?.hasAuthenticated).falseOrValue) {
-        _closeAll();
-      }
-    });
-
-    return initialState;
-  }
-
-  Future<NostrRelay> getOrCreate(String url) async {
-    if (state[url] != null) {
-      return state[url]!;
-    }
-    final relay = await NostrRelay.connect(url);
-    state = Map<String, NostrRelay>.unmodifiable(
-      <String, NostrRelay>{...state, url: relay},
-    );
-    return relay;
-  }
-
-  void _closeAll() {
-    for (final relay in state.values) {
-      relay.close();
-    }
-    state = initialState;
-  }
+Future<NostrRelay> relay(RelayRef ref, String url) async {
+  //TODO: when to close?
+  return NostrRelay.connect(url);
 }
