2ed1039ea1fc0cf16ed07a1b724f526faff6cf52
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-10-10 18:50:15 +0300

    feat: add NostrKeyStore, SecureStorage, flutter_secure_storage

---
+++ lib/app/features/nostr/providers/nostr_keystore_provider.dart
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:ice/app/features/auth/providers/auth_provider.dart';
+import 'package:ice/app/services/storage/secure_storage.dart';
+import 'package:nostr_dart/nostr_dart.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'nostr_keystore_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+class NostrKeyStore extends _$NostrKeyStore {
+  @override
+  Future<KeyStore?> build(String identityKeyName) async {
+    final storage = ref.watch(secureStorageProvider);
+    final storedKey = await storage.getString(key: _storageKey);
+    if (storedKey != null) {
+      return KeyStore.fromPrivate(storedKey);
+    }
+    return null;
+  }
+
+  Future<void> generate() async {
+    final keyStore = KeyStore.generate();
+    final storage = ref.read(secureStorageProvider);
+    await storage.setString(key: _storageKey, value: keyStore.privateKey);
+    state = AsyncData(keyStore);
+  }
+
+  Future<void> delete() async {
+    await ref.read(secureStorageProvider).remove(key: _storageKey);
+  }
+
+  String get _storageKey => '${identityKeyName}_nostr_key_store';
+}
+
+@riverpod
+Future<KeyStore?> currentUserNostrKeyStore(CurrentUserNostrKeyStoreRef ref) async {
+  final currentIdentityKeyName = ref.watch(currentIdentityKeyNameSelectorProvider);
+  if (currentIdentityKeyName == null) {
+    return null;
+  }
+  return ref.watch(nostrKeyStoreProvider(currentIdentityKeyName).future);
+}
