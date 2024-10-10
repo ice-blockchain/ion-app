ba76f2b15465b9ddbc281eb2f90586940d9fce0a
ice-tychon <175587874+ice-tychon@users.noreply.github.com>
2024-10-10 14:19:16 +0300

    feature(ion identity client): add empty example project (#225)
    ### What does this PR do?
    This PR creates an "example" project. It's a fresh project from `flutter
    create`. We'll add examples in future.

---
+++ packages/ion_identity_client/example/android/app/src/main/kotlin/com/example/ion_client_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ion_client_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ packages/ion_identity_client/example/test/empty_test.dart
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('empty test', () {
+    expect(true, isTrue);
+  });
+}
