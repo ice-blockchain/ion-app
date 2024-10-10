13b928e0f1182cc750b16b76e35e37297982a5b4
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-10-10 16:20:29 +0300

    refactor: code clean up

---
+++ lib/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_poll_block/components/poll_close_button/poll_close_button.dart
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:flutter/material.dart';
+import 'package:ice/app/components/button/button.dart';
+import 'package:ice/app/extensions/extensions.dart';
+import 'package:ice/generated/assets.gen.dart';
+
+class PollCloseButton extends StatelessWidget {
+  const PollCloseButton({required this.onClosePress, super.key});
+
+  final VoidCallback onClosePress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: -10.0.s,
+      right: 13.0.s,
+      child: Button.icon(
+        size: 24.0.s,
+        type: ButtonType.outlined,
+        icon: Assets.svg.iconSheetClose.icon(
+          size: 14.4.s,
+          color: context.theme.appColors.primaryText,
+        ),
+        borderColor: context.theme.appColors.onTerararyFill,
+        style: OutlinedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(9.6.s)),
+          ),
+          backgroundColor: context.theme.appColors.tertararyBackground,
+        ),
+        onPressed: onClosePress,
+      ),
+    );
+  }
+}
---
+++ lib/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_poll_block/components/poll_length_button/poll_length_button.dart
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:flutter/material.dart';
+import 'package:ice/app/components/button/button.dart';
+import 'package:ice/app/extensions/extensions.dart';
+import 'package:ice/generated/assets.gen.dart';
+
+class PollLengthButton extends StatelessWidget {
+  const PollLengthButton({required this.onPollLengthPress, super.key});
+
+  final VoidCallback onPollLengthPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Button(
+        type: ButtonType.secondary,
+        style: const ButtonStyle(
+          padding: WidgetStatePropertyAll<EdgeInsetsGeometry>(EdgeInsets.zero),
+        ),
+        label: Text(
+          context.i18n.poll_length_button_title,
+          style: context.theme.appTextThemes.caption.copyWith(
+            color: context.theme.appColors.primaryAccent,
+          ),
+        ),
+        backgroundColor: context.theme.appColors.secondaryBackground,
+        borderColor: context.theme.appColors.secondaryBackground,
+        leadingIcon: Assets.svg.iconBlockTime.icon(size: 16.0.s),
+        onPressed: onPollLengthPress,
+      ),
+    );
+  }
+}
