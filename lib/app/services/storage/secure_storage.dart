3e6d006962dc5027522773583013ac7370220d0b
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-10-10 18:50:15 +0300

    feat: add NostrKeyStore, SecureStorage, flutter_secure_storage

---
+++ lib/app/services/storage/secure_storage.dart
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'dart:async';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'secure_storage.g.dart';
+
+class SecureStorage {
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  Future<String?> getString({required String key}) {
+    return _storage.read(key: key);
+  }
+
+  Future<void> setString({required String key, required String value}) {
+    return _storage.write(key: key, value: value);
+  }
+
+  Future<void> remove({required String key}) {
+    return _storage.delete(key: key);
+  }
+}
+
+@Riverpod(keepAlive: true)
+SecureStorage secureStorage(SecureStorageRef ref) {
+  return SecureStorage();
+}
