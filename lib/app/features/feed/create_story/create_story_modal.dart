3c2d535da8b1b0dd0ad1c0932f0014cbebfdd273
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-10-11 14:27:23 +0300

    chore: move opening of the camera to feed main modal page

--- lib/app/features/feed/create_story/create_story_modal.dart
+++ lib/app/features/feed/create_story/create_story_modal.dart
@@ -2,10 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:ice/app/extensions/extensions.dart';
-import 'package:ice/app/features/core/permissions/data/models/permissions_types.dart';
-import 'package:ice/app/features/core/permissions/views/components/permission_aware_widget.dart';
-import 'package:ice/app/features/core/permissions/views/components/permission_dialogs/permission_sheets.dart';
-import 'package:ice/app/router/app_routes.dart';
 import 'package:ice/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ice/app/router/components/sheet_content/sheet_content.dart';
 
@@ -20,25 +16,6 @@ class CreateStoryModal extends StatelessWidget {
         children: [
           NavigationAppBar.modal(
             title: Text(context.i18n.create_story_nav_title),
-          ),
-          const Spacer(),
-          Center(
-            child: PermissionAwareWidget(
-              permissionType: Permission.camera,
-              builder: (context, onPressed) {
-                return GestureDetector(
-                  onTap: onPressed,
-                  child: const Text(
-                    'Create Story',
-                  ),
-                );
-              },
-              onGranted: () {
-                StoryCameraRoute().push<void>(context);
-              },
-              requestDialog: PermissionRequestSheet.fromType(context, Permission.camera),
-              settingsDialog: SettingsRedirectSheet.fromType(context, Permission.camera),
-            ),
           ),
           const Spacer(),
         ],
