3cc77576cad01ef523ebaf93f719cd3529384357
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-10-15 14:15:38 +0300

    chore: enable looping

--- lib/app/features/feed/create_story/views/components/story_video/story_video_preview.dart
+++ lib/app/features/feed/create_story/views/components/story_video/story_video_preview.dart
@@ -32,6 +32,7 @@ class StoryVideoPreview extends ConsumerWidget {
                   videoControllerProvider(
                     filePath,
                     autoPlay: true,
+                    looping: true,
                   ),
                 );
 
