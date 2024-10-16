1eba4def01c513ef2c885008d3df1d6f136b2835
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-10-16 17:16:56 +0300

    refactor: code clean up

--- lib/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_poll_block/text_editor_poll_block.dart
+++ lib/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_poll_block/text_editor_poll_block.dart
@@ -2,19 +2,23 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_quill/quill_delta.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ice/app/extensions/extensions.dart';
+import 'package:ice/app/features/feed/providers/poll/poll_length_time_provider.dart';
 import 'package:ice/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_poll_block/poll_add_answer_button.dart';
 import 'package:ice/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_poll_block/poll_answers_list_view.dart';
 import 'package:ice/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_poll_block/poll_close_button.dart';
 import 'package:ice/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_poll_block/poll_length_button.dart';
 import 'package:ice/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_poll_block/poll_title.dart';
+import 'package:ice/app/features/feed/views/pages/poll_length_time_modal/poll_length_time_modal.dart';
+import 'package:ice/app/router/utils/show_simple_bottom_sheet.dart';
 
 const textEditorPollKey = 'text-editor-poll';
 
 ///
 /// Embeds a poll in the text editor.
 ///
-
 class TextEditorPollEmbed extends CustomBlockEmbed {
   TextEditorPollEmbed() : super(textEditorPollKey, '');
 }
@@ -22,14 +26,14 @@ class TextEditorPollEmbed extends CustomBlockEmbed {
 ///
 /// Embed builder for [TextEditorPollBuilder].
 ///
-
 class TextEditorPollBuilder extends EmbedBuilder {
   TextEditorPollBuilder({
-    this.onPollLengthPress,
-    this.onRemovePollPress,
+    required this.controller,
+    required this.ref,
   });
-  final VoidCallback? onPollLengthPress;
-  final ValueChanged<Embed>? onRemovePollPress;
+
+  final QuillController controller;
+  final WidgetRef ref;
 
   @override
   String get key => textEditorPollKey;
@@ -43,6 +47,9 @@ class TextEditorPollBuilder extends EmbedBuilder {
     bool inline,
     TextStyle textStyle,
   ) {
+    final selectedDay = ref.watch(selectedDayNotifierProvider);
+    final selectedHour = ref.watch(selectedHourNotifierProvider);
+
     return Stack(
       clipBehavior: Clip.none,
       children: [
@@ -72,15 +79,72 @@ class TextEditorPollBuilder extends EmbedBuilder {
             ),
             PollLengthButton(
               onPollLengthPress: () {
-                onPollLengthPress?.call();
+                _showPollLengthTimeModal(
+                  context,
+                  selectedDay,
+                  selectedHour,
+                  (newDay, newHour) {
+                    ref.read(selectedDayNotifierProvider.notifier).day = newDay;
+                    ref.read(selectedHourNotifierProvider.notifier).hour = newHour;
+                  },
+                );
               },
             ),
           ],
         ),
         PollCloseButton(
-          onClosePress: () => onRemovePollPress?.call(node),
+          onClosePress: () => _removePoll(node),
         ),
       ],
     );
+  }
+
+  Future<void> _showPollLengthTimeModal(
+    BuildContext context,
+    int selectedDay,
+    int selectedHour,
+    void Function(int, int) onApply,
+  ) async {
+    return showSimpleBottomSheet<void>(
+      context: context,
+      child: PollLengthTimeModal(
+        selectedDay: selectedDay,
+        selectedHour: selectedHour,
+        onApply: onApply,
+      ),
+    );
+  }
+
+  void _removePoll(Embed node) {
+    final delta = controller.document.toDelta();
+    var pollIndex = -1;
+    var pollLength = 0;
+    var currentIndex = 0;
+
+    for (final operation in delta.operations) {
+      final length = operation.length ?? 1;
+
+      if (operation.isInsert && operation.data is Map<String, dynamic>) {
+        final data = operation.data! as Map<String, dynamic>;
+        if (data.containsKey('custom')) {
+          pollIndex = currentIndex;
+          pollLength = length;
+          break;
+        }
+      }
+      currentIndex += length;
+    }
+
+    if (pollIndex != -1) {
+      final deleteDelta = Delta()
+        ..retain(pollIndex)
+        ..delete(pollLength);
+
+      controller.compose(
+        deleteDelta,
+        TextSelection.collapsed(offset: pollIndex),
+        ChangeSource.local,
+      );
+    }
   }
 }
--- lib/app/features/feed/views/components/text_editor/text_editor.dart
+++ lib/app/features/feed/views/components/text_editor/text_editor.dart
@@ -2,14 +2,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
-import 'package:flutter_quill/quill_delta.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ice/app/extensions/extensions.dart';
-import 'package:ice/app/features/feed/providers/poll/poll_length_time_provider.dart';
 import 'package:ice/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_poll_block/text_editor_poll_block.dart';
 import 'package:ice/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_single_image_block/text_editor_single_image_block.dart';
-import 'package:ice/app/features/feed/views/pages/poll_length_time_modal/poll_length_time_modal.dart';
-import 'package:ice/app/router/utils/show_simple_bottom_sheet.dart';
 
 class TextEditor extends ConsumerWidget {
   const TextEditor(
@@ -20,62 +16,8 @@ class TextEditor extends ConsumerWidget {
   final QuillController controller;
   final String? placeholder;
 
-  Future<void> _showPollLengthTimeModal(
-    BuildContext context,
-    int selectedDay,
-    int selectedHour,
-    void Function(int, int) onApply,
-  ) async {
-    return showSimpleBottomSheet<void>(
-      context: context,
-      child: PollLengthTimeModal(
-        selectedDay: selectedDay,
-        selectedHour: selectedHour,
-        onApply: (newDay, newHour) {
-          onApply(newDay, newHour);
-        },
-      ),
-    );
-  }
-
-  void _removePoll(Embed node) {
-    final delta = controller.document.toDelta();
-    var pollIndex = -1;
-    var pollLength = 0;
-    var currentIndex = 0;
-
-    for (final operation in delta.operations) {
-      final length = operation.length ?? 1;
-
-      if (operation.isInsert && operation.data is Map<String, dynamic>) {
-        final data = operation.data! as Map<String, dynamic>;
-        if (data.containsKey('custom')) {
-          pollIndex = currentIndex;
-          pollLength = length;
-          break;
-        }
-      }
-      currentIndex += length;
-    }
-
-    if (pollIndex != -1) {
-      final deleteDelta = Delta()
-        ..retain(pollIndex)
-        ..delete(pollLength);
-
-      controller.compose(
-        deleteDelta,
-        TextSelection.collapsed(offset: pollIndex),
-        ChangeSource.local,
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final selectedDay = ref.watch(selectedDayNotifierProvider);
-    final selectedHour = ref.watch(selectedHourNotifierProvider);
-
     return Column(
       children: [
         QuillEditor.basic(
@@ -84,18 +26,8 @@ class TextEditor extends ConsumerWidget {
             embedBuilders: [
               TextEditorSingleImageBuilder(),
               TextEditorPollBuilder(
-                onPollLengthPress: () {
-                  _showPollLengthTimeModal(
-                    context,
-                    selectedDay,
-                    selectedHour,
-                    (newDay, newHour) {
-                      ref.read(selectedDayNotifierProvider.notifier).day = newDay;
-                      ref.read(selectedHourNotifierProvider.notifier).hour = newHour;
-                    },
-                  );
-                },
-                onRemovePollPress: _removePoll,
+                controller: controller,
+                ref: ref,
               ),
             ],
             autoFocus: true,
