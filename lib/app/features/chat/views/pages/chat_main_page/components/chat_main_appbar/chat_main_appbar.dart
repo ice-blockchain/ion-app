af8f91e106f833afa291f805903fbf3d59881b6e
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2024-10-18 04:40:58 +0300

    add edit mode provider

--- lib/app/features/chat/views/pages/chat_main_page/components/chat_main_appbar/chat_main_appbar.dart
+++ lib/app/features/chat/views/pages/chat_main_page/components/chat_main_appbar/chat_main_appbar.dart
@@ -1,22 +1,28 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ice/app/extensions/extensions.dart';
+import 'package:ice/app/features/chat/recent_chats/providers/conversations_edit_mode_provider.dart';
 import 'package:ice/app/router/app_routes.dart';
 import 'package:ice/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ice/app/router/components/navigation_app_bar/navigation_text_button.dart';
 import 'package:ice/generated/assets.gen.dart';
 
-class ChatMainAppBar extends StatelessWidget implements PreferredSizeWidget {
+class ChatMainAppBar extends ConsumerWidget implements PreferredSizeWidget {
   const ChatMainAppBar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final editMode = ref.watch(conversationsEditModeProvider);
+
     return NavigationAppBar.screen(
       leading: NavigationTextButton(
-        label: context.i18n.button_edit,
+        label: editMode ? context.i18n.core_done : context.i18n.button_edit,
         textStyle: context.theme.appTextThemes.subtitle2,
-        onPressed: () {},
+        onPressed: () {
+          ref.read(conversationsEditModeProvider.notifier).editMode = !editMode;
+        },
       ),
       title: GestureDetector(
         onDoubleTap: () {
