7fcadaf2b7f9cdcef59a85e4622ab0d859bae4d6
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-10-20 11:36:32 +0300

    fix: resolve conflicts

--- lib/app/features/feed/create_story/views/components/story_video/story_published_notification.dart
+++ lib/app/features/feed/create_story/views/components/story_video/story_published_notification.dart
@@ -1,37 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ice/app/effects/slide_in_out_effects.dart';
 import 'package:ice/app/extensions/extensions.dart';
+import 'package:ice/app/features/feed/create_story/providers/story_camera_provider.dart';
 import 'package:ice/generated/assets.gen.dart';
 
-class StoryPublishedNotification extends StatelessWidget {
-  const StoryPublishedNotification({
-    required this.height,
-    super.key,
-  });
+class StoryPublishedNotification extends ConsumerWidget {
+  const StoryPublishedNotification({required this.height, super.key});
 
   final double height;
 
   @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: height,
-      child: ColoredBox(
-        color: context.theme.appColors.primaryAccent,
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 3.0.s),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Assets.svg.iconBlockCheckboxOn.icon(
-                color: context.theme.appColors.onPrimaryAccent,
-              ),
-              SizedBox(width: 12.0.s),
-              Text(
-                context.i18n.story_has_been_posted,
-                style: context.theme.appTextThemes.body2.copyWith(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isStoryPublished = ref.watch(
+      storyCameraControllerProvider.select((state) => state.isStoryPublished),
+    );
+
+    return Animate(
+      effects: topSlideInOutEffects,
+      onComplete: (controller) =>
+          ref.read(storyCameraControllerProvider.notifier).resetStoryPublished(),
+      autoPlay: isStoryPublished,
+      child: SizedBox(
+        height: height,
+        child: ColoredBox(
+          color: context.theme.appColors.primaryAccent,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 3.0.s),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Assets.svg.iconBlockCheckboxOn.icon(
                   color: context.theme.appColors.onPrimaryAccent,
                 ),
-              ),
-            ],
+                SizedBox(width: 12.0.s),
+                Text(
+                  context.i18n.story_has_been_posted,
+                  style: context.theme.appTextThemes.body2.copyWith(
+                    color: context.theme.appColors.onPrimaryAccent,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
