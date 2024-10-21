53eb9cf005191bb33c47c1cd63fb6e7f0b094f5c
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2024-10-21 17:32:12 +0300

    feat: add chat edit UI and refactor bottom nav bar

--- lib/app/features/chat/recent_chats/providers/conversations_edit_mode_provider.dart
+++ lib/app/features/chat/recent_chats/providers/conversations_edit_mode_provider.dart
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:ion/app/features/chat/providers/mock.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'conversations_edit_mode_provider.g.dart';
 
-@Riverpod(keepAlive: true)
+@Riverpod()
+class Conversations extends _$Conversations {
+  @override
+  Future<List<RecentChatDataModel>> build() {
+    return Future.delayed(const Duration(seconds: 2), () {
+      return mockConversationData;
+    });
+  }
+}
+
+@Riverpod()
 class ConversationsEditMode extends _$ConversationsEditMode {
   @override
   bool build() {
@@ -12,6 +23,27 @@ class ConversationsEditMode extends _$ConversationsEditMode {
   }
 
   set editMode(bool editMode) {
+    ref.read(selectedConversationsIdsProvider.notifier).clear();
     state = editMode;
+  }
+}
+
+@Riverpod()
+class SelectedConversationsIds extends _$SelectedConversationsIds {
+  @override
+  List<String> build() {
+    return [];
+  }
+
+  void toggle(String conversationId) {
+    if (state.contains(conversationId)) {
+      state = List.from(state)..remove(conversationId);
+    } else {
+      state = List.from(state)..add(conversationId);
+    }
+  }
+
+  void clear() {
+    state = [];
   }
 }
