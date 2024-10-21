56f3552c01e697e08d1848f76de29d8aa02a4ea5
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-10-21 18:32:30 +0300

    chore: start work on separator

--- lib/app/features/feed/views/components/toolbar_buttons/toolbar_separator_button.dart
+++ lib/app/features/feed/views/components/toolbar_buttons/toolbar_separator_button.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:ion/app/features/feed/views/components/actions_toolbar_button/actions_toolbar_button.dart';
+import 'package:ion/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_separator_block/text_editor_separator_block.dart';
 import 'package:ion/generated/assets.gen.dart';
 
 class ToolbarSeparatorButton extends StatelessWidget {
@@ -13,7 +14,18 @@ class ToolbarSeparatorButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ActionsToolbarButton(
       icon: Assets.svg.iconArticleSeparator,
-      onPressed: () {},
+      onPressed: () {
+        final index = textEditorController.selection.baseOffset;
+
+        textEditorController.document.insert(
+          index,
+          BlockEmbed.custom(
+            TextEditorSeparatorEmbed(),
+          ),
+        );
+
+        textEditorController.moveCursorToPosition(index + 1);
+      },
     );
   }
 }
