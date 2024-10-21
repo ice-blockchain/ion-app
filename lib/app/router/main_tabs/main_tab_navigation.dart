b24286b4870f2448fba455ffe670a2a6df2079e7
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2024-10-21 19:02:43 +0300

    improve bottom nav bar

--- lib/app/router/main_tabs/main_tab_navigation.dart
+++ lib/app/router/main_tabs/main_tab_navigation.dart
@@ -6,10 +6,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/chat/recent_chats/providers/conversations_edit_mode_provider.dart';
+import 'package:ion/app/features/chat/recent_chats/views/components/conversation_edit_bottom_bar/conversation_edit_bottom_bar.dart';
 import 'package:ion/app/router/main_tabs/components/components.dart';
 
-class MainTabNavigation extends HookWidget {
+class MainTabNavigation extends HookConsumerWidget {
   const MainTabNavigation({
     required this.shell,
     required this.state,
@@ -20,58 +23,68 @@ class MainTabNavigation extends HookWidget {
   final GoRouterState state;
 
   @override
-  Widget build(BuildContext context) {
-    final currentTab = TabItem.fromNavigationIndex(shell.currentIndex);
+  Widget build(BuildContext context, WidgetRef ref) {
     final tabPressStreamController = useStreamController<TabPressSteamData>();
+    final currentTab = TabItem.fromNavigationIndex(shell.currentIndex);
+    final conversationsEditMode = ref.watch(conversationsEditModeProvider);
 
     return Scaffold(
       body: MainTabNavigationContainer(
         tabPressStream: tabPressStreamController.stream,
         child: shell,
       ),
-      bottomNavigationBar: Container(
-        decoration: state.isMainModalOpen
-            ? BoxDecoration(
-                color: context.theme.appColors.secondaryBackground,
-              )
-            : BoxDecoration(
-                color: context.theme.appColors.secondaryBackground,
-                boxShadow: [
-                  BoxShadow(
-                    color: context.theme.appColors.darkBlue.withAlpha(14),
-                    blurRadius: 10,
+      bottomNavigationBar: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: context.theme.appColors.secondaryBackground,
+              boxShadow: state.isMainModalOpen
+                  ? null
+                  : [
+                      BoxShadow(
+                        color: context.theme.appColors.darkBlue.withAlpha(14),
+                        blurRadius: 10,
+                      ),
+                    ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: TabItem.values.map((tabItem) {
+                final isSelected = currentTab == tabItem;
+
+                return Expanded(
+                  child: GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () =>
+                        _onTabPress(context, tabItem.index, currentTab, tabPressStreamController),
+                    child: Container(
+                      padding: EdgeInsets.only(
+                        top: 9.0.s,
+                        // Adding extra padding to the bottom of the tab bar if the device has a bottom padding
+                        bottom: MediaQuery.of(context).padding.bottom > 0 ? 23.0.s : 9.0.s,
+                      ),
+                      color: context.theme.appColors.secondaryBackground,
+                      child: _getTabButton(tabItem, isSelected),
+                    ),
                   ),
-                ],
-              ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: TabItem.values.map((tabItem) {
-            final isSelected = currentTab == tabItem;
-            return Expanded(
-              child: GestureDetector(
-                behavior: HitTestBehavior.opaque,
-                // hit
-                onTap: () =>
-                    _onTabPress(context, tabItem.index, currentTab, tabPressStreamController),
-                child: Container(
-                  padding: EdgeInsets.only(
-                    top: 9.0.s,
-                    bottom: MediaQuery.of(context).padding.bottom > 0 ? 23.0.s : 9.0.s,
-                  ),
-                  color: context.theme.appColors.secondaryBackground,
-                  child: tabItem == TabItem.main
-                      ? const MainTabButton()
-                      : TabIcon(
-                          icon: tabItem.icon,
-                          isSelected: isSelected,
-                        ),
-                ),
-              ),
-            );
-          }).toList(),
-        ),
+                );
+              }).toList(),
+            ),
+          ),
+          if (conversationsEditMode && currentTab == TabItem.chat)
+            const ConversationEditBottomBar(),
+        ],
       ),
     );
+  }
+
+  Widget _getTabButton(TabItem tabItem, bool isSelected) {
+    return tabItem == TabItem.main
+        ? const MainTabButton()
+        : TabIcon(
+            icon: tabItem.icon,
+            isSelected: isSelected,
+          );
   }
 
   void _onTabPress(
