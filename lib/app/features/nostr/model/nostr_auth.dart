5fa5525f4fda5ef25e67097b8473c18b9c2b0ff3
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-10-22 15:21:05 +0300

    feat: add NostrAuth model

---
+++ lib/app/features/nostr/model/nostr_auth.dart
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'dart:convert';
+
+import 'package:convert/convert.dart';
+import 'package:cryptography/cryptography.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:nostr_dart/nostr_dart.dart';
+
+part 'nostr_auth.freezed.dart';
+
+@freezed
+class NostrAuth with _$NostrAuth {
+  const factory NostrAuth({
+    required String url,
+    required String method,
+    List<int>? payload,
+  }) = _NostrAuth;
+
+  const NostrAuth._();
+
+  /// https://github.com/nostr-protocol/nips/blob/master/98.md
+  Future<EventMessage> toEventMessage(KeyStore keyStore) async {
+    final tags = [
+      ['u', url],
+      ['method', method],
+    ];
+
+    if (payload != null) {
+      final hash = await Sha256().hash(payload!);
+      tags.add(['payload', hex.encode(hash.bytes)]);
+    }
+
+    return EventMessage.fromData(
+      signer: keyStore,
+      kind: kind,
+      tags: tags,
+      content: '',
+    );
+  }
+
+  Future<String> toAuthorizationHeader(KeyStore keyStore) async {
+    final event = await toEventMessage(keyStore);
+    final eventPayload = event.toJson().last;
+    final headerValue = base64Encode(utf8.encode(jsonEncode(eventPayload)));
+    return 'Nostr $headerValue';
+  }
+
+  static const int kind = 27235;
+}
