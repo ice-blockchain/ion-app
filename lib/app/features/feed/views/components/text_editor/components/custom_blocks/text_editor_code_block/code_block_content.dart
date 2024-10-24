bc5ad73a23d441296b449b3205d942417fa13a8d
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-10-24 15:26:25 +0300

    chore: handle backspace

--- lib/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_code_block/code_block_content.dart
+++ lib/app/features/feed/views/components/text_editor/components/custom_blocks/text_editor_code_block/code_block_content.dart
@@ -1,37 +1,53 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
 
 class CodeBlockContent extends HookConsumerWidget {
-  const CodeBlockContent({super.key});
+  const CodeBlockContent({
+    required this.onRemoveBlock,
+    super.key,
+  });
+
+  final VoidCallback onRemoveBlock;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final codeText = useState('');
-
     final textController = useTextEditingController(text: codeText.value);
+    final focusNode = useFocusNode();
 
     return Padding(
       padding: EdgeInsets.only(bottom: 12.0.s, top: 8.0.s, left: 12.0.s, right: 12.0.s),
-      child: TextField(
-        controller: textController,
-        onChanged: (value) {
-          codeText.value = value;
+      child: KeyboardListener(
+        focusNode: focusNode,
+        onKeyEvent: (KeyEvent event) {
+          if (event is KeyDownEvent &&
+              event.logicalKey == LogicalKeyboardKey.backspace &&
+              codeText.value.trim().isEmpty) {
+            onRemoveBlock();
+          }
         },
-        maxLines: null,
-        cursorColor: context.theme.appColors.primaryAccent,
-        cursorHeight: 22.0.s,
-        style: TextStyle(
-          fontFamily: 'Monaco',
-          fontSize: 12.0.s,
-          color: context.theme.appColors.primaryText,
-        ),
-        decoration: const InputDecoration(
-          border: InputBorder.none,
-          contentPadding: EdgeInsets.zero,
+        child: TextField(
+          controller: textController,
+          onChanged: (value) {
+            codeText.value = value;
+          },
+          maxLines: null,
+          cursorColor: context.theme.appColors.primaryAccent,
+          cursorHeight: 22.0.s,
+          style: TextStyle(
+            fontFamily: 'Monaco-Regular',
+            fontSize: 12.0.s,
+            color: context.theme.appColors.primaryText,
+          ),
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.zero,
+          ),
         ),
       ),
     );
