3a76a5baf4395403881cfbe03fecc910d82a9879
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-10-27 18:18:19 +0300

    feat: wrap avatar picker with PermissionAwareWidget

--- lib/app/features/components/avatar_picker/avatar_picker.dart
+++ lib/app/features/components/avatar_picker/avatar_picker.dart
@@ -7,6 +7,10 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/avatar/avatar.dart';
 import 'package:ion/app/components/progress_bar/ion_loading_indicator.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/core/permissions/data/models/permissions_types.dart';
+import 'package:ion/app/features/core/permissions/views/components/permission_aware_widget.dart';
+import 'package:ion/app/features/core/permissions/views/components/permission_dialogs/permission_request_sheet.dart';
+import 'package:ion/app/features/core/permissions/views/components/permission_dialogs/settings_redirect_sheet.dart';
 import 'package:ion/app/features/user/providers/avatar_picker_notifier.dart';
 import 'package:ion/app/services/media_service/media_service.dart';
 import 'package:ion/generated/assets.gen.dart';
@@ -44,25 +48,35 @@ class AvatarPicker extends ConsumerWidget {
         Positioned(
           bottom: -6.0.s,
           right: 0,
-          child: GestureDetector(
-            onTap: avatarPickerState is AvatarPickerStatePicked
-                ? null
-                : () => ref.read(avatarPickerNotifierProvider.notifier).pick(
+          child: PermissionAwareWidget(
+            permissionType: Permission.camera,
+            onGranted: () {
+              if (avatarPickerState is! AvatarPickerStatePicked) {
+                ref.read(avatarPickerNotifierProvider.notifier).pick(
                       cropUiSettings:
                           ref.read(mediaServiceProvider).buildCropImageUiSettings(context),
-                    ),
-            child: Container(
-              width: 36.0.s,
-              height: 36.0.s,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: context.theme.appColors.primaryAccent,
-              ),
-              child: avatarPickerState is AvatarPickerStatePicked
-                  ? const IonLoadingIndicator()
-                  : Assets.svg.iconLoginCamera.icon(),
-            ),
+                    );
+              }
+            },
+            requestDialog: PermissionRequestSheet.fromType(context, Permission.camera),
+            settingsDialog: SettingsRedirectSheet.fromType(context, Permission.camera),
+            builder: (context, onPressed) {
+              return GestureDetector(
+                onTap: onPressed,
+                child: Container(
+                  width: 36.0.s,
+                  height: 36.0.s,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: context.theme.appColors.primaryAccent,
+                  ),
+                  child: avatarPickerState is AvatarPickerStatePicked
+                      ? const IonLoadingIndicator()
+                      : Assets.svg.iconLoginCamera.icon(),
+                ),
+              );
+            },
           ),
         ),
       ],
