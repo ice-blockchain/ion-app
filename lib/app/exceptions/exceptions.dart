90c62684737a9e3a83990a327ee767e41b270583
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-11-01 12:59:50 +0300

    refacotr: IonException -> IONException

--- lib/app/exceptions/exceptions.dart
+++ lib/app/exceptions/exceptions.dart
@@ -1,49 +1,49 @@
 // SPDX-License-Identifier: ice License 1.0
 
-sealed class IonException implements Exception {
-  IonException(this.code, this.message);
+sealed class IONException implements Exception {
+  IONException(this.code, this.message);
 
   final int code;
   final String message;
 
   @override
-  String toString() => 'IonException(code: $code, message: $message)';
+  String toString() => 'IONException(code: $code, message: $message)';
 }
 
-class FollowListNotFoundException extends IonException {
+class FollowListNotFoundException extends IONException {
   FollowListNotFoundException() : super(10001, 'Follow list is null');
 }
 
-class FollowListIsEmptyException extends IonException {
+class FollowListIsEmptyException extends IONException {
   FollowListIsEmptyException() : super(10002, 'Follow list is empty');
 }
 
-class KeystoreNotFoundException extends IonException {
+class KeystoreNotFoundException extends IONException {
   KeystoreNotFoundException() : super(10003, 'KeyStore is null');
 }
 
-class UserRelaysNotFoundException extends IonException {
+class UserRelaysNotFoundException extends IONException {
   UserRelaysNotFoundException() : super(10004, 'User relays not found');
 }
 
-class UserIndexersNotFoundException extends IonException {
+class UserIndexersNotFoundException extends IONException {
   UserIndexersNotFoundException() : super(10005, 'User indexers not found');
 }
 
-class IncorrectEventKindException extends IonException {
+class IncorrectEventKindException extends IONException {
   IncorrectEventKindException({
     required int actual,
     required int expected,
   }) : super(10006, 'Incorrect event with kind $actual, expected $expected');
 }
 
-class IncorrectEventTagException extends IonException {
+class IncorrectEventTagException extends IONException {
   IncorrectEventTagException({
     required String actual,
     required String expected,
   }) : super(10007, 'Incorrect event tag $actual, expected $expected');
 }
 
-class MainWalletNotFoundException extends IonException {
+class MainWalletNotFoundException extends IONException {
   MainWalletNotFoundException() : super(10008, 'Main wallet not found');
 }
