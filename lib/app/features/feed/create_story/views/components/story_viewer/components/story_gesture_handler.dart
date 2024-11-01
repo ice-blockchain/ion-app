7181ef4a01c54f7eb51d7b6f2caf5a77e24fa0c9
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-11-01 04:19:27 +0200

    chore: simpify UserStoryPageView

---
+++ lib/app/features/feed/create_story/views/components/story_viewer/components/story_gesture_handler.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class StoryGestureHandler extends HookConsumerWidget {
+  const StoryGestureHandler({
+    required this.child,
+    required this.onTapLeft,
+    required this.onTapRight,
+    required this.onLongPressStart,
+    required this.onLongPressEnd,
+    super.key,
+  });
+
+  final Widget child;
+  final VoidCallback onTapLeft;
+  final VoidCallback onTapRight;
+  final VoidCallback onLongPressStart;
+  final VoidCallback onLongPressEnd;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tapPosition = useState<Offset?>(null);
+
+    return GestureDetector(
+      onTapDown: (details) => tapPosition.value = details.globalPosition,
+      onTap: () {
+        final screenWidth = MediaQuery.sizeOf(context).width;
+        final isLeftSide = (tapPosition.value?.dx ?? 0) < screenWidth / 2;
+
+        if (isLeftSide) {
+          onTapLeft();
+        } else {
+          onTapRight();
+        }
+      },
+      onLongPress: onLongPressStart,
+      onLongPressEnd: (_) => onLongPressEnd,
+      child: child,
+    );
+  }
+}
--- lib/app/features/feed/create_story/views/components/story_viewer/components/user_story_page_view.dart
+++ lib/app/features/feed/create_story/views/components/story_viewer/components/user_story_page_view.dart
@@ -1,21 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/core/providers/video_player_provider.dart';
 import 'package:ion/app/features/feed/create_story/data/models/story.dart';
-import 'package:ion/app/features/feed/create_story/providers/emoji_reaction_provider.dart';
 import 'package:ion/app/features/feed/create_story/providers/story_viewing_provider.dart';
-import 'package:ion/app/features/feed/create_story/views/components/story_viewer/components/story_input_field.dart';
-import 'package:ion/app/features/feed/create_story/views/components/story_viewer/components/story_reaction_notification.dart';
-import 'package:ion/app/features/feed/create_story/views/components/story_viewer/components/story_reaction_overlay.dart';
-import 'package:ion/app/features/feed/create_story/views/components/story_viewer/components/story_viewer_action_buttons.dart';
-import 'package:ion/app/features/feed/create_story/views/components/story_viewer/components/story_viewer_content.dart';
-import 'package:ion/app/features/feed/create_story/views/components/story_viewer/components/story_viewer_header.dart';
-import 'package:ion/app/features/feed/create_story/views/pages/story_viewer_page.dart';
+import 'package:ion/app/features/feed/create_story/views/components/story_viewer/components/story_content.dart';
+import 'package:ion/app/features/feed/create_story/views/components/story_viewer/components/story_gesture_handler.dart';
 
-class UserStoryPageView extends HookConsumerWidget {
+class UserStoryPageView extends ConsumerWidget {
   const UserStoryPageView({
     required this.user,
     required this.isCurrentUser,
@@ -35,94 +26,38 @@ class UserStoryPageView extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final tapPosition = useState<Offset?>(null);
-    final storyPageController = usePageControllerWithInitialPage(0);
-    final textController = useTextEditingController.fromValue(
-      TextEditingValue.empty,
-    );
+    final currentStory = user.stories[currentStoryIndex];
+    final isVideoPlaying = currentStory is VideoStory;
 
-    final emojiState = ref.watch(emojiReactionsControllerProvider);
-
-    useEffect(
-      () {
-        if (storyPageController.hasClients && isCurrentUser) {
-          storyPageController.jumpToPage(currentStoryIndex);
+    return StoryGestureHandler(
+      onTapLeft: () {
+        if (currentStoryIndex > 0) {
+          onPreviousStory();
+        } else {
+          ref.read(storyViewingControllerProvider.notifier).moveToPreviousUser();
         }
-        return null;
       },
-      [currentStoryIndex, isCurrentUser],
-    );
-
-    return KeyboardVisibilityBuilder(
-      builder: (context, isKeyboardVisible) {
-        final bottomPadding =
-            isKeyboardVisible ? MediaQuery.of(context).viewInsets.bottom - 30.0.s : 16.0.s;
-
-        return GestureDetector(
-          onTapDown: (details) => tapPosition.value = details.globalPosition,
-          onTap: () => _handleTap(
-            context,
-            tapPosition.value?.dx ?? 0,
-            ref,
-          ),
-          onLongPress: () {
-            if (user.stories[currentStoryIndex] is VideoStory) {
-              ref
-                  .read(videoControllerProvider(user.stories[currentStoryIndex].data.contentUrl))
-                  .pause();
-            }
-          },
-          onLongPressEnd: (_) {
-            if (user.stories[currentStoryIndex] is VideoStory) {
-              ref
-                  .read(videoControllerProvider(user.stories[currentStoryIndex].data.contentUrl))
-                  .play();
-            }
-          },
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              StoryViewerContent(story: user.stories[currentStoryIndex]),
-              StoryViewerHeader(currentStory: user.stories[currentStoryIndex]),
-              StoryInputField(
-                controller: textController,
-                bottomPadding: bottomPadding,
-              ),
-              StoryViewerActionButtons(
-                story: user.stories[currentStoryIndex],
-                bottomPadding: bottomPadding,
-              ),
-              if (isKeyboardVisible)
-                StoryReactionOverlay(
-                  textController: textController,
-                ),
-              if (emojiState.showNotification && emojiState.selectedEmoji != null)
-                StoryReactionNotification(
-                  emoji: emojiState.selectedEmoji!,
-                ),
-            ],
-          ),
-        );
+      onTapRight: () {
+        if (currentStoryIndex < user.stories.length - 1) {
+          onNextStory();
+        } else {
+          ref.read(storyViewingControllerProvider.notifier).moveToNextUser();
+        }
       },
+      onLongPressStart: () {
+        if (isVideoPlaying) {
+          ref.read(videoControllerProvider(currentStory.data.contentUrl)).pause();
+        }
+      },
+      onLongPressEnd: () {
+        if (isVideoPlaying) {
+          ref.read(videoControllerProvider(currentStory.data.contentUrl)).play();
+        }
+      },
+      child: StoryContent(
+        story: currentStory,
+        isVideoPlaying: isVideoPlaying,
+      ),
     );
-  }
-
-  void _handleTap(BuildContext context, double tapX, WidgetRef ref) {
-    final screenWidth = MediaQuery.sizeOf(context).width;
-    final isLeftSide = tapX < screenWidth / 2;
-
-    if (isLeftSide) {
-      if (currentStoryIndex > 0) {
-        onPreviousStory();
-      } else {
-        ref.read(storyViewingControllerProvider.notifier).moveToPreviousUser();
-      }
-    } else {
-      if (currentStoryIndex < user.stories.length - 1) {
-        onNextStory();
-      } else {
-        ref.read(storyViewingControllerProvider.notifier).moveToNextUser();
-      }
-    }
   }
 }
