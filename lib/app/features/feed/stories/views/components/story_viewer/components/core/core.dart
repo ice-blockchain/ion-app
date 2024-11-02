7aaf8db451498897aa23465e09fef07ee80edb9c
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-11-02 14:21:55 +0200

    chore: move StoryViewerContent to the story_viewer folder

--- lib/app/features/feed/stories/views/components/story_viewer/components/core/core.dart
+++ lib/app/features/feed/stories/views/components/story_viewer/components/core/core.dart
@@ -5,6 +5,5 @@ export 'story_content.dart';
 export 'story_gesture_handler.dart';
 export 'story_input_field.dart';
 export 'story_viewer_action_buttons.dart';
-export 'story_viewer_content.dart';
 export 'story_viewer_header.dart';
 export 'user_story_page_view.dart';
--- lib/app/features/feed/stories/views/components/story_viewer/components/viewers/viewers.dart
+++ lib/app/features/feed/stories/views/components/story_viewer/components/viewers/viewers.dart
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: ice License 1.0
 
 export 'image_story_viewer.dart';
+export 'story_viewer_content.dart';
 export 'video_story_viewer.dart';
