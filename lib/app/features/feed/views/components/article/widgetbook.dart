9ad2bb6d84b002e8782233cc387bea82ba024b6e
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-11-04 16:34:46 +0200

    fix: widgetbook

--- lib/app/features/feed/views/components/article/widgetbook.dart
+++ lib/app/features/feed/views/components/article/widgetbook.dart
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:ion/app/features/feed/data/models/article/article_data.dart';
 import 'package:ion/app/features/feed/views/components/article/article.dart';
+import 'package:nostr_dart/nostr_dart.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 @widgetbook.UseCase(
@@ -9,43 +11,42 @@ import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
   type: Article,
 )
 Widget feedPostUseCase(BuildContext context) {
-  return const Scaffold(
-    body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox.shrink(),
-        // Article(
-        //   id: 'test_article_id',
-        //   title: context.knobs.string(
-        //     label: 'Title',
-        //     initialValue: 'The volume of the BNB Chain network increased in May 65%',
-        //   ),
-        //   publishedAt: context.knobs.dateTime(
-        //     label: 'Published at',
-        //     initialValue: DateTime(2024, 2, 27, 11),
-        //     start: DateTime(1970, 1, 1, 1),
-        //     end: DateTime.now(),
-        //   ),
-        //   imageUrl: 'https://ice.io/wp-content/uploads/2023/03/Pre-Release-600x403.png',
-        //   minutesToRead: context.knobs.int.input(
-        //     label: 'Minutes to read',
-        //     initialValue: 7,
-        //   ),
-        //   userMetadata: UserMetadata(
-        //     name: context.knobs.string(
-        //       label: 'Name',
-        //       initialValue: 'Alina Proxima',
-        //     ),
-        //     displayName: context.knobs.string(
-        //       label: 'Username',
-        //       initialValue: 'alinaproxima',
-        //     ),
-        //     about: '',
-        //     picture: 'https://ice-staging.b-cdn.net/profile/default-profile-picture-16.png',
-        //     verified: true,
-        //   ),
-        // ),
+  final eventMessage = EventMessage(
+    id: '098ab7d5b828b58dd650daaad0ac37e0421eb5f875eb48759332e7bb4c58c632',
+    pubkey: '6e24af77db17d8f891e1967d2eb61877f18e4186980fa16958875a45b3f1350b',
+    createdAt: DateTime.fromMillisecondsSinceEpoch(1730726666 * 1000),
+    kind: 30023,
+    tags: const [
+      ['d', 'a54ae6cf-2a30-4248-a35e-3712f6e7d131'],
+      [
+        'a',
+        '30023:6e24af77db17d8f891e1967d2eb61877f18e4186980fa16958875a45b3f1350b:a54ae6cf-2a30-4248-a35e-3712f6e7d131',
       ],
+      ['r', 'localhost:5173'],
+      ['published_at', '1730726666'],
+      ['title', 'test 3'],
+      [
+        'image',
+        'https://yakihonne.s3.ap-east-1.amazonaws.com/c73818cc95d6adf098fbff289cda4c3cf50d5f370d25f0b6f3677231ccd5c890/files/1730728571101-YAKIHONNES3.jpeg',
+      ],
+      ['summary', 'test 3'],
+      ['nsfw', 'false'],
+      ['t', 'tag 1'],
+      ['t', ' tag2'],
+      ['t', ' tag 3'],
+    ],
+    content: 'test 3',
+    sig:
+        'ef4e0d4977f110000b1a563119a11a4c60b5ff1cc595a33c9a59f8afd1e3c5cde05cae06ac13af7c86c67cd05dca409a1e97d78a357c64fb7501746a75bd6441',
+  );
+
+  final article = ArticleEntity.fromEventMessage(eventMessage);
+
+  return Scaffold(
+    body: Center(
+      child: Article(
+        article: article,
+      ),
     ),
   );
 }
