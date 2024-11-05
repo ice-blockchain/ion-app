b29c6e8182d5547ff6c906088d33c714ba89048a
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-11-05 09:28:27 +0300

    chore: tmp

--- lib/app/features/feed/views/pages/feed_page/components/stories/components/story_list.dart
+++ lib/app/features/feed/views/pages/feed_page/components/stories/components/story_list.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
+import 'package:ion/app/components/scroll_view/load_more_builder.dart';
 import 'package:ion/app/features/feed/stories/data/models/story.dart';
 import 'package:ion/app/features/feed/views/pages/feed_page/components/stories/components/story_list_item.dart';
 import 'package:ion/app/features/feed/views/pages/feed_page/components/stories/components/story_list_separator.dart';
@@ -19,23 +20,33 @@ class StoryList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: StoryListItem.height,
-      child: ListView.separated(
-        padding: EdgeInsets.symmetric(
-          horizontal: ScreenSideOffset.defaultSmallMargin,
-        ),
-        scrollDirection: Axis.horizontal,
-        itemCount: stories.length,
-        separatorBuilder: (BuildContext context, int index) {
-          return const StoryListSeparator();
+      child: LoadMoreBuilder(
+        slivers: const [],
+        hasMore: true,
+        onLoadMore: () async {
+          print('STORIES LOAD MORE');
+          await Future.delayed(const Duration(seconds: 4));
         },
-        itemBuilder: (BuildContext context, int index) {
-          final story = stories[index];
-          return StoryListItem(
-            imageUrl: story.data.imageUrl,
-            label: story.data.author,
-            nft: story.data.nft,
-            me: story.data.me,
-            gradient: storyBorderGradients[story.data.gradientIndex],
+        builder: (context, slivers) {
+          return ListView.separated(
+            padding: EdgeInsets.symmetric(
+              horizontal: ScreenSideOffset.defaultSmallMargin,
+            ),
+            scrollDirection: Axis.horizontal,
+            itemCount: stories.length,
+            separatorBuilder: (BuildContext context, int index) {
+              return const StoryListSeparator();
+            },
+            itemBuilder: (BuildContext context, int index) {
+              final story = stories[index];
+              return StoryListItem(
+                imageUrl: story.data.imageUrl,
+                label: story.data.author,
+                nft: story.data.nft,
+                me: story.data.me,
+                gradient: storyBorderGradients[story.data.gradientIndex],
+              );
+            },
           );
         },
       ),
--- lib/app/features/feed/views/pages/feed_page/components/trending_videos/components/trending_videos_list.dart
+++ lib/app/features/feed/views/pages/feed_page/components/trending_videos/components/trending_videos_list.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
+import 'package:ion/app/components/scroll_view/load_more_builder.dart';
 import 'package:ion/app/features/feed/data/models/trending_videos_overlay.dart';
 import 'package:ion/app/features/feed/views/pages/feed_page/components/trending_videos/components/trending_videos_list_item.dart';
 import 'package:ion/app/features/feed/views/pages/feed_page/components/trending_videos/components/trending_videos_list_separator.dart';
@@ -19,19 +20,29 @@ class TrendingVideosList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: listOverlay.itemSize.height,
-      child: ListView.separated(
-        padding: EdgeInsets.symmetric(
-          horizontal: ScreenSideOffset.defaultSmallMargin,
-        ),
-        scrollDirection: Axis.horizontal,
-        itemCount: trendingVideos.length,
-        separatorBuilder: (BuildContext context, int index) {
-          return const TrendingVideosListSeparator();
+      child: LoadMoreBuilder(
+        slivers: const [],
+        hasMore: true,
+        onLoadMore: () async {
+          print('VIDEOS LOAD MORE');
+          await Future.delayed(const Duration(seconds: 4));
         },
-        itemBuilder: (BuildContext context, int index) {
-          return TrendingVideoListItem(
-            video: trendingVideos[index],
-            itemSize: listOverlay.itemSize,
+        builder: (context, slivers) {
+          return ListView.separated(
+            padding: EdgeInsets.symmetric(
+              horizontal: ScreenSideOffset.defaultSmallMargin,
+            ),
+            scrollDirection: Axis.horizontal,
+            itemCount: trendingVideos.length,
+            separatorBuilder: (BuildContext context, int index) {
+              return const TrendingVideosListSeparator();
+            },
+            itemBuilder: (BuildContext context, int index) {
+              return TrendingVideoListItem(
+                video: trendingVideos[index],
+                itemSize: listOverlay.itemSize,
+              );
+            },
           );
         },
       ),
