133d5e46fa46d4a6882a408be7b3e6f6a7f4af1a
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-11-12 18:03:14 +0200

    chore: scroll indicator

--- lib/app/features/feed/views/pages/article_details_page/article_details_page.dart
+++ lib/app/features/feed/views/pages/article_details_page/article_details_page.dart
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/components/separated/separator.dart';
@@ -13,13 +16,18 @@ import 'package:ion/app/features/feed/views/components/text_editor/text_editor_p
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/article_details_date_topics.dart';
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/article_details_header.dart';
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/article_details_section_header.dart';
-import 'package:ion/app/features/feed/views/pages/article_details_page/components/article_details_topics.dart';
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/articles_carousel.dart';
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/user_biography.dart';
 import 'package:ion/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ion/generated/assets.gen.dart';
 
-class ArticleDetailsPage extends ConsumerWidget {
+final indicatorWidthProvider = StateProvider<double>((ref) {
+  final screenWidth = PlatformDispatcher.instance.views.first.physicalSize.width /
+      PlatformDispatcher.instance.views.first.devicePixelRatio;
+  return screenWidth * 0.05;
+});
+
+class ArticleDetailsPage extends HookConsumerWidget {
   const ArticleDetailsPage({
     required this.articleId,
     required this.pubkey,
@@ -39,6 +47,28 @@ class ArticleDetailsPage extends ConsumerWidget {
       return const SizedBox.shrink();
     }
 
+    final scrollController = useScrollController();
+
+    useEffect(
+      () {
+        void onScroll() {
+          if (!scrollController.hasClients) return;
+
+          final maxScroll = scrollController.position.maxScrollExtent;
+          final currentScroll = scrollController.offset;
+          final viewportWidth = MediaQuery.of(context).size.width;
+
+          final scrollFraction = (currentScroll / maxScroll).clamp(0.0, 1.0);
+          ref.read(indicatorWidthProvider.notifier).state =
+              viewportWidth * (0.05 + (0.95 * scrollFraction));
+        }
+
+        scrollController.addListener(onScroll);
+        return () => scrollController.removeListener(onScroll);
+      },
+      [scrollController],
+    );
+
     return Scaffold(
       appBar: NavigationAppBar.screen(
         actions: [
@@ -53,8 +83,23 @@ class ArticleDetailsPage extends ConsumerWidget {
       ),
       body: Column(
         children: [
+          Consumer(
+            builder: (context, ref, child) {
+              final indicatorWidth = ref.watch(indicatorWidthProvider);
+              return Align(
+                alignment: Alignment.centerLeft,
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 200),
+                  height: 1.0.s,
+                  width: indicatorWidth,
+                  color: context.theme.appColors.primaryAccent,
+                ),
+              );
+            },
+          ),
           Flexible(
             child: CustomScrollView(
+              controller: scrollController,
               slivers: [
                 SliverList(
                   delegate: SliverChildListDelegate([
@@ -66,7 +111,7 @@ class ArticleDetailsPage extends ConsumerWidget {
                           topics: const [
                             'Technology',
                             'Crypto',
-                          ], //TODO: get topics from articleEntity
+                          ], // TODO: get topics from articleEntity
                         ),
                       ),
                     ),
@@ -74,7 +119,7 @@ class ArticleDetailsPage extends ConsumerWidget {
                       article: articleEntity,
                     ),
                     ScreenSideOffset.small(child: const TextEditorPreview()),
-                    const ArticleDetailsTopics(),
+                    const ArticleDetailsDateTopics(),
                     ScreenSideOffset.small(
                       child: FeedItemFooter(
                         entityId: articleEntity.id,
@@ -111,8 +156,8 @@ class ArticleDetailsPage extends ConsumerWidget {
                     ),
                     ScreenSideOffset.small(
                       child: ArticleDetailsSectionHeader(
-                        title: ArticleTopic.values[0]
-                            .toString(), //TODO: replace with first topic from articleEntity
+                        title: ArticleTopic
+                            .values[0].name, // TODO: replace with first topic from articleEntity
                         count: 3,
                         trailing: GestureDetector(
                           onTap: () {},
