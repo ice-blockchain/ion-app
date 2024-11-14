52afd8bd2fd18b14b4a7a755ecda772abb9dfb05
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-11-14 14:47:55 +0200

    refactor: move scroll indicator calculations to hook

--- lib/app/features/feed/views/pages/article_details_page/article_details_page.dart
+++ lib/app/features/feed/views/pages/article_details_page/article_details_page.dart
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -18,14 +16,9 @@ import 'package:ion/app/features/feed/views/pages/article_details_page/component
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/article_details_section_header.dart';
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/articles_carousel.dart';
 import 'package:ion/app/features/feed/views/pages/article_details_page/components/user_biography.dart';
+import 'package:ion/app/features/feed/views/pages/article_details_page/hooks/use_scroll_indicator.dart';
 import 'package:ion/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ion/generated/assets.gen.dart';
-
-final indicatorWidthProvider = StateProvider<double>((ref) {
-  final screenWidth = PlatformDispatcher.instance.views.first.physicalSize.width /
-      PlatformDispatcher.instance.views.first.devicePixelRatio;
-  return screenWidth * 0.05;
-});
 
 class ArticleDetailsPage extends HookConsumerWidget {
   const ArticleDetailsPage({
@@ -48,32 +41,7 @@ class ArticleDetailsPage extends HookConsumerWidget {
     }
 
     final scrollController = useScrollController();
-
-    useEffect(
-      () {
-        void onScroll() {
-          if (!scrollController.hasClients) return;
-
-          final maxScroll = scrollController.position.maxScrollExtent;
-          final currentScroll = scrollController.offset;
-          final viewportWidth = MediaQuery.sizeOf(context).width;
-
-          if (maxScroll == 0) {
-            ref.read(indicatorWidthProvider.notifier).state = viewportWidth;
-          } else {
-            final scrollFraction = (currentScroll / maxScroll).clamp(0.0, 1.0);
-            ref.read(indicatorWidthProvider.notifier).state =
-                viewportWidth * (0.05 + (0.95 * scrollFraction));
-          }
-        }
-
-        WidgetsBinding.instance.addPostFrameCallback((_) => onScroll());
-
-        scrollController.addListener(onScroll);
-        return () => scrollController.removeListener(onScroll);
-      },
-      [scrollController],
-    );
+    useScrollIndicator(context, scrollController, ref);
 
     return Scaffold(
       appBar: NavigationAppBar.screen(
