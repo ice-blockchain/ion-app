8bce65afee17b4e420a04f2d9a782e82a72cb687
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-11-14 05:54:39 +0200

    chore: refactor use_story_pause

--- lib/app/features/feed/stories/hooks/use_story_pause.dart
+++ lib/app/features/feed/stories/hooks/use_story_pause.dart
@@ -5,34 +5,40 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/core/providers/video_player_provider.dart';
 
 void useStoryPause({
-  required bool isPaused,
+  required bool isPaused, 
   required String? storyUrl,
   required AnimationController progressController,
   required WidgetRef ref,
 }) {
   useEffect(
     () {
-      if (isPaused) {
-        progressController.stop();
-
-        if (storyUrl != null) {
-          final controller = ref.read(videoControllerProvider(storyUrl));
-          if (controller.value.isPlaying) {
-            controller.pause();
-          }
-        }
-      } else {
-        progressController.forward();
-
-        if (storyUrl != null) {
-          final controller = ref.read(videoControllerProvider(storyUrl));
-          if (!controller.value.isPlaying) {
-            controller.play();
-          }
-        }
-      }
+      _handleImageProgress(isPaused, progressController);
+      _handleVideoProgress(isPaused, storyUrl, ref);
       return null;
     },
     [isPaused],
   );
+}
+
+void _handleImageProgress(bool isPaused, AnimationController controller) {
+  if (isPaused) {
+    controller.stop();
+  } else {
+    controller.forward(); 
+  }
+}
+
+void _handleVideoProgress(bool isPaused, String? videoUrl, WidgetRef ref) {
+  if (videoUrl == null) return;
+
+  final controller = ref.read(videoControllerProvider(videoUrl));
+  if (isPaused) {
+    if (controller.value.isPlaying) {
+      controller.pause();
+    }
+  } else {
+    if (!controller.value.isPlaying) {
+      controller.play();
+    }
+  }
 }
