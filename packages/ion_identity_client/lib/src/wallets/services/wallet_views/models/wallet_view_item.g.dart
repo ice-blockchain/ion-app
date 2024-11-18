20bbd4896be28835f60d5573808c42ca63cdeb25
ice-tychon <175587874+ice-tychon@users.noreply.github.com>
2024-11-19 01:38:21 +0200

    feat(ion identity client): add wallet views requests

---
+++ packages/ion_identity_client/lib/src/wallets/services/wallet_views/models/wallet_view_item.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'wallet_view_item.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$WalletViewItemImpl _$$WalletViewItemImplFromJson(Map<String, dynamic> json) =>
+    _$WalletViewItemImpl(
+      coin: json['coin'] as String,
+      walletId: json['walletId'] as String,
+    );
+
+Map<String, dynamic> _$$WalletViewItemImplToJson(
+        _$WalletViewItemImpl instance) =>
+    <String, dynamic>{
+      'coin': instance.coin,
+      'walletId': instance.walletId,
+    };
