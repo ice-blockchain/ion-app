18625c37e935f733d44eec087121bad998592af2
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-11-19 10:59:34 +0200

    feat: update wallet icon (#320)
    ## Description
    This PR introduces a new wallet icon
    
    ## Additional Notes
    Replaced the wallet icon on the wallet screen, and in wallets and
    managed wallets modals
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <img width="542" alt="Screenshot 2024-11-19 at 09 50 23"
    src="https://github.com/user-attachments/assets/4c530921-cc5b-4d92-86f8-47180f1105ad">
    <img width="542" alt="Screenshot 2024-11-19 at 09 50 27"
    src="https://github.com/user-attachments/assets/ddc4e61e-e8c5-41ae-b224-12bb0e12daff">
    <img width="542" alt="Screenshot 2024-11-19 at 09 50 28"
    src="https://github.com/user-attachments/assets/f6f2d7e1-4444-4999-a08c-76fcd3030d06">

---
+++ lib/app/features/wallets/components/wallet_icon/wallet_icon.dart
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:flutter/material.dart';
+import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/generated/assets.gen.dart';
+
+class WalletIcon extends StatelessWidget {
+  const WalletIcon({
+    this.size = 36.0,
+    this.borderRadius = 10.0,
+    this.iconSize = 24.0,
+    super.key,
+  });
+
+  final double size;
+  final double borderRadius;
+  final double iconSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size.s,
+      height: size.s,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(borderRadius.s),
+        color: context.theme.appColors.darkBlue,
+      ),
+      child: Center(
+        child: SizedBox(
+          width: iconSize.s,
+          height: iconSize.s,
+          child: Assets.svg.iconWallet.icon(size: iconSize.s),
+        ),
+      ),
+    );
+  }
+}
--- lib/app/features/wallets/pages/wallets_modal/components/wallets_list/wallet_tile.dart
+++ lib/app/features/wallets/pages/wallets_modal/components/wallets_list/wallet_tile.dart
@@ -2,13 +2,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/components/avatar/avatar.dart';
 import 'package:ion/app/components/list_item/list_item.dart';
 import 'package:ion/app/extensions/asset_gen_image.dart';
 import 'package:ion/app/extensions/build_context.dart';
 import 'package:ion/app/extensions/num.dart';
 import 'package:ion/app/extensions/theme_data.dart';
 import 'package:ion/app/features/wallet/model/wallet_data.dart';
+import 'package:ion/app/features/wallets/components/wallet_icon/wallet_icon.dart';
 import 'package:ion/app/features/wallets/providers/selected_wallet_id_provider.dart';
 import 'package:ion/app/features/wallets/providers/wallets_data_provider.dart';
 import 'package:ion/app/utils/num.dart';
@@ -36,10 +36,7 @@ class WalletTile extends ConsumerWidget {
             ref.read(selectedWalletIdNotifierProvider.notifier).selectedWalletId = walletData.id;
           }
         },
-        leading: Avatar(
-          size: 36.0.s,
-          imageUrl: walletData.icon,
-        ),
+        leading: const WalletIcon(),
         title: Text(
           walletData.name,
         ),
