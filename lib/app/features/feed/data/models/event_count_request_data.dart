74f28388f945a2865838ce77b3114a702c5b6380
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-11-21 10:40:48 +0300

    feat: add EventCountResultEntity, EventCountRequestEntity

---
+++ lib/app/features/feed/data/models/event_count_request_data.dart
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'dart:convert';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:ion/app/exceptions/exceptions.dart';
+import 'package:ion/app/features/nostr/model/event_serializable.dart';
+import 'package:ion/app/features/nostr/model/nostr_entity.dart';
+import 'package:ion/app/features/nostr/providers/nostr_cache.dart';
+import 'package:nostr_dart/nostr_dart.dart';
+
+part 'event_count_request_data.freezed.dart';
+
+@Freezed(equal: false)
+class EventCountRequestEntity
+    with _$EventCountRequestEntity, NostrEntity
+    implements CacheableEntity {
+  const factory EventCountRequestEntity({
+    required String id,
+    required String pubkey,
+    required DateTime createdAt,
+    required EventCountRequestData data,
+  }) = _EventCountRequestEntity;
+
+  const EventCountRequestEntity._();
+
+  /// https://github.com/nostr-protocol/nips/blob/vending-machine/90.md
+  factory EventCountRequestEntity.fromEventMessage(EventMessage eventMessage) {
+    if (eventMessage.kind != kind) {
+      throw IncorrectEventKindException(eventId: eventMessage.id, kind: kind);
+    }
+
+    return EventCountRequestEntity(
+      id: eventMessage.id,
+      pubkey: eventMessage.pubkey,
+      createdAt: eventMessage.createdAt,
+      data: EventCountRequestData.fromEventMessage(eventMessage),
+    );
+  }
+
+  @override
+  String get cacheKey => cacheKeyBuilder(id: id);
+
+  static String cacheKeyBuilder({required String id}) => id;
+
+  static const int kind = 5400;
+}
+
+@freezed
+class EventCountRequestData with _$EventCountRequestData implements EventSerializable {
+  const factory EventCountRequestData({
+    required RequestFilter filter,
+    required EventCountRequestParams params,
+    String? output,
+  }) = _EventCountRequestData;
+
+  const EventCountRequestData._();
+
+  factory EventCountRequestData.fromEventMessage(EventMessage eventMessage) {
+    final tags = eventMessage.tags.fold<Map<String, List<List<String>>>>({}, (result, tag) {
+      return result..putIfAbsent(tag[0], () => []).add(tag);
+    });
+    return EventCountRequestData(
+      filter: RequestFilter.fromJson(jsonDecode(eventMessage.content) as Map<String, dynamic>),
+      params: EventCountRequestParams.fromTags(tags[EventCountRequestParams.tagName] ?? []),
+      output: tags['output']?.first[1],
+    );
+  }
+
+  @override
+  EventMessage toEventMessage(EventSigner signer) {
+    return EventMessage.fromData(
+      signer: signer,
+      kind: EventCountRequestEntity.kind,
+      content: filter.toString(),
+      tags: params.toTags(),
+    );
+  }
+}
+
+@freezed
+class EventCountRequestParams with _$EventCountRequestParams {
+  const factory EventCountRequestParams({
+    String? relay,
+    String? group,
+  }) = _EventCountRequestParams;
+
+  const EventCountRequestParams._();
+
+  factory EventCountRequestParams.fromTags(List<List<String>> tags) {
+    String? relay;
+    String? group;
+    for (final tag in tags) {
+      if (tag[0] == tagName) {
+        if (tag.length != 3) {
+          throw IncorrectEventTagException(tag: tag.toString());
+        }
+        if (tag[1] == 'group') group = tag[2];
+        if (tag[1] == 'relay') group = tag[2];
+      }
+    }
+
+    return EventCountRequestParams(
+      relay: relay,
+      group: group,
+    );
+  }
+
+  List<List<String>> toTags() {
+    return [
+      if (group != null) [tagName, 'group', group!],
+      if (relay != null) [tagName, 'group', relay!],
+    ];
+  }
+
+  static const String tagName = 'param';
+}
---
+++ lib/app/features/feed/data/models/event_count_result_data.dart
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'dart:convert';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:ion/app/exceptions/exceptions.dart';
+import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/feed/data/models/event_count_request_data.dart';
+import 'package:ion/app/features/nostr/model/nostr_entity.dart';
+import 'package:ion/app/features/nostr/providers/nostr_cache.dart';
+import 'package:nostr_dart/nostr_dart.dart';
+
+part 'event_count_result_data.freezed.dart';
+
+enum EventCountResultType {
+  replies,
+  reposts,
+  quotes,
+  reactions,
+}
+
+@Freezed(equal: false)
+class EventCountResultEntity<T>
+    with _$EventCountResultEntity<T>, NostrEntity
+    implements CacheableEntity {
+  const factory EventCountResultEntity({
+    required String id,
+    required String pubkey,
+    required DateTime createdAt,
+    required EventCountResultSummary<T> data,
+  }) = _EventCountResultEntity;
+
+  const EventCountResultEntity._();
+
+  /// https://github.com/nostr-protocol/nips/blob/vending-machine/90.md
+  factory EventCountResultEntity.fromEventMessage(EventMessage eventMessage) {
+    if (eventMessage.kind != kind) {
+      throw IncorrectEventKindException(eventId: eventMessage.id, kind: kind);
+    }
+
+    final data = EventCountResultData.fromEventMessage(eventMessage);
+    final summary = EventCountResultSummary(
+      key: data.key,
+      type: data.type,
+      content: data.content as T,
+    );
+
+    return EventCountResultEntity(
+      id: eventMessage.id,
+      pubkey: eventMessage.pubkey,
+      createdAt: eventMessage.createdAt,
+      data: summary,
+    );
+  }
+
+  @override
+  String get cacheKey => cacheKeyBuilder(key: data.key, type: data.type);
+
+  static String cacheKeyBuilder({required String key, required EventCountResultType type}) =>
+      '$key:${type.toShortString()}';
+
+  static const int kind = 6400;
+}
+
+@freezed
+class EventCountResultSummary<T> with _$EventCountResultSummary<T> {
+  const factory EventCountResultSummary({
+    required T content,
+    required String key,
+    required EventCountResultType type,
+  }) = _EventCountResultSummary;
+}
+
+@freezed
+class EventCountResultData with _$EventCountResultData {
+  const factory EventCountResultData({
+    required EventCountRequestEntity request,
+    required dynamic content,
+    required String eventId,
+    required String pubkey,
+  }) = _EventCountResultData;
+
+  const EventCountResultData._();
+
+  factory EventCountResultData.fromEventMessage(EventMessage eventMessage) {
+    EventCountRequestEntity? request;
+    String? eventId;
+    String? pubkey;
+
+    for (final tag in eventMessage.tags) {
+      if (tag[0] == 'request') {
+        request = EventCountRequestEntity.fromEventMessage(
+          EventMessage.fromJson(jsonDecode(tag[1]) as List<dynamic>),
+        );
+      }
+      if (tag[0] == 'e') eventId = tag[1];
+      if (tag[0] == 'p') pubkey = tag[1];
+    }
+
+    if (request == null || eventId == null || pubkey == null) {
+      throw IncorrectEventTagsException(eventId: eventMessage.id);
+    }
+
+    return EventCountResultData(
+      content: jsonDecode(eventMessage.content),
+      request: request,
+      eventId: eventId,
+      pubkey: pubkey,
+    );
+  }
+
+  EventCountResultType get type {
+    //TODO:impl
+    return EventCountResultType.quotes;
+  }
+
+  String get key {
+    //TODO:impl
+    return '';
+  }
+}
