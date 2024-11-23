607f54781ec497840a6e9439716dc2ec4b48a66a
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2024-11-23 22:48:52 +0200

    feat: replied and forwarded message chips

---
+++ lib/app/features/chat/messages/views/components/forwarded_message_info/forwarded_message_info.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:ion/app/components/avatar/avatar.dart';
+import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/generated/assets.gen.dart';
+
+class ForwardedMessageInfo extends StatelessWidget {
+  const ForwardedMessageInfo({required this.isMe, required this.isPublic, super.key});
+
+  final bool isMe;
+  final bool isPublic;
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicWidth(
+      child: Container(
+        margin: EdgeInsets.only(bottom: 12.0.s),
+        decoration: BoxDecoration(
+          color: isMe
+              ? context.theme.appColors.primaryAccent
+              : context.theme.appColors.onPrimaryAccent,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Assets.svg.iconChatForward.icon(
+                  size: 16.0.s,
+                  color: isMe
+                      ? context.theme.appColors.strokeElements
+                      : context.theme.appColors.onTertararyBackground,
+                ),
+                SizedBox(width: 4.0.s),
+                Text(
+                  isPublic ? context.i18n.common_forwarded_from : context.i18n.common_forwarded,
+                  style: context.theme.appTextThemes.body2.copyWith(
+                    color: isMe
+                        ? context.theme.appColors.strokeElements
+                        : context.theme.appColors.onTertararyBackground,
+                  ),
+                ),
+              ],
+            ),
+            if (isPublic)
+              Padding(
+                padding: EdgeInsets.only(top: 4.0.s),
+                child: Row(
+                  children: [
+                    Avatar(size: 20.0.s, imageUrl: 'https://picsum.photos/200/300'),
+                    SizedBox(width: 8.0.s),
+                    Text(
+                      'John Doe',
+                      style: context.theme.appTextThemes.body2.copyWith(
+                        color: isMe
+                            ? context.theme.appColors.onPrimaryAccent
+                            : context.theme.appColors.primaryText,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
