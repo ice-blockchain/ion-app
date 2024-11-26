2678fedb87f1facf7375be3f73eb333db100ec16
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-11-26 10:42:51 +0300

    feat: add UserMetadataNotifier

---
+++ lib/app/features/user/providers/user_metadata_notifier.dart
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:ion/app/features/nostr/providers/nostr_notifier.dart';
+import 'package:ion/app/features/user/model/user_metadata.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'user_metadata_notifier.g.dart';
+
+@riverpod
+class UserMetadataNotifier extends _$UserMetadataNotifier {
+  @override
+  FutureOr<void> build() {}
+
+  Future<void> publish({
+    required UserMetadata userMetadata,
+  }) async {
+    state = const AsyncValue.loading();
+
+    state = await AsyncValue.guard(() async {
+      await ref.read(nostrNotifierProvider.notifier).sendEntityData(userMetadata);
+    });
+  }
+}
