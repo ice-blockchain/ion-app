e615eeb2965e6692d28c06b37179324be1ee93b8
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-11-27 15:24:23 +0300

    refactor: add useAnimatedOpacityOnScroll for profile page

---
+++ lib/app/features/user/pages/profile_page/hooks/use_animated_opacity_on_scroll.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:ion/app/extensions/extensions.dart';
+
+({double opacity}) useAnimatedOpacityOnScroll(
+  ScrollController scrollController, {
+  required double topOffset,
+}) {
+  final opacityValue = useState<double>(0.0.s);
+
+  useEffect(
+    () {
+      void listener() {
+        opacityValue.value = (scrollController.offset / topOffset).clamp(0.0, 1.0);
+      }
+
+      scrollController.addListener(listener);
+      return () => scrollController.removeListener(listener);
+    },
+    [scrollController, topOffset],
+  );
+
+  return (opacity: opacityValue.value);
+}
