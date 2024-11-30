9784385ffd025f13141c93dd14d48b3f5365166b
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-11-30 10:44:37 +0200

    feat: add shadow for the avatar's text and the svg icons

---
+++ lib/app/components/shadow/svg_shadow.dart
@@ -0,0 +1,58 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:ion/app/extensions/extensions.dart';
+
+class SvgShadow extends StatelessWidget {
+  const SvgShadow({
+    required this.child,
+    super.key,
+    this.opacity = 0.5,
+    this.sigma,
+    this.color = Colors.black,
+    this.offset,
+  });
+  final Widget child;
+  final double opacity;
+  final double? sigma;
+  final Color color;
+  final Offset? offset;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        if (color.alpha != 0)
+          Transform.translate(
+            offset: offset ??  Offset(0, 1.5.s),
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(
+                sigmaY: sigma ?? 1.5,
+                sigmaX: sigma ?? 1.5,
+                tileMode: TileMode.decal,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.transparent,
+                    width: 0,
+                  ),
+                ),
+                child: Opacity(
+                  opacity: opacity,
+                  child: ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                      color,
+                      BlendMode.srcATop,
+                    ),
+                    child: child,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        child,
+      ],
+    );
+  }
+}
