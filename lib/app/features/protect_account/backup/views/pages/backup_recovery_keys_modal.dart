3ae1ed63723d4c6a19ef1db8b7573d64fb6d77d5
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2024-12-03 14:37:29 +0100

    fix: add next arrow to lets start button on secure your recovery keys info modal (#370)
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <img width="180" alt="image"
    src="https://github.com/user-attachments/assets/e2e782c7-2a15-421e-9291-98143e278dcb">

--- lib/app/features/protect_account/backup/views/pages/backup_recovery_keys_modal.dart
+++ lib/app/features/protect_account/backup/views/pages/backup_recovery_keys_modal.dart
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:ion/app/components/button/button.dart';
 import 'package:ion/app/components/modal_sheets/simple_modal_sheet.dart';
+import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/router/app_routes.dart';
 import 'package:ion/generated/assets.gen.dart';
@@ -15,8 +17,16 @@ class BackupRecoveryKeysModal extends StatelessWidget {
       title: context.i18n.secure_your_recovery_keys_title,
       description: context.i18n.secure_your_recovery_keys_description,
       iconAsset: Assets.svg.actionWalletLock,
-      buttonText: context.i18n.button_lets_start,
-      onPressed: () => CreateRecoveryKeyRoute().push<void>(context),
+      button: ScreenSideOffset.large(
+        child: Button(
+          mainAxisSize: MainAxisSize.max,
+          label: Text(context.i18n.button_lets_start),
+          onPressed: () => CreateRecoveryKeyRoute().push<void>(context),
+          trailingIcon: Assets.svg.iconButtonNext.icon(
+            color: context.theme.appColors.onPrimaryAccent,
+          ),
+        ),
+      ),
     );
   }
 }
