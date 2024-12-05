1cd533c2fe4adda19acaf1e5021aa17d735fb17d
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-12-05 16:22:58 +0200

    feat: mock articles

---
+++ lib/app/features/feed/providers/fake_articles_generator.dart
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'dart:math';
+
+import 'package:ion/app/features/feed/data/models/entities/article_data.dart';
+import 'package:ion/app/features/feed/views/components/article/mocked_data.dart';
+
+ArticleEntity generateFakeArticle() {
+  final random = Random.secure();
+  final randomEvent = mockedArticleEvents[random.nextInt(mockedArticleEvents.length)];
+
+  final masterPubkeyTag = randomEvent.tags.firstWhere(
+    (tag) => tag.isNotEmpty && tag[0] == 'b',
+  );
+
+  final masterPubkey = masterPubkeyTag.length > 1
+      ? masterPubkeyTag[1]
+      : '92c6aa30124f5edfe679a0e0676a9d86eaf2d9556d6eed73debf8a02d58a6b9e';
+
+  final postEntity = ArticleEntity.fromEventMessage(randomEvent);
+
+  return postEntity.copyWith(masterPubkey: masterPubkey);
+}
