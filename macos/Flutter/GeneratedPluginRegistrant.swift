5b6905f322a551a3b87ddf953d7b79887cdd348d
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2024-12-09 17:12:17 +0700

    feat: db setup and db event model (#399)
    ## Description
    Basic DB setup and custom row class and companion
    https://drift.simonbinder.eu/dart_api/rows/?h=row#custom-dataclass
    
    ## Additional Notes
    <!-- Add any extra context or relevant information here. -->
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <!-- Include screenshots to demonstrate any UI changes. -->
    <!-- <img width="180" alt="image" src="image_url_here"> -->

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,6 +18,7 @@ import screen_retriever
 import share_plus
 import shared_preferences_foundation
 import sqflite
+import sqlite3_flutter_libs
 import url_launcher_macos
 import video_player_avfoundation
 import window_manager
@@ -36,6 +37,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
