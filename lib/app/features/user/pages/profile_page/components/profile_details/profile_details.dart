9ea41d94ba811708e70ae708e2c6c8178d477b19
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-12-09 18:12:43 +0300

    tmp

--- lib/app/features/user/pages/profile_page/components/profile_details/profile_details.dart
+++ lib/app/features/user/pages/profile_page/components/profile_details/profile_details.dart
@@ -6,7 +6,7 @@ import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
 import 'package:ion/app/features/user/pages/components/user_name_tile/user_name_tile.dart';
-import 'package:ion/app/features/user/pages/profile_page/components/profile_details/followed_by/followed_by.dart';
+import 'package:ion/app/features/user/pages/profile_page/components/profile_details/followers_you_know/followers_you_know.dart';
 import 'package:ion/app/features/user/pages/profile_page/components/profile_details/profile_about.dart';
 import 'package:ion/app/features/user/pages/profile_page/components/profile_details/profile_actions/edit_user_button.dart';
 import 'package:ion/app/features/user/pages/profile_page/components/profile_details/profile_actions/profile_actions.dart';
@@ -30,18 +30,11 @@ class ProfileDetails extends ConsumerWidget {
         children: [
           UserNameTile(pubkey: pubkey),
           SizedBox(height: 8.0.s),
-          if (isCurrentUserProfile)
-            const EditUserButton()
-          else
-            ProfileActions(
-              pubkey: pubkey,
-            ),
+          if (isCurrentUserProfile) const EditUserButton() else ProfileActions(pubkey: pubkey),
           SizedBox(height: 16.0.s),
-          ProfileFollowers(
-            pubkey: pubkey,
-          ),
+          ProfileFollowers(pubkey: pubkey),
           SizedBox(height: 10.0.s),
-          FollowedBy(pubkey: pubkey),
+          if (!isCurrentUserProfile) FollowersYouKnow(pubkey: pubkey),
           SizedBox(height: 12.0.s),
           ProfileAbout(pubkey: pubkey),
           SizedBox(height: 12.0.s),
