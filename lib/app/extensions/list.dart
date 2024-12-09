a894efd78536aa715b532accd13c9066583e0edb
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2024-12-09 18:57:56 +0200

    refactor: create entity media data mixin

--- lib/app/extensions/list.dart
+++ lib/app/extensions/list.dart
@@ -2,8 +2,6 @@
 
 import 'dart:math';
 
-import 'package:ion/app/features/nostr/model/media_attachment.dart';
-
 extension ListExtension<T> on List<T>? {
   List<T> get emptyOrValue => this ?? <T>[];
 
@@ -13,22 +11,5 @@ extension ListExtension<T> on List<T>? {
 extension ListRandomExtension<T> on List<T> {
   T get random {
     return this[Random().nextInt(length)];
-  }
-}
-
-extension ImetaParser on List<List<String>>? {
-  Map<String, MediaAttachment> parseImeta() {
-    if (this == null) {
-      return {};
-    }
-
-    final imeta = <String, MediaAttachment>{};
-    for (final tag in this!) {
-      if (tag[0] == 'imeta') {
-        final mediaAttachment = MediaAttachment.fromTag(tag);
-        imeta[mediaAttachment.url] = mediaAttachment;
-      }
-    }
-    return imeta;
   }
 }
