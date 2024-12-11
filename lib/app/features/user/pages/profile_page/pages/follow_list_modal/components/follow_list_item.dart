9ce366502b4aeaefa94af6572714eca7888d95a9
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-12-11 16:34:14 +0300

    refactor: return UserMetadata from followersDataSource

--- lib/app/features/user/pages/profile_page/pages/follow_list_modal/components/follow_list_item.dart
+++ lib/app/features/user/pages/profile_page/pages/follow_list_modal/components/follow_list_item.dart
@@ -6,40 +6,44 @@ import 'package:ion/app/components/list_item/list_item.dart';
 import 'package:ion/app/components/list_items_loading_state/item_loading_state.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/components/user/follow_user_button/follow_user_button.dart';
-import 'package:ion/app/features/user/model/user_metadata.c.dart';
+import 'package:ion/app/features/user/providers/user_metadata_provider.c.dart';
 import 'package:ion/app/utils/username.dart';
 
 class FollowListItem extends ConsumerWidget {
   const FollowListItem({
-    required this.userMetadata,
+    required this.pubkey,
     super.key,
   });
 
-  final UserMetadataEntity? userMetadata;
+  final String pubkey;
 
   static double get itemHeight => 35.0.s;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    if (userMetadata == null) {
-      return ItemLoadingState(itemHeight: itemHeight);
-    }
+    final userMetadataResult = ref.watch(userMetadataProvider(pubkey));
 
-    final UserMetadataEntity(:masterPubkey, :data) = userMetadata!;
-
-    return ListItem.user(
-      title: Text(data.name),
-      trailing: FollowUserButton(pubkey: masterPubkey),
-      subtitle: Text(
-        prefixUsername(
-          username: data.displayName,
-          context: context,
-        ),
-      ),
-      profilePicture: data.picture,
-      onTap: () {
-        Navigator.of(context).pop(masterPubkey);
+    return userMetadataResult.maybeWhen(
+      data: (userMetadata) {
+        if (userMetadata == null) {
+          return const SizedBox.shrink();
+        }
+        return ListItem.user(
+          title: Text(userMetadata.data.name),
+          trailing: FollowUserButton(pubkey: pubkey),
+          subtitle: Text(
+            prefixUsername(
+              username: userMetadata.data.displayName,
+              context: context,
+            ),
+          ),
+          profilePicture: userMetadata.data.picture,
+          onTap: () {
+            Navigator.of(context).pop(userMetadata.masterPubkey);
+          },
+        );
       },
+      orElse: () => ItemLoadingState(itemHeight: itemHeight),
     );
   }
 }
--- lib/app/features/user/pages/profile_page/pages/follow_list_modal/following_list.dart
+++ lib/app/features/user/pages/profile_page/pages/follow_list_modal/following_list.dart
@@ -10,7 +10,6 @@ import 'package:ion/app/features/user/pages/profile_page/pages/follow_list_modal
 import 'package:ion/app/features/user/pages/profile_page/pages/follow_list_modal/components/follow_list_loading.dart';
 import 'package:ion/app/features/user/pages/profile_page/pages/follow_list_modal/components/follow_search_bar.dart';
 import 'package:ion/app/features/user/providers/follow_list_provider.c.dart';
-import 'package:ion/app/features/user/providers/user_metadata_provider.c.dart';
 
 class FollowingList extends ConsumerWidget {
   const FollowingList({required this.pubkey, super.key});
@@ -31,26 +30,14 @@ class FollowingList extends ConsumerWidget {
           SliverList.separated(
             separatorBuilder: (_, __) => SizedBox(height: 16.0.s),
             itemCount: pubkeys.length,
-            itemBuilder: (context, index) => _ListItem(pubkey: pubkeys[index]),
+            itemBuilder: (context, index) => ScreenSideOffset.small(
+              child: FollowListItem(pubkey: pubkeys[index]),
+            ),
           )
         else
           const FollowListLoading(),
         SliverPadding(padding: EdgeInsets.only(bottom: 32.0.s)),
       ],
-    );
-  }
-}
-
-class _ListItem extends ConsumerWidget {
-  const _ListItem({required this.pubkey});
-
-  final String pubkey;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final userMetadata = ref.watch(userMetadataProvider(pubkey)).valueOrNull;
-    return ScreenSideOffset.small(
-      child: FollowListItem(userMetadata: userMetadata),
     );
   }
 }
