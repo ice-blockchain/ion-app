17d41bd67729654b74b5d9372d0ed22e6756cc1f
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-12-11 16:08:55 +0300

    chore: add licences

--- lib/app/features/user/pages/profile_page/pages/follow_list_modal/components/follow_list_loading.dart
+++ lib/app/features/user/pages/profile_page/pages/follow_list_modal/components/follow_list_loading.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:flutter/material.dart';
 import 'package:ion/app/components/list_items_loading_state/list_items_loading_state.dart';
 import 'package:ion/app/features/user/pages/profile_page/pages/follow_list_modal/components/follow_list_item.dart';
