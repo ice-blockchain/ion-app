135187bdcce8b922ccd211a8b5104482946001bb
ion-endymion <188437551+ice-endymion@users.noreply.github.com>
2024-12-13 16:11:42 +0100

    fix: global notification bar safe area

--- lib/app/router/components/app_router_builder.dart
+++ lib/app/router/components/app_router_builder.dart
@@ -1,21 +1,44 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/global_notification_bar/global_notification_bar.dart';
-import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/components/global_notification_bar/providers/global_notification_provider.c.dart';
 
-class AppRouterBuilder extends StatelessWidget {
+class AppRouterBuilder extends HookConsumerWidget {
   const AppRouterBuilder({super.key, this.child});
 
   final Widget? child;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final notification = ref.watch(globalNotificationProvider);
+    final isShowSafeArea = useState(false);
+
+    useEffect(
+      () {
+        if (notification.isShow) {
+          isShowSafeArea.value = true;
+        } else {
+          // Delay to hide safe area top to prevent animation glitch
+          Future.delayed(GlobalNotificationBar.animationDuration, () {
+            isShowSafeArea.value = false;
+          });
+        }
+
+        return null;
+      },
+      [notification.isShow],
+    );
+
     return Scaffold(
       body: SafeArea(
+        top: isShowSafeArea.value,
+        bottom: false,
         child: Column(
           children: [
-            if (Theme.of(context).isInitialized) const GlobalNotificationBar(),
+            const GlobalNotificationBar(),
             Expanded(
               child: child ?? const SizedBox.shrink(),
             ),
