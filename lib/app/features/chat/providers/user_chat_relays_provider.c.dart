19b1e6b19c56e8b350e6dc3014ce84295d52679d
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2024-12-17 03:36:57 +0200

    refactor: create a new provider to get random url

--- lib/app/features/chat/providers/user_chat_relays_provider.c.dart
+++ lib/app/features/chat/providers/user_chat_relays_provider.c.dart
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/nostr/providers/nostr_notifier.c.dart';
 import 'package:ion/app/features/user/model/user_chat_relays.c.dart';
@@ -11,29 +12,27 @@ part 'user_chat_relays_provider.c.g.dart';
 ///
 /// This provider is used to fetch the user chat relays for a given pubkey.
 /// It is different from the [UserRelays] provider because it is used to fetch the user chat relays
-/// for a given pubkey with a specific random relay.
 ///
 @riverpod
-class UserChatRelays extends _$UserChatRelays {
-  @override
-  Future<UserChatRelaysEntity?> build(String pubkey) async {
-    final requestMessage = RequestMessage()
-      ..addFilter(
-        const RequestFilter(kinds: [UserChatRelaysEntity.kind]),
-      );
+Future<UserChatRelaysEntity?> userChatRelays(
+  Ref ref,
+  String pubkey,
+) async {
+  final requestMessage = RequestMessage()
+    ..addFilter(
+      const RequestFilter(kinds: [UserChatRelaysEntity.kind]),
+    );
 
-    final entity = await ref
-        .read(nostrNotifierProvider.notifier)
-        .requestEntity<UserChatRelaysEntity>(requestMessage);
+  final entity = await ref
+      .read(nostrNotifierProvider.notifier)
+      .requestEntity<UserChatRelaysEntity>(requestMessage);
 
-    return entity;
-  }
+  return entity;
+}
 
-  ///
-  /// Returns the random relay url for the user chat relays.
-  ///
-  Future<String?> getRandomRelayUrl() async {
-    final value = await future;
-    return value?.data.list.random.url;
-  }
+/// Helper function to get a random relay URL from the user chat relays
+@riverpod
+String? userChatRelaysRandomUrl(Ref ref, String pubkey) {
+  final relays = ref.watch(userChatRelaysProvider(pubkey)).valueOrNull;
+  return relays?.data.list.random.url;
 }
