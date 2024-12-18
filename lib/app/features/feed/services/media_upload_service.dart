bd5926146ec9257a11d22b5a3725753c3282cc37
ice-hector <96414297+ice-hector@users.noreply.github.com>
2024-12-18 16:33:15 +0200

    feat: add license

--- lib/app/features/feed/services/media_upload_service.dart
+++ lib/app/features/feed/services/media_upload_service.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/nostr/model/file_alt.dart';
 import 'package:ion/app/features/nostr/model/file_metadata.c.dart';
