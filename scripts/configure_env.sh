f33fce286ca79d8d29ef0ec2b55c4e327f019bbe
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-12-18 15:46:23 +0300

    feat: add testnet env to build / set-env scripts

--- scripts/configure_env.sh
+++ scripts/configure_env.sh
@@ -3,10 +3,10 @@
 environment="$1"
 
 if [ -z $1 ]; then
-	echo "Please pass environment as argument (staging or production)"
+	echo "Please pass environment as argument (staging / production / testnet)"
 	exit 0
-elif [ "$environment" != "production" ] && [ "$environment" != "staging" ]; then
-	echo "Please pass a valid environment (staging or production)"
+elif [ "$environment" != "production" ] && [ "$environment" != "staging" ] && [ "$environment" != "testnet" ]; then
+	echo "Please pass a valid environment (staging / production / testnet)"
 	exit 0
 else
 	cp -rf ../flutter-app-secrets/$environment/* ./ &&
