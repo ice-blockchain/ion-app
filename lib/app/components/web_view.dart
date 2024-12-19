330fed7d1a1c96c73e47199540be5abfa102a97e
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2024-12-19 12:56:56 +0200

    chore: remove theme dependency from web_view

--- lib/app/components/web_view.dart
+++ lib/app/components/web_view.dart
@@ -6,7 +6,6 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/progress_bar/ion_loading_indicator.dart';
 import 'package:ion/app/extensions/extensions.dart';
-import 'package:ion/app/features/core/providers/theme_mode_provider.c.dart';
 
 class WebView extends HookConsumerWidget {
   const WebView({required this.url, super.key});
@@ -16,7 +15,6 @@ class WebView extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isLoadingPage = useState<bool>(false);
-    final isLightTheme = ref.watch(appThemeModeProvider) == ThemeMode.light;
 
     return Stack(
       children: [
@@ -32,7 +30,7 @@ class WebView extends HookConsumerWidget {
         if (isLoadingPage.value)
           Center(
             child: IONLoadingIndicator(
-              type: isLightTheme ? IndicatorType.dark : IndicatorType.light,
+              type: IndicatorType.dark,
               size: Size.square(30.0.s),
             ),
           )
