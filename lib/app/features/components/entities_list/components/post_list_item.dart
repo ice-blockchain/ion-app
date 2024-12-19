4c194e844af06057854dda912591917156681378
ice-orion <102020833+ice-orion@users.noreply.github.com>
2024-12-19 18:38:40 +0300

    chore: remove unnecessary consumer

--- lib/app/features/components/entities_list/components/post_list_item.dart
+++ lib/app/features/components/entities_list/components/post_list_item.dart
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/widgets.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/features/feed/data/models/entities/post_data.c.dart';
 import 'package:ion/app/features/feed/views/components/post/post.dart';
 import 'package:ion/app/features/nostr/model/event_reference.c.dart';
 import 'package:ion/app/router/app_routes.c.dart';
 
-class PostListItem extends ConsumerWidget {
+class PostListItem extends StatelessWidget {
   const PostListItem({required this.post, this.showParent = false, super.key});
 
   final PostEntity post;
@@ -16,7 +15,7 @@ class PostListItem extends ConsumerWidget {
   final bool showParent;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final eventReference = EventReference.fromNostrEntity(post);
 
     return GestureDetector(
