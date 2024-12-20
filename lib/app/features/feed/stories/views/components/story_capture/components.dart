23a5940a31e959af53c6677af16516ed16633bbe
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-12-20 09:53:02 +0200

    fix: linter warnings

--- lib/app/features/feed/stories/views/components/story_capture/components.dart
+++ lib/app/features/feed/stories/views/components/story_capture/components.dart
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: ice License 1.0
 
-export 'camera/custom_camera_preview.dart';
 export 'camera/camera_idle_preview.dart';
+export 'camera/custom_camera_preview.dart';
 export 'controls/camera_capture_button.dart';
 export 'controls/camera_recording_indicator.dart';
 export 'controls/story_control_button.dart';
