7d169a1104d5a62c8362d82b3ccfc10d5fe85969
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2024-12-20 09:04:47 +0100

    refactor: use EventReference in bookmarks notifier, merge bookmarking buttons into one widget, merge bookmarking methods into one for all types

--- lib/app/features/components/entities_list/components/bookmark_button/bookmark_button.dart
+++ lib/app/features/components/entities_list/components/bookmark_button/bookmark_button.dart
@@ -3,21 +3,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/features/feed/data/models/bookmarks/bookmarks_set.c.dart';
+import 'package:ion/app/features/feed/providers/bookmarks_notifier.c.dart';
+import 'package:ion/app/features/nostr/model/event_reference.c.dart';
 import 'package:ion/generated/assets.gen.dart';
 
 class BookmarkButton extends ConsumerWidget {
-  const BookmarkButton({required this.isBookmarked, required this.onBookmark, super.key});
+  const BookmarkButton.post({required this.eventReference, super.key})
+      : type = BookmarksSetType.posts;
 
-  final bool isBookmarked;
-  final VoidCallback onBookmark;
+  const BookmarkButton.story({required this.eventReference, super.key})
+      : type = BookmarksSetType.stories;
+
+  const BookmarkButton.video({required this.eventReference, super.key})
+      : type = BookmarksSetType.videos;
+
+  const BookmarkButton.article({required this.eventReference, super.key})
+      : type = BookmarksSetType.articles;
+
+  final EventReference? eventReference;
+  final BookmarksSetType type;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    if (eventReference == null) {
+      return const SizedBox.shrink();
+    }
+
+    final isBookmarked = ref.watch(isBookmarkedProvider(eventReference!)).value ?? false;
     return IconButton(
       icon: SvgPicture.asset(
         isBookmarked ? Assets.svg.iconBookmarksOn : Assets.svg.iconBookmarks,
       ),
-      onPressed: onBookmark,
+      onPressed: () => ref.read(bookmarksNotifierProvider.notifier).toggleBookmark(
+            eventReference!,
+            type: type,
+          ),
     );
   }
 }
--- lib/app/features/feed/providers/bookmarks_notifier.c.dart
+++ lib/app/features/feed/providers/bookmarks_notifier.c.dart
@@ -7,16 +7,19 @@ import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
 import 'package:ion/app/features/feed/data/models/bookmarks/bookmarks.c.dart';
 import 'package:ion/app/features/feed/data/models/bookmarks/bookmarks_set.c.dart';
 import 'package:ion/app/features/feed/data/models/entities/article_data.c.dart';
+import 'package:ion/app/features/feed/data/models/entities/post_data.c.dart';
 import 'package:ion/app/features/nostr/model/action_source.dart';
+import 'package:ion/app/features/nostr/model/event_reference.c.dart';
 import 'package:ion/app/features/nostr/model/replaceable_event_reference.c.dart';
 import 'package:ion/app/features/nostr/providers/nostr_cache.c.dart';
+import 'package:ion/app/features/nostr/providers/nostr_entity_provider.c.dart';
 import 'package:ion/app/features/nostr/providers/nostr_notifier.c.dart';
 import 'package:nostr_dart/nostr_dart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'bookmarks_notifier.c.g.dart';
 
-@Riverpod(keepAlive: true)
+@riverpod
 Future<Map<BookmarksSetType, BookmarksSetEntity?>> bookmarks(
   Ref ref,
   String pubkey,
@@ -58,7 +61,7 @@ Future<Map<BookmarksSetType, BookmarksSetEntity?>> bookmarks(
   );
 }
 
-@Riverpod(keepAlive: true)
+@riverpod
 Future<Map<BookmarksSetType, BookmarksSetEntity?>> currentUserBookmarks(Ref ref) async {
   final currentPubkey = ref.watch(currentPubkeySelectorProvider);
   if (currentPubkey == null) {
@@ -68,13 +71,27 @@ Future<Map<BookmarksSetType, BookmarksSetEntity?>> currentUserBookmarks(Ref ref)
 }
 
 @riverpod
-bool isPostBookmarked(Ref ref, String id) {
+Future<bool> isBookmarked(Ref ref, EventReference eventReference) async {
+  final nostrEntity = await ref.read(
+    nostrEntityProvider(eventReference: eventReference).future,
+  );
+  if (nostrEntity == null) return false;
+
   return ref.watch(
     currentUserBookmarksProvider.select(
-      (state) =>
-          state.valueOrNull?.values
-              .any((bookmarksSet) => bookmarksSet?.data.postsIds.contains(id) ?? false) ??
-          false,
+      (state) {
+        if (nostrEntity is PostEntity) {
+          return state.valueOrNull?.values.any(
+                (bookmarksSet) => bookmarksSet?.data.postsIds.contains(nostrEntity.id) ?? false,
+              ) ??
+              false;
+        } else if (nostrEntity is ArticleEntity) {
+          return state.valueOrNull?[BookmarksSetType.articles]?.data.articlesRefs
+                  .contains(nostrEntity.toReplaceableEventReference()) ??
+              false;
+        }
+        return false;
+      },
     ),
   );
 }
@@ -96,18 +113,9 @@ class BookmarksNotifier extends _$BookmarksNotifier {
   @override
   FutureOr<void> build() async {}
 
-  Future<void> togglePostBookmark(String id, {required BookmarksSetType type}) async {
-    await _toggleBookmark(postId: id, type: type);
-  }
-
-  Future<void> toggleArticleBookmark(ArticleEntity article) async {
-    await _toggleBookmark(article: article, type: BookmarksSetType.articles);
-  }
-
-  Future<void> _toggleBookmark({
+  Future<void> toggleBookmark(
+    EventReference eventReference, {
     required BookmarksSetType type,
-    String? postId,
-    ArticleEntity? article,
   }) async {
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() async {
@@ -117,14 +125,16 @@ class BookmarksNotifier extends _$BookmarksNotifier {
         throw UserMasterPubkeyNotFoundException();
       }
 
+      final nostrEntity = await ref.read(
+        nostrEntityProvider(eventReference: eventReference).future,
+      );
+      if (nostrEntity == null) return;
+
       final bookmarksMap = await ref.read(currentUserBookmarksProvider.future);
       final bookmarksSet = bookmarksMap[type];
 
-      // Determine whether it's a post or an article
-      final isPost = postId != null;
-      final isArticle = article != null;
-
-      if (isPost) {
+      if (nostrEntity is PostEntity) {
+        final postId = nostrEntity.id;
         final bookmarksIds = Set<String>.from(bookmarksSet?.data.postsIds ?? []);
         if (bookmarksIds.contains(postId)) {
           bookmarksIds.remove(postId);
@@ -139,8 +149,8 @@ class BookmarksNotifier extends _$BookmarksNotifier {
         );
 
         bookmarksMap[type] = bookmarksSet?.copyWith(data: newSingleBookmarksSetData);
-      } else if (isArticle) {
-        final articleRef = article.toReplaceableEventReference();
+      } else if (nostrEntity is ArticleEntity) {
+        final articleRef = nostrEntity.toReplaceableEventReference();
         final bookmarksRefs =
             Set<ReplaceableEventReference>.from(bookmarksSet?.data.articlesRefs ?? []);
         if (bookmarksRefs.contains(articleRef)) {
@@ -158,7 +168,6 @@ class BookmarksNotifier extends _$BookmarksNotifier {
         bookmarksMap[type] = bookmarksSet?.copyWith(data: newSingleBookmarksSetData);
       }
 
-      // Update global bookmarks data
       final bookmarksData = BookmarksData(
         ids: [],
         bookmarksSetRefs: bookmarksMap.values
