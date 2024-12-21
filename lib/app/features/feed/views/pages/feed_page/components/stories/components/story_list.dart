f748a23ef3b36d9ded9a32a305ecd7fd11eaf0e5
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2024-12-21 11:58:10 +0200

    fix: show the stories of the current user, sort by the story date creation

--- lib/app/features/feed/views/pages/feed_page/components/stories/components/story_list.dart
+++ lib/app/features/feed/views/pages/feed_page/components/stories/components/story_list.dart
@@ -5,11 +5,16 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
+import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
+import 'package:ion/app/features/feed/create_post/model/create_post_option.dart';
+import 'package:ion/app/features/feed/create_post/providers/create_post_notifier.c.dart';
+import 'package:ion/app/features/feed/providers/feed_stories_data_source_provider.c.dart';
 import 'package:ion/app/features/feed/views/pages/feed_page/components/stories/components/current_user_story_list_item.dart';
 import 'package:ion/app/features/feed/views/pages/feed_page/components/stories/components/story_list_item.dart';
 import 'package:ion/app/features/feed/views/pages/feed_page/components/stories/components/story_list_separator.dart';
 import 'package:ion/app/features/feed/views/pages/feed_page/components/stories/mock.dart';
+import 'package:ion/app/features/nostr/providers/entities_paged_data_provider.c.dart';
 
 class StoryList extends ConsumerWidget {
   const StoryList({
@@ -22,11 +27,19 @@ class StoryList extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final currentUserPubkey = ref.watch(currentPubkeySelectorProvider) ?? '';
+    final filteredPubkeys = pubkeys.where((pubkey) => pubkey != currentUserPubkey).toList();
+
+    ref.listenSuccess(createPostNotifierProvider(CreatePostOption.story), (next) {
+      final dataSources = ref.read(feedStoriesDataSourceProvider) ?? [];
+      if (dataSources.isNotEmpty) {
+        ref.invalidate(entitiesPagedDataProvider(dataSources));
+      }
+    });
 
     return SliverPadding(
       padding: EdgeInsets.symmetric(horizontal: ScreenSideOffset.defaultSmallMargin),
       sliver: SliverList.separated(
-        itemCount: pubkeys.length + 1,
+        itemCount: filteredPubkeys.length + 1,
         separatorBuilder: (_, __) => const StoryListSeparator(),
         itemBuilder: (_, index) {
           if (index == 0) {
@@ -36,7 +49,7 @@ class StoryList extends ConsumerWidget {
             );
           }
 
-          final pubkey = pubkeys[index - 1];
+          final pubkey = filteredPubkeys[index - 1];
           return StoryListItem(
             pubkey: pubkey,
             gradient: storyBorderGradients[Random().nextInt(storyBorderGradients.length)],
