18a9a0f9893c04a125224f9f0cbfcda6daaa0e12
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2024-12-21 14:41:54 +0200

    fix: add license text

--- lib/app/features/chat/providers/fetch_conversation_provider.c.dart
+++ lib/app/features/chat/providers/fetch_conversation_provider.c.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
 import 'package:ion/app/features/chat/model/entities/private_direct_message_data.c.dart';
