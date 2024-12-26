f336156cdf75998bf428bb3c01707b90e13de61e
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2024-12-26 15:39:39 +0200

    fix: profile tabs (#472)
    ## Description
    1. Update image for reporting user dialog.
    2. Update texts/icons for empty state of the profile tabs
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [x] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    
    https://github.com/user-attachments/assets/23de9c75-245e-4198-8ceb-1a6b77919a9d

---
+++ lib/app/features/user/providers/tab_data_source_provider.c.dart
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/features/nostr/providers/entities_paged_data_provider.c.dart';
+import 'package:ion/app/features/user/model/tab_entity_type.dart';
+import 'package:ion/app/features/user/providers/user_articles_data_source_provider.c.dart';
+import 'package:ion/app/features/user/providers/user_posts_data_source_provider.c.dart';
+import 'package:ion/app/features/user/providers/user_replies_data_source_provider.c.dart';
+import 'package:ion/app/features/user/providers/user_videos_data_source_provider.c.dart';
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'tab_data_source_provider.c.g.dart';
+
+@riverpod
+List<EntitiesDataSource>? tabDataSource(
+  Ref ref, {
+  required TabEntityType type,
+  required String pubkey,
+}) {
+  switch (type) {
+    case TabEntityType.posts:
+      return ref.watch(userPostsDataSourceProvider(pubkey));
+    case TabEntityType.articles:
+      return ref.watch(userArticlesDataSourceProvider(pubkey));
+    case TabEntityType.replies:
+      return ref.watch(userRepliesDataSourceProvider(pubkey));
+    case TabEntityType.videos:
+      return ref.watch(userVideosDataSourceProvider(pubkey));
+  }
+}
