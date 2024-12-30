aa32048c1709b6ba7ffa4131f1a471df8bcecb25
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2024-12-30 17:44:53 +0200

    chore: add license

--- lib/app/features/wallet/components/coin_icon_with_network/coin_icon_with_network.dart
+++ lib/app/features/wallet/components/coin_icon_with_network/coin_icon_with_network.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:flutter/material.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/wallet/model/coin_data.c.dart';
