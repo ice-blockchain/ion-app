0ad493d663c056bf6e37b508c9025484f9ec5957
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-01-03 19:57:50 +0200

    style: code format

--- lib/app/features/chat/providers/conversation_message_actions_provider.c.dart
+++ lib/app/features/chat/providers/conversation_message_actions_provider.c.dart
@@ -25,16 +25,14 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'conversation_message_actions_provider.c.g.dart';
 
 @Riverpod(keepAlive: true)
-Raw<Future<ConversationMessageActionsService>>
-    conversationMessageActionsService(
+Raw<Future<ConversationMessageActionsService>> conversationMessageActionsService(
   Ref ref,
 ) async {
   final databaseService = ref.watch(conversationsDBServiceProvider);
   final conversationMessageManagementService =
       await ref.watch(conversationMessageManagementServiceProvider);
 
-  final eventSigner =
-      await ref.watch(currentUserNostrEventSignerProvider.future);
+  final eventSigner = await ref.watch(currentUserNostrEventSignerProvider.future);
 
   return ConversationMessageActionsService(
     eventSigner: eventSigner,
@@ -67,8 +65,7 @@ class ConversationMessageActionsService {
   final IonConnectSealService sealService;
   final IonConnectGiftWrapService wrapService;
   final ConversationsDBService databaseService;
-  final ConversationMessageManagementService
-      conversationMessageManagementService;
+  final ConversationMessageManagementService conversationMessageManagementService;
 
   Future<void> deleteMessage(String id) async {
     await databaseService.markConversationMessageAsDeleted(id);
