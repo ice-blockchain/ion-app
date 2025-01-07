ddd45601d0632042d5d70ff270c2e29f7f84719d
ice-hector <96414297+ice-hector@users.noreply.github.com>
2025-01-07 15:41:02 +0200

    feat: add avatar displaying for unfocused state (#513)
    ## Description
    This PR introduces the author avatar displaying a near reply field for
    an unfocused state
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <img width="180" alt="Screenshot 2025-01-07 at 13 07 30"
    src="https://github.com/user-attachments/assets/ea513306-861c-4d22-bbde-1525c496cb17"
    />
    <img width="180" alt="Screenshot 2025-01-07 at 13 07 36"
    src="https://github.com/user-attachments/assets/fd315bb0-04bd-48e7-9560-7c34148b4b64"
    />

--- lib/app/features/feed/create_post/views/components/reply_input_field/reply_input_field.dart
+++ lib/app/features/feed/create_post/views/components/reply_input_field/reply_input_field.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_quill/quill_delta.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/components/avatar/avatar.dart';
 import 'package:ion/app/components/inputs/hooks/use_node_focused.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/extensions/asset_gen_image.dart';
@@ -20,6 +21,7 @@ import 'package:ion/app/features/feed/views/components/text_editor/text_editor.d
 import 'package:ion/app/features/feed/views/components/toolbar_buttons/toolbar_image_button.dart';
 import 'package:ion/app/features/feed/views/components/toolbar_buttons/toolbar_poll_button.dart';
 import 'package:ion/app/features/nostr/model/event_reference.c.dart';
+import 'package:ion/app/features/user/providers/user_metadata_provider.c.dart';
 import 'package:ion/app/router/app_routes.c.dart';
 import 'package:ion/app/services/media_service/media_service.c.dart';
 import 'package:ion/generated/assets.gen.dart';
@@ -36,6 +38,8 @@ class ReplyInputField extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final textEditorController = useQuillController();
 
+    final currentUserMetadata = ref.watch(currentUserMetadataProvider).valueOrNull;
+
     final inputContainerKey = useRef(GlobalKey());
     final focusNode = useFocusNode();
     final hasFocus = useNodeFocused(focusNode);
@@ -50,64 +54,77 @@ class ReplyInputField extends HookConsumerWidget {
               padding: EdgeInsets.only(bottom: 12.0.s),
               child: ReplyAuthorHeader(pubkey: eventReference.pubkey),
             ),
-          DecoratedBox(
-            decoration: BoxDecoration(
-              color: context.theme.appColors.onSecondaryBackground,
-              borderRadius: BorderRadius.circular(16.0.s),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                if (attachedMediaNotifier.value.isNotEmpty) ...[
-                  SizedBox(height: 6.0.s),
-                  AttachedMediaPreview(attachedMediaNotifier: attachedMediaNotifier),
-                ],
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 12.0.s, vertical: 9.0.s),
-                  // When we focus the TextField, a new child is added to the Column,
-                  // This causes Flutter to rebuild all subsequent children from scratch,
-                  // which results in the TextField losing focus and the keyboard hiding.
-                  // Assigning a key to this child ensures that Flutter preserves it during rebuilds.
-                  key: inputContainerKey.value,
-                  constraints: BoxConstraints(
-                    maxHeight: 68.0.s,
-                    minHeight: 36.0.s,
+          Row(
+            children: [
+              if (!hasFocus.value && currentUserMetadata != null)
+                Avatar(
+                  imageUrl: currentUserMetadata.data.picture,
+                  size: 36.0.s,
+                  borderRadius: BorderRadius.all(Radius.circular(12.0.s)),
+                ),
+              SizedBox(width: 6.0.s),
+              Expanded(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: context.theme.appColors.onSecondaryBackground,
+                    borderRadius: BorderRadius.circular(16.0.s),
                   ),
-                  child: Row(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Flexible(
-                        child: TextEditor(
-                          textEditorController,
-                          focusNode: focusNode,
-                          autoFocus: false,
-                          placeholder: context.i18n.post_reply_hint,
+                      if (attachedMediaNotifier.value.isNotEmpty) ...[
+                        SizedBox(height: 6.0.s),
+                        AttachedMediaPreview(attachedMediaNotifier: attachedMediaNotifier),
+                      ],
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 12.0.s, vertical: 9.0.s),
+                        // When we focus the TextField, a new child is added to the Column,
+                        // This causes Flutter to rebuild all subsequent children from scratch,
+                        // which results in the TextField losing focus and the keyboard hiding.
+                        // Assigning a key to this child ensures that Flutter preserves it during rebuilds.
+                        key: inputContainerKey.value,
+                        constraints: BoxConstraints(
+                          maxHeight: 68.0.s,
+                          minHeight: 36.0.s,
+                        ),
+                        child: Row(
+                          children: [
+                            Flexible(
+                              child: TextEditor(
+                                textEditorController,
+                                focusNode: focusNode,
+                                autoFocus: false,
+                                placeholder: context.i18n.post_reply_hint,
+                              ),
+                            ),
+                            if (hasFocus.value)
+                              GestureDetector(
+                                onTap: () async {
+                                  final content = await CreatePostRoute(
+                                    parentEvent: eventReference.toString(),
+                                    showCollapseButton: true,
+                                    content: textEditorController.document.toPlainText().trim(),
+                                  ).push<String>(context);
+                                  if (content != null) {
+                                    textEditorController
+                                      ..setContents(
+                                        Delta.fromJson([
+                                          {'insert': content},
+                                        ]),
+                                      )
+                                      ..moveCursorToEnd();
+                                  }
+                                },
+                                child: Assets.svg.iconReplysearchScale.icon(size: 20.0.s),
+                              ),
+                          ],
                         ),
                       ),
-                      if (hasFocus.value)
-                        GestureDetector(
-                          onTap: () async {
-                            final content = await CreatePostRoute(
-                              parentEvent: eventReference.toString(),
-                              showCollapseButton: true,
-                              content: textEditorController.document.toPlainText().trim(),
-                            ).push<String>(context);
-                            if (content != null) {
-                              textEditorController
-                                ..setContents(
-                                  Delta.fromJson([
-                                    {'insert': content},
-                                  ]),
-                                )
-                                ..moveCursorToEnd();
-                            }
-                          },
-                          child: Assets.svg.iconReplysearchScale.icon(size: 20.0.s),
-                        ),
                     ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
           SizedBox(height: 12.0.s),
           if (hasFocus.value)
