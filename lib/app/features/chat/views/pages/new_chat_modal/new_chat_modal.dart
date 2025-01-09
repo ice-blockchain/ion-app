2b126c83b1c3f1a3f040f063ebed45f98df59085
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-01-09 16:49:24 +0200

    refactor: Merge with master

--- lib/app/features/chat/views/pages/new_chat_modal/new_chat_modal.dart
+++ lib/app/features/chat/views/pages/new_chat_modal/new_chat_modal.dart
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/button/button.dart';
 import 'package:ion/app/exceptions/exceptions.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
 import 'package:ion/app/features/chat/providers/e2ee_group_conversation_management_provider.c.dart';
-import 'package:ion/app/features/user/pages/user_picker_sheet/user_picker_sheet.dart';
 import 'package:ion/app/features/core/views/pages/error_modal.dart';
 import 'package:ion/app/features/user/model/user_metadata.c.dart';
+import 'package:ion/app/features/user/pages/user_picker_sheet/user_picker_sheet.dart';
 import 'package:ion/app/router/app_routes.c.dart';
 import 'package:ion/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ion/app/router/components/navigation_app_bar/navigation_close_button.dart';
@@ -23,8 +22,6 @@ class NewChatModal extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final searchText = ref.watch(usersSearchTextProvider);
-
     ref.listen(
       e2EEGroupConversationManagementProvider,
       (previous, next) async {
@@ -41,7 +38,8 @@ class NewChatModal extends ConsumerWidget {
       final ee2eGroupConversationService =
           ref.watch(e2EEGroupConversationManagementProvider.notifier);
 
-      final currentPubkey = ref.watch(currentPubkeySelectorProvider).valueOrNull;
+      final currentPubkey =
+          ref.watch(currentPubkeySelectorProvider).valueOrNull;
 
       if (currentPubkey == null) {
         throw UserMasterPubkeyNotFoundException();
