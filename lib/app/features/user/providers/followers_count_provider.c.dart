9f19559eb82ee3263112bfcde1cffd2b3aa3285a
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-01-15 11:04:16 +0300

    chore: temp commit with debug flow

--- lib/app/features/user/providers/followers_count_provider.c.dart
+++ lib/app/features/user/providers/followers_count_provider.c.dart
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'dart:math';
-
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/exceptions/exceptions.dart';
+import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/feed/data/models/entities/event_count_request_data.c.dart';
 import 'package:ion/app/features/feed/data/models/entities/event_count_result_data.c.dart';
+import 'package:ion/app/features/ion_connect/model/action_source.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_cache.c.dart';
+import 'package:ion/app/features/ion_connect/providers/ion_connect_notifier.c.dart';
+import 'package:ion/app/features/user/model/follow_list.c.dart';
+import 'package:ion/app/features/user/providers/user_relays_manager.c.dart';
+import 'package:nostr_dart/nostr_dart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'followers_count_provider.c.g.dart';
@@ -23,21 +29,49 @@ Future<int?> followersCount(Ref ref, String pubkey) async {
   );
 
   if (followersCountEntity != null) {
-    return 0;
+    return followersCountEntity.data.content as int;
   }
 
-  // TODO:uncomment when impl
-  // final followersCountRequest = EventCountRequestData(
-  //   params: const EventCountRequestParams(group: 'p'),
-  //   filters: [
-  //     RequestFilter(kinds: const [FollowListEntity.kind], p: [pubkey]),
-  //   ],
-  // );
+  final userRelays = await ref.read(currentUserRelayProvider.future);
+  if (userRelays == null) {
+    throw UserRelaysNotFoundException();
+  }
 
-  // final response = await ref.read(ionConnectNotifierProvider.notifier).requestCount(
-  //       followersCountRequest,
-  //       actionSource: ActionSourceUser(pubkey),
-  //     );
+  final relayUrl = userRelays.data.list.random.url;
 
-  return Random().nextInt(100);
+  final followersCountRequest = EventCountRequestData(
+    relays: [relayUrl],
+    params: const EventCountRequestParams(group: 'p'),
+    filters: [
+      RequestFilter(kinds: const [FollowListEntity.kind], p: [pubkey]),
+    ],
+  );
+
+  final followersCountRequestEvent =
+      await ref.read(ionConnectNotifierProvider.notifier).sign(followersCountRequest);
+
+  await ref.read(ionConnectNotifierProvider.notifier).sendEvent(
+        followersCountRequestEvent,
+        actionSource: ActionSourceRelayUrl(relayUrl),
+        cache: false,
+      );
+
+  await Future<void>.delayed(const Duration(seconds: 5));
+
+  final requestMessage = RequestMessage()
+    ..addFilter(
+      const RequestFilter(
+        kinds: [EventCountResultEntity.kind],
+        // e: [followersCountRequestEvent.id],
+        limit: 1,
+      ),
+    );
+
+  final foo = await ref
+      .read(ionConnectNotifierProvider.notifier)
+      .requestEntity(requestMessage, actionSource: ActionSourceRelayUrl(relayUrl));
+  // ignore: avoid_print
+  print(foo);
+  throw Exception('1');
+  // return response.data.content as int;
 }
