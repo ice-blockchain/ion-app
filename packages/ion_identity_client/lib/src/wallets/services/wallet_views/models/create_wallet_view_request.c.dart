0585b75e99a779c300d987eece244c5b8b0fb379
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-01-15 14:35:07 +0200

    chore: update data models in ion client

--- packages/ion_identity_client/lib/src/wallets/services/wallet_views/models/create_wallet_view_request.c.dart
+++ packages/ion_identity_client/lib/src/wallets/services/wallet_views/models/create_wallet_view_request.c.dart
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:ion_identity_client/src/wallets/services/wallet_views/models/wallet_view_item.dart';
+import 'package:ion_identity_client/src/wallets/services/wallet_views/models/wallet_view_item.c.dart';
 
-part 'create_wallet_view_request.freezed.dart';
-part 'create_wallet_view_request.g.dart';
+part 'create_wallet_view_request.c.freezed.dart';
+part 'create_wallet_view_request.c.g.dart';
 
 @freezed
 class CreateWalletViewRequest with _$CreateWalletViewRequest {
   const factory CreateWalletViewRequest({
     required List<WalletViewItem> items,
+    required List<String> symbolGroups,
     required String name,
   }) = _CreateWalletViewRequest;
 
