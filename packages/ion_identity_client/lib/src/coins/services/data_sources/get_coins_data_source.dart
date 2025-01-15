2ec27a46399c31a88b4639e453fb3c8d52cca6a6
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-01-15 13:17:51 +0200

    fix: handle empty string response

--- packages/ion_identity_client/lib/src/coins/services/data_sources/get_coins_data_source.dart
+++ packages/ion_identity_client/lib/src/coins/services/data_sources/get_coins_data_source.dart
@@ -42,15 +42,13 @@ class GetCoinsDataSource {
         username: username,
       ),
       decoder: (response) {
+        if (response is String && response.isEmpty) {
+          return CoinsResponse(coins: [], version: currentVersion);
+        }
+
         return parseJsonObject(
           response,
-          fromJson: (response) {
-            if (response.isEmpty) {
-              return CoinsResponse(coins: [], version: currentVersion);
-            }
-
-            return CoinsResponse.fromJson(response);
-          },
+          fromJson: CoinsResponse.fromJson,
         );
       },
     );
