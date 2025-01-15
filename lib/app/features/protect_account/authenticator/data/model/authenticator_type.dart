8d5823fa72318b60f95dc9d6a4909d015ad8d0b1
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-01-15 18:15:04 +0100

    feat: add app redirect for each authenticator option on authenticator setup page (#549)
    ## Additional Notes
    external_app_launcher lib currently doesn't work on iOS because of a
    bug. Until it is fixed, iOS is using an alternative solution.
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <!-- Include screenshots to demonstrate any UI changes. -->
    <!-- <img width="180" alt="image" src="image_url_here"> -->

--- lib/app/features/protect_account/authenticator/data/model/authenticator_type.dart
+++ lib/app/features/protect_account/authenticator/data/model/authenticator_type.dart
@@ -1,32 +1,63 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:ion/app/extensions/build_context.dart';
 import 'package:ion/generated/assets.gen.dart';
 
-enum AutethenticatorType {
+enum AuthenticatorType {
   google,
-  micsrosoft,
+  microsoft,
   lastpass,
   authy,
   userLockPush;
 
-  String getDisplayName(BuildContext context) {
-    return switch (this) {
-      AutethenticatorType.google => 'Google Authenticator',
-      AutethenticatorType.micsrosoft => 'Microsoft Authenticator',
-      AutethenticatorType.lastpass => 'LastPass Authenticator',
-      AutethenticatorType.authy => 'Authy',
-      AutethenticatorType.userLockPush => 'UserLock Push',
-    };
-  }
+  String getDisplayName(BuildContext context) => switch (this) {
+        AuthenticatorType.google => context.i18n.authenticator_google,
+        AuthenticatorType.microsoft => context.i18n.authenticator_microsoft,
+        AuthenticatorType.lastpass => context.i18n.authenticator_lastpass,
+        AuthenticatorType.authy => context.i18n.authenticator_authy,
+        AuthenticatorType.userLockPush => context.i18n.authenticator_userLockPush,
+      };
 
   String get iconAsset {
     return switch (this) {
-      AutethenticatorType.google => Assets.svg.icon2faGoogleauth,
-      AutethenticatorType.micsrosoft => Assets.svg.icon2famicrosoft,
-      AutethenticatorType.lastpass => Assets.svg.icon2faLastpass,
-      AutethenticatorType.authy => Assets.svg.icon2faAuthy,
-      AutethenticatorType.userLockPush => Assets.svg.icon2faUserlock,
+      AuthenticatorType.google => Assets.svg.icon2faGoogleauth,
+      AuthenticatorType.microsoft => Assets.svg.icon2famicrosoft,
+      AuthenticatorType.lastpass => Assets.svg.icon2faLastpass,
+      AuthenticatorType.authy => Assets.svg.icon2faAuthy,
+      AuthenticatorType.userLockPush => Assets.svg.icon2faUserlock,
+    };
+  }
+
+  String get androidPackageName {
+    return switch (this) {
+      AuthenticatorType.google => 'com.google.android.apps.authenticator2',
+      AuthenticatorType.microsoft => 'com.azure.authenticator',
+      AuthenticatorType.lastpass => 'com.lastpass.authenticator',
+      AuthenticatorType.authy => 'com.authy.authy',
+      AuthenticatorType.userLockPush => 'com.isdecisions.userlockpush',
+    };
+  }
+
+  String get iosAppUrlScheme {
+    return switch (this) {
+      AuthenticatorType.google => 'totp://',
+      AuthenticatorType.microsoft => 'msauthv3://',
+      AuthenticatorType.lastpass => 'lastpassmfa://',
+      AuthenticatorType.authy => 'authy://',
+      AuthenticatorType.userLockPush => 'userlock://',
+    };
+  }
+
+  String get appStoreLink {
+    return switch (this) {
+      AuthenticatorType.google => 'https://apps.apple.com/us/app/google-authenticator/id388497605',
+      AuthenticatorType.microsoft =>
+        'https://apps.apple.com/us/app/microsoft-authenticator/id983156458',
+      AuthenticatorType.lastpass =>
+        'https://apps.apple.com/us/app/lastpass-authenticator/id1079110004',
+      AuthenticatorType.authy => 'https://apps.apple.com/us/app/authy/id494168017',
+      AuthenticatorType.userLockPush => 'https://apps.apple.com/us/app/userlock-push/id1633085926',
     };
   }
 }
