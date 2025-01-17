197f3e9ae34836e09564faf68557a60f881bba1b
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-01-17 10:59:27 +0200

    fix: add multiselection for the account notifications (#554)
    ## Description
    <!-- Briefly explain the purpose of this PR and what it accomplishes.
    -->
    
    Choosing `none` should unmark the rest of the options.
    `None` can’t be chosen with any other option together.
    `None` should be chosen by default for all other users.
    
    
    
    https://github.com/user-attachments/assets/efbbb87b-e6a7-49ca-98c7-08ba34abec01
    
    
    
    ## Additional Notes
    <!-- Add any extra context or relevant information here. -->
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <!-- Include screenshots to demonstrate any UI changes. -->
    <!-- <img width="180" alt="image" src="image_url_here"> -->

--- lib/app/features/user/pages/profile_page/pages/account_notifications_modal/account_notifications_modal.dart
+++ lib/app/features/user/pages/profile_page/pages/account_notifications_modal/account_notifications_modal.dart
@@ -8,25 +8,38 @@ import 'package:ion/app/components/screen_offset/screen_bottom_offset.dart';
 import 'package:ion/app/components/separated/separator.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/user/model/user_notifications_type.dart';
+import 'package:ion/app/features/user/pages/profile_page/providers/user_notifications_provider.c.dart';
 import 'package:ion/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ion/generated/assets.gen.dart';
 
 class AccountNotificationsModal extends HookConsumerWidget {
   const AccountNotificationsModal({
-    required this.selectedUserNotificationsType,
+    required this.selectedUserNotificationsTypes,
     super.key,
   });
 
-  final UserNotificationsType selectedUserNotificationsType;
+  final List<UserNotificationsType> selectedUserNotificationsTypes;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final colors = context.theme.appColors;
     final textStyles = context.theme.appTextThemes;
-    final selectedOption = useState(selectedUserNotificationsType);
+    final selectedOptions = useState<Set<UserNotificationsType>>(
+      selectedUserNotificationsTypes.toSet(),
+    );
 
-    final iconBorderSize = Border.fromBorderSide(
-      BorderSide(color: colors.onTerararyFill, width: 1.0.s),
+    final handleOptionSelection = useCallback(
+      (UserNotificationsType option) {
+        selectedOptions.value = UserNotificationsType.toggleNotificationType(
+          selectedOptions.value,
+          option,
+        );
+
+        ref
+            .read(userNotificationsNotifierProvider.notifier)
+            .updateNotifications(selectedOptions.value.toList());
+      },
+      [selectedOptions, ref],
     );
 
     return Column(
@@ -41,10 +54,7 @@ class AccountNotificationsModal extends HookConsumerWidget {
           Column(
             children: [
               MenuItemButton(
-                onPressed: () {
-                  selectedOption.value = option;
-                  Navigator.of(context).pop(selectedOption.value);
-                },
+                onPressed: () => handleOptionSelection(option),
                 leadingIcon: ButtonIconFrame(
                   containerSize: 36.0.s,
                   borderRadius: BorderRadius.circular(10.0.s),
@@ -53,9 +63,11 @@ class AccountNotificationsModal extends HookConsumerWidget {
                     size: 24.0.s,
                     color: colors.primaryAccent,
                   ),
-                  border: iconBorderSize,
+                  border: Border.fromBorderSide(
+                    BorderSide(color: colors.onTerararyFill, width: 1.0.s),
+                  ),
                 ),
-                trailingIcon: selectedOption.value == option
+                trailingIcon: selectedOptions.value.contains(option)
                     ? Assets.svg.iconBlockCheckboxOnblue.icon(
                         color: colors.success,
                       )
