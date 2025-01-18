3eb82b0ae72f5c45308318f6e12dfbd2e6a09573
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-01-18 20:47:14 +0200

    chore: add license

--- packages/ion_identity_client/lib/src/wallets/services/wallet_views/models/wallet_view_aggregation_item.c.dart
+++ packages/ion_identity_client/lib/src/wallets/services/wallet_views/models/wallet_view_aggregation_item.c.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:ion_identity_client/src/wallets/services/wallet_views/models/wallet_view_aggregation_wallet.c.dart';
 
