3f4d900dd4a93a4e620d4683c0459c71b7e2fb4f
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-01-21 00:16:46 +0200

    feat: imrpove community members count provider

--- lib/app/features/chat/community/providers/community_members_count_provider.c.dart
+++ lib/app/features/chat/community/providers/community_members_count_provider.c.dart
@@ -9,7 +9,10 @@
 // }
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/features/chat/community/providers/community_metadata_provider.c.dart';
+import 'package:ion/app/features/feed/data/models/entities/event_count_request_data.c.dart';
 import 'package:ion/app/features/ion_connect/ion_connect.dart';
+import 'package:ion/app/features/ion_connect/model/action_source.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_notifier.c.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -18,14 +21,24 @@ part 'community_members_count_provider.c.g.dart';
 @riverpod
 Future<int> communityMembersCount(Ref ref, String communityUuid) async {
   //TODO: implement
-  await ref.watch(ionConnectNotifierProvider.notifier).requestEntity(
-        RequestMessage()
-          ..addFilter(
-            RequestFilter(
-              kinds: const [1750],
-              ids: [communityUuid],
-            ),
-          ),
-      );
-  return 123332;
+
+  final communityOwner =
+      (await ref.watch(communityMetadataProvider(communityUuid).future)).data.owner;
+
+  final request = EventCountRequestData(
+    filters: [
+      RequestFilter(
+        kinds: const [1750],
+        tags: {
+          '#h': [communityUuid],
+        },
+      ),
+    ],
+    params: const EventCountRequestParams(group: 'p'),
+  );
+  final response = await ref
+      .watch(ionConnectNotifierProvider.notifier)
+      .requestCount(request, actionSource: ActionSourceUser(communityOwner));
+
+  return response.data.content as int;
 }
