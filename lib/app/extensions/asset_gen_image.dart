d81149bc378ad306742c8b4bbbf399452c31fe7e
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-01-20 21:53:11 +0200

    chore: remove unused import

--- lib/app/extensions/asset_gen_image.dart
+++ lib/app/extensions/asset_gen_image.dart
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ion/app/extensions/num.dart';
