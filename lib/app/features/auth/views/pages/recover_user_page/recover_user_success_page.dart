f235f2fe9e293117c8bf08ac74f0ba5394fa79b6
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-01-21 14:16:05 +0100

    fix: restore identity success page ui

--- lib/app/features/auth/views/pages/recover_user_page/recover_user_success_page.dart
+++ lib/app/features/auth/views/pages/recover_user_page/recover_user_success_page.dart
@@ -3,14 +3,12 @@
 import 'package:flutter/material.dart';
 import 'package:ion/app/components/button/button.dart';
 import 'package:ion/app/components/card/info_card.dart';
+import 'package:ion/app/components/card/warning_card.dart';
 import 'package:ion/app/components/screen_offset/screen_bottom_offset.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
-import 'package:ion/app/extensions/asset_gen_image.dart';
 import 'package:ion/app/extensions/build_context.dart';
 import 'package:ion/app/extensions/num.dart';
-import 'package:ion/app/features/auth/views/components/auth_scrolled_body/auth_scrolled_body.dart';
 import 'package:ion/app/router/app_routes.c.dart';
-import 'package:ion/app/router/components/navigation_app_bar/navigation_close_button.dart';
 import 'package:ion/app/router/components/sheet_content/sheet_content.dart';
 import 'package:ion/generated/assets.gen.dart';
 
@@ -24,35 +22,32 @@ class RecoverUserSuccessPage extends StatelessWidget {
     void onLogin() => GetStartedRoute().go(context);
 
     return SheetContent(
-      body: AuthScrollContainer(
-        showBackButton: false,
-        actions: [
-          NavigationCloseButton(onPressed: onLogin),
-        ],
-        title: context.i18n.two_fa_title,
-        description: context.i18n.two_fa_desc,
-        icon: Assets.svg.iconWalletProtect.icon(size: 36.0.s),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 38.0.s),
-            child: Column(
-              children: [
-                SizedBox(height: 12.0.s),
-                InfoCard(
+          Column(
+            children: [
+              SizedBox(height: 24.0.s),
+              ScreenSideOffset.medium(
+                child: InfoCard(
                   iconAsset: Assets.svg.actionWalletSuccess2Fa,
                   title: context.i18n.common_congratulations,
                   description: context.i18n.two_fa_success_desc,
                 ),
-                SizedBox(height: 12.0.s),
-              ],
-            ),
+              ),
+              SizedBox(height: 16.0.s),
+              ScreenSideOffset.small(
+                child: WarningCard(text: context.i18n.two_fa_success_warning_desc),
+              ),
+              SizedBox(height: 16.0.s),
+            ],
           ),
           ScreenBottomOffset(
             margin: 36.0.s,
-            child: ScreenSideOffset.large(
+            child: ScreenSideOffset.small(
               child: Button(
                 onPressed: onLogin,
-                label: Text(context.i18n.button_login),
+                label: Text(context.i18n.button_log_in),
                 mainAxisSize: MainAxisSize.max,
               ),
             ),
