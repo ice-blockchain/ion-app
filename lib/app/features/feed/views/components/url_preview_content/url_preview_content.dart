9eef9989490b5c5a17eb2fa2b7900d9b3a7038af
ice-hector <96414297+ice-hector@users.noreply.github.com>
2025-01-22 20:28:14 +0200

    feat: url preview block ui

---
+++ lib/app/features/feed/views/components/url_preview_content/url_preview_content.dart
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:ion/app/components/url_preview/url_preview.dart';
+import 'package:ion/app/extensions/extensions.dart';
+import 'package:ogp_data_extract/ogp_data_extract.dart';
+
+part 'components/url_metadata_preview.dart';
+
+class UrlPreviewContent extends HookWidget {
+  const UrlPreviewContent({
+    required this.url,
+    super.key,
+  });
+
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    useAutomaticKeepAlive();
+
+    if (url.isEmpty) {
+      return const SizedBox.shrink();
+    }
+
+    return UrlPreview(
+      url: url,
+      builder: (meta, favIconUrl) {
+        if (meta == null) {
+          return const SizedBox.shrink();
+        }
+
+        return Container(
+          decoration: BoxDecoration(
+            color: context.theme.appColors.onPrimaryAccent,
+            borderRadius: BorderRadius.all(Radius.circular(16.0.s)),
+            border: Border.all(
+              width: 1.0.s,
+              color: context.theme.appColors.onTerararyFill,
+            ),
+          ),
+          child: _UrlMetadataPreview(
+            meta: meta,
+            favIconUrl: favIconUrl,
+            url: url,
+          ),
+        );
+      },
+    );
+  }
+}
