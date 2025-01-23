c121445f3cd0b7e85545b916c0468f860259d868
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-01-23 09:22:12 +0200

    refactor: update get community members count provider

--- lib/app/features/chat/community/providers/community_members_count_provider.c.dart
+++ lib/app/features/chat/community/providers/community_members_count_provider.c.dart
@@ -1,37 +1,115 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/exceptions/exceptions.dart';
+import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/chat/community/models/entities/community_join_data.c.dart';
 import 'package:ion/app/features/chat/community/providers/community_metadata_provider.c.dart';
 import 'package:ion/app/features/feed/data/models/entities/event_count_request_data.c.dart';
-import 'package:ion/app/features/ion_connect/ion_connect.dart';
+import 'package:ion/app/features/feed/data/models/entities/event_count_result_data.c.dart';
 import 'package:ion/app/features/ion_connect/model/action_source.dart';
+import 'package:ion/app/features/ion_connect/providers/ion_connect_cache.c.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_notifier.c.dart';
+import 'package:ion/app/features/ion_connect/providers/relays_provider.c.dart';
+import 'package:ion/app/features/user/providers/user_relays_manager.c.dart';
+import 'package:nostr_dart/nostr_dart.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'community_members_count_provider.c.g.dart';
 
-@riverpod
-Future<int> communityMembersCount(Ref ref, String communityUuid) async {
-  //TODO: implement
-
-  final communityOwner =
-      (await ref.watch(communityMetadataProvider(communityUuid).future)).ownerPubkey;
-
-  final request = EventCountRequestData(
-    filters: [
-      RequestFilter(
-        kinds: const [CommunityJoinEntity.kind],
-        tags: {
-          '#h': [communityUuid],
-        },
+@Riverpod(keepAlive: true)
+class CommunityMembersCount extends _$CommunityMembersCount {
+  @override
+  Future<int> build(String communityUUID) async {
+    final communityMembersCountEntity = ref.watch(
+      ionConnectCacheProvider.select(
+        cacheSelector<EventCountResultEntity>(
+          EventCountResultEntity.cacheKeyBuilder(
+            key: communityUUID,
+            type: EventCountResultType.members,
+          ),
+        ),
       ),
-    ],
-    params: const EventCountRequestParams(group: 'p'),
-  );
-  final response = await ref
-      .watch(ionConnectNotifierProvider.notifier)
-      .requestCount(request, actionSource: ActionSourceUser(communityOwner));
+    );
 
-  return response.data.content as int;
+    if (communityMembersCountEntity != null) {
+      return communityMembersCountEntity.data.content as int;
+    }
+
+    return _fetchCommunityMembersCount(communityUUID);
+  }
+
+  Future<int> _fetchCommunityMembersCount(String communityUUID) async {
+    final relay = await _getOwnerRandomUserRelay(communityUUID);
+
+    final requestEvent =
+        await _buildRequestEvent(relayUrl: relay.url, communityUUID: communityUUID);
+    final subscriptionMessage = RequestMessage()
+      ..addFilter(
+        RequestFilter(
+          kinds: const [EventCountResultEntity.kind],
+          tags: {
+            '#h': [communityUUID],
+          },
+        ),
+      );
+
+    final subscription = relay.subscribe(subscriptionMessage);
+    EventMessage? responseMessage;
+
+    try {
+      await ref.read(ionConnectNotifierProvider.notifier).sendEvent(
+            requestEvent,
+            actionSource: ActionSourceRelayUrl(relay.url),
+            cache: false,
+          );
+
+      responseMessage = await subscription.messages
+          .firstWhere((message) => message is EventMessage)
+          .timeout(const Duration(seconds: 10)) as EventMessage;
+
+      final eventCountResultEntity =
+          EventCountResultEntity.fromEventMessage(responseMessage, key: communityUUID);
+      ref.read(ionConnectCacheProvider.notifier).cache(eventCountResultEntity);
+
+      return eventCountResultEntity.data.content as int;
+    } catch (e) {
+      return 0;
+    } finally {
+      relay.unsubscribe(subscription.id);
+    }
+  }
+
+  Future<NostrRelay> _getOwnerRandomUserRelay(String communityUUID) async {
+    final community = await ref.read(communityMetadataProvider(communityUUID).future);
+    final userRelays = await ref.read(UserRelayProvider(community.ownerPubkey).future);
+    if (userRelays == null) {
+      throw UserRelaysNotFoundException();
+    }
+
+    final relayUrl = userRelays.data.list.random.url;
+    return await ref.read(relayProvider(relayUrl).future);
+  }
+
+  Future<EventMessage> _buildRequestEvent({
+    required String relayUrl,
+    required String communityUUID,
+  }) async {
+    final communityMembersCountRequest = EventCountRequestData(
+      params: EventCountRequestParams(
+        relay: relayUrl,
+        group: 'p',
+      ),
+      relays: [relayUrl],
+      filters: [
+        RequestFilter(
+          kinds: const [CommunityJoinEntity.kind],
+          tags: {
+            '#h': [communityUUID],
+          },
+        ),
+      ],
+    );
+
+    return ref.read(ionConnectNotifierProvider.notifier).sign(communityMembersCountRequest);
+  }
 }
