f4cb9ac353d59db4a76134b64fdc4476bf83bfe4
ice-tychon <175587874+ice-tychon@users.noreply.github.com>
2025-01-28 11:25:37 +0200

    feature: add BTC Psbt (#614)
    ## Description
    This PR adds PSBT transactions support to `ion_identity_client`.
    
    ## Additional Notes
    - The main logic is in the `BtcTransactionCreator`
    - I had to fork `bdk_flutter` to update `flutter_rust_bridge` to the
    latest version
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore

--- packages/ion_identity_client/lib/src/wallets/services/broadcast_transaction/domain/transaction_creator/transaction_creator_factory.dart
+++ packages/ion_identity_client/lib/src/wallets/services/broadcast_transaction/domain/transaction_creator/transaction_creator_factory.dart
@@ -1,23 +1,22 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:bdk_flutter/bdk_flutter.dart';
 import 'package:ion_identity_client/src/wallets/services/broadcast_transaction/domain/transaction_creator/btc_transaction_creator.dart';
 import 'package:ion_identity_client/src/wallets/services/broadcast_transaction/domain/transaction_creator/evm_transaction_creator.dart';
 import 'package:ion_identity_client/src/wallets/services/broadcast_transaction/domain/transaction_creator/ton_transaction_creator.dart';
 import 'package:ion_identity_client/src/wallets/services/broadcast_transaction/domain/transaction_creator/transaction_creator.dart';
 
 class TransactionCreatorFactory {
-  TransactionCreatorFactory()
-      : btcCreator = BtcTransactionCreator(),
-        ethCreator = EvmTransactionCreator();
+  TransactionCreatorFactory() : ethCreator = EvmTransactionCreator();
 
-  final BtcTransactionCreator btcCreator;
   final EvmTransactionCreator ethCreator;
 
   TransactionCreator getCreator(String network) {
     switch (network.toLowerCase()) {
       case 'bitcoin':
+        return BtcTransactionCreator(network: Network.bitcoin);
       case 'bitcointestnet3':
-        return btcCreator;
+        return BtcTransactionCreator(network: Network.testnet);
       case 'ethereum':
       case 'ethereumsepolia':
         return ethCreator;
@@ -30,7 +29,6 @@ class TransactionCreatorFactory {
       case 'iontestnet':
         return TonTransactionCreator(endpoint: 'https://api.testnet.ice.io/http/v2/jsonRPC');
       default:
-        // Clearly handle unknown networks
         throw UnsupportedError('Unsupported network: $network');
     }
   }
