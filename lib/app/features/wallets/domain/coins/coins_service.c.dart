a575c428008b5e29292bde8b4f72e209b878d87a
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-01-28 15:37:27 +0200

    chore: remove unused import

--- lib/app/features/wallets/domain/coins/coins_service.c.dart
+++ lib/app/features/wallets/domain/coins/coins_service.c.dart
@@ -5,7 +5,6 @@ import 'dart:async';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/wallets/data/coins/repository/coins_repository.c.dart';
 import 'package:ion/app/features/wallets/model/coin_data.c.dart';
-import 'package:ion/app/services/storage/local_storage.c.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'coins_service.c.g.dart';
