ac9e4bb6e4f5dded5e3b911631cc2d52372a51ba
ion-endymion <188437551+ice-endymion@users.noreply.github.com>
2025-01-29 11:48:20 +0100

    chore: add license

--- test/app/features/ion_connect/providers/active_relays_provider_test.dart
+++ test/app/features/ion_connect/providers/active_relays_provider_test.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/ion_connect/providers/active_relays_provider.c.dart';
