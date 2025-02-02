cffce7ad4a95782f13826d73cd777992a12d5246
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-02-02 13:19:00 +0200

    chore: revert some changes

--- lib/app/features/wallets/domain/wallet_views/wallet_views_service.c.dart
+++ lib/app/features/wallets/domain/wallet_views/wallet_views_service.c.dart
@@ -6,14 +6,14 @@ import 'dart:math';
 import 'package:collection/collection.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/exceptions/exceptions.dart';
+import 'package:ion/app/features/core/main_wallet_provider.c.dart';
+import 'package:ion/app/features/core/wallets_provider.c.dart';
 import 'package:ion/app/features/wallets/model/coin_data.c.dart';
 import 'package:ion/app/features/wallets/model/coin_in_wallet_data.c.dart';
 import 'package:ion/app/features/wallets/model/coins_group.c.dart';
 import 'package:ion/app/features/wallets/model/wallet_view_data.c.dart';
 import 'package:ion/app/services/ion_identity/ion_identity_client_provider.c.dart';
 import 'package:ion/app/services/logger/logger.dart';
-import 'package:ion/app/services/wallets/main_wallet_provider.c.dart';
-import 'package:ion/app/services/wallets/wallets_provider.c.dart';
 import 'package:ion_identity_client/ion_identity.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
