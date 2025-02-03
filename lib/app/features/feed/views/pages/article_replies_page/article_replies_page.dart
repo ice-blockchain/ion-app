ba3c20eab5edcc1290d2e46ebd31d57c39cef43f
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-02-03 16:24:59 +0300

    feat: add bookmark button

--- lib/app/features/feed/views/pages/article_replies_page/article_replies_page.dart
+++ lib/app/features/feed/views/pages/article_replies_page/article_replies_page.dart
@@ -4,11 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/separated/separator.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/components/entities_list/components/bookmark_button/bookmark_button.dart';
 import 'package:ion/app/features/feed/data/models/entities/article_data.c.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_entity_provider.c.dart';
 import 'package:ion/app/router/components/navigation_app_bar/navigation_app_bar.dart';
-import 'package:ion/generated/assets.gen.dart';
 
 class ArticleRepliesPage extends HookConsumerWidget {
   const ArticleRepliesPage({
@@ -28,14 +28,9 @@ class ArticleRepliesPage extends HookConsumerWidget {
 
     return Scaffold(
       appBar: NavigationAppBar.screen(
+        title: Text(context.i18n.common_comments),
         actions: [
-          IconButton(
-            icon: Assets.svg.iconBookmarks.icon(
-              size: NavigationAppBar.actionButtonSide,
-              color: context.theme.appColors.primaryText,
-            ),
-            onPressed: () {},
-          ),
+          BookmarkButton(eventReference: eventReference),
         ],
       ),
       body: const Column(
