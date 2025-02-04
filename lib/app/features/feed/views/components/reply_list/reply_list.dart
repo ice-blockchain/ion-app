c37437117ce1d507d419353e94918641817ad259
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-02-04 15:08:10 +0300

    feat: add replies list emplt state

--- lib/app/features/feed/views/components/reply_list/reply_list.dart
+++ lib/app/features/feed/views/components/reply_list/reply_list.dart
@@ -2,12 +2,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/components/empty_list/empty_list.dart';
 import 'package:ion/app/components/scroll_view/load_more_builder.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/components/entities_list/entities_list.dart';
 import 'package:ion/app/features/components/entities_list/entities_list_skeleton.dart';
 import 'package:ion/app/features/feed/providers/replies_provider.c.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
+import 'package:ion/generated/assets.gen.dart';
 
 class ReplyList extends ConsumerWidget {
   const ReplyList({
@@ -33,6 +35,8 @@ class ReplyList extends ConsumerWidget {
         ...headers,
         if (entities == null)
           const EntitiesListSkeleton()
+        else if (entities.isEmpty)
+          const _EmptyState()
         else
           EntitiesList(
             entities: entities.toList(),
@@ -42,6 +46,21 @@ class ReplyList extends ConsumerWidget {
       ],
       builder: (context, slivers) => CustomScrollView(
         slivers: slivers,
+      ),
+    );
+  }
+}
+
+class _EmptyState extends StatelessWidget {
+  const _EmptyState();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverFillRemaining(
+      hasScrollBody: false,
+      child: EmptyList(
+        asset: Assets.svg.walletIconProfileEmptyprofile,
+        title: context.i18n.feed_replies_empty,
       ),
     );
   }
