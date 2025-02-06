d3ef4f4712a9dc6d665e1fb7f7fa037039c80f6c
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-02-06 10:26:01 +0100

    fix: add empty state for feed posts list

--- lib/app/features/feed/views/pages/feed_page/components/feed_posts/feed_posts.dart
+++ lib/app/features/feed/views/pages/feed_page/components/feed_posts/feed_posts.dart
@@ -2,11 +2,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/components/empty_list/empty_list.dart';
+import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/components/entities_list/entities_list.dart';
 import 'package:ion/app/features/components/entities_list/entities_list_skeleton.dart';
 import 'package:ion/app/features/feed/providers/feed_posts_data_source_provider.c.dart';
 import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
 import 'package:ion/app/features/ion_connect/providers/entities_paged_data_provider.c.dart';
+import 'package:ion/generated/assets.gen.dart';
 
 class FeedPosts extends ConsumerWidget {
   const FeedPosts({super.key});
@@ -22,12 +25,29 @@ class FeedPosts extends ConsumerWidget {
       entities = entitiesPagedData?.data.items?.toList();
     }
 
-    if (entities == null || entities.isEmpty) {
+    if (entities == null) {
       return const EntitiesListSkeleton();
+    } else if (entities.isEmpty) {
+      return const _EmptyState();
     }
 
     return EntitiesList(
       entities: entities,
+    );
+  }
+}
+
+class _EmptyState extends StatelessWidget {
+  const _EmptyState();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverFillRemaining(
+      hasScrollBody: false,
+      child: EmptyList(
+        asset: Assets.svg.walletIconProfileEmptyposts,
+        title: context.i18n.feed_posts_empty,
+      ),
     );
   }
 }
