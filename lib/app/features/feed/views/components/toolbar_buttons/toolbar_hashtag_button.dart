c12b18e022c2790dc63da7b744dba10e2a86251c
ice-hector <96414297+ice-hector@users.noreply.github.com>
2025-02-06 16:28:49 +0200

    fix: pr comment

--- lib/app/features/feed/views/components/toolbar_buttons/toolbar_hashtag_button.dart
+++ lib/app/features/feed/views/components/toolbar_buttons/toolbar_hashtag_button.dart
@@ -2,18 +2,17 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/text_editor/attributes.dart';
 import 'package:ion/app/components/text_editor/text_editor.dart';
 import 'package:ion/app/features/feed/views/components/actions_toolbar_button/actions_toolbar_button.dart';
 import 'package:ion/generated/assets.gen.dart';
 
-class ToolbarHashtagButton extends ConsumerWidget {
+class ToolbarHashtagButton extends StatelessWidget {
   const ToolbarHashtagButton({required this.textEditorController, super.key});
   final QuillController textEditorController;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return ActionsToolbarButton(
       icon: Assets.svg.iconArticleHashtag,
       onPressed: () {
