30d65ea3a240e292a43447c31caf5a589b12854a
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-02-07 18:13:42 +0300

    feat: add SoftDeletableEntity mixin

---
+++ lib/app/features/ion_connect/model/soft_deletable_entity.dart
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:ion/app/features/ion_connect/model/entity_published_at.c.dart';
+
+mixin SoftDeletableEntity {
+  SoftDeletableEntityData get data;
+  DateTime get createdAt;
+
+  bool get isDeleted => data.content.isEmpty && createdAt != data.publishedAt.value;
+}
+
+mixin SoftDeletableEntityData {
+  String get content;
+  EntityPublishedAt get publishedAt;
+}
