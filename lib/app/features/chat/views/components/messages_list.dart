e8e4f7beee0cf4b99713a84a629da6f7fdaa857a
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-02-09 16:14:53 +0200

    fix: hand message list with custom scroll view

--- lib/app/features/chat/views/components/messages_list.dart
+++ lib/app/features/chat/views/components/messages_list.dart
@@ -8,6 +8,7 @@ import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/chat/views/components/message_items/chat_date_header_text/chat_date_header_text.dart';
 import 'package:ion/app/features/chat/views/components/message_items/message_types/text_message/text_message.dart';
 import 'package:ion/app/features/ion_connect/ion_connect.dart';
+import 'package:ion/app/hooks/use_on_init.dart';
 
 class ChatMessagesList extends HookConsumerWidget {
   const ChatMessagesList(
@@ -23,54 +24,39 @@ class ChatMessagesList extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final scrollController = useScrollController();
 
-    useEffect(
-      () {
-        WidgetsBinding.instance.addPostFrameCallback((_) {
-          scrollController.animateTo(
-            scrollController.position.maxScrollExtent,
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.easeInOut,
-          );
-        });
-        return null;
-      },
-      [messages],
-    );
+    useOnInit(() {
+      scrollController.jumpTo(scrollController.position.maxScrollExtent);
+    });
 
     return ColoredBox(
       color: context.theme.appColors.primaryBackground,
       child: ScreenSideOffset.small(
-        child: ListView.builder(
+        child: CustomScrollView(
           controller: scrollController,
-          itemCount: messages.entries.length,
-          itemBuilder: (context, index) {
-            final date = messages.entries.toList()[index].key;
-            final messagesForDate = messages.entries.toList()[index].value;
-
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 12.0.s),
-                  child: ChatDateHeaderText(date: date),
+          slivers: [
+            for (final entry in messages.entries) ...[
+              SliverToBoxAdapter(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 12.0.s),
+                    child: ChatDateHeaderText(date: entry.key),
+                  ),
                 ),
-                ListView.separated(
-                  //TODO: use sliver list to improve performance
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: messagesForDate.length,
-                  separatorBuilder: (context, index) => SizedBox(height: 8.0.s),
-                  itemBuilder: (context, msgIndex) {
-                    final message = messagesForDate[msgIndex];
-
-                    return TextMessage(
-                      eventMessage: message,
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, msgIndex) {
+                    final message = entry.value[msgIndex];
+                    return Padding(
+                      padding: EdgeInsets.only(bottom: 8.0.s),
+                      child: TextMessage(eventMessage: message),
                     );
                   },
+                  childCount: entry.value.length,
                 ),
-              ],
-            );
-          },
+              ),
+            ],
+          ],
         ),
       ),
     );
