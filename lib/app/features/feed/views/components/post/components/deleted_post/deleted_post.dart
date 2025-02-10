2333a7e40bbea83493931f920aad5f8319cae499
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-02-10 14:25:38 +0300

    chore: improve deleted post markdown

--- lib/app/features/feed/views/components/post/components/deleted_post/deleted_post.dart
+++ lib/app/features/feed/views/components/post/components/deleted_post/deleted_post.dart
@@ -7,25 +7,28 @@ class DeletedPost extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12.0.s),
-        color: context.theme.appColors.tertararyBackground,
-        border: Border.all(color: context.theme.appColors.onTerararyFill, width: 1.0.s),
-      ),
-      child: Padding(
-        padding: EdgeInsets.only(top: 12.0.s, right: 16.0.s, bottom: 12.0.s, left: 12.0.s),
-        child: Row(
-          children: [
-            Assets.svg.iconFeedDeletedpost
-                .icon(size: 20.0.s, color: context.theme.appColors.quaternaryText),
-            SizedBox(width: 8.0.s),
-            Text(
-              context.i18n.feed_post_deleted,
-              style: context.theme.appTextThemes.caption
-                  .copyWith(color: context.theme.appColors.quaternaryText),
-            ),
-          ],
+    return Padding(
+      padding: EdgeInsets.only(top: 10.0.s, bottom: 22.0.s),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.0.s),
+          color: context.theme.appColors.tertararyBackground,
+          border: Border.all(color: context.theme.appColors.onTerararyFill, width: 1.0.s),
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(top: 12.0.s, right: 16.0.s, bottom: 12.0.s, left: 12.0.s),
+          child: Row(
+            children: [
+              Assets.svg.iconFeedDeletedpost
+                  .icon(size: 20.0.s, color: context.theme.appColors.quaternaryText),
+              SizedBox(width: 8.0.s),
+              Text(
+                context.i18n.feed_post_deleted,
+                style: context.theme.appTextThemes.caption
+                    .copyWith(color: context.theme.appColors.quaternaryText),
+              ),
+            ],
+          ),
         ),
       ),
     );
