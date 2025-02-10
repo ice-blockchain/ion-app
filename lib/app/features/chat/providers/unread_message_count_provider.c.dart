6bf9217e4c46ad65d3a5ee1d2793445a57f20dd8
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-02-10 17:50:17 +0200

    fix: do not use class notifier for unread message count riverpod

--- lib/app/features/chat/providers/unread_message_count_provider.c.dart
+++ lib/app/features/chat/providers/unread_message_count_provider.c.dart
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/chat/model/database/chat_database.c.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'unread_message_count_provider.c.g.dart';
 
 @riverpod
-class UnreadMessageCountProvider extends _$UnreadMessageCountProvider {
-  @override
-  Stream<int> build(String conversationId) {
-    return ref.watch(conversationMessageDaoProvider).getUnreadMessagesCount(conversationId);
-  }
+Stream<int> getUnreadMessagesCount(Ref ref, String conversationId) {
+  return ref.watch(conversationMessageDaoProvider).getUnreadMessagesCount(conversationId);
 }
