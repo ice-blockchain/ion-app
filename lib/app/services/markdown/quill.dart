002e4e3abbde663a75e9573b27d4f2c36c96c234
ice-hector <96414297+ice-hector@users.noreply.github.com>
2025-02-10 15:12:19 +0200

    feat: support for text-editor-single-image element

--- lib/app/services/markdown/quill.dart
+++ lib/app/services/markdown/quill.dart
@@ -1,14 +1,68 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter_quill/quill_delta.dart';
+import 'package:ion/app/components/text_editor/components/custom_blocks/text_editor_single_image_block/text_editor_single_image_block.dart';
 import 'package:markdown/markdown.dart' as md;
 import 'package:markdown_quill/markdown_quill.dart';
 
-final _deltaToMd = DeltaToMarkdown();
-final _mdToDelta = MarkdownToDelta(markdownDocument: md.Document(encodeHtml: false));
+final _mdDocument = md.Document(
+  encodeHtml: false,
+  extensionSet: md.ExtensionSet.gitHubFlavored,
+);
 
-//TODO: process text-editor-single-image, hashtags and other custom attr
-String deltaToMarkdown(Delta delta) => _deltaToMd.convert(delta);
+final _mdToDelta = MarkdownToDelta(
+  markdownDocument: _mdDocument,
+  customElementToEmbeddable: {
+    'img': (attrs) {
+      final imageUrl = attrs['src'] ?? '';
+      return TextEditorSingleImageEmbed(imageUrl);
+    },
+  },
+);
 
-//TODO: process text-editor-single-image, hashtags and other custom attr
-Delta markdownToDelta(String markdown) => _mdToDelta.convert(markdown);
+final _deltaToMd = DeltaToMarkdown(
+  customEmbedHandlers: {
+    'text-editor-single-image': (embed, out) {
+      final imageUrl = embed.value.data;
+      out.write('![image]($imageUrl)');
+    },
+  },
+);
+
+String deltaToMarkdown(Delta delta) {
+  final processedDelta = Delta();
+  for (final op in delta.toList()) {
+    if (op.key == 'insert' && (op.attributes?.containsKey('text-editor-single-image') ?? false)) {
+      processedDelta.insert({
+        'text-editor-single-image': op.attributes!['text-editor-single-image'],
+      });
+    } else {
+      processedDelta.insert(op.data, op.attributes);
+    }
+  }
+
+  return _deltaToMd.convert(processedDelta);
+}
+
+Delta markdownToDelta(String markdown) {
+  final delta = _mdToDelta.convert(markdown);
+
+  final processedDelta = Delta();
+  for (final op in delta.toList()) {
+    if (op.key == 'insert' && op.data is Map) {
+      final data = op.data! as Map;
+      if (data.containsKey('image')) {
+        final imageUrl = data['image'] as String;
+        processedDelta.insert({
+          'text-editor-single-image': imageUrl,
+        });
+      } else {
+        processedDelta.insert(op.data, op.attributes);
+      }
+    } else {
+      processedDelta.insert(op.data, op.attributes);
+    }
+  }
+
+  return processedDelta;
+}
