272c770d47971e0dacd6d68a51e7f1d1ba34a95b
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-02-10 13:36:23 +0100

    fix: recover user form validation (#676)
    ## Description
    This PR adds a similar form validation pattern to recovery keys setup
    page
    
    ## Additional Notes
    <!-- Add any extra context or relevant information here. -->
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <!-- Include screenshots to demonstrate any UI changes. -->
    <!-- <img width="180" alt="image" src="image_url_here"> -->

--- lib/app/features/auth/views/pages/recover_user_page/components/recovery_creds_step.dart
+++ lib/app/features/auth/views/pages/recover_user_page/components/recovery_creds_step.dart
@@ -39,6 +39,7 @@ class RecoveryCredsStep extends HookConsumerWidget {
     final controllers = {
       for (final key in RecoveryKeyProperty.values) key: useTextEditingController(),
     };
+    final validationMode = useState(AutovalidateMode.disabled);
 
     return SheetContent(
       topPadding: 0,
@@ -72,6 +73,7 @@ class RecoveryCredsStep extends HookConsumerWidget {
                             textInputAction: key == RecoveryKeyProperty.recoveryCode
                                 ? TextInputAction.done
                                 : TextInputAction.next,
+                            autoValidateMode: validationMode.value,
                           ),
                         ),
                       ),
@@ -89,6 +91,7 @@ class RecoveryCredsStep extends HookConsumerWidget {
                               controllers[RecoveryKeyProperty.recoveryCode]!.text,
                             );
                           } else {
+                            validationMode.value = AutovalidateMode.onUserInteraction;
                             showSimpleBottomSheet<void>(
                               context: context,
                               child: const RecoverInvalidCredentialsErrorAlert(),
