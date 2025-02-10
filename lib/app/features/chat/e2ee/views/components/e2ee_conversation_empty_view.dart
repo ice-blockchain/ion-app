4a8fb58d3de1b96f59aba72d4470ead2da87d34d
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-02-10 18:48:10 +0200

    feat: add license

--- lib/app/features/chat/e2ee/views/components/e2ee_conversation_empty_view.dart
+++ lib/app/features/chat/e2ee/views/components/e2ee_conversation_empty_view.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
