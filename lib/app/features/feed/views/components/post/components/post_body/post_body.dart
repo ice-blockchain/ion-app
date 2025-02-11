7ff0e82d0fc18a88ffff18b50db47678ca806826
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-02-11 17:19:18 +0300

    feat: handle firstLinkOperation

--- lib/app/features/feed/views/components/post/components/post_body/post_body.dart
+++ lib/app/features/feed/views/components/post/components/post_body/post_body.dart
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/text_editor/text_editor_preview.dart';
+import 'package:ion/app/components/text_editor/utils/is_attributed_operation.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.c.dart';
 import 'package:ion/app/features/feed/data/models/entities/post_data.c.dart';
 import 'package:ion/app/features/feed/views/components/post/components/post_body/components/post_media/post_media.dart';
+import 'package:ion/app/features/feed/views/components/url_preview_content/url_preview_content.dart';
 import 'package:ion/app/features/ion_connect/model/entity_data_with_media_content.dart';
 import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
 import 'package:ion/app/features/ion_connect/views/hooks/use_content_without_media.dart';
@@ -34,6 +39,12 @@ class PostBody extends HookConsumerWidget {
     }
 
     final content = useContentWithoutMedia(data: postData);
+    final firstLinkOperation = useMemoized(
+      () => content.operations.firstWhereOrNull(
+        (operation) => isAttributedOperation(operation, attribute: Attribute.link),
+      ),
+      [content],
+    );
 
     final postMedia = postData.media.values.toList();
 
@@ -46,12 +57,11 @@ class PostBody extends HookConsumerWidget {
             padding: EdgeInsets.only(top: 10.0.s),
             child: PostMedia(media: postMedia),
           ),
-        //TODO::impl
-        // if (postData.firstUrl != null)
-        //   Padding(
-        //     padding: EdgeInsets.only(top: 10.0.s),
-        //     child: UrlPreviewContent(url: postData.firstUrl!),
-        //   ),
+        if (firstLinkOperation != null)
+          Padding(
+            padding: EdgeInsets.only(top: 10.0.s),
+            child: UrlPreviewContent(url: firstLinkOperation.value as String),
+          ),
       ],
     );
   }
