4d78c450c9fdd650ca95182499d26df1824c95f8
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-02-12 01:56:55 +0200

    feat: support android for banuba 1.40

--- android/app/src/main/kotlin/io/ion/app/MainActivity.kt
+++ android/app/src/main/kotlin/io/ion/app/MainActivity.kt
@@ -1,18 +1,37 @@
 package io.ion.app
 
+
+
+
+import android.view.KeyEvent
+
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.KeyEvent
+import android.util.Log
+import androidx.core.content.FileProvider
+import androidx.core.os.bundleOf
+import com.banuba.sdk.cameraui.data.PipConfig
+import com.banuba.sdk.core.license.BanubaVideoEditor
+import com.banuba.sdk.core.license.LicenseStateCallback
+import com.banuba.sdk.core.EditorUtilityManager
+import com.banuba.sdk.export.data.ExportResult
+import com.banuba.sdk.export.utils.EXTRA_EXPORTED_SUCCESS
 import com.banuba.sdk.pe.PhotoCreationActivity
-import com.banuba.sdk.pe.BanubaPhotoEditor
 import com.banuba.sdk.pe.data.PhotoEditorConfig
-import dev.fluttercommunity.shake_gesture_android.ShakeGesturePlugin
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.embedding.engine.FlutterEngine
+import com.banuba.sdk.pe.BanubaPhotoEditor
+import com.banuba.sdk.ve.flow.VideoCreationActivity
+import com.banuba.sdk.ve.ext.VideoEditorUtils.getKoin
+import org.koin.core.context.stopKoin
+import org.koin.core.error.InstanceCreationException
+import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 import java.io.File
+
+import dev.fluttercommunity.shake_gesture_android.ShakeGesturePlugin
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity : FlutterFragmentActivity() {
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
@@ -31,16 +50,36 @@ class MainActivity : FlutterFragmentActivity() {
         private const val PHOTO_EDITOR_REQUEST_CODE = 8888
         private const val METHOD_INIT_PHOTO_EDITOR = "initPhotoEditor"
         private const val METHOD_START_PHOTO_EDITOR = "startPhotoEditor"
-
         private const val ARG_EXPORTED_PHOTO_FILE = "argExportedPhotoFilePath"
+
+        // For Video Editor
+        const val CONFIG_ENABLE_CUSTOM_AUDIO_BROWSER = false
+        private const val VIDEO_EDITOR_REQUEST_CODE = 7788
+        private const val METHOD_INIT_VIDEO_EDITOR = "initVideoEditor"
+        private const val METHOD_START_VIDEO_EDITOR = "startVideoEditor"
+        private const val METHOD_START_VIDEO_EDITOR_PIP = "startVideoEditorPIP"
+        private const val METHOD_START_VIDEO_EDITOR_TRIMMER = "startVideoEditorTrimmer"
+        private const val METHOD_RELEASE_VIDEO_EDITOR = "releaseVideoEditor"
+        private const val METHOD_DEMO_PLAY_EXPORTED_VIDEO = "playExportedVideo"
+        private const val ARG_EXPORTED_VIDEO_FILE = "argExportedVideoFilePath"
+        private const val ARG_EXPORTED_VIDEO_COVER = "argExportedVideoCoverPreviewPath"
 
         // Errors code
         private const val ERR_CODE_SDK_NOT_INITIALIZED = "ERR_SDK_NOT_INITIALIZED"
         private const val ERR_CODE_SDK_LICENSE_REVOKED = "ERR_SDK_LICENSE_REVOKED"
+
+
     }
 
     private var exportResult: MethodChannel.Result? = null
     private var photoEditorSDK: BanubaPhotoEditor? = null
+    private var photoEditorSDK: BanubaPhotoEditor? = null
+    private var videoEditorModule: VideoEditorModule? = null
+
+    // Bundle for enabling Editor V2
+    private val extras = bundleOf(
+        "EXTRA_USE_EDITOR_V2" to true
+    )
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
@@ -55,6 +94,68 @@ class MainActivity : FlutterFragmentActivity() {
             exportResult = result
 
             when (call.method) {
+                 METHOD_INIT_VIDEO_EDITOR -> {
+                    val licenseToken = call.arguments as String
+                    videoEditorSDK = BanubaVideoEditor.initialize(licenseToken)
+
+                    if (videoEditorSDK == null) {
+                        // The SDK token is incorrect - empty or truncated
+                        result.error(ERR_CODE_SDK_NOT_INITIALIZED, "", null)
+                    } else {
+                        if (videoEditorModule == null) {
+                            // Initialize video editor sdk dependencies
+                            videoEditorModule = VideoEditorModule().apply {
+                                initialize(application)
+                            }
+                        }
+                        result.success(null)
+                    }
+                }
+
+                METHOD_START_VIDEO_EDITOR_TRIMMER -> {
+                    val videoFilePath = call.arguments as? String
+                    val trimmerVideoUri = videoFilePath?.let { Uri.fromFile(File(it)) }
+                    if (trimmerVideoUri == null) {
+                        Log.w(
+                            TAG,
+                            "Cannot start video editor in Trimmer mode: missing or invalid passed video = $videoFilePath"
+                        )
+                        exportResult?.error("ERR_START_TRIMMER_MISSING_VIDEO", "", null)
+                    } else {
+                        checkSdkLicenseVideoEditor(
+                            callback = { isValid ->
+                                if (isValid) {
+                                    // ✅ The license is active
+                                    startVideoEditorModeTrimmer(trimmerVideoUri)
+                                } else {
+                                    // ❌ Use of SDK is restricted: the license is revoked or expired
+                                    result.error(ERR_CODE_SDK_LICENSE_REVOKED, "", null)
+                                }
+                            },
+                            onError = { result.error(ERR_CODE_SDK_NOT_INITIALIZED, "", null) }
+                        )
+                    }
+                }
+
+                METHOD_RELEASE_VIDEO_EDITOR -> {
+                    Log.d(TAG, "Release Video Editor SDK")
+                    if (videoEditorModule != null) {
+                        val utilityManager = try {
+                            // EditorUtilityManager is NULL when the token is expired or revoked.
+                            // This dependency is not explicitly created in DI.
+                            getKoin().getOrNull<EditorUtilityManager>()
+                        } catch (e: InstanceCreationException) {
+                            Log.w(TAG, "EditorUtilityManager was not initialized!", e)
+                            result.error("EditorUtilityManager was not initialized!", "", null)
+                            null
+                        }
+                        utilityManager?.release()
+                        stopKoin()
+                        videoEditorModule = null
+                    }
+                    videoEditorSDK = null
+                    result.success(null)
+                }
                 METHOD_INIT_PHOTO_EDITOR -> {
                     val licenseToken = call.arguments as String
                     photoEditorSDK = BanubaPhotoEditor.initialize(licenseToken)
