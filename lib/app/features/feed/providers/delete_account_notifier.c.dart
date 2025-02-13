149e257b7cbbb132476994c573a2912695850f4b
Ice Hades <119406114+ice-hades@users.noreply.github.com>
2025-02-13 12:48:08 +0400

    fix: fixed the sing in after the account deletion flow

--- lib/app/features/feed/providers/delete_account_notifier.c.dart
+++ lib/app/features/feed/providers/delete_account_notifier.c.dart
@@ -5,11 +5,14 @@ import 'dart:convert';
 import 'package:ion/app/exceptions/exceptions.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
 import 'package:ion/app/features/core/providers/main_wallet_provider.c.dart';
+import 'package:ion/app/features/core/providers/wallets_provider.c.dart';
 import 'package:ion/app/features/ion_connect/model/deletion_request.c.dart';
-import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_cache.c.dart';
+import 'package:ion/app/features/ion_connect/providers/ion_connect_event_signer_provider.c.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_notifier.c.dart';
-import 'package:ion/app/features/user/model/user_metadata.c.dart';
+import 'package:ion/app/features/user/providers/current_user_identity_provider.c.dart';
+import 'package:ion/app/features/user/providers/user_delegation_provider.c.dart';
+import 'package:ion/app/features/user/providers/user_metadata_provider.c.dart';
 import 'package:ion/app/services/ion_identity/ion_identity_provider.c.dart';
 import 'package:ion/app/services/storage/local_storage.c.dart';
 import 'package:ion_identity_client/ion_identity.dart';
@@ -45,26 +48,27 @@ class DeleteAccountNotifier extends _$DeleteAccountNotifier {
         onVerifyIdentity: onVerifyIdentity,
       );
 
+      // clean ion connect data
+      await ref.read(ionConnectNotifierProvider.notifier).sendEvent(event, cache: false);
+
       // clean ion identity data
       final ionIdentity = await ref.read(ionIdentityProvider.future);
       await ionIdentity(username: currentIdentityKeyName).auth.deleteUser(
             base64Kind5Event: base64.encode(utf8.encode(json.encode(event.toJson().last))),
           );
 
-      // clean ion connect data
-      ref.read(ionConnectCacheProvider.notifier).remove(
-            CacheableEntity.cacheKeyBuilder(
-              eventReference: ReplaceableEventReference(
-                pubkey: mainWallet.signingKey.publicKey,
-                kind: UserMetadataEntity.kind,
-              ),
-            ),
-          );
-      await ref.read(ionConnectNotifierProvider.notifier).sendEvent(event, cache: false);
-
       // clean local storage data
       final localStorage = ref.read(localStorageProvider);
       await localStorage.clear();
+
+      // // invalidate providers
+      ref
+        ..invalidate(currentUserIdentityProvider)
+        ..invalidate(ionConnectCacheProvider)
+        ..invalidate(currentUserIonConnectEventSignerProvider)
+        ..invalidate(currentUserDelegationProvider)
+        ..invalidate(walletsNotifierProvider)
+        ..invalidate(userMetadataProvider);
     });
   }
 }
