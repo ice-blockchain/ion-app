f6807d9c29ab61ed5e04a977c89c9b8d4ec3c466
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-02-17 18:33:28 +0300

    chore: reset user relays on logout

--- lib/app/features/user/providers/user_relays_manager.c.dart
+++ lib/app/features/user/providers/user_relays_manager.c.dart
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/exceptions/exceptions.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
 import 'package:ion/app/features/ion_connect/ion_connect.dart';
 import 'package:ion/app/features/ion_connect/model/action_source.dart';
@@ -16,6 +17,10 @@ part 'user_relays_manager.c.g.dart';
 
 @riverpod
 Future<UserRelaysEntity?> userRelay(Ref ref, String pubkey) async {
+  final currentUser = ref.watch(currentIdentityKeyNameSelectorProvider);
+  if (currentUser == null) {
+    throw const CurrentUserNotFoundException();
+  }
   final relays = await ref.watch(userRelaysManagerProvider.notifier).fetch([pubkey]);
   return relays.elementAtOrNull(0);
 }
