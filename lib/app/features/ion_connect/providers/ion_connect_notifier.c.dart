894a902154e2e5af41cc691355d3922f5c8ed613
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-02-18 10:56:02 +0200

    bugfix: Add expiration to e2e 1063

--- lib/app/features/ion_connect/providers/ion_connect_notifier.c.dart
+++ lib/app/features/ion_connect/providers/ion_connect_notifier.c.dart
@@ -4,10 +4,13 @@ import 'dart:async';
 
 import 'package:ion/app/exceptions/exceptions.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
+import 'package:ion/app/features/core/providers/env_provider.c.dart';
 import 'package:ion/app/features/core/providers/main_wallet_provider.c.dart';
 import 'package:ion/app/features/ion_connect/ion_connect.dart' as ion;
 import 'package:ion/app/features/ion_connect/ion_connect.dart' hide requestEvents;
 import 'package:ion/app/features/ion_connect/model/action_source.dart';
+import 'package:ion/app/features/ion_connect/model/auth_event.c.dart';
+import 'package:ion/app/features/ion_connect/model/entity_expiration.c.dart';
 import 'package:ion/app/features/ion_connect/model/event_serializable.dart';
 import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_cache.c.dart';
@@ -199,10 +202,18 @@ class IonConnectNotifier extends _$IonConnectNotifier {
       throw EventSignerNotFoundException();
     }
 
+    final expirationTag = EntityExpiration(
+      value: DateTime.now().add(
+        Duration(
+            hours: ref.read(envProvider.notifier).get<int>(EnvVariable.GIFT_WRAP_EXPIRATION_HOURS)),
+      ),
+    ).toTag();
+
     return entityData.toEventMessage(
       eventSigner,
       tags: [
         if (includeMasterPubkey && !encrypted) ['b', mainWallet.signingKey.publicKey],
+        if (encrypted) expirationTag,
       ],
     );
   }
