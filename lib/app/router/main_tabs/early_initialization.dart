d0af670d8d96707839952f84e927a29bac2a9d9d
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-02-18 18:57:28 +0200

    chore: license

--- lib/app/router/main_tabs/early_initialization.dart
+++ lib/app/router/main_tabs/early_initialization.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/chat/e2ee/providers/e2ee_messages_subscriber.c.dart';
