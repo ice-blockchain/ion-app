21bbda5e2b6819e5676f339a093c839594cbad8a
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-02-20 13:38:06 +0300

    feat: add userCategoriesProvider

---
+++ lib/app/features/user/model/user_category.c.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_category.c.g.dart';
+
+@JsonSerializable()
+class UserCategory {
+  UserCategory({
+    required this.key,
+    required this.name,
+  });
+
+  factory UserCategory.fromJson(Map<String, dynamic> json) => _$UserCategoryFromJson(json);
+
+  final String key;
+
+  final String name;
+}
