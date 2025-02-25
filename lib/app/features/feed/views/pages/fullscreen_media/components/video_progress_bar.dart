80bb4d854c95bb8d6b5508a7d38a07163fa01c34
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-02-25 10:01:54 +0200

    chore: add license

--- lib/app/features/feed/views/pages/fullscreen_media/components/video_progress_bar.dart
+++ lib/app/features/feed/views/pages/fullscreen_media/components/video_progress_bar.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:flutter/material.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:video_player/video_player.dart';
