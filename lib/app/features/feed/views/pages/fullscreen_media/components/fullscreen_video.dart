6a26604f9f3ccb13486cbc0556e1b07bbce8aca9
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-02-25 12:22:43 +0200

    refactor: remove duplicated video progress bar component

--- lib/app/features/feed/views/pages/fullscreen_media/components/fullscreen_video.dart
+++ lib/app/features/feed/views/pages/fullscreen_media/components/fullscreen_video.dart
@@ -8,8 +8,9 @@ import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/core/providers/video_player_provider.c.dart';
 import 'package:ion/app/features/feed/views/pages/fullscreen_media/components/video_gradient_overlay.dart';
 import 'package:ion/app/features/feed/views/pages/fullscreen_media/components/video_post_info.dart';
-import 'package:ion/app/features/feed/views/pages/fullscreen_media/components/video_progress_bar.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
+import 'package:ion/app/features/video/views/components/video_progress.dart';
+import 'package:ion/app/features/video/views/components/video_slider.dart';
 import 'package:video_player/video_player.dart';
 
 class FullscreenVideo extends HookConsumerWidget {
@@ -71,7 +72,22 @@ class FullscreenVideo extends HookConsumerWidget {
             ],
           ),
         ),
-        VideoProgressBar(controller: controller),
+        VideoProgress(
+          controller: controller,
+          builder: (context, position, duration) => VideoSlider(
+            position: position,
+            duration: duration,
+            onChangeStart: (_) => controller.pause(),
+            onChangeEnd: (_) => controller.play(),
+            onChanged: (value) {
+              if (controller.value.isInitialized) {
+                controller.seekTo(
+                  Duration(milliseconds: value.toInt()),
+                );
+              }
+            },
+          ),
+        ),
         SizedBox(height: 20.0.s),
         ColoredBox(
           color: context.theme.appColors.primaryText,
