359d7877a0831d3567596dd4cc3c65463238cc87
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-02-27 12:43:28 +0300

    fix: routing collision

--- lib/app/router/chat_routes.dart
+++ lib/app/router/chat_routes.dart
@@ -5,8 +5,8 @@ part of 'app_routes.c.dart';
 class ChatRoutes {
   static const routes = <TypedRoute<RouteData>>[
     TypedGoRoute<AppTestRoute>(path: 'app-test'),
-    TypedGoRoute<ChatSimpleSearchRoute>(path: 'simple-search'),
-    TypedGoRoute<ChatAdvancedSearchRoute>(path: 'feed-advanced-search'),
+    TypedGoRoute<ChatSimpleSearchRoute>(path: 'chat-simple-search'),
+    TypedGoRoute<ChatAdvancedSearchRoute>(path: 'chat-advanced-search'),
     TypedGoRoute<ArchivedChatsMainRoute>(path: 'archived-chats'),
     TypedShellRoute<ModalShellRouteData>(
       routes: [
