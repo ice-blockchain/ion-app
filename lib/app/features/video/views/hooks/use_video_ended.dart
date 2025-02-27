ba7939dbd73d25d88376525b5b7cf6a5c0fbebde
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-02-27 14:18:50 +0200

    feat: replace video_player with cached_video_player_plus

--- lib/app/features/video/views/hooks/use_video_ended.dart
+++ lib/app/features/video/views/hooks/use_video_ended.dart
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:cached_video_player_plus/cached_video_player_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:video_player/video_player.dart';
 
 void useVideoEnded({
-  required VideoPlayerController playerController,
+  required CachedVideoPlayerPlusController playerController,
   VoidCallback? onVideoEnded,
 }) {
   final hasVideoEnded = useState(false);
