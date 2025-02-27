9539feedacb1ef7b48cde402ce53ce696c215ef8
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-02-27 10:29:09 +0200

    fix: networks initialized

--- lib/app/features/wallets/domain/networks/networks_initializer.c.dart
+++ lib/app/features/wallets/domain/networks/networks_initializer.c.dart
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/wallets/data/repository/networks_repository.c.dart';
+import 'package:ion/app/features/wallets/model/network_data.c.dart';
+import 'package:ion/generated/assets.gen.dart';
+import 'package:ion_identity_client/ion_identity.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'networks_initializer.c.g.dart';
@@ -24,17 +30,14 @@ class NetworksInitializer {
   }
 
   Future<void> _loadNetworks() async {
-    // TODO: Not implemented, cos need to fix thing with assets
+    final networksJson = (await rootBundle.loadString(Assets.ionIdentity.networks).then(jsonDecode))
+        as List<dynamic>;
 
-    // load networks from assets file
-    // final networksJson = (await rootBundle.loadString(Assets.ionIdentity.networks).then(jsonDecode))
-    //     as List<dynamic>;
+    final networks = networksJson
+        .map((json) => Network.fromJson(json as Map<String, dynamic>))
+        .map(NetworkData.fromDTO)
+        .toList();
 
-    // final networks = networksJson
-    //     .map((json) => ion_identity.Network.fromJson(json as Map<String, dynamic>))
-    //     .map(NetworkData.fromDTO)
-    //     .toList();
-
-    // await _networksRepository.save(networks);
+    await _networksRepository.save(networks);
   }
 }
