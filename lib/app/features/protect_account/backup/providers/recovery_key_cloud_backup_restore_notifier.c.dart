2c8b48ba5af5c1319da66cf19dcec1b96f5c225c
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-02-28 15:46:00 +0100

    feat: add icloud recovery keys storage

---
+++ lib/app/features/protect_account/backup/providers/recovery_key_cloud_backup_restore_notifier.c.dart
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'dart:convert';
+
+import 'package:ion/app/services/cloud_storage/cloud_storage_service.c.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'recovery_key_cloud_backup_restore_notifier.c.g.dart';
+
+@riverpod
+class RecoveryKeyCloudBackupRestoreNotifier extends _$RecoveryKeyCloudBackupRestoreNotifier {
+  @override
+  FutureOr<Map<String, dynamic>> build() async => {};
+
+  Future<void> restore(
+    String identityKeyName,
+  ) async {
+    state = const AsyncValue.loading();
+
+    state = await AsyncValue.guard(() async {
+      final cloudStorage = ref.read(cloudStorageProvider);
+      final fileContent = await cloudStorage.downloadFile('ion/$identityKeyName.json');
+      // TODO: Decrypt the file first
+      final fileMap = jsonDecode(fileContent) as Map<String, dynamic>;
+      return fileMap;
+    });
+  }
+}
