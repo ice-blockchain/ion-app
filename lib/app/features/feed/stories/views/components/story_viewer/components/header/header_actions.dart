052d6a73d07cb0c68f8fb735fd4d217d72d24ff9
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-03-03 10:12:56 +0200

    fix(stories): conditionally show the context menu for non-owner stories

--- lib/app/features/feed/stories/views/components/story_viewer/components/header/header_actions.dart
+++ lib/app/features/feed/stories/views/components/story_viewer/components/header/header_actions.dart
@@ -2,29 +2,35 @@
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/shadow/svg_shadow.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
 import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.c.dart';
 import 'package:ion/app/features/feed/stories/views/components/story_viewer/components/header/story_context_menu.dart';
 import 'package:ion/generated/assets.gen.dart';
 
-class HeaderActions extends StatelessWidget {
+class HeaderActions extends ConsumerWidget {
   const HeaderActions({required this.post, super.key});
 
   final ModifiablePostEntity post;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentUserPubkey = ref.watch(currentPubkeySelectorProvider);
+    final isCurrentUser = currentUserPubkey == post.masterPubkey;
+
     return Row(
       children: [
-        StoryContextMenu(
-          pubkey: post.masterPubkey,
-          child: SvgShadow(
-            child: Assets.svg.iconMoreStories.icon(
-              color: context.theme.appColors.onPrimaryAccent,
+        if (!isCurrentUser)
+          StoryContextMenu(
+            pubkey: post.masterPubkey,
+            child: SvgShadow(
+              child: Assets.svg.iconMoreStories.icon(
+                color: context.theme.appColors.onPrimaryAccent,
+              ),
             ),
           ),
-        ),
         SizedBox(width: 16.0.s),
         GestureDetector(
           child: SvgShadow(
