d1c8a454d6c27dd7df39fb2dcf5149bf78f8883b
ice-hades <119406114+ice-hades@users.noreply.github.com>
2025-03-03 12:12:34 +0400

    fix: 2fa action success pop up changes (#789)
    ## Description
    Changed 2fa actions success pop up to showModalBottomSheet to display on
    top of previous flow step
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore

--- lib/app/features/protect_account/components/two_fa_success_step.dart
+++ lib/app/features/protect_account/components/two_fa_success_step.dart
@@ -6,51 +6,50 @@ import 'package:ion/app/components/card/info_card.dart';
 import 'package:ion/app/components/screen_offset/screen_bottom_offset.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/protect_account/model/two_fa_action_type.dart';
 import 'package:ion/app/router/app_routes.c.dart';
-import 'package:ion/app/router/components/sheet_content/sheet_content.dart';
 
 class TwoFaSuccessStep extends StatelessWidget {
   const TwoFaSuccessStep({
-    required this.iconAsset,
-    required this.description,
+    required this.actionType,
     super.key,
   });
 
-  final String iconAsset;
-  final String description;
+  final TwoFaActionType actionType;
 
   @override
   Widget build(BuildContext context) {
     final locale = context.i18n;
 
-    return SheetContent(
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 45.0.s,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 45.0.s,
+        ),
+        ScreenSideOffset.medium(
+          child: InfoCard(
+            iconAsset: actionType.successIconAsset,
+            title: locale.common_congratulations,
+            description: actionType.getSuccessDesc(context),
           ),
-          ScreenSideOffset.medium(
-            child: InfoCard(
-              iconAsset: iconAsset,
-              title: locale.common_congratulations,
-              description: description,
-            ),
+        ),
+        SizedBox(
+          height: 22.0.s,
+        ),
+        ScreenSideOffset.large(
+          child: Button(
+            mainAxisSize: MainAxisSize.max,
+            label: Text(locale.button_back_to_security),
+            onPressed: () {
+              Navigator.of(context).pop();
+              SecureAccountOptionsRoute().replace(context);
+            },
           ),
-          SizedBox(
-            height: 22.0.s,
-          ),
-          ScreenSideOffset.large(
-            child: Button(
-              mainAxisSize: MainAxisSize.max,
-              label: Text(locale.button_back_to_security),
-              onPressed: () => SecureAccountOptionsRoute().replace(context),
-            ),
-          ),
-          ScreenBottomOffset(margin: 16.0.s),
-        ],
-      ),
+        ),
+        ScreenBottomOffset(margin: 16.0.s),
+      ],
     );
   }
 }
