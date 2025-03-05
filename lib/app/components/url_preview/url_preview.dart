8c82e4cc07afb238a2f403214acc0e6509ac8884
ice-hector <96414297+ice-hector@users.noreply.github.com>
2025-03-05 18:36:38 +0200

    fix: pr comment

--- lib/app/components/url_preview/url_preview.dart
+++ lib/app/components/url_preview/url_preview.dart
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/url_preview/providers/url_metadata_provider.c.dart';
 import 'package:ogp_data_extract/ogp_data_extract.dart';
@@ -23,28 +22,8 @@ class UrlPreview extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final metadataAsync = ref.watch(urlMetadataProvider(url));
-    final isLoading = useState(false);
 
-    useEffect(
-      () {
-        if (!metadataAsync.isLoading) return null;
-        isLoading.value = true;
-        return null;
-      },
-      [metadataAsync.isLoading],
-    );
-
-    useEffect(
-      () {
-        if (metadataAsync.hasValue) {
-          isLoading.value = false;
-        }
-        return null;
-      },
-      [metadataAsync.hasValue],
-    );
-
-    if (isLoading.value || metadataAsync.isLoading) {
+    if (metadataAsync.isLoading || metadataAsync.hasError) {
       return const SizedBox.shrink();
     }
 
