088249bcf8f64b88f80d31bfcdb7992f0fd167e3
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-03-06 12:42:18 +0200

    fix: add CounterItemsFooter to SingleMediaView for image media

--- lib/app/features/feed/views/pages/fullscreen_media/components/single_media_view.dart
+++ lib/app/features/feed/views/pages/fullscreen_media/components/single_media_view.dart
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:ion/app/components/counter_items_footer/counter_items_footer.dart';
+import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/core/model/media_type.dart';
 import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.c.dart';
 import 'package:ion/app/features/feed/views/pages/fullscreen_media/components/fullscreen_image.dart';
@@ -22,15 +24,42 @@ class SingleMediaView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final primaryTextColor = context.theme.appColors.primaryText;
+    final onPrimaryAccentColor = context.theme.appColors.onPrimaryAccent;
+    final sizedBoxHeight = 20.0.s;
+    final horizontalPadding = 16.0.s;
+
     return media.mediaType == MediaType.video
         ? VideoPage(
             video: post,
             eventReference: eventReference,
             looping: true,
           )
-        : FullscreenImage(
-            imageUrl: media.url,
-            eventReference: eventReference,
+        : Column(
+            children: [
+              Expanded(
+                child: FullscreenImage(
+                  imageUrl: media.url,
+                  eventReference: eventReference,
+                ),
+              ),
+              SizedBox(height: sizedBoxHeight),
+              ColoredBox(
+                color: primaryTextColor,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+                  child: SafeArea(
+                    top: false,
+                    child: CounterItemsFooter(
+                      eventReference: eventReference,
+                      color: onPrimaryAccentColor,
+                      bottomPadding: 0,
+                      topPadding: 0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           );
   }
 }
