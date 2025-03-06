4a77a76e0e77e0c0a9a642fa792298b8e984bf41
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-03-06 15:28:33 +0200

    chore: support remote svg images in the NetworkIconWidget.dart

--- lib/app/features/wallets/views/components/network_icon_widget.dart
+++ lib/app/features/wallets/views/components/network_icon_widget.dart
@@ -2,6 +2,7 @@
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ion/app/extensions/extensions.dart';
 
 class NetworkIconWidget extends StatelessWidget {
@@ -14,23 +15,31 @@ class NetworkIconWidget extends StatelessWidget {
   final String imageUrl;
   final double? size;
 
+  bool get isSvg => imageUrl.toLowerCase().endsWith('.svg');
+
   @override
   Widget build(BuildContext context) {
     final iconSize = size ?? 24.0.s;
 
-    return CachedNetworkImage(
-      imageUrl: imageUrl,
-      width: iconSize,
-      height: iconSize,
-      imageBuilder: (context, imageProvider) => Container(
-        width: iconSize,
-        height: iconSize,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: imageProvider,
-          ),
-        ),
-      ),
-    );
+    return isSvg
+        ? SvgPicture.network(
+            imageUrl,
+            width: iconSize,
+            height: iconSize,
+          )
+        : CachedNetworkImage(
+            imageUrl: imageUrl,
+            width: iconSize,
+            height: iconSize,
+            imageBuilder: (context, imageProvider) => Container(
+              width: iconSize,
+              height: iconSize,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: imageProvider,
+                ),
+              ),
+            ),
+          );
   }
 }
