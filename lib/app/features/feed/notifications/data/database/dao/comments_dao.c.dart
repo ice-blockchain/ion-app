ccba8d156ce32c562be9ba8ad196d30fabcda9bc
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-03-10 16:10:33 +0300

    chore: add insert entity dao method

--- lib/app/features/feed/notifications/data/database/dao/comments_dao.c.dart
+++ lib/app/features/feed/notifications/data/database/dao/comments_dao.c.dart
@@ -4,6 +4,7 @@ import 'package:drift/drift.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/feed/notifications/data/database/notifications_database.c.dart';
 import 'package:ion/app/features/feed/notifications/data/database/tables/comments_table.c.dart';
+import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'comments_dao.c.g.dart';
@@ -21,5 +22,12 @@ class CommentsDao extends DatabaseAccessor<NotificationsDatabase> with _$Comment
 
   Future<List<Comment>> getAll() {
     return select(commentsTable).get();
+  }
+
+  Future<void> insert(IonConnectEntity entity) async {
+    await into(db.commentsTable).insert(
+      Comment(eventReference: entity.toEventReference(), createdAt: entity.createdAt),
+      mode: InsertMode.insertOrReplace,
+    );
   }
 }
