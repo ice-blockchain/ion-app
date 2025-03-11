d7aa8e8301debff9ba8dd899b5ef0c2d139d7b10
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-03-11 15:35:48 +0300

    chore: process reposts in notifications

--- lib/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_item.dart
+++ lib/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_item.dart
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
+import 'package:ion/app/components/skeleton/skeleton.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/feed/data/models/generic_repost.c.dart';
 import 'package:ion/app/features/feed/notifications/data/model/ion_connect_notification.c.dart';
 import 'package:ion/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_info.dart';
 import 'package:ion/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_type_icon.dart';
 import 'package:ion/app/features/feed/notifications/views/notifications_history_page/components/notification_item/user_avatar.dart';
 import 'package:ion/app/features/feed/views/components/post/post.dart';
+import 'package:ion/app/features/feed/views/components/post/post_skeleton.dart';
+import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
+import 'package:ion/app/features/ion_connect/providers/ion_connect_entity_provider.c.dart';
 import 'package:ion/app/router/app_routes.c.dart';
 
 class NotificationItem extends StatelessWidget {
@@ -62,16 +68,36 @@ class NotificationItem extends StatelessWidget {
                 PostDetailsRoute(eventReference: notification.eventReference!.encode())
                     .push<void>(context);
               },
-              child: Post(
-                eventReference: notification.eventReference!,
-                header: const SizedBox.shrink(),
-                footer: const SizedBox.shrink(),
-                topOffset: 6.0.s,
-                headerOffset: 0,
-              ),
+              child: _NotificationItemEvent(eventReference: notification.eventReference!),
             ),
         ],
       ),
+    );
+  }
+}
+
+class _NotificationItemEvent extends ConsumerWidget {
+  const _NotificationItemEvent({required this.eventReference});
+
+  final EventReference eventReference;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final entity = ref.watch(ionConnectEntityProvider(eventReference: eventReference)).valueOrNull;
+
+    if (entity == null) {
+      return ScreenSideOffset.small(child: const Skeleton(child: PostSkeleton()));
+    }
+
+    return Post(
+      eventReference: switch (entity) {
+        GenericRepostEntity() => entity.data.eventReference,
+        _ => eventReference,
+      },
+      header: const SizedBox.shrink(),
+      footer: const SizedBox.shrink(),
+      topOffset: 6.0.s,
+      headerOffset: 0,
     );
   }
 }
