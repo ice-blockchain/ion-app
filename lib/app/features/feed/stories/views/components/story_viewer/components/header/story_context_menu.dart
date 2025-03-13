8287b2e605feff53ae5d117046ce4d37cacc0721
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-03-13 13:45:45 +0200

    feat: add DeleteStoryModal for confirming story deletion

--- lib/app/features/feed/stories/views/components/story_viewer/components/header/story_context_menu.dart
+++ lib/app/features/feed/stories/views/components/story_viewer/components/header/story_context_menu.dart
@@ -4,21 +4,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/components/button/button.dart';
-import 'package:ion/app/components/modal_sheets/simple_modal_sheet.dart';
 import 'package:ion/app/components/overlay_menu/overlay_menu.dart';
 import 'package:ion/app/components/overlay_menu/overlay_menu_container.dart';
-import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/core/providers/mute_provider.c.dart';
 import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.c.dart';
-import 'package:ion/app/features/feed/stories/providers/delete_story_provider.c.dart';
 import 'package:ion/app/features/feed/stories/providers/story_pause_provider.c.dart';
+import 'package:ion/app/features/feed/stories/views/pages/delete_story_modal/delete_story_modal.dart';
 import 'package:ion/app/features/user/pages/profile_page/components/header/context_menu_item.dart';
 import 'package:ion/app/features/user/pages/profile_page/components/header/context_menu_item_divider.dart';
 import 'package:ion/app/features/user/pages/profile_page/pages/report_user_modal/report_user_modal.dart';
 import 'package:ion/app/router/utils/show_simple_bottom_sheet.dart';
-import 'package:ion/app/services/logger/logger.dart';
 import 'package:ion/generated/assets.gen.dart';
 
 class StoryContextMenu extends HookConsumerWidget {
@@ -54,52 +50,14 @@ class StoryContextMenu extends HookConsumerWidget {
     final handleDeleteConfirmation = useCallback(
       () async {
         isDeletingStory.value = true;
+
         final confirmed = await showSimpleBottomSheet<bool>(
           context: context,
-          child: SimpleModalSheet.alert(
-            isBottomSheet: true,
-            title: context.i18n.delete_story_title,
-            description: context.i18n.delete_story_description,
-            iconAsset: Assets.svg.actionCreatePostDeletepost,
-            button: ScreenSideOffset.small(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Button.compact(
-                      type: ButtonType.outlined,
-                      label: Text(context.i18n.button_cancel),
-                      minimumSize: Size(56.0.s, 56.0.s),
-                      onPressed: () => context.pop(false),
-                    ),
-                  ),
-                  SizedBox(width: 16.0.s),
-                  Expanded(
-                    child: Button.compact(
-                      label: Text(context.i18n.button_delete),
-                      minimumSize: Size(56.0.s, 56.0.s),
-                      backgroundColor: context.theme.appColors.attentionRed,
-                      onPressed: () => context.pop(true),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          child: DeleteStoryModal(eventReference: post.toEventReference()),
         );
 
-        if (confirmed.falseOrValue) {
-          final success = await ref.read(deleteStoryProvider(post.toEventReference()).future);
-
-          if (success) {
-            if (context.mounted) {
-              context.pop();
-            }
-          } else {
-            Logger.error('Failed to delete story');
-
-            ref.read(storyPauseControllerProvider.notifier).paused = false;
-          }
+        if (confirmed.falseOrValue && context.mounted) {
+          context.pop();
         } else {
           ref.read(storyPauseControllerProvider.notifier).paused = false;
         }
@@ -152,59 +110,97 @@ class _StoryContextMenuContent extends HookConsumerWidget {
   final VoidCallback onClose;
   final VoidCallback onDeleteRequest;
 
-  List<Widget> _buildMenuItems(BuildContext context, WidgetRef ref) {
-    final i18n = context.i18n;
-    final colors = context.theme.appColors;
-    final isMuted = ref.watch(globalMuteProvider);
-    // final minSize = Size(56.0.s, 56.0.s);
-
-    if (isCurrentUser) {
-      return [
-        ContextMenuItem(
-          label: i18n.button_delete,
-          iconAsset: Assets.svg.iconBlockDelete,
-          onPressed: onDeleteRequest,
-          onLayout: onUpdateWidth,
-          textColor: colors.attentionRed,
-          iconColor: colors.attentionRed,
-        ),
-      ];
-    }
-
-    return [
-      ContextMenuItem(
-        label: i18n.button_report,
-        iconAsset: Assets.svg.iconReport,
-        onPressed: () {
-          showSimpleBottomSheet<void>(
-            context: context,
-            child: ReportUserModal(pubkey: pubkey),
-          );
-        },
-        onLayout: onUpdateWidth,
-      ),
-      const ContextMenuItemDivider(),
-      ContextMenuItem(
-        label: isMuted ? i18n.button_unmute : i18n.button_mute,
-        iconAsset: isMuted ? Assets.svg.iconChannelUnmute : Assets.svg.iconChannelMute,
-        onPressed: () => ref.read(globalMuteProvider.notifier).toggle(),
-        onLayout: onUpdateWidth,
-      ),
-      const ContextMenuItemDivider(),
-      ContextMenuItem(
-        label: i18n.button_unfollow,
-        iconAsset: Assets.svg.iconCategoriesUnflow,
-        onPressed: onClose,
-        onLayout: onUpdateWidth,
-      ),
-    ];
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Column(
       mainAxisSize: MainAxisSize.min,
-      children: _buildMenuItems(context, ref),
+      children: [
+        if (isCurrentUser)
+          _CurrentUserMenuItems(
+            onDeleteRequest: onDeleteRequest,
+            onUpdateWidth: onUpdateWidth,
+          )
+        else
+          _OtherUserMenuItems(
+            pubkey: pubkey,
+            onClose: onClose,
+            onUpdateWidth: onUpdateWidth,
+          ),
+      ],
+    );
+  }
+}
+
+class _CurrentUserMenuItems extends ConsumerWidget {
+  const _CurrentUserMenuItems({
+    required this.onDeleteRequest,
+    required this.onUpdateWidth,
+  });
+
+  final VoidCallback onDeleteRequest;
+  final void Function(Size) onUpdateWidth;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final i18n = context.i18n;
+    final colors = context.theme.appColors;
+
+    return ContextMenuItem(
+      label: i18n.button_delete,
+      iconAsset: Assets.svg.iconBlockDelete,
+      onPressed: onDeleteRequest,
+      onLayout: onUpdateWidth,
+      textColor: colors.attentionRed,
+      iconColor: colors.attentionRed,
+    );
+  }
+}
+
+class _OtherUserMenuItems extends ConsumerWidget {
+  const _OtherUserMenuItems({
+    required this.pubkey,
+    required this.onClose,
+    required this.onUpdateWidth,
+  });
+
+  final String pubkey;
+  final VoidCallback onClose;
+  final void Function(Size) onUpdateWidth;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final i18n = context.i18n;
+    final isMuted = ref.watch(globalMuteProvider);
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ContextMenuItem(
+          label: i18n.button_report,
+          iconAsset: Assets.svg.iconReport,
+          onPressed: () {
+            showSimpleBottomSheet<void>(
+              context: context,
+              child: ReportUserModal(pubkey: pubkey),
+            );
+          },
+          onLayout: onUpdateWidth,
+        ),
+        const ContextMenuItemDivider(),
+        ContextMenuItem(
+          label: isMuted ? i18n.button_unmute : i18n.button_mute,
+          iconAsset: isMuted ? Assets.svg.iconChannelUnmute : Assets.svg.iconChannelMute,
+          onPressed: () => ref.read(globalMuteProvider.notifier).toggle(),
+          onLayout: onUpdateWidth,
+        ),
+        const ContextMenuItemDivider(),
+        ContextMenuItem(
+          label: i18n.button_unfollow,
+          iconAsset: Assets.svg.iconCategoriesUnflow,
+          onPressed: onClose,
+          onLayout: onUpdateWidth,
+        ),
+      ],
     );
   }
 }
---
+++ lib/app/features/feed/stories/views/pages/delete_story_modal/delete_story_modal.dart
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/components/button/button.dart';
+import 'package:ion/app/components/card/info_card.dart';
+import 'package:ion/app/components/screen_offset/screen_bottom_offset.dart';
+import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
+import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/feed/stories/providers/delete_story_provider.c.dart';
+import 'package:ion/app/features/feed/stories/providers/story_pause_provider.c.dart';
+import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
+import 'package:ion/app/services/logger/logger.dart';
+import 'package:ion/generated/assets.gen.dart';
+
+class DeleteStoryModal extends ConsumerWidget {
+  const DeleteStoryModal({
+    required this.eventReference,
+    super.key,
+  });
+
+  final EventReference eventReference;
+
+  static double get buttonsSize => 56.0.s;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final buttonMinimalSize = Size(buttonsSize, buttonsSize);
+
+    return ScreenSideOffset.small(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(height: 48.0.s),
+          InfoCard(
+            iconAsset: Assets.svg.actionCreatepostDeleterole,
+            title: context.i18n.delete_story_title,
+            description: context.i18n.delete_story_description,
+          ),
+          SizedBox(height: 28.0.s),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Button.compact(
+                  type: ButtonType.outlined,
+                  label: Text(context.i18n.button_cancel),
+                  onPressed: () {
+                    ref.read(storyPauseControllerProvider.notifier).paused = false;
+                    context.pop(false);
+                  },
+                  minimumSize: buttonMinimalSize,
+                ),
+              ),
+              SizedBox(width: 15.0.s),
+              Expanded(
+                child: Button.compact(
+                  label: Text(context.i18n.button_delete),
+                  onPressed: () async {
+                    final success = await ref.read(deleteStoryProvider(eventReference).future);
+
+                    if (success) {
+                      if (context.mounted) {
+                        context.pop(true);
+                      }
+                    } else {
+                      Logger.error('Failed to delete story');
+                      if (context.mounted) {
+                        context.pop(false);
+                      }
+                      ref.read(storyPauseControllerProvider.notifier).paused = false;
+                    }
+                  },
+                  minimumSize: buttonMinimalSize,
+                  backgroundColor: context.theme.appColors.attentionRed,
+                ),
+              ),
+            ],
+          ),
+          ScreenBottomOffset(),
+        ],
+      ),
+    );
+  }
+}
