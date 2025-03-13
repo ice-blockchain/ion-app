50a88b3efdcd7f82bfbab41726e2217b3a9f107a
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-03-13 12:20:53 +0300

    feat: add likes subscription

---
+++ lib/app/features/feed/notifications/providers/notification_likes_subscription_provider.c.dart
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/exceptions/exceptions.dart';
+import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
+import 'package:ion/app/features/feed/data/models/entities/reaction_data.c.dart';
+import 'package:ion/app/features/ion_connect/ion_connect.dart';
+import 'package:ion/app/features/ion_connect/providers/ion_connect_notifier.c.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'notification_likes_subscription_provider.c.g.dart';
+
+@riverpod
+Future<void> notificationLikesSubscription(Ref ref) async {
+  final currentPubkey = ref.watch(currentPubkeySelectorProvider);
+
+  if (currentPubkey == null) {
+    throw UserMasterPubkeyNotFoundException();
+  }
+
+  // final since = await commentsRepository.lastCommentCreatedAt(NotificationsType.quote);
+
+  final requestFilter = RequestFilter(
+    kinds: const [ReactionEntity.kind],
+    tags: {
+      '#p': [currentPubkey],
+    },
+    // since: since?.subtract(const Duration(seconds: 2)),
+  );
+  final requestMessage = RequestMessage()..addFilter(requestFilter);
+
+  final events = ref.watch(ionConnectNotifierProvider.notifier).requestEvents(
+    requestMessage,
+    subscriptionBuilder: (requestMessage, relay) {
+      final subscription = relay.subscribe(requestMessage);
+      ref.onDispose(() => relay.unsubscribe(subscription.id));
+      return subscription.messages;
+    },
+  );
+
+  final subscription = events.listen((eventMessage) {
+    print('FOO $eventMessage');
+    // commentsRepository.save(eventParser.parse(eventMessage));
+  });
+
+  ref.onDispose(subscription.cancel);
+}
