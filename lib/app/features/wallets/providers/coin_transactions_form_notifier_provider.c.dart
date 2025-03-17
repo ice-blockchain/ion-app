5d94e51cdaa0291d7c0667d0adde881eef1586db
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-03-17 17:53:12 +0200

    chore: add license

--- lib/app/features/wallets/providers/coin_transactions_form_notifier_provider.c.dart
+++ lib/app/features/wallets/providers/coin_transactions_form_notifier_provider.c.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:ion/app/features/wallets/model/coin_transaction_data.c.dart';
 import 'package:ion/app/features/wallets/model/coin_transactions_form.c.dart';
 import 'package:ion/app/features/wallets/model/network_data.c.dart';
