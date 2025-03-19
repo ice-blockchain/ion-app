78525e4be823e6b4c855448a56c21ea1749a1fe5
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-03-19 12:30:29 +0300

    refactor: notification list item

---
+++ lib/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_icons.dart
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:flutter/material.dart';
+import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
+import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/feed/notifications/data/model/ion_notification.c.dart';
+import 'package:ion/app/features/feed/notifications/views/notifications_history_page/components/notification_item/user_avatar.dart';
+
+class NotificationIcons extends StatelessWidget {
+  const NotificationIcons({
+    required this.notification,
+    super.key,
+  });
+
+  final IonNotification notification;
+
+  static double get separator => 4.0.s;
+
+  static int get iconsCount => 10;
+
+  @override
+  Widget build(BuildContext context) {
+    final iconSize = ((MediaQuery.sizeOf(context).width - ScreenSideOffset.defaultSmallMargin * 2) -
+            separator * (iconsCount - 1)) /
+        iconsCount;
+
+    return Row(
+      children: [
+        Container(
+          width: iconSize,
+          height: iconSize,
+          padding: EdgeInsets.all(6.0.s),
+          decoration: BoxDecoration(
+            color: notification.getBackgroundColor(context),
+            borderRadius: BorderRadius.circular(10.0.s),
+          ),
+          child: notification.asset.icon(size: 18.0.s, color: Colors.white),
+        ),
+        ...notification.pubkeys.take(iconsCount - 1).map((pubkey) {
+          return Padding(
+            padding: EdgeInsets.only(left: separator),
+            child: UserAvatar(
+              pubkey: pubkey,
+              avatarSize: iconSize,
+            ),
+          );
+        }),
+      ],
+    );
+  }
+}
---
+++ lib/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_related_entity.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/feed/data/models/entities/article_data.c.dart';
+import 'package:ion/app/features/feed/data/models/generic_repost.c.dart';
+import 'package:ion/app/features/feed/views/components/article/article.dart';
+import 'package:ion/app/features/feed/views/components/post/post.dart';
+import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
+import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
+import 'package:ion/app/router/app_routes.c.dart';
+
+class NotificationRelatedEntity extends StatelessWidget {
+  const NotificationRelatedEntity({required this.entity, super.key});
+
+  final IonConnectEntity entity;
+
+  @override
+  Widget build(BuildContext context) {
+    final eventReference = switch (entity) {
+      final GenericRepostEntity repost => repost.data.eventReference,
+      _ => entity.toEventReference(),
+    };
+
+    if (eventReference is ReplaceableEventReference && eventReference.kind == ArticleEntity.kind) {
+      return Padding(
+        padding: EdgeInsets.only(top: 12.0.s, bottom: 12.0.s, right: 16.0.s),
+        child: GestureDetector(
+          onTap: () {
+            ArticleDetailsRoute(eventReference: entity.toEventReference().encode())
+                .push<void>(context);
+          },
+          child: Article(
+            eventReference: eventReference,
+            header: const SizedBox.shrink(),
+          ),
+        ),
+      );
+    }
+
+    return GestureDetector(
+      onTap: () {
+        PostDetailsRoute(eventReference: entity.toEventReference().encode()).push<void>(context);
+      },
+      child: Post(
+        eventReference: eventReference,
+        header: const SizedBox.shrink(),
+        footer: const SizedBox.shrink(),
+        topOffset: 6.0.s,
+        headerOffset: 0,
+      ),
+    );
+  }
+}
