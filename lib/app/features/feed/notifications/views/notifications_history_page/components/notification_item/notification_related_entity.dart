3f591f2b4ad62e7852b3897dc790cbe3f8aa2a45
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-03-19 15:41:44 +0300

    chore: add licence

--- lib/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_related_entity.dart
+++ lib/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_related_entity.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:flutter/material.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/feed/data/models/entities/article_data.c.dart';
