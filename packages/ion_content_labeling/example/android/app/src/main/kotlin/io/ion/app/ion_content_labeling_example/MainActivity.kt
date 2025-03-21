7e3497d09fa74350413d40236bf305f0a28dfed4
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-03-21 14:41:30 +0300

    feat: define empty ffi package ion_content_labeling

---
+++ packages/ion_content_labeling/example/android/app/src/main/kotlin/io/ion/app/ion_content_labeling_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.ion.app.ion_content_labeling_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ packages/ion_content_labeling/ios/Classes/ion_content_labeling.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../ion_content_labeling.podspec for more information.
+#include "../../src/ion_content_labeling.c"
