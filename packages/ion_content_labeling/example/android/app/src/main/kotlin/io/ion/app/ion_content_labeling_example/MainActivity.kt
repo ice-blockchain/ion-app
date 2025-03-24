84ee995495847d70a053fe582acd99de4f1afd6f
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-03-24 12:02:04 +0300

    feat: reinit the lib

--- packages/ion_content_labeling/example/android/app/src/main/kotlin/io/ion/app/ion_content_labeling_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package io.ion.app.ion_content_labeling_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- packages/ion_content_labeling/ios/Classes/ion_content_labeling.c
+++
@@ -1,3 +0,0 @@
-// Relative import to be able to reuse the C sources.
-// See the comment in ../ion_content_labeling.podspec for more information.
-#include "../../src/ion_content_labeling.c"
