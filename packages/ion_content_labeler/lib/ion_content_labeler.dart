ea877d279536adadcfabdd8b327d1c6dc887b5f6
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-03-24 13:48:14 +0300

    chore: works on android

--- packages/ion_content_labeler/lib/ion_content_labeler.dart
+++ packages/ion_content_labeler/lib/ion_content_labeler.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 import 'dart:ffi';
 import 'dart:io';
 import 'dart:isolate';
+import 'package:ffi/ffi.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'ion_content_labeler_bindings_generated.dart';
 
@@ -128,9 +131,49 @@ Future<SendPort> _helperIsolateSendPort = () async {
   return completer.future;
 }();
 
-void detectLanguage(String content) {
+typedef LoadModelNative = Void Function(Pointer<Utf8> str);
+typedef LoadModel = void Function(Pointer<Utf8> str);
+
+typedef PredictNative = Void Function(Pointer<Utf8> str, Pointer<Utf8> output);
+typedef Predict = void Function(Pointer<Utf8> str, Pointer<Utf8> output);
+
+void detectLanguage(String content) async {
   final lib = DynamicLibrary.open('fasttext_predict.so');
   // final loadModelFn = lib.providesSymbol<LoadModelNative, LoadModel>('load_model');
-  final loadModelFn = lib.providesSymbol('load_model');
-  print('loadModelFn $loadModelFn');
+  // final loadModelFn = lib.providesSymbol('load_model');
+  // print('loadModelFn $loadModelFn');
+
+  final loadModelFn =
+      lib.lookupFunction<LoadModelNative, LoadModel>('load_model'); // Lookup function in library
+
+  ///
+  final byteData = await rootBundle.load('assets/language_identification.176.ftz');
+  print('FOO $byteData');
+
+  final directory = await getApplicationDocumentsDirectory();
+  final file = File('${directory.path}/language_identification.176.ftz');
+
+  // Write the asset data to a file
+  await file
+      .writeAsBytes(byteData.buffer.asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+
+  final path = file.path;
+  print('PATH $path');
+
+  ///
+
+  final filenameUtf8 = path.toNativeUtf8();
+
+  loadModelFn(filenameUtf8);
+  calloc.free(filenameUtf8);
+
+  final predictFn =
+      lib.lookupFunction<PredictNative, Predict>('predict'); // Lookup function in library
+  final inputUtf8 = content.toNativeUtf8();
+  Pointer<Utf8> outputPrediction = calloc.allocate(20);
+  predictFn(inputUtf8, outputPrediction);
+  print("Prediction:");
+  print(outputPrediction.toDartString());
+  calloc.free(inputUtf8);
+  calloc.free(outputPrediction);
 }
