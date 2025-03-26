1e197b3ccb52152351708129a41ca5e1726e66bc
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-03-26 13:34:17 +0300

    chore: add stub test

---
+++ packages/ion_content_labeler/test/simple_test.dart
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    'simple test',
+    () {
+      expect(1, equals(1));
+    },
+  );
+}
