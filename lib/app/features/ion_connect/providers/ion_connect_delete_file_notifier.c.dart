d417f6c418aebd021e0ac1b2c01ee01f14aee040
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-03-28 14:44:57 +0100

    refactor: unify logic for file storage auth and url methods

--- lib/app/features/ion_connect/providers/ion_connect_delete_file_notifier.c.dart
+++ lib/app/features/ion_connect/providers/ion_connect_delete_file_notifier.c.dart
@@ -5,13 +5,9 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:ion/app/exceptions/exceptions.dart';
-import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/core/providers/dio_provider.c.dart';
 import 'package:ion/app/features/ion_connect/ion_connect.dart';
-import 'package:ion/app/features/ion_connect/model/file_storage_metadata.c.dart';
-import 'package:ion/app/features/ion_connect/model/ion_connect_auth.c.dart';
-import 'package:ion/app/features/ion_connect/providers/ion_connect_notifier.c.dart';
-import 'package:ion/app/features/user/providers/user_relays_manager.c.dart';
+import 'package:ion/app/features/ion_connect/utils/file_storage_utils.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'ion_connect_delete_file_notifier.c.freezed.dart';
@@ -37,15 +33,17 @@ class IonConnectDeleteFileNotifier extends _$IonConnectDeleteFileNotifier {
       return;
     }
 
-    final storageUrl = await _getFileStorageApiUrl();
+    final storageUrl = await getFileStorageApiUrl(ref);
 
     await Future.wait(
       fileHashes.map(
         (fileHash) async {
           final url = '$storageUrl/$fileHash';
-          final authorizationToken = await _generateAuthorizationToken(
+          final authorizationToken = await generateAuthorizationToken(
+            ref: ref,
             url: url,
             customEventSigner: customEventSigner,
+            method: 'DELETE',
           );
 
           await _makeDeleteRequest(
@@ -56,50 +54,6 @@ class IonConnectDeleteFileNotifier extends _$IonConnectDeleteFileNotifier {
         },
       ),
     );
-  }
-
-  Future<String> _generateAuthorizationToken({
-    required String url,
-    EventSigner? customEventSigner,
-  }) async {
-    final ionConnectAuth = IonConnectAuth(url: url, method: 'DELETE');
-
-    if (customEventSigner != null) {
-      final authEvent = await ionConnectAuth.toEventMessage(customEventSigner);
-
-      return ionConnectAuth.toAuthorizationHeader(authEvent);
-    } else {
-      final authEvent = await ref.read(ionConnectNotifierProvider.notifier).sign(ionConnectAuth);
-
-      return ionConnectAuth.toAuthorizationHeader(authEvent);
-    }
-  }
-
-  // TODO: handle delegatedToUrl when migrating to common relays
-  Future<String> _getFileStorageApiUrl() async {
-    final userRelays = await ref.read(currentUserRelayProvider.future);
-    if (userRelays == null) {
-      throw UserRelaysNotFoundException();
-    }
-    final relayUrl = userRelays.data.list.random.url;
-
-    try {
-      final parsedRelayUrl = Uri.parse(relayUrl);
-      final metadataUri = Uri(
-        scheme: 'https',
-        host: parsedRelayUrl.host,
-        port: parsedRelayUrl.hasPort ? parsedRelayUrl.port : null,
-        path: FileStorageMetadata.path,
-      );
-
-      final response = await ref.read(dioProvider).getUri<dynamic>(metadataUri);
-      final deletePath =
-          FileStorageMetadata.fromJson(json.decode(response.data as String) as Map<String, dynamic>)
-              .apiUrl;
-      return metadataUri.replace(path: deletePath).toString();
-    } catch (error) {
-      throw GetFileStorageUrlException(error);
-    }
   }
 
   Future<void> _makeDeleteRequest({
