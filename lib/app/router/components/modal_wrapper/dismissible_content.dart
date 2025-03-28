9c62db4d0610e0ab937a061942d3f7acec9a64b5
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-03-28 13:27:36 +0200

    fix: change zooming

--- lib/app/router/components/modal_wrapper/dismissible_content.dart
+++ lib/app/router/components/modal_wrapper/dismissible_content.dart
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:dismissible_page/dismissible_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/features/feed/views/pages/fullscreen_media/components/fullscreen_image.dart';
 
-class DismissibleContent extends StatelessWidget {
+class DismissibleContent extends ConsumerWidget {
   const DismissibleContent({
     required this.child,
     required this.state,
@@ -15,14 +18,16 @@ class DismissibleContent extends StatelessWidget {
   final GoRouterState state;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isZoomed = ref.watch(isImageZoomedProvider);
     return DismissiblePage(
       onDismissed: () {
         if (context.canPop()) {
           context.pop();
         }
       },
-      direction: DismissiblePageDismissDirection.multi,
+      direction:
+          isZoomed ? DismissiblePageDismissDirection.none : DismissiblePageDismissDirection.multi,
       child: child,
     );
   }
