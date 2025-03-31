3e4499d6f16dceb2c93ca96fb110444d06e802b4
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-03-31 14:45:08 +0300

    chore(test): apply labeler lib to post creation

--- lib/app/features/feed/create_post/views/components/post_submit_button/post_submit_button.dart
+++ lib/app/features/feed/create_post/views/components/post_submit_button/post_submit_button.dart
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_quill/flutter_quill.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+<<<<<<< HEAD
 import 'package:ion/app/components/text_editor/hooks/use_text_editor_character_limit_exceed_amount.dart';
+=======
+import 'package:ion/app/components/screen_offset/screen_bottom_offset.dart';
+>>>>>>> ae81a0f7d (chore(test): apply labeler lib to post creation)
 import 'package:ion/app/components/text_editor/hooks/use_text_editor_has_content.dart';
 import 'package:ion/app/features/core/providers/poll/poll_answers_provider.c.dart';
 import 'package:ion/app/features/core/providers/poll/poll_title_notifier.c.dart';
 import 'package:ion/app/features/feed/create_post/model/create_post_option.dart';
+<<<<<<< HEAD
 import 'package:ion/app/features/feed/create_post/providers/create_post_notifier.c.dart';
 import 'package:ion/app/features/feed/create_post/views/pages/post_form_modal/hooks/use_has_poll.dart';
 import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.c.dart';
@@ -19,8 +21,16 @@ import 'package:ion/app/features/feed/providers/selected_who_can_reply_option_pr
 import 'package:ion/app/features/feed/views/components/toolbar_buttons/toolbar_send_button.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
 import 'package:ion/app/features/ion_connect/model/media_attachment.dart';
+=======
+import 'package:ion/app/features/feed/create_post/views/pages/create_post_modal/hooks/use_has_poll.dart';
+import 'package:ion/app/features/feed/views/components/toolbar_buttons/toolbar_send_button.dart';
+import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
+import 'package:ion/app/router/components/navigation_app_bar/navigation_app_bar.dart';
+import 'package:ion/app/router/utils/show_simple_bottom_sheet.dart';
+>>>>>>> ae81a0f7d (chore(test): apply labeler lib to post creation)
 import 'package:ion/app/services/media_service/media_service.c.dart';
 import 'package:ion/app/utils/validators.dart';
+import 'package:ion_content_labeler/ion_content_labeler.dart';
 
 class PostSubmitButton extends HookConsumerWidget {
   const PostSubmitButton({
@@ -62,7 +72,7 @@ class PostSubmitButton extends HookConsumerWidget {
     final pollTitle = ref.watch(pollTitleNotifierProvider);
     final pollAnswers = ref.watch(pollAnswersNotifierProvider);
     final hasPoll = useHasPoll(textEditorController);
-    final whoCanReply = ref.watch(selectedWhoCanReplyOptionProvider);
+    // final whoCanReply = ref.watch(selectedWhoCanReplyOptionProvider);
 
     final isSubmitButtonEnabled = useMemoized(
       () {
@@ -80,6 +90,7 @@ class PostSubmitButton extends HookConsumerWidget {
     return ToolbarSendButton(
       enabled: isSubmitButtonEnabled,
       onPressed: () async {
+<<<<<<< HEAD
         final convertedMediaFiles = await ref
             .read(mediaServiceProvider)
             .convertAssetIdsToMediaFiles(ref, mediaFiles: mediaFiles);
@@ -107,14 +118,68 @@ class PostSubmitButton extends HookConsumerWidget {
               mediaFiles: convertedMediaFiles,
               whoCanReply: whoCanReply,
             ),
+=======
+        final content = textEditorController.document.toPlainText().trim();
+        final language = await detectTextLanguage(content);
+        final category = await detectTextCategory(content);
+        if (context.mounted) {
+          await showSimpleBottomSheet<void>(
+            child: Column(
+              children: [
+                NavigationAppBar.modal(
+                  showBackButton: false,
+                  title: const Text('Labeling results'),
+                ),
+                Text('language: $language', textAlign: TextAlign.left),
+                Text('category: $category', textAlign: TextAlign.left),
+                Text('input: $content', textAlign: TextAlign.left),
+                ScreenBottomOffset(),
+              ],
+            ),
+            context: context,
+>>>>>>> ae81a0f7d (chore(test): apply labeler lib to post creation)
           );
         }
+        // if (modifiedEvent != null) {
+        //   unawaited(
+        //     ref
+        //         .read(
+        //           createPostNotifierProvider(
+        //             createOption,
+        //           ).notifier,
+        //         )
+        //         .modify(
+        //           content: textEditorController.document.toDelta(),
+        //           eventReference: modifiedEvent!,
+        //           whoCanReply: whoCanReply,
+        //         ),
+        //   );
+        // } else {
+        //   final convertedMediaFiles = await ref
+        //       .read(mediaServiceProvider)
+        //       .convertAssetIdsToMediaFiles(ref, mediaFiles: mediaFiles);
+        //   unawaited(
+        //     ref
+        //         .read(
+        //           createPostNotifierProvider(
+        //             createOption,
+        //           ).notifier,
+        //         )
+        //         .create(
+        //           content: textEditorController.document.toDelta(),
+        //           parentEvent: parentEvent,
+        //           quotedEvent: quotedEvent,
+        //           mediaFiles: convertedMediaFiles,
+        //           whoCanReply: whoCanReply,
+        //         ),
+        //   );
+        // }
 
-        if (onSubmitted != null) {
-          onSubmitted!();
-        } else if (context.mounted) {
-          ref.context.pop(true);
-        }
+        // if (onSubmitted != null) {
+        //   onSubmitted!();
+        // } else if (context.mounted) {
+        //   ref.context.pop(true);
+        // }
       },
     );
   }
