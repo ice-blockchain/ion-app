e139b4bf2c9a731f4abc6e9c2e2c19778cbdfe82
ice-hector <96414297+ice-hector@users.noreply.github.com>
2025-03-31 13:08:43 +0300

    refactor: add listener

--- lib/app/components/text_editor/utils/links_handler.dart
+++ lib/app/components/text_editor/utils/links_handler.dart
@@ -1,58 +1,36 @@
-import 'dart:async';
-
-import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/components/text_editor/utils/text_editor_typing_listener.dart';
 import 'package:ion/app/services/text_parser/model/text_matcher.dart';
 
-class LinksHandler {
+class LinksHandler extends TextEditorTypingListener {
   LinksHandler({
-    required this.controller,
-    required this.focusNode,
-    required this.context,
-    required this.ref,
+    required super.controller,
+    required super.focusNode,
+    required super.context,
+    required super.ref,
   });
 
-  final QuillController controller;
-  final FocusNode focusNode;
-  final BuildContext context;
-  final WidgetRef ref;
-  Timer? _debounce;
-  String? previousText;
   final _urlMatcher = const UrlMatcher();
 
-  void initialize() {
-    controller.addListener(_editorListener);
-    focusNode.addListener(_focusListener);
-  }
+  @override
+  void onTextChanged(String text, int cursorIndex, bool isBackspace, bool cursorMoved) {
+    _cleanAndReformatLinks(text);
 
-  void dispose() {
-    controller.removeListener(_editorListener);
-    focusNode.removeListener(_focusListener);
-    _debounce?.cancel();
-  }
-
-  void _editorListener() {
-    final cursorIndex = controller.selection.baseOffset;
-    final text = controller.document.toPlainText();
-
-    previousText = text;
-
-    _debounce?.cancel();
-    _debounce = Timer(const Duration(milliseconds: 1), () {
-      _cleanAndReformatLinks(text);
-
-      if (cursorIndex > 0) {
-        final char = text.substring(cursorIndex - 1, cursorIndex);
-        if (_isWordBoundary(char)) {
-          _checkAndFormatLink(text, cursorIndex);
-        }
+    if (cursorIndex > 0) {
+      final char = text.substring(cursorIndex - 1, cursorIndex);
+      if (isWordBoundary(char)) {
+        _checkAndFormatLink(text, cursorIndex);
       }
-    });
+    }
+  }
+
+  @override
+  void onFocusLost() {
+    _cleanAndReformatLinks(controller.document.toPlainText());
   }
 
   void _cleanAndReformatLinks(String text) {
-    controller.removeListener(_editorListener);
+    controller.removeListener(editorListener);
 
     try {
       final matches = RegExp(_urlMatcher.pattern).allMatches(text);
@@ -67,7 +45,7 @@ class LinksHandler {
         }
       }
     } finally {
-      controller.addListener(_editorListener);
+      controller.addListener(editorListener);
     }
   }
 
@@ -77,7 +55,7 @@ class LinksHandler {
       if (match.end == cursorIndex) {
         final url = match.group(0);
         if (url != null) {
-          controller.removeListener(_editorListener);
+          controller.removeListener(editorListener);
           try {
             controller.formatText(
               match.start,
@@ -85,31 +63,11 @@ class LinksHandler {
               LinkAttribute(url),
             );
           } finally {
-            controller.addListener(_editorListener);
+            controller.addListener(editorListener);
           }
         }
         break;
       }
     }
-  }
-
-  bool _isWordBoundary(String char) {
-    return char == ' ' || char == '\n' || _isPunctuation(char);
-  }
-
-  bool _isPunctuation(String char) {
-    const regularPunctuation = '.,;:!?()[]{}"\'\\/\\`~=+<>*&^%_-|';
-    const specialQuotes = '\u2018\u2019\u201C\u201D\u2032\u2035\u00B4\u0060\u00AB\u00BB';
-    return regularPunctuation.contains(char) || specialQuotes.contains(char);
-  }
-
-  void _focusListener() {
-    if (!focusNode.hasFocus) {
-      _cleanAndReformatLinks(controller.document.toPlainText());
-    }
-  }
-
-  void triggerListener() {
-    _editorListener();
   }
 }
