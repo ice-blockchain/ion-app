ff45a1ba6a701a39b69b014b3cecba10844f946f
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-03-31 17:32:37 +0300

    chore: use drift tool for db migration

--- lib/app/features/wallets/data/database/wallets_database.c.dart
+++ lib/app/features/wallets/data/database/wallets_database.c.dart
@@ -8,6 +8,7 @@ import 'package:ion/app/features/wallets/data/database/tables/duration_type.dart
 import 'package:ion/app/features/wallets/data/database/tables/networks_table.c.dart';
 import 'package:ion/app/features/wallets/data/database/tables/sync_coins_table.c.dart';
 import 'package:ion/app/features/wallets/data/database/tables/transactions_table.c.dart';
+import 'package:ion/app/features/wallets/data/database/wallets_database.c.steps.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'wallets_database.c.g.dart';
@@ -35,14 +36,15 @@ class WalletsDatabase extends _$WalletsDatabase {
   int get schemaVersion => 2;
 
   @override
-  MigrationStrategy get migration => MigrationStrategy(
-        onCreate: (migration) => migration.createAll(),
-        onUpgrade: (Migrator m, int from, int to) async {
-          if (from < 2) {
-            await m.createTable(transactionsTable);
-          }
+  MigrationStrategy get migration {
+    return MigrationStrategy(
+      onUpgrade: stepByStep(
+        from1To2: (m, schema) async {
+          await m.createTable(schema.transactionsTable);
         },
-      );
+      ),
+    );
+  }
 
   static QueryExecutor _openConnection() {
     return driftDatabase(name: 'wallets_database');
