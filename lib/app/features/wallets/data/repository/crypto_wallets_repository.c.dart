5cd617ceee032b006376e0c78705853ec9b060b4
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-04-04 17:07:04 +0300

    chore: add license

--- lib/app/features/wallets/data/repository/crypto_wallets_repository.c.dart
+++ lib/app/features/wallets/data/repository/crypto_wallets_repository.c.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/wallets/data/database/dao/crypto_wallets_dao.c.dart';
 import 'package:ion/app/features/wallets/data/database/wallets_database.c.dart';
