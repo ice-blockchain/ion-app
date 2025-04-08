4d7d3be5a8b2fac4882e955db203e776e257f99c
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-04-08 13:48:38 +0300

    chore: add licence

--- lib/app/features/ion_connect/providers/ion_connect_subscription_provider.c.dart
+++ lib/app/features/ion_connect/providers/ion_connect_subscription_provider.c.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/ion_connect/ion_connect.dart';
 import 'package:ion/app/features/ion_connect/model/action_source.c.dart';
