133d742b4a418db5a8aee4ee20411fe524e72c74
ice-hector <96414297+ice-hector@users.noreply.github.com>
2025-04-09 12:28:35 +0300

    fix: article details ui

--- lib/app/features/feed/views/pages/article_details_page/components/user_biography.dart
+++ lib/app/features/feed/views/pages/article_details_page/components/user_biography.dart
@@ -1,14 +1,10 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
-import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
 import 'package:ion/app/features/components/user/user_about/user_about.dart';
 import 'package:ion/app/features/components/user/user_info_summary/user_info_summary.dart';
-import 'package:ion/app/features/feed/views/components/overlay_menu/own_entity_menu.dart';
-import 'package:ion/app/features/feed/views/components/overlay_menu/user_info_menu.dart';
 import 'package:ion/app/features/feed/views/components/user_info/user_info.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
 
@@ -19,8 +15,6 @@ class UserBiography extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isOwnedByCurrentUser = ref.watch(isCurrentUserSelectorProvider(eventReference.pubkey));
-
     return Container(
       alignment: AlignmentDirectional.topStart,
       decoration: BoxDecoration(
@@ -38,9 +32,6 @@ class UserBiography extends ConsumerWidget {
         children: [
           UserInfo(
             pubkey: eventReference.pubkey,
-            trailing: isOwnedByCurrentUser
-                ? OwnEntityMenu(eventReference: eventReference, onDelete: context.pop)
-                : UserInfoMenu(pubkey: eventReference.pubkey),
           ),
           SizedBox(height: 12.0.s),
           UserAbout(
