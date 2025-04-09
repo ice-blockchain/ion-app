9a9f650886edfb6b67c4dc1e9272eb0ad66b1240
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-04-10 02:22:11 +0300

    chore: remove unused components

--- lib/app/features/core/views/photo_gallery_page/components/components.dart
+++ lib/app/features/core/views/photo_gallery_page/components/components.dart
@@ -1,22 +1,13 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/overlay_menu/components/overlay_menu_item.dart';
 import 'package:ion/app/components/overlay_menu/components/overlay_menu_item_separator.dart';
 import 'package:ion/app/components/overlay_menu/overlay_menu.dart';
 import 'package:ion/app/components/overlay_menu/overlay_menu_container.dart';
-import 'package:ion/app/components/video_preview/video_preview.dart';
 import 'package:ion/app/extensions/extensions.dart';
-import 'package:ion/app/features/chat/e2ee/model/entities/private_direct_message_data.c.dart';
-import 'package:ion/app/features/chat/e2ee/providers/chat_message_load_media_provider.c.dart';
-import 'package:ion/app/features/chat/model/database/chat_database.c.dart';
-import 'package:ion/app/features/core/model/media_type.dart';
 import 'package:ion/app/utils/date.dart';
 import 'package:ion/generated/assets.gen.dart';
 
-part 'photo_gallery_carousel.dart';
 part 'photo_gallery_context_menu.dart';
 part 'photo_gallery_meta.dart';
-part 'photo_gallery_title.dart';
