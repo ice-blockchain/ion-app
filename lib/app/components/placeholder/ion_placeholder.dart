ca19ee20406cc02ba196792c7985c19b26e35858
ion-endymion <188437551+ice-endymion@users.noreply.github.com>
2025-04-16 15:22:48 +0200

    fix: add license

--- lib/app/components/placeholder/ion_placeholder.dart
+++ lib/app/components/placeholder/ion_placeholder.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:flutter/material.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/generated/assets.gen.dart';
