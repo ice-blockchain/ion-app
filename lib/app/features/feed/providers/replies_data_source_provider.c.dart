98a5119cd6c0460d1ea0b3ffaf40dfd6a697f2c6
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-04-18 13:03:56 +0300

    chore: remove TagMarkerSearchExtension from replies data source

--- lib/app/features/feed/providers/replies_data_source_provider.c.dart
+++ lib/app/features/feed/providers/replies_data_source_provider.c.dart
@@ -1,17 +1,12 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/exceptions/exceptions.dart';
-import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
-import 'package:ion/app/features/feed/data/models/entities/article_data.c.dart';
 import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.c.dart';
 import 'package:ion/app/features/feed/data/models/entities/post_data.c.dart';
 import 'package:ion/app/features/ion_connect/ion_connect.dart';
 import 'package:ion/app/features/ion_connect/model/action_source.c.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
-import 'package:ion/app/features/ion_connect/model/related_event.c.dart';
-import 'package:ion/app/features/ion_connect/model/related_event_marker.dart';
 import 'package:ion/app/features/ion_connect/model/search_extension.dart';
 import 'package:ion/app/features/ion_connect/providers/entities_paged_data_provider.c.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_entity_provider.c.dart';
@@ -33,13 +28,6 @@ List<EntitiesDataSource>? repliesDataSource(
     return null;
   }
 
-  final hasParent = switch (entity) {
-    ArticleEntity() => false,
-    ModifiablePostEntity() => entity.data.parentEvent != null,
-    PostEntity() => entity.data.parentEvent != null,
-    _ => throw UnsupportedEventReference(eventReference)
-  };
-
   final dataSources = [
     EntitiesDataSource(
       actionSource: ActionSourceUser(eventReference.pubkey),
@@ -55,11 +43,6 @@ List<EntitiesDataSource>? repliesDataSource(
             [
               ...SearchExtensions.withCounters(
                 [
-                  TagMarkerSearchExtension(
-                    tagName: RelatedReplaceableEvent.tagName,
-                    marker: RelatedEventMarker.reply.toShortString(),
-                    negative: !hasParent,
-                  ),
                   GenericIncludeSearchExtension(
                     forKind: ModifiablePostEntity.kind,
                     includeKind: UserMetadataEntity.kind,
@@ -83,11 +66,6 @@ List<EntitiesDataSource>? repliesDataSource(
             [
               ...SearchExtensions.withCounters(
                 [
-                  TagMarkerSearchExtension(
-                    tagName: RelatedImmutableEvent.tagName,
-                    marker: RelatedEventMarker.reply.toShortString(),
-                    negative: !hasParent,
-                  ),
                   GenericIncludeSearchExtension(
                     forKind: PostEntity.kind,
                     includeKind: UserMetadataEntity.kind,
