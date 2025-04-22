5f651e5f9991a982f89dd8f4fe38016477f42fa4
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-04-22 18:11:24 +0300

    refactor: content language selection to use optimistic provider

--- lib/app/features/settings/views/content_language_modal.dart
+++ lib/app/features/settings/views/content_language_modal.dart
@@ -4,8 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/core/views/pages/language_selector_page.dart';
-import 'package:ion/app/features/user/model/interest_set.c.dart';
-import 'package:ion/app/features/user/providers/user_interests_set_provider.c.dart';
+import 'package:ion/app/features/settings/providers/optimistic_content_language_provider.c.dart';
 import 'package:ion/app/router/components/navigation_app_bar/navigation_app_bar.dart';
 import 'package:ion/app/router/components/navigation_app_bar/navigation_close_button.dart';
 
@@ -14,29 +13,16 @@ class ContentLanguageModal extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final languageInterestSet =
-        ref.watch(currentUserInterestsSetProvider(InterestSetType.languages));
-    final selectedLanguages = languageInterestSet.valueOrNull?.data.hashtags;
+    final langsAsync = ref.watch(contentLangSetProvider);
+    final selectedLanguages = langsAsync.valueOrNull?.hashtags ?? [];
 
     return LanguageSelectorPage(
       title: context.i18n.content_language_title,
       description: context.i18n.content_language_description,
       toggleLanguageSelection: (String lang) {
-        if (!languageInterestSet.isLoading && selectedLanguages != null) {
-          final updatedLanguages = {...selectedLanguages};
-          if (updatedLanguages.contains(lang)) {
-            updatedLanguages.remove(lang);
-          } else {
-            updatedLanguages.add(lang);
-          }
-          if (updatedLanguages.isNotEmpty) {
-            ref
-                .read(currentUserInterestsSetProvider(InterestSetType.languages).notifier)
-                .set(updatedLanguages.toList());
-          }
-        }
+        ref.read(contentLangSetNotifierProvider.notifier).toggle(lang);
       },
-      selectedLanguages: selectedLanguages ?? [],
+      selectedLanguages: selectedLanguages,
       appBar: NavigationAppBar.modal(
         actions: const [NavigationCloseButton()],
       ),
