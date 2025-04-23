1375ce066fd04093cf4735b6618eefe9e06bc44e
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-04-23 16:01:41 +0300

    refactor: remove unused context menu width calculation

--- lib/app/features/user/pages/profile_page/components/header/context_menu_item.dart
+++ lib/app/features/user/pages/profile_page/components/header/context_menu_item.dart
@@ -1,17 +1,13 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
-import 'package:ion/app/hooks/use_on_init.dart';
 
-class ContextMenuItem extends HookConsumerWidget {
+class ContextMenuItem extends StatelessWidget {
   const ContextMenuItem({
     required this.label,
     required this.iconAsset,
     required this.onPressed,
-    required this.onLayout,
     this.textColor,
     this.iconColor,
     super.key,
@@ -22,25 +18,15 @@ class ContextMenuItem extends HookConsumerWidget {
   final String label;
   final String iconAsset;
   final VoidCallback onPressed;
-  final void Function(Size) onLayout;
   final Color? textColor;
   final Color? iconColor;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final globalKey = useMemoized(GlobalKey.new);
-    useOnInit(() {
-      final renderBox = globalKey.currentContext?.findRenderObject() as RenderBox?;
-      if (renderBox != null) {
-        onLayout(renderBox.size);
-      }
-    });
-
+  Widget build(BuildContext context) {
     final colors = context.theme.appColors;
     final textStyles = context.theme.appTextThemes;
 
     return GestureDetector(
-      key: globalKey,
       onTap: onPressed,
       child: SizedBox(
         height: 44.0.s,
