d2f6e629ab93734a44faaa870ccb1222691fc61c
ice-tychon <175587874+ice-tychon@users.noreply.github.com>
2025-04-25 22:26:25 +0300

    feat(wallet): toggle Search filed on Search icon tap

---
+++ lib/app/features/wallets/views/pages/wallet_page/helpers/cancel_search_helper.dart
@@ -0,0 +1,12 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/features/wallets/providers/filtered_assets_provider.c.dart';
+import 'package:ion/app/features/wallets/views/pages/wallet_page/providers/search_visibility_provider.c.dart';
+import 'package:ion/app/features/wallets/views/pages/wallet_page/tab_type.dart';
+
+void cancelSearch(
+  WidgetRef ref,
+  WalletTabType tabType,
+) {
+  ref.read(walletSearchQueryControllerProvider(tabType.walletAssetType).notifier).query = '';
+  ref.read(walletSearchVisibilityProvider(tabType).notifier).isVisible = false;
+}
