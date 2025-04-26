61808e1942bac0a809d84accf4c053e674a71d3e
ion-endymion <188437551+ice-endymion@users.noreply.github.com>
2025-04-26 12:57:33 +0200

    feat: add follow back button to followers list

--- lib/app/features/user/pages/profile_page/pages/follow_list_modal/components/follow_list_item.dart
+++ lib/app/features/user/pages/profile_page/pages/follow_list_modal/components/follow_list_item.dart
@@ -6,6 +6,7 @@ import 'package:ion/app/components/list_item/list_item.dart';
 import 'package:ion/app/components/list_items_loading_state/item_loading_state.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/components/user/follow_user_button/follow_user_button.dart';
+import 'package:ion/app/features/user/providers/follow_list_provider.c.dart';
 import 'package:ion/app/features/user/providers/user_metadata_provider.c.dart';
 import 'package:ion/app/router/app_routes.c.dart';
 import 'package:ion/app/utils/username.dart';
@@ -23,6 +24,7 @@ class FollowListItem extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final userMetadataResult = ref.watch(userMetadataProvider(pubkey));
+    final isCurrentUserFollowed = ref.watch(isCurrentUserFollowedProvider(pubkey));
 
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 8.0.s),
@@ -34,7 +36,10 @@ class FollowListItem extends ConsumerWidget {
 
           return ListItem.user(
             title: Text(userMetadata.data.displayName),
-            trailing: FollowUserButton(pubkey: pubkey),
+            trailing: FollowUserButton(
+              pubkey: pubkey,
+              followLabel: isCurrentUserFollowed ? context.i18n.button_follow_back : null,
+            ),
             subtitle: Text(
               prefixUsername(
                 username: userMetadata.data.name,
