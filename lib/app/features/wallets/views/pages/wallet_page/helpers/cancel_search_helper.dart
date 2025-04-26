5183441571f503d172cf81fc72f758cdcb152d87
ice-tychon <175587874+ice-tychon@users.noreply.github.com>
2025-04-26 11:33:11 +0300

    fix: add missing licence

--- lib/app/features/wallets/views/pages/wallet_page/helpers/cancel_search_helper.dart
+++ lib/app/features/wallets/views/pages/wallet_page/helpers/cancel_search_helper.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/wallets/providers/filtered_assets_provider.c.dart';
 import 'package:ion/app/features/wallets/views/pages/wallet_page/providers/search_visibility_provider.c.dart';
