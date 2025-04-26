7aec1266003ac8485e914f2f3c0ee109ddfdbcad
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-04-26 16:01:33 +0200

    fix: update external app launcher lib to latest version to cover ios bug (#1093)
    ## Description
    <!-- Briefly explain the purpose of this PR and what it accomplishes.
    -->
    
    ## Additional Notes
    <!-- Add any extra context or relevant information here. -->
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <!-- Include screenshots to demonstrate any UI changes. -->
    <!-- <img width="180" alt="image" src="image_url_here"> -->

--- lib/app/features/protect_account/authenticator/views/pages/setup_authenticator/authenticator_setup_options_page.dart
+++ lib/app/features/protect_account/authenticator/views/pages/setup_authenticator/authenticator_setup_options_page.dart
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'dart:io';
-
 import 'package:external_app_launcher/external_app_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:ion/app/components/button/button.dart';
@@ -18,7 +16,6 @@ import 'package:ion/app/router/components/navigation_app_bar/navigation_app_bar.
 import 'package:ion/app/router/components/navigation_app_bar/navigation_close_button.dart';
 import 'package:ion/app/router/components/sheet_content/sheet_content.dart';
 import 'package:ion/generated/assets.gen.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class AuthenticatorSetupOptionsPage extends StatelessWidget {
   const AuthenticatorSetupOptionsPage({
@@ -101,20 +98,10 @@ class AuthenticatorSetupOptionsPage extends StatelessWidget {
   }
 
   Future<void> _onOptionTap(AuthenticatorType type) async {
-    // LaunchApp currently doesn't work on iOS because of a bug
-    // https://github.com/GeekyAnts/external_app_launcher/issues/41
-    // TODO: Replace canLaunch and launchUrl with LaunchApp.openApp when library is fixed
-    if (Platform.isAndroid) {
-      await LaunchApp.openApp(
-        androidPackageName: type.androidPackageName,
-      );
-    } else {
-      final canLaunch = await canLaunchUrl(Uri.parse(type.iosAppUrlScheme));
-      if (canLaunch) {
-        await launchUrl(Uri.parse(type.iosAppUrlScheme));
-      } else {
-        await launchUrl(Uri.parse(type.appStoreLink));
-      }
-    }
+    await LaunchApp.openApp(
+      androidPackageName: type.androidPackageName,
+      iosUrlScheme: type.iosAppUrlScheme,
+      appStoreLink: type.appStoreLink,
+    );
   }
 }
