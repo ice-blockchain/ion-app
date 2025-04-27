b89d93423dbb9822422c0fba9304077af04a9b93
ice-hector <96414297+ice-hector@users.noreply.github.com>
2025-04-27 17:21:59 +0300

    fix: notification tap zone

--- lib/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_item.dart
+++ lib/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_item.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/feed/data/models/entities/article_data.c.dart';
 import 'package:ion/app/features/feed/data/models/generic_repost.c.dart';
 import 'package:ion/app/features/feed/notifications/data/model/ion_notification.c.dart';
 import 'package:ion/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_icons.dart';
@@ -11,8 +12,10 @@ import 'package:ion/app/features/feed/notifications/views/notifications_history_
 import 'package:ion/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_related_entity.dart';
 import 'package:ion/app/features/feed/providers/ion_connect_entity_with_counters_provider.c.dart';
 import 'package:ion/app/features/feed/views/components/list_separator/list_separator.dart';
+import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
 import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
 import 'package:ion/app/features/ion_connect/model/soft_deletable_entity.dart';
+import 'package:ion/app/router/app_routes.c.dart';
 
 class NotificationItem extends ConsumerWidget {
   const NotificationItem({
@@ -39,21 +42,30 @@ class NotificationItem extends ConsumerWidget {
       }
     }
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(height: 16.0.s),
-        ScreenSideOffset.small(
-          child: NotificationIcons(notification: notification),
-        ),
-        SizedBox(height: 8.0.s),
-        ScreenSideOffset.small(
-          child: NotificationInfo(notification: notification),
-        ),
-        if (entity != null) NotificationRelatedEntity(entity: entity, notification: notification),
-        SizedBox(height: 16.0.s),
-        FeedListSeparator(),
-      ],
+    return GestureDetector(
+      onTap: () => _onTap(context, entity),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 16.0.s),
+          ScreenSideOffset.small(
+            child: NotificationIcons(notification: notification),
+          ),
+          SizedBox(height: 8.0.s),
+          ScreenSideOffset.small(
+            child: NotificationInfo(
+              notification: notification,
+            ),
+          ),
+          if (entity != null)
+            NotificationRelatedEntity(
+              entity: entity,
+              notification: notification,
+            ),
+          SizedBox(height: 16.0.s),
+          FeedListSeparator(),
+        ],
+      ),
     );
   }
 
@@ -69,5 +81,22 @@ class NotificationItem extends ConsumerWidget {
           (repostedEntity is SoftDeletableEntity && repostedEntity.isDeleted);
     }
     return false;
+  }
+
+  void _onTap(BuildContext context, IonConnectEntity? entity) {
+    if (entity == null) {
+      return;
+    }
+
+    final eventReference = switch (entity) {
+      final GenericRepostEntity repost => repost.data.eventReference,
+      _ => entity.toEventReference(),
+    };
+
+    if (eventReference is ReplaceableEventReference && eventReference.kind == ArticleEntity.kind) {
+      ArticleDetailsRoute(eventReference: eventReference.encode()).push<void>(context);
+    } else {
+      PostDetailsRoute(eventReference: eventReference.encode()).push<void>(context);
+    }
   }
 }
--- lib/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_related_entity.dart
+++ lib/app/features/feed/notifications/views/notifications_history_page/components/notification_item/notification_related_entity.dart
@@ -9,7 +9,6 @@ import 'package:ion/app/features/feed/views/components/article/article.dart';
 import 'package:ion/app/features/feed/views/components/post/post.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
 import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
-import 'package:ion/app/router/app_routes.c.dart';
 
 class NotificationRelatedEntity extends StatelessWidget {
   const NotificationRelatedEntity({required this.entity, required this.notification, super.key});
@@ -28,30 +27,20 @@ class NotificationRelatedEntity extends StatelessWidget {
     if (eventReference is ReplaceableEventReference && eventReference.kind == ArticleEntity.kind) {
       return Padding(
         padding: EdgeInsetsDirectional.only(top: 12.0.s, end: 16.0.s),
-        child: GestureDetector(
-          onTap: () {
-            ArticleDetailsRoute(eventReference: eventReference.encode()).push<void>(context);
-          },
-          child: Article(
-            eventReference: eventReference,
-            header: const SizedBox.shrink(),
-            footer: const SizedBox.shrink(),
-          ),
+        child: Article(
+          eventReference: eventReference,
+          header: const SizedBox.shrink(),
+          footer: const SizedBox.shrink(),
         ),
       );
     }
 
-    return GestureDetector(
-      onTap: () {
-        PostDetailsRoute(eventReference: eventReference.encode()).push<void>(context);
-      },
-      child: Post(
-        eventReference: eventReference,
-        header: const SizedBox.shrink(),
-        footer: const SizedBox.shrink(),
-        topOffset: 6.0.s,
-        headerOffset: 0,
-      ),
+    return Post(
+      eventReference: eventReference,
+      header: const SizedBox.shrink(),
+      footer: const SizedBox.shrink(),
+      topOffset: 6.0.s,
+      headerOffset: 0,
     );
   }
 }
