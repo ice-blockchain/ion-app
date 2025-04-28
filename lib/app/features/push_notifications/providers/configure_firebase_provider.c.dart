21dd1d05a6cb210156d465ea8eae71b3839f2e05
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-04-28 15:49:29 +0300

    chore: check if configured app is available

--- lib/app/features/push_notifications/providers/configure_firebase_provider.c.dart
+++ lib/app/features/push_notifications/providers/configure_firebase_provider.c.dart
@@ -7,32 +7,35 @@ import 'package:ion/app/features/ion_connect/providers/relay_info_provider.c.dar
 import 'package:ion/app/features/user/providers/user_relays_manager.c.dart';
 import 'package:ion/app/services/firebase/firebase_service_provider.c.dart';
 import 'package:ion/app/services/logger/logger.dart';
+import 'package:ion/app/services/storage/local_storage.c.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'configure_firebase_provider.c.g.dart';
 
 @Riverpod(keepAlive: true)
 class ConfigureFirebase extends _$ConfigureFirebase {
+  static const String _configuredFirebaseRelayKey = 'configured_firebase_relay_key';
+
   @override
   Future<void> build() async {
-    final firebaseService = ref.watch(firebaseServiceProvider);
-
     // onLogout should be at the top of the build method
-    onLogout(ref, firebaseService.deleteApp);
+    onLogout(ref, _deleteConfiguredFirebaseApp);
 
     final authState = await ref.watch(authProvider.future);
-
-    if (!authState.isAuthenticated || firebaseService.hasApp()) {
+    if (!authState.isAuthenticated) {
       return;
     }
 
     final userRelay = await ref.watch(currentUserRelayProvider.future);
-
     if (userRelay == null) {
       return;
     }
 
     final relayUrls = [...userRelay.urls];
+
+    if (await _hasConfiguredAppForRelays(relayUrls)) {
+      return;
+    }
 
     while (relayUrls.isNotEmpty) {
       final relayUrl = relayUrls.random;
@@ -64,6 +67,7 @@ class ConfigureFirebase extends _$ConfigureFirebase {
           projectId: fbConfig.projectId,
         ),
       );
+      await ref.read(localStorageProvider).setString(_configuredFirebaseRelayKey, relayUrl);
       return true;
     } catch (error, stackTrace) {
       Logger.error(
@@ -73,5 +77,29 @@ class ConfigureFirebase extends _$ConfigureFirebase {
       );
       return false;
     }
+  }
+
+  void _deleteConfiguredFirebaseApp() {
+    ref.read(firebaseServiceProvider).deleteApp();
+    ref.read(localStorageProvider).remove(_configuredFirebaseRelayKey);
+  }
+
+  Future<bool> _hasConfiguredAppForRelays(List<String> relayUrls) async {
+    final firebaseService = ref.watch(firebaseServiceProvider);
+
+    if (!firebaseService.hasApp()) {
+      return false;
+    }
+
+    // It might be that the app was configured for a relay that is not available anymore.
+    final configuredRelayIsAvailable =
+        relayUrls.contains(ref.watch(localStorageProvider).getString(_configuredFirebaseRelayKey));
+
+    if (!configuredRelayIsAvailable) {
+      _deleteConfiguredFirebaseApp();
+      return false;
+    }
+
+    return true;
   }
 }
