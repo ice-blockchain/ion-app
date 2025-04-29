3052e4fa7f42bf4952c50ddf736ce534198a2d5c
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-04-29 14:15:18 +0300

    feat: add PushSubscription model and related tags / models

---
+++ lib/app/features/ion_connect/model/related_relay.c.dart
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:ion/app/exceptions/exceptions.dart';
+
+part 'related_relay.c.freezed.dart';
+
+@freezed
+class RelatedRelay with _$RelatedRelay {
+  const factory RelatedRelay({
+    required String url,
+  }) = _RelatedRelay;
+
+  const RelatedRelay._();
+
+  factory RelatedRelay.fromTag(List<String> tag) {
+    if (tag[0] != tagName) {
+      throw IncorrectEventTagNameException(actual: tag[0], expected: tagName);
+    }
+    if (tag.length != 2) {
+      throw IncorrectEventTagException(tag: tag.toString());
+    }
+
+    return RelatedRelay(url: tag[1]);
+  }
+
+  static const String tagName = 'relay';
+
+  List<String> toTag() {
+    return [tagName, url];
+  }
+}
---
+++ lib/app/features/push_notifications/data/models/push_subscription.c.dart
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:collection/collection.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:ion/app/exceptions/exceptions.dart';
+import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/ion_connect/ion_connect.dart';
+import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
+import 'package:ion/app/features/ion_connect/model/event_serializable.dart';
+import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
+import 'package:ion/app/features/ion_connect/model/related_relay.c.dart';
+import 'package:ion/app/features/ion_connect/model/related_token.c.dart';
+import 'package:ion/app/features/ion_connect/model/replaceable_event_identifier.c.dart';
+import 'package:ion/app/features/ion_connect/providers/ion_connect_cache.c.dart';
+import 'package:ion/app/features/push_notifications/data/models/push_subscription_platform.c.dart';
+
+part 'push_subscription.c.freezed.dart';
+
+@Freezed(equal: false)
+class PushSubscription
+    with IonConnectEntity, CacheableEntity, ReplaceableEntity, _$PushSubscription {
+  const factory PushSubscription({
+    required String id,
+    required String pubkey,
+    required String masterPubkey,
+    required String signature,
+    required DateTime createdAt,
+    required PushSubscriptionData data,
+  }) = _PushSubscription;
+
+  const PushSubscription._();
+
+  /// https://github.com/ice-blockchain/subzero/blob/master/.ion-connect-protocol/ICIP-8000.md#registering-device-tokens
+  factory PushSubscription.fromEventMessage(EventMessage eventMessage) {
+    if (eventMessage.kind != kind) {
+      throw IncorrectEventKindException(eventMessage.id, kind: kind);
+    }
+
+    return PushSubscription(
+      id: eventMessage.id,
+      pubkey: eventMessage.pubkey,
+      masterPubkey: eventMessage.masterPubkey,
+      signature: eventMessage.sig!,
+      createdAt: eventMessage.createdAt,
+      data: PushSubscriptionData.fromEventMessage(eventMessage),
+    );
+  }
+
+  static const int kind = 31751;
+}
+
+@freezed
+class PushSubscriptionData
+    with _$PushSubscriptionData
+    implements EventSerializable, ReplaceableEntityData {
+  const factory PushSubscriptionData({
+    required String deviceId,
+    required PushSubscriptionPlatform platform,
+    required RelatedRelay relay,
+    required RelatedToken fcmToken,
+    required List<RequestFilter> filters,
+  }) = _PushSubscriptionData;
+
+  const PushSubscriptionData._();
+
+  factory PushSubscriptionData.fromEventMessage(EventMessage eventMessage) {
+    final tags = groupBy(eventMessage.tags, (tag) => tag[0]);
+
+    return PushSubscriptionData(
+      deviceId: tags[ReplaceableEventIdentifier.tagName]!
+          .map(ReplaceableEventIdentifier.fromTag)
+          .first
+          .value,
+      platform: tags[PushSubscriptionPlatform.tagName]!.map(PushSubscriptionPlatform.fromTag).first,
+      relay: tags[RelatedRelay.tagName]!.map(RelatedRelay.fromTag).first,
+      fcmToken: tags[RelatedToken.tagName]!.map(RelatedToken.fromTag).first,
+      filters: (jsonDecode(eventMessage.content) as List<dynamic>)
+          .map<RequestFilter>(
+            (filterJson) => RequestFilter.fromJson(filterJson as Map<String, dynamic>),
+          )
+          .toList(),
+    );
+  }
+
+  @override
+  FutureOr<EventMessage> toEventMessage(
+    EventSigner signer, {
+    List<List<String>> tags = const [],
+    DateTime? createdAt,
+  }) {
+    return EventMessage.fromData(
+      signer: signer,
+      createdAt: createdAt,
+      kind: PushSubscription.kind,
+      content: jsonEncode(filters),
+      tags: [
+        ...tags,
+        [ReplaceableEventIdentifier.tagName, deviceId],
+        relay.toTag(),
+        fcmToken.toTag(),
+        platform.toTag(),
+      ],
+    );
+  }
+
+  @override
+  ReplaceableEventReference toReplaceableEventReference(String pubkey) {
+    return ReplaceableEventReference(
+      kind: PushSubscription.kind,
+      pubkey: pubkey,
+      dTag: deviceId,
+    );
+  }
+}
