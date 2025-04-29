d048c3e880e95b571d1e09a0bc0f726f48b832c0
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-04-29 09:58:50 +0300

    fix: resend failed message and foreign key issue on local db

--- lib/app/features/chat/e2ee/providers/send_chat_message/send_e2ee_chat_message_service.c.dart
+++ lib/app/features/chat/e2ee/providers/send_chat_message/send_e2ee_chat_message_service.c.dart
@@ -433,7 +433,8 @@ class SendE2eeChatMessageService {
       conversationId: entity.data.conversationId,
       failedEventMessageId: messageEvent.id,
       participantsMasterPubkeys: entity.allPubkeys,
-      failedParticipantsMasterPubkeys: failedParticipantsMasterPubkeys,
+      failedParticipantsMasterPubkeys:
+          failedParticipantsMasterPubkeys.isNotEmpty ? failedParticipantsMasterPubkeys : null,
     );
   }
 
