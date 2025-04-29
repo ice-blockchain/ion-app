e4d19a674f590721ea3b5ee9349b3b0e19a98600
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-04-29 08:22:57 +0200

    feat: fetch user relays from db instead of memory cache, add refetch logic when most of the cached relays are dead (#1112)
    ## Description
    This PR migrates `UserRelaysManager` to use db cache instead of memory
    cache.
    It also adds logic to refetch the relays if most of them are marked as
    dead
    
    ## Additional Notes
    <!-- Add any extra context or relevant information here. -->
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <!-- Include screenshots to demonstrate any UI changes. -->
    <!-- <img width="180" alt="image" src="image_url_here"> -->

--- lib/app/features/ion_connect/providers/ion_connect_db_cache_notifier.c.dart
+++ lib/app/features/ion_connect/providers/ion_connect_db_cache_notifier.c.dart
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:async/async.dart';
+import 'package:ion/app/features/ion_connect/ion_connect.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
 import 'package:ion/app/features/ion_connect/model/event_serializable.dart';
 import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
@@ -12,16 +14,45 @@ part 'ion_connect_db_cache_notifier.c.g.dart';
 @riverpod
 class IonConnectDbCache extends _$IonConnectDbCache {
   @override
-  Stream<List<IonConnectEntity>> build(List<EventReference> eventReferences) {
+  FutureOr<void> build() async {}
+
+  Stream<List<IonConnectEntity>> watch(List<EventReference> eventReferences) {
     final eventMessagesStream =
         ref.watch(eventMessagesRepositoryProvider).watchAll(eventReferences);
     final parser = ref.read(eventParserProvider);
     return eventMessagesStream.map((eventMessages) => eventMessages.map(parser.parse).toList());
   }
 
+  Future<List<IonConnectEntity>> get(List<EventReference> eventReferences) async {
+    final eventMessages =
+        await ref.read(eventMessagesRepositoryProvider).watchAll(eventReferences).firstOrNull;
+    final parser = ref.read(eventParserProvider);
+    return eventMessages?.map(parser.parse).toList() ?? [];
+  }
+
   Future<void> save(EntityEventSerializable eventSerializable) async {
     final eventMessage = await eventSerializable.toEntityEventMessage();
     final eventReference = eventSerializable.toEventReference();
     await ref.read(eventMessagesRepositoryProvider).save(eventMessage, eventReference);
+  }
+
+  Future<void> saveAll(List<EntityEventSerializable> eventSerializables) async {
+    if (eventSerializables.isEmpty) return;
+    final eventMessages = await Future.wait(
+      eventSerializables.map(
+        (eventSerializable) {
+          final eventMessage = eventSerializable.toEntityEventMessage();
+          if (eventMessage is Future) {
+            return eventMessage as Future<EventMessage>;
+          } else {
+            return Future<EventMessage>.value(eventMessage);
+          }
+        },
+      ),
+    );
+    final eventReferences = eventSerializables
+        .map((eventSerializable) => eventSerializable.toEventReference())
+        .toList();
+    await ref.read(eventMessagesRepositoryProvider).saveAll(eventMessages, eventReferences);
   }
 }
