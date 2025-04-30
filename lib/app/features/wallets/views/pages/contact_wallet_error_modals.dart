855f31d06250c6f4698c4986e32b2eff95c81cb7
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-04-30 15:53:53 +0300

    chore: rename locales

--- lib/app/features/wallets/views/pages/contact_wallet_error_modals.dart
+++ lib/app/features/wallets/views/pages/contact_wallet_error_modals.dart
@@ -96,9 +96,9 @@ Future<void> showContactWalletError(
       ? context.i18n.contact_wallet_is_private_title
       : context.i18n.contact_wallet_not_found_title;
 
-final description = isPrivate
-    ? '${context.i18n.contact_wallet_is_private_description_1}\n${context.i18n.contact_wallet_is_private_description_2}'
-    : context.i18n.contact_wallet_not_found_description(network.displayName);
+  final description = isPrivate
+      ? '${context.i18n.contact_wallet_is_private}\n${context.i18n.contact_wallet_is_private_description}'
+      : context.i18n.contact_wallet_not_found_description(network.displayName);
 
   final iconPath =
       isPrivate ? Assets.svg.actionwalletprivatewallet : Assets.svg.actionwalleterrorwallet;
