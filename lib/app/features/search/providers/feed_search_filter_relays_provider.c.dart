9f52011a9cc10041b47c9f96856c3e90035ca54d
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-05-06 12:59:49 +0300

    chore: remove fetching user relays from indexers

--- lib/app/features/search/providers/feed_search_filter_relays_provider.c.dart
+++ lib/app/features/search/providers/feed_search_filter_relays_provider.c.dart
@@ -2,7 +2,6 @@
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/exceptions/exceptions.dart';
-import 'package:ion/app/features/ion_connect/model/action_source.c.dart';
 
 import 'package:ion/app/features/search/model/feed_search_source.dart';
 import 'package:ion/app/features/user/model/user_relays.c.dart';
@@ -18,10 +17,7 @@ Future<Map<String, List<String>>> feedSearchFilterRelays(Ref ref, FeedSearchSour
   final followList = await ref.watch(currentUserFollowListProvider.future);
 
   final followListRelays = followList != null
-      ? await ref.read(userRelaysManagerProvider.notifier).fetch(
-            followList.pubkeys,
-            actionSource: const ActionSourceCurrentUser(),
-          )
+      ? await ref.read(userRelaysManagerProvider.notifier).fetch(followList.pubkeys)
       : <UserRelaysEntity>[];
 
   switch (source) {
--- lib/app/features/user/providers/user_relays_manager.c.dart
+++ lib/app/features/user/providers/user_relays_manager.c.dart
@@ -4,8 +4,6 @@ import 'package:collection/collection.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/exceptions/exceptions.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
-import 'package:ion/app/features/ion_connect/ion_connect.dart';
-import 'package:ion/app/features/ion_connect/model/action_source.c.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_cache.c.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_db_cache_notifier.c.dart';
@@ -50,10 +48,7 @@ class UserRelaysManager extends _$UserRelaysManager {
   @override
   FutureOr<void> build() async {}
 
-  Future<List<UserRelaysEntity>> fetch(
-    List<String> pubkeys, {
-    ActionSource actionSource = const ActionSourceIndexers(),
-  }) async {
+  Future<List<UserRelaysEntity>> fetch(List<String> pubkeys) async {
     final result = <UserRelaysEntity>[];
     final pubkeysToFetch = <String>[];
     final fetchedRelays = <UserRelaysEntity>[];
@@ -88,33 +83,10 @@ class UserRelaysManager extends _$UserRelaysManager {
       return result;
     }
 
-    final requestMessage = RequestMessage()
-      ..addFilter(
-        RequestFilter(kinds: const [UserRelaysEntity.kind], authors: pubkeysToFetch),
-      );
+    final userRelays = await _fetchRelaysFromIdentity(pubkeys: pubkeysToFetch);
 
-    final entitiesStream = ref
-        .read(ionConnectNotifierProvider.notifier)
-        .requestEntities(requestMessage, actionSource: actionSource);
-
-    await for (final entity in entitiesStream) {
-      if (entity is UserRelaysEntity) {
-        result.add(entity);
-        fetchedRelays.add(entity);
-        pubkeysToFetch.removeWhere((pubkey) {
-          // In some corner cases we might use `pubkey` instead on `masterPubkey`,
-          // For example for kind14 chat events that don't have masterPubkey
-          return pubkey == entity.masterPubkey || pubkey == entity.pubkey;
-        });
-      }
-    }
-
-    if (pubkeysToFetch.isNotEmpty) {
-      final userRelays = await _fetchRelaysFromIdentityFor(pubkeys: pubkeysToFetch);
-
-      fetchedRelays.addAll(userRelays);
-      result.addAll(userRelays);
-    }
+    fetchedRelays.addAll(userRelays);
+    result.addAll(userRelays);
 
     if (fetchedRelays.isNotEmpty) {
       await _clearReachabilityInfoFor(fetchedRelays);
@@ -123,7 +95,7 @@ class UserRelaysManager extends _$UserRelaysManager {
     return result;
   }
 
-  Future<List<UserRelaysEntity>> _fetchRelaysFromIdentityFor({
+  Future<List<UserRelaysEntity>> _fetchRelaysFromIdentity({
     required List<String> pubkeys,
   }) async {
     final ionIdentity = await ref.read(ionIdentityClientProvider.future);
