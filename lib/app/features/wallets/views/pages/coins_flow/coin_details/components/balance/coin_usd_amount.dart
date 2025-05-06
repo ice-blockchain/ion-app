81f1c842f920f866825806afd83c437316aeb61b
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-05-06 15:28:24 +0200

    feat: add balance display order switch on top balance amount tap

---
+++ lib/app/features/wallets/views/pages/coins_flow/coin_details/components/balance/coin_usd_amount.dart
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/extensions/asset_gen_image.dart';
+import 'package:ion/app/extensions/build_context.dart';
+import 'package:ion/app/extensions/num.dart';
+import 'package:ion/app/extensions/theme_data.dart';
+import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
+import 'package:ion/app/features/wallets/model/balance_display_order.dart';
+import 'package:ion/app/features/wallets/model/coins_group.c.dart';
+import 'package:ion/app/features/wallets/providers/wallet_user_preferences/user_preferences_selectors.c.dart';
+import 'package:ion/app/features/wallets/providers/wallet_user_preferences/wallet_user_preferences_provider.c.dart';
+import 'package:ion/app/utils/num.dart';
+import 'package:ion/generated/assets.gen.dart';
+
+class CoinUsdAmount extends HookConsumerWidget {
+  const CoinUsdAmount({required this.coinsGroup, super.key});
+
+  final CoinsGroup coinsGroup;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final coinText = '${formatDouble(coinsGroup.totalAmount)} ${coinsGroup.abbreviation}';
+    final usdText = '~ ${formatToCurrency(coinsGroup.totalBalanceUSD)}';
+    final displayOrder = ref.watch(balanceDisplayOrderProvider);
+
+    return Column(
+      children: [
+        GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            final identityKeyName = ref.read(currentIdentityKeyNameSelectorProvider) ?? '';
+            ref
+                .read(
+                  walletUserPreferencesNotifierProvider(identityKeyName: identityKeyName).notifier,
+                )
+                .switchBalanceDisplayOrder();
+          },
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                displayOrder == BalanceDisplayOrder.coinUsd ? coinText : usdText,
+                style: context.theme.appTextThemes.headline1.copyWith(
+                  color: context.theme.appColors.primaryText,
+                ),
+              ),
+              SizedBox(
+                width: 4.0.s,
+              ),
+              Assets.svg.iconArrowSelect.icon(size: 12.0.s),
+            ],
+          ),
+        ),
+        Text(
+          displayOrder == BalanceDisplayOrder.coinUsd ? usdText : coinText,
+          style: context.theme.appTextThemes.subtitle2.copyWith(
+            color: context.theme.appColors.onTertararyBackground,
+          ),
+        ),
+      ],
+    );
+  }
+}
