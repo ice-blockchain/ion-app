9d42550d291b8f2ea5fb41e4b2d29e71e16afd25
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-05-06 11:52:19 +0300

    feat: add copy functionality to message reaction context menu and clean up layout

--- lib/app/features/chat/views/components/message_items/message_reaction_dialog/components/message_reaction_context_menu.dart
+++ lib/app/features/chat/views/components/message_items/message_reaction_dialog/components/message_reaction_context_menu.dart
@@ -20,19 +20,18 @@ import 'package:ion/app/features/chat/recent_chats/providers/selected_reply_mess
 import 'package:ion/app/features/core/model/feature_flags.dart';
 import 'package:ion/app/features/core/providers/feature_flags_provider.c.dart';
 import 'package:ion/app/router/app_routes.c.dart';
+import 'package:ion/app/services/clipboard/clipboard.dart';
 import 'package:ion/generated/assets.gen.dart';
 
 class MessageReactionContextMenu extends HookConsumerWidget {
   const MessageReactionContextMenu({
     required this.isMe,
-    required this.height,
     required this.messageItem,
     required this.messageStatus,
     super.key,
   });
 
   final bool isMe;
-  final double height;
   final ChatMessageInfoItem messageItem;
   final MessageDeliveryStatus messageStatus;
 
@@ -47,77 +46,77 @@ class MessageReactionContextMenu extends HookConsumerWidget {
     final canEdit = entityData.editingEndedAt.value.isAfter(DateTime.now());
     final hideChatBookmark =
         ref.watch(featureFlagsProvider.notifier).get(ChatFeatureFlag.hideChatBookmark);
+    final canCopy = messageItem.eventMessage.content.isNotEmpty;
 
-    return SizedBox(
-      height: height,
-      child: Padding(
-        padding: EdgeInsetsDirectional.only(top: 6.0.s),
-        child: OverlayMenuContainer(
-          child: Column(
-            children: [
-              if (messageStatus == MessageDeliveryStatus.failed)
+    return Padding(
+      padding: EdgeInsetsDirectional.only(top: 6.0.s),
+      child: OverlayMenuContainer(
+        child: Column(
+          children: [
+            if (messageStatus == MessageDeliveryStatus.failed)
+              OverlayMenuItem(
+                label: context.i18n.button_retry,
+                icon: Assets.svg.iconMessageRetry.icon(
+                  size: iconSize,
+                  color: context.theme.appColors.quaternaryText,
+                ),
+                onPressed: () async {
+                  unawaited(
+                    ref.read(sendE2eeChatMessageServiceProvider).resendMessage(
+                          messageEvent: messageItem.eventMessage,
+                        ),
+                  );
+
+                  if (context.mounted) {
+                    context.pop();
+                  }
+                },
+                minWidth: 140.0.s,
+                verticalPadding: 12.0.s,
+              )
+            else ...[
+              OverlayMenuItem(
+                label: context.i18n.button_reply,
+                icon: Assets.svg.iconChatReply.icon(
+                  size: iconSize,
+                  color: context.theme.appColors.quaternaryText,
+                ),
+                onPressed: () {
+                  ref.read(selectedReplyMessageProvider.notifier).selectedReplyMessage =
+                      messageItem;
+                  context.pop();
+                },
+                minWidth: 140.0.s,
+                verticalPadding: 12.0.s,
+              ),
+              const OverlayMenuItemSeparator(),
+              if (isMe &&
+                  canEdit &&
+                  (((messageItem is TextItem) && !(messageItem as TextItem).isStoryReply) ||
+                      messageItem is EmojiItem))
                 OverlayMenuItem(
-                  label: context.i18n.button_retry,
-                  icon: Assets.svg.iconMessageRetry.icon(
-                    size: iconSize,
-                    color: context.theme.appColors.quaternaryText,
-                  ),
-                  onPressed: () async {
-                    unawaited(
-                      ref.read(sendE2eeChatMessageServiceProvider).resendMessage(
-                            messageEvent: messageItem.eventMessage,
-                          ),
-                    );
-
-                    if (context.mounted) {
-                      context.pop();
-                    }
-                  },
-                  minWidth: 140.0.s,
+                  label: context.i18n.button_edit,
                   verticalPadding: 12.0.s,
-                )
-              else ...[
-                OverlayMenuItem(
-                  label: context.i18n.button_reply,
-                  icon: Assets.svg.iconChatReply.icon(
-                    size: iconSize,
-                    color: context.theme.appColors.quaternaryText,
-                  ),
+                  icon: Assets.svg.iconEditLink
+                      .icon(size: iconSize, color: context.theme.appColors.quaternaryText),
                   onPressed: () {
-                    ref.read(selectedReplyMessageProvider.notifier).selectedReplyMessage =
+                    ref.read(selectedReplyMessageProvider.notifier).clear();
+                    ref.read(selectedEditMessageProvider.notifier).selectedEditMessage =
                         messageItem;
                     context.pop();
                   },
-                  minWidth: 140.0.s,
+                )
+              else
+                OverlayMenuItem(
+                  label: context.i18n.button_forward,
                   verticalPadding: 12.0.s,
+                  icon: Assets.svg.iconChatForward
+                      .icon(size: iconSize, color: context.theme.appColors.quaternaryText),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
                 ),
-                const OverlayMenuItemSeparator(),
-                if (isMe &&
-                    canEdit &&
-                    (((messageItem is TextItem) && !(messageItem as TextItem).isStoryReply) ||
-                        messageItem is EmojiItem))
-                  OverlayMenuItem(
-                    label: context.i18n.button_edit,
-                    verticalPadding: 12.0.s,
-                    icon: Assets.svg.iconEditLink
-                        .icon(size: iconSize, color: context.theme.appColors.quaternaryText),
-                    onPressed: () {
-                      ref.read(selectedReplyMessageProvider.notifier).clear();
-                      ref.read(selectedEditMessageProvider.notifier).selectedEditMessage =
-                          messageItem;
-                      context.pop();
-                    },
-                  )
-                else
-                  OverlayMenuItem(
-                    label: context.i18n.button_forward,
-                    verticalPadding: 12.0.s,
-                    icon: Assets.svg.iconChatForward
-                        .icon(size: iconSize, color: context.theme.appColors.quaternaryText),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
+              if (canCopy) ...[
                 const OverlayMenuItemSeparator(),
                 OverlayMenuItem(
                   label: context.i18n.button_copy,
@@ -125,49 +124,50 @@ class MessageReactionContextMenu extends HookConsumerWidget {
                   icon: Assets.svg.iconBlockCopyBlue
                       .icon(size: iconSize, color: context.theme.appColors.quaternaryText),
                   onPressed: () {
+                    copyToClipboard(messageItem.eventMessage.content);
                     Navigator.of(context).pop();
                   },
                 ),
-                if (!hideChatBookmark) ...[
-                  const OverlayMenuItemSeparator(),
-                  OverlayMenuItem(
-                    label: context.i18n.button_bookmark,
-                    verticalPadding: 12.0.s,
-                    icon: Assets.svg.iconBookmarks
-                        .icon(size: iconSize, color: context.theme.appColors.quaternaryText),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
               ],
-              const OverlayMenuItemSeparator(),
-              OverlayMenuItem(
-                label: context.i18n.button_delete,
-                labelColor: context.theme.appColors.attentionRed,
-                verticalPadding: 12.0.s,
-                icon: Assets.svg.iconBlockDelete
-                    .icon(size: iconSize, color: context.theme.appColors.attentionRed),
-                onPressed: () async {
-                  final forEveryone = await DeleteMessageRoute(
-                    isMe: isMe,
-                  ).push<bool>(context);
-
-                  if (forEveryone != null && context.mounted) {
-                    final messageEventsList = [messageItem.eventMessage];
-
-                    ref.read(
-                      e2eeDeleteMessageProvider(
-                        forEveryone: forEveryone,
-                        messageEvents: messageEventsList,
-                      ),
-                    );
-                    context.pop();
-                  }
-                },
-              ),
+              if (!hideChatBookmark) ...[
+                const OverlayMenuItemSeparator(),
+                OverlayMenuItem(
+                  label: context.i18n.button_bookmark,
+                  verticalPadding: 12.0.s,
+                  icon: Assets.svg.iconBookmarks
+                      .icon(size: iconSize, color: context.theme.appColors.quaternaryText),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
             ],
-          ),
+            const OverlayMenuItemSeparator(),
+            OverlayMenuItem(
+              label: context.i18n.button_delete,
+              labelColor: context.theme.appColors.attentionRed,
+              verticalPadding: 12.0.s,
+              icon: Assets.svg.iconBlockDelete
+                  .icon(size: iconSize, color: context.theme.appColors.attentionRed),
+              onPressed: () async {
+                final forEveryone = await DeleteMessageRoute(
+                  isMe: isMe,
+                ).push<bool>(context);
+
+                if (forEveryone != null && context.mounted) {
+                  final messageEventsList = [messageItem.eventMessage];
+
+                  ref.read(
+                    e2eeDeleteMessageProvider(
+                      forEveryone: forEveryone,
+                      messageEvents: messageEventsList,
+                    ),
+                  );
+                  context.pop();
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
