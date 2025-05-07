5b14cbcec0a742b26abaf3c23dcdb9017abeb7ac
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-05-07 17:18:33 +0300

    feat: add local notifications, configure notification styles

--- lib/app/features/push_notifications/background/firebase_messaging_background_service.dart
+++ lib/app/features/push_notifications/background/firebase_messaging_background_service.dart
@@ -2,11 +2,23 @@ import 'dart:io';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
+import 'package:ion/app/services/local_notifications/local_notifications.c.dart';
 
 @pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  //TODO::null is arrived when dismissed manually
-  print('Handling a background message: ${message.messageId}');
+  final notificationsService = LocalNotificationsService();
+  await notificationsService.initialize();
+
+  if (message.notification != null) {
+    return;
+  }
+
+  await notificationsService.showNotification(
+    id: message.messageId.hashCode,
+    title: 'No Title',
+    body: 'No Body',
+    payload: message.data.toString(),
+  );
 }
 
 void initFirebaseMessagingBackgroundHandler() {
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -22,6 +22,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
   blurhash_ffi
+  flutter_local_notifications_windows
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
