08052aec645e327fc68b9834d79dae8ce13b21ee
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-05-08 17:40:54 +0100

    bugfix: add reply to media message

--- lib/app/features/chat/views/components/message_items/message_types/visual_media_message/visual_media_message.dart
+++ lib/app/features/chat/views/components/message_items/message_types/visual_media_message/visual_media_message.dart
@@ -8,7 +8,9 @@ import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
 import 'package:ion/app/features/chat/e2ee/model/entities/private_direct_message_data.c.dart';
 import 'package:ion/app/features/chat/e2ee/providers/chat_medias_provider.c.dart';
 import 'package:ion/app/features/chat/model/message_list_item.c.dart';
+import 'package:ion/app/features/chat/recent_chats/providers/replied_message_list_item_provider.c.dart';
 import 'package:ion/app/features/chat/views/components/message_items/message_item_wrapper/message_item_wrapper.dart';
+import 'package:ion/app/features/chat/views/components/message_items/message_types/reply_message/reply_message.dart';
 import 'package:ion/app/features/chat/views/components/message_items/message_types/visual_media_message/visual_media_custom_grid.dart';
 import 'package:ion/app/features/chat/views/components/message_items/message_types/visual_media_message/visual_media_metadata.dart';
 import 'package:ion/app/features/ion_connect/ion_connect.dart';
@@ -36,6 +38,19 @@ class VisualMediaMessage extends HookConsumerWidget {
     final messageMedias =
         ref.watch(chatMediasProvider(eventReference: eventReference)).valueOrNull ?? [];
 
+    final messageItem = MediaItem(
+      eventMessage: eventMessage,
+      contentDescription: context.i18n.common_media,
+      medias: messageMedias,
+    );
+
+    final repliedEventMessage = ref.watch(repliedMessageListItemProvider(messageItem));
+
+    final repliedMessageItem = getRepliedMessageListItem(
+      ref: ref,
+      repliedEventMessage: repliedEventMessage.valueOrNull,
+    );
+
     return MessageItemWrapper(
       isMe: isMe,
       messageItem: MediaItem(
@@ -45,9 +60,13 @@ class VisualMediaMessage extends HookConsumerWidget {
       ),
       contentPadding: EdgeInsets.all(padding),
       child: SizedBox(
-        width: messageMedias.length > 1 ? double.infinity : 146.0.s,
+        width: messageMedias.length > 1 || repliedEventMessage.valueOrNull != null
+            ? double.infinity
+            : 146.0.s,
         child: Column(
           children: [
+            if (repliedMessageItem != null)
+              ReplyMessage(messageItem, repliedMessageItem, onTapReply),
             VisualMediaCustomGrid(
               messageMedias: messageMedias,
               eventMessage: eventMessage,
