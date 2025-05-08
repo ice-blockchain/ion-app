8d8d6e32d4279f9db2c9a322b05a2ac77f696fbb
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-05-08 17:09:29 +0300

    chore: use custom scroll view instead of scrollable positioned list

--- lib/app/features/chat/e2ee/views/components/one_to_one_messages_list.dart
+++ lib/app/features/chat/e2ee/views/components/one_to_one_messages_list.dart
@@ -1,6 +1,3 @@
-// SPDX-License-Identifier: ice License 1.0
-
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -8,128 +5,90 @@ import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/chat/e2ee/model/entities/private_direct_message_data.c.dart';
 import 'package:ion/app/features/chat/model/message_type.dart';
-import 'package:ion/app/features/chat/views/components/message_items/components.dart';
+import 'package:ion/app/features/chat/views/components/message_items/chat_date_header_text/chat_date_header_text.dart';
+import 'package:ion/app/features/chat/views/components/message_items/message_types/audio_message/audio_message.dart';
 import 'package:ion/app/features/chat/views/components/message_items/message_types/document_message/document_message.dart';
+import 'package:ion/app/features/chat/views/components/message_items/message_types/emoji_message/emoji_message.dart';
+import 'package:ion/app/features/chat/views/components/message_items/message_types/money_message/money_message.dart';
+import 'package:ion/app/features/chat/views/components/message_items/message_types/profile_share_message/profile_share_message.dart';
 import 'package:ion/app/features/chat/views/components/message_items/message_types/story_reply_message/story_reply_message.dart';
+import 'package:ion/app/features/chat/views/components/message_items/message_types/text_message/text_message.dart';
 import 'package:ion/app/features/chat/views/components/message_items/message_types/visual_media_message/visual_media_message.dart';
 import 'package:ion/app/features/ion_connect/ion_connect.dart';
-import 'package:ion/app/hooks/use_on_init.dart';
-import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class OneToOneMessageList extends HookConsumerWidget {
-  const OneToOneMessageList(this.messages, {super.key});
+  const OneToOneMessageList(
+    this.messages, {
+    super.key,
+    this.displayAuthorsIncomingMessages = false,
+  });
 
+  final bool displayAuthorsIncomingMessages;
   final Map<DateTime, List<EventMessage>> messages;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final allMessages = messages.values.expand((e) => e).toList()..sortBy((e) => e.publishedAt);
-    final itemScrollController = useMemoized(ItemScrollController.new);
-
-    useOnInit(() {
-      if (allMessages.isNotEmpty && itemScrollController.isAttached) {
-        itemScrollController.scrollTo(
-          index: allMessages.length,
-          duration: const Duration(milliseconds: 300),
-        );
-      }
-    });
-
-    useEffect(
-      () {
-        if (itemScrollController.isAttached) {
-          itemScrollController.scrollTo(
-            index: allMessages.length,
-            duration: const Duration(milliseconds: 300),
-          );
-        }
-        return null;
-      },
-      [allMessages],
-    );
-
-    final onTapReply = useCallback(
-      (ReplaceablePrivateDirectMessageEntity entity) {
-        final replyMessage = entity.data.relatedEvents?.singleOrNull;
-
-        if (replyMessage != null) {
-          final replyMessageIndex = allMessages.indexWhere(
-            (element) => element.sharedId == replyMessage.eventReference.dTag,
-          );
-          itemScrollController.scrollTo(
-            index: replyMessageIndex,
-            duration: const Duration(milliseconds: 300),
-          );
-        }
-      },
-      [allMessages, itemScrollController],
-    );
+    final scrollController = useScrollController();
 
     return ColoredBox(
       color: context.theme.appColors.primaryBackground,
       child: ScreenSideOffset.small(
-        child: ScrollablePositionedList.builder(
+        child: CustomScrollView(
+          controller: scrollController,
           physics: const ClampingScrollPhysics(),
-          itemCount: allMessages.length,
-          itemScrollController: itemScrollController,
-          itemBuilder: (context, index) {
-            final message = allMessages[index];
-            final entity = ReplaceablePrivateDirectMessageEntity.fromEventMessage(message);
-            final displayDate = messages.entries
-                .singleWhereOrNull((entry) => entry.value.last.id == message.id)
-                ?.key;
-
-            final previousMessage = index > 0 ? allMessages[index - 1] : null;
-            final isLastMessage = index == 0;
-
-            final isLastMessageFromAuthor =
-                previousMessage == null || previousMessage.pubkey == message.pubkey;
-
-            return Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                if (displayDate != null)
-                  Center(
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(vertical: 12.0.s),
-                      child: ChatDateHeaderText(date: displayDate),
-                    ),
-                  ),
-                Padding(
-                  padding: EdgeInsetsDirectional.only(
-                    bottom: isLastMessage
-                        ? 20.0.s
-                        : isLastMessageFromAuthor
-                            ? 8.0.s
-                            : 16.0.s,
-                  ),
-                  child: switch (entity.data.messageType) {
-                    MessageType.text => TextMessage(
-                        eventMessage: message,
-                        onTapReply: () => onTapReply(entity),
-                      ),
-                    MessageType.storyReply => StoryReplyMessage(eventMessage: message),
-                    MessageType.profile => ProfileShareMessage(
-                        eventMessage: message,
-                        onTapReply: () => onTapReply(entity),
-                      ),
-                    MessageType.visualMedia => VisualMediaMessage(
-                        eventMessage: message,
-                        onTapReply: () => onTapReply(entity),
-                      ),
-                    MessageType.requestFunds =>
-                      MoneyMessage(eventMessage: message, onTapReply: () => onTapReply(entity)),
-                    MessageType.emoji =>
-                      EmojiMessage(eventMessage: message, onTapReply: () => onTapReply(entity)),
-                    MessageType.audio =>
-                      AudioMessage(eventMessage: message, onTapReply: () => onTapReply(entity)),
-                    MessageType.document =>
-                      DocumentMessage(eventMessage: message, onTapReply: () => onTapReply(entity)),
+          reverse: true,
+          slivers: [
+            for (final entry in messages.entries) ...[
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  findChildIndexCallback: (key) {
+                    final valueKey = key as ValueKey<String>;
+                    return entry.value.indexWhere((e) => e.id == valueKey.value);
                   },
+                  (context, msgIndex) {
+                    final message = entry.value[msgIndex];
+                    final entity = ReplaceablePrivateDirectMessageEntity.fromEventMessage(message);
+
+                    final previousMessage = msgIndex > 0 ? entry.value[msgIndex - 1] : null;
+                    final isLastMessage = msgIndex == 0;
+
+                    final isLastMessageFromAuthor =
+                        previousMessage == null || previousMessage.pubkey == message.pubkey;
+
+                    return Padding(
+                      key: ValueKey(message.id),
+                      padding: EdgeInsetsDirectional.only(
+                        bottom: isLastMessage
+                            ? 20.0.s
+                            : isLastMessageFromAuthor
+                                ? 8.0.s
+                                : 16.0.s,
+                      ),
+                      child: switch (entity.data.messageType) {
+                        MessageType.text => TextMessage(eventMessage: message),
+                        MessageType.emoji => EmojiMessage(eventMessage: message),
+                        MessageType.audio => AudioMessage(eventMessage: message),
+                        MessageType.document => DocumentMessage(eventMessage: message),
+                        MessageType.requestFunds => MoneyMessage(eventMessage: message),
+                        MessageType.profile => ProfileShareMessage(eventMessage: message),
+                        MessageType.storyReply => StoryReplyMessage(eventMessage: message),
+                        MessageType.visualMedia => VisualMediaMessage(eventMessage: message),
+                      },
+                    );
+                  },
+                  childCount: entry.value.length,
                 ),
-              ],
-            );
-          },
+              ),
+              SliverToBoxAdapter(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 12.0.s),
+                    child: ChatDateHeaderText(date: entry.key),
+                  ),
+                ),
+              ),
+            ],
+          ],
         ),
       ),
     );
