4def3f68dc196e2ed92b87a621721986f906c3a7
ice-tychon <175587874+ice-tychon@users.noreply.github.com>
2025-05-12 11:48:46 +0300

    fix: add IPs to check, add lifecycle check (#1184)
    ## Description
    We still have the false "No internet connection" banner shown in the
    app. This PR tries to fix it by adding more URIs to check and increasing
    timeout.
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore

--- lib/app/features/core/providers/internet_status_stream_provider.c.dart
+++ lib/app/features/core/providers/internet_status_stream_provider.c.dart
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'dart:ui';
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:ion/app/features/core/providers/app_lifecycle_provider.c.dart';
 import 'package:ion/app/features/core/providers/internet_connection_checker_provider.c.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'internet_status_stream_provider.c.g.dart';
 
 @Riverpod(keepAlive: true)
-Stream<InternetStatus> internetStatusStream(Ref ref) {
-  return ref.watch(internetConnectionCheckerProvider).onStatusChange;
+Stream<InternetStatus> internetStatusStream(Ref ref) async* {
+  final lifecycleState = ref.watch(appLifecycleProvider);
+  if (lifecycleState != AppLifecycleState.resumed) {
+    yield InternetStatus.connected;
+    return;
+  }
+  yield* ref.watch(internetConnectionCheckerProvider).onStatusChange;
 }
