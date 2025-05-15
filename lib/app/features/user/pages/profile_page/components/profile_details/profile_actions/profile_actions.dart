d7f3ce468053fd399b85732d1f5309c936e5e67c
ice-hector <96414297+ice-hector@users.noreply.github.com>
2025-05-15 14:31:45 +0300

    fix: remove button if not following

--- lib/app/features/user/pages/profile_page/components/profile_details/profile_actions/profile_actions.dart
+++ lib/app/features/user/pages/profile_page/components/profile_details/profile_actions/profile_actions.dart
@@ -8,6 +8,7 @@ import 'package:ion/app/features/user/model/user_notifications_type.dart';
 import 'package:ion/app/features/user/pages/profile_page/components/profile_details/profile_actions/profile_action.dart';
 import 'package:ion/app/features/user/pages/profile_page/pages/account_notifications_modal/account_notifications_modal.dart';
 import 'package:ion/app/features/user/pages/profile_page/providers/user_notifications_provider.c.dart';
+import 'package:ion/app/features/user/providers/follow_list_provider.c.dart';
 import 'package:ion/app/features/user/providers/user_metadata_provider.c.dart';
 import 'package:ion/app/router/app_routes.c.dart';
 import 'package:ion/app/router/utils/show_simple_bottom_sheet.dart';
@@ -29,6 +30,7 @@ class ProfileActions extends ConsumerWidget {
     final walletsState =
         ref.watch(userMetadataProvider(pubkey).select((state) => state.value?.data.wallets));
     final hasPrivateWallets = walletsState == null;
+    final following = ref.watch(isCurrentUserFollowingSelectorProvider(pubkey));
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -57,20 +59,21 @@ class ProfileActions extends ConsumerWidget {
           assetName: Assets.svg.iconChatOff,
         ),
         SizedBox(width: 8.0.s),
-        ProfileAction(
-          onPressed: () {
-            showSimpleBottomSheet<void>(
-              context: context,
-              child: AccountNotificationsModal(
-                selectedUserNotificationsTypes: userNotificationsTypes,
-              ),
-            );
-          },
-          isAccent: notificationsEnabled,
-          assetName: notificationsEnabled
-              ? Assets.svg.iconProfileNotificationOn
-              : Assets.svg.iconProfileNotificationOff,
-        ),
+        if (following)
+          ProfileAction(
+            onPressed: () {
+              showSimpleBottomSheet<void>(
+                context: context,
+                child: AccountNotificationsModal(
+                  selectedUserNotificationsTypes: userNotificationsTypes,
+                ),
+              );
+            },
+            isAccent: notificationsEnabled,
+            assetName: notificationsEnabled
+                ? Assets.svg.iconProfileNotificationOn
+                : Assets.svg.iconProfileNotificationOff,
+          ),
       ],
     );
   }
