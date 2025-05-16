a9292ee0826a6c4365f5e6fb2343d28eaa5a9c58
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-05-16 13:02:49 +0300

    chore: add licence

--- lib/app/features/chat/e2ee/providers/gift_unwrap_service_provider.c.dart
+++ lib/app/features/chat/e2ee/providers/gift_unwrap_service_provider.c.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/exceptions/exceptions.dart';
