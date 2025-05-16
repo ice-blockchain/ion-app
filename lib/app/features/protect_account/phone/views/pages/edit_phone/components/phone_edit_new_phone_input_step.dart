cda6cbaa51a70a840b9f6e63fa211154a8ff6f69
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-05-16 13:56:52 +0200

    fix: add error when inputted phone number is already linked (#1224)
    ## Description
    <!-- Briefly explain the purpose of this PR and what it accomplishes.
    -->
    
    ## Additional Notes
    <!-- Add any extra context or relevant information here. -->
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    ![ScreenShot 2025-05-16 at 09 15
    58@2x](https://github.com/user-attachments/assets/943c8776-2d8f-4731-b191-c4df7e5d2760)

--- lib/app/features/protect_account/phone/views/pages/edit_phone/components/phone_edit_new_phone_input_step.dart
+++ lib/app/features/protect_account/phone/views/pages/edit_phone/components/phone_edit_new_phone_input_step.dart
@@ -2,9 +2,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/components/modal_sheets/simple_modal_sheet.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/protect_account/components/twofa_step_scaffold.dart';
+import 'package:ion/app/features/protect_account/email/providers/linked_phone_provider.c.dart';
 import 'package:ion/app/features/protect_account/phone/views/components/phone/phone_input_step.dart';
+import 'package:ion/app/router/utils/show_simple_bottom_sheet.dart';
 import 'package:ion/generated/assets.gen.dart';
 
 class PhoneEditNewPhoneInputStep extends HookConsumerWidget {
@@ -15,13 +18,37 @@ class PhoneEditNewPhoneInputStep extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final locale = context.i18n;
+    final linkedPhone = ref.watch(linkedPhoneProvider).valueOrNull;
 
     return TwoFAStepScaffold(
       headerIcon: Assets.svg.icon2faPhoneconfirm.icon(size: 36.0.s),
       headerTitle: locale.two_fa_edit_phone_title,
       headerDescription: locale.two_fa_edit_phone_new_phone_description,
       contentPadding: 0,
-      child: PhoneInputStep(onNext: onNext),
+      child: PhoneInputStep(
+        onNext: (String phone) {
+          if (linkedPhone != null && linkedPhone == phone) {
+            _showPhoneAlreadyLinkedModal(ref);
+          } else {
+            onNext(phone);
+          }
+        },
+      ),
+    );
+  }
+
+  void _showPhoneAlreadyLinkedModal(WidgetRef ref) {
+    showSimpleBottomSheet<void>(
+      context: ref.context,
+      child: SimpleModalSheet.alert(
+        iconAsset: Assets.svg.actionWalletKeyserror,
+        title: ref.context.i18n.common_error,
+        description: ref.context.i18n.two_fa_edit_phone_already_linked,
+        buttonText: ref.context.i18n.button_try_again,
+        isBottomSheet: true,
+        bottomOffset: 0.0.s,
+        topOffset: 30.0.s,
+      ),
     );
   }
 }
