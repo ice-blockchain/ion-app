fdc5f0a4d2382443bb74457c85e81597d1fc325e
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-05-16 13:00:19 +0300

    chore: revert test config

--- lib/app/features/push_notifications/providers/configure_firebase_app_provider.c.dart
+++ lib/app/features/push_notifications/providers/configure_firebase_app_provider.c.dart
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'package:ion/app/features/ion_connect/model/relay_info.c.dart';
 import 'package:ion/app/features/push_notifications/providers/relay_firebase_app_config_provider.c.dart';
 import 'package:ion/app/services/firebase/firebase_messaging_service_provider.c.dart';
 import 'package:ion/app/services/firebase/firebase_service_provider.c.dart';
@@ -14,40 +13,28 @@ class ConfigureFirebaseApp extends _$ConfigureFirebaseApp {
   @override
   Future<bool> build() async {
     final firebaseAppService = ref.watch(firebaseAppServiceProvider);
-    // final savedFirebaseAppConfig = ref.watch(savedFirebaseAppConfigProvider);
+    final savedFirebaseAppConfig = ref.watch(savedFirebaseAppConfigProvider);
 
-    const savedFirebaseAppConfig = RelayFirebaseConfig(
-      firebaseConfig: FirebaseConfig(
-        apiKey: 'AIzaSyBLM-Nmg-YblI5Vl0dzgcKlG8Lob2dlEuk',
-        appId: '1:355345238028:ios:ee303913dfee1a19520629',
-        messagingSenderId: '355345238028',
-        projectId: 'staging3-76387',
-      ),
-      relayUrl: '',
-      relayPubkey: '',
-    );
-
-    if (!firebaseAppService.hasApp()) {
+    if (!firebaseAppService.hasApp() && savedFirebaseAppConfig != null) {
       // Always first initialize the previously configured app.
       // Otherwise we won't be able to re-configure it (delete + init again)
       // in case the configured relay is not available anymore.
       await _initializeFirebaseApp(savedFirebaseAppConfig);
-      return true;
     }
 
-    // final relayFirebaseAppConfig = await ref.watch(relayFirebaseAppConfigProvider.future);
+    final relayFirebaseAppConfig = await ref.watch(relayFirebaseAppConfigProvider.future);
 
-    // if (relayFirebaseAppConfig != null) {
-    //   if (savedFirebaseAppConfig != null && relayFirebaseAppConfig == savedFirebaseAppConfig) {
-    //     return true;
-    //   }
+    if (relayFirebaseAppConfig != null) {
+      if (savedFirebaseAppConfig != null && relayFirebaseAppConfig == savedFirebaseAppConfig) {
+        return true;
+      }
 
-    //   final initialized = await _initializeFirebaseApp(relayFirebaseAppConfig);
-    //   if (initialized) {
-    //     ref.watch(savedFirebaseAppConfigProvider.notifier).config = relayFirebaseAppConfig;
-    //     return true;
-    //   }
-    // }
+      final initialized = await _initializeFirebaseApp(relayFirebaseAppConfig);
+      if (initialized) {
+        ref.watch(savedFirebaseAppConfigProvider.notifier).config = relayFirebaseAppConfig;
+        return true;
+      }
+    }
 
     return false;
   }
