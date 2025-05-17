d3af25e13616feacb9394a0ec51492bc62bba376
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-05-17 09:52:52 +0200

    fix: make link preview in post/article body clickable

--- lib/app/features/feed/views/components/url_preview_content/url_preview_content.dart
+++ lib/app/features/feed/views/components/url_preview_content/url_preview_content.dart
@@ -5,6 +5,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:ion/app/components/image/ion_network_image.dart';
 import 'package:ion/app/components/url_preview/url_preview.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/services/browser/browser.dart';
 import 'package:ion/app/utils/url.dart';
 import 'package:ogp_data_extract/ogp_data_extract.dart';
 
@@ -13,38 +14,44 @@ part 'components/url_metadata_preview.dart';
 class UrlPreviewContent extends HookWidget {
   const UrlPreviewContent({
     required this.url,
+    this.clickable = true,
     super.key,
   });
 
   final String url;
+  final bool clickable;
 
   @override
   Widget build(BuildContext context) {
-    return UrlPreview(
-      key: ValueKey(url),
-      url: url,
-      builder: (meta, favIconUrl) {
-        if (meta == null || meta.title == null || meta.title!.isEmpty) {
-          return const SizedBox.shrink();
-        }
+    return GestureDetector(
+      onTap: clickable ? () => openUrlInAppBrowser(normalizeUrl(url)) : null,
+      behavior: HitTestBehavior.opaque,
+      child: UrlPreview(
+        key: ValueKey(url),
+        url: url,
+        builder: (meta, favIconUrl) {
+          if (meta == null || meta.title == null || meta.title!.isEmpty) {
+            return const SizedBox.shrink();
+          }
 
-        return Container(
-          decoration: BoxDecoration(
-            color: context.theme.appColors.onPrimaryAccent,
-            borderRadius: BorderRadius.all(Radius.circular(16.0.s)),
-            border: Border.all(
-              width: 1.0.s,
-              color: context.theme.appColors.onTerararyFill,
+          return Container(
+            decoration: BoxDecoration(
+              color: context.theme.appColors.onPrimaryAccent,
+              borderRadius: BorderRadius.all(Radius.circular(16.0.s)),
+              border: Border.all(
+                width: 1.0.s,
+                color: context.theme.appColors.onTerararyFill,
+              ),
             ),
-          ),
-          child: _UrlMetadataPreview(
-            key: ValueKey('${url}_${meta.hashCode}'),
-            meta: meta,
-            url: url,
-            favIconUrl: favIconUrl,
-          ),
-        );
-      },
+            child: _UrlMetadataPreview(
+              key: ValueKey('${url}_${meta.hashCode}'),
+              meta: meta,
+              url: url,
+              favIconUrl: favIconUrl,
+            ),
+          );
+        },
+      ),
     );
   }
 }
