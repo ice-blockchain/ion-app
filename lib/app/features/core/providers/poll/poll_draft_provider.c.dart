fad2b0377bb08c46cede6d5c9e08e7bb25b0423a
ice-hector <96414297+ice-hector@users.noreply.github.com>
2025-05-19 18:24:45 +0300

    fix: license

--- lib/app/features/core/providers/poll/poll_draft_provider.c.dart
+++ lib/app/features/core/providers/poll/poll_draft_provider.c.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:ion/app/features/feed/data/models/poll/poll_answer.c.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
