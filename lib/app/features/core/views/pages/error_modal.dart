4e1e5a517bd6bddc251ada2191fce356c073fe6a
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-05-23 09:41:21 +0300

    chore: implement custom error for the paymentNoDestination reason (#1263)
    ## Description
    Implement a custom description for the `paymentNoDestination` exception.
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <img
    src="https://github.com/user-attachments/assets/3c5380fc-0811-4d8e-b21e-812b7eff18af"
    width=200>

--- lib/app/features/core/views/pages/error_modal.dart
+++ lib/app/features/core/views/pages/error_modal.dart
@@ -7,13 +7,10 @@ import 'package:ion/app/components/button/button.dart';
 import 'package:ion/app/components/card/info_card.dart';
 import 'package:ion/app/components/screen_offset/screen_bottom_offset.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
-import 'package:ion/app/exceptions/exceptions.dart';
 import 'package:ion/app/extensions/extensions.dart';
-import 'package:ion/app/features/core/providers/env_provider.c.dart';
+import 'package:ion/app/features/core/providers/exception_presenter_provider.c.dart';
 import 'package:ion/app/router/utils/show_simple_bottom_sheet.dart';
 import 'package:ion/app/services/logger/logger.dart';
-import 'package:ion/generated/assets.gen.dart';
-import 'package:ion_identity_client/ion_identity.dart';
 
 class ErrorModal extends ConsumerWidget {
   ErrorModal({required this.error, super.key}) {
@@ -24,20 +21,8 @@ class ErrorModal extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final showDebugInfo = ref.watch(envProvider.notifier).get<bool>(EnvVariable.SHOW_DEBUG_INFO);
-
-    final title = switch (error) {
-      final IONIdentityException identityException => identityException.title(context),
-      _ => context.i18n.error_general_title,
-    };
-
-    final description = switch (error) {
-      final IONIdentityException identityException => identityException.description(context),
-      Object _ when showDebugInfo => error.toString(),
-      IONException(code: final int code) =>
-        context.i18n.error_general_description(context.i18n.error_general_error_code(code)),
-      _ => context.i18n.error_general_description('')
-    };
+    final exceptionPresenter = ref.watch(exceptionPresenterProvider);
+    final exceptionPresentation = exceptionPresenter.getPresentation(context, error);
 
     return ConstrainedBox(
       constraints: BoxConstraints(maxHeight: MediaQuery.sizeOf(context).height * 0.9),
@@ -48,9 +33,9 @@ class ErrorModal extends ConsumerWidget {
             Padding(
               padding: EdgeInsetsDirectional.only(start: 30.0.s, end: 30.0.s, top: 30.0.s),
               child: InfoCard(
-                title: title,
-                description: description,
-                iconAsset: Assets.svg.actionWalletKeyserror,
+                title: exceptionPresentation.title,
+                description: exceptionPresentation.description,
+                iconAsset: exceptionPresentation.iconPath,
               ),
             ),
             SizedBox(height: 24.0.s),
