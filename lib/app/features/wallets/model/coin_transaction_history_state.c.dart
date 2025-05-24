eac48dadbd22ad9bfa6062de73eb2ef5832da281
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-05-24 11:50:18 +0300

    chore: add license

--- lib/app/features/wallets/model/coin_transaction_history_state.c.dart
+++ lib/app/features/wallets/model/coin_transaction_history_state.c.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:ion/app/features/wallets/model/coin_transaction_data.c.dart';
 
