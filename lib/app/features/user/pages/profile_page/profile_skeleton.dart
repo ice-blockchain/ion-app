d1da3d460896a9f44a9d259a14e9e878a5a09efe
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-05-26 14:46:23 +0100

    chore: license

--- lib/app/features/user/pages/profile_page/profile_skeleton.dart
+++ lib/app/features/user/pages/profile_page/profile_skeleton.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
