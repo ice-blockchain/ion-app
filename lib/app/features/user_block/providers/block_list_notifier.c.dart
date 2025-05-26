a1ef7a5e052ed5d0226ce0012a87e46f7e2c6508
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-05-26 12:01:25 +0100

    feat: Rebase with master

--- lib/app/features/user_block/providers/block_list_notifier.c.dart
+++ lib/app/features/user_block/providers/block_list_notifier.c.dart
@@ -5,8 +5,6 @@ import 'package:ion/app/exceptions/exceptions.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
 import 'package:ion/app/features/feed/data/models/entities/generic_repost.c.dart';
 import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.c.dart';
-import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
-import 'package:ion/app/features/feed/data/models/generic_repost.c.dart';
 import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_entity_provider.c.dart';
 import 'package:ion/app/features/user/providers/follow_list_provider.c.dart';
