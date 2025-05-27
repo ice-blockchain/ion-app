d6791ca4f4f221f6419a294750311f53d512d158
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-05-27 18:27:15 +0300

    feat: add tests for FeedUserInterestPicker

--- lib/app/features/feed/providers/feed_user_interest_picker_provider.c.dart
+++ lib/app/features/feed/providers/feed_user_interest_picker_provider.c.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/feed/data/models/feed_config.c.dart';
 import 'package:ion/app/features/feed/data/models/feed_interests.c.dart';
 import 'package:ion/app/features/feed/data/models/feed_type.dart';
@@ -15,12 +14,16 @@ class FeedUserInterestPicker {
   FeedUserInterestPicker({
     required FeedConfig config,
     required FeedInterests interests,
+    required Random random,
   })  : _config = config,
-        _interests = interests;
+        _interests = interests,
+        _random = random;
 
   final FeedConfig _config;
 
   final FeedInterests _interests;
+
+  final Random _random;
 
   String roll() {
     final categories = _interests.categories.entries.toList();
@@ -29,8 +32,8 @@ class FeedUserInterestPicker {
       // if rolled the scenario where we have to pick a parent category
       // the user is not interested in, we pick an entirely random child
       // of an entirely random parent.
-      final randomCategory = categories.random;
-      return randomCategory.value.children.entries.toList().random.key;
+      final randomCategory = _randomItem(categories);
+      return _randomItem(randomCategory.value.children.entries.toList()).key;
     }
 
     final interestedCategory = _getRandomInterestedByWeight(categories);
@@ -40,7 +43,7 @@ class FeedUserInterestPicker {
       // If rolled the scenario where you have to pick a child category
       // the user is not interested in, we pick an entirely random child
       // of that parent.
-      return interestedCategoryChildren.random.key;
+      return _randomItem(interestedCategoryChildren).key;
     }
 
     return _getRandomInterestedByWeight(interestedCategoryChildren).key;
@@ -48,11 +51,13 @@ class FeedUserInterestPicker {
 
   bool _roll(double chance) => chance > _random.nextDouble();
 
+  T _randomItem<T>(List<T> items) => items[_random.nextInt(items.length)];
+
   T _getRandomInterestedByWeight<T extends MapEntry<String, CategoryWithWeight>>(List<T> items) {
     final totalWeight = items.fold<int>(0, (sum, category) => sum + category.value.weight);
 
     if (totalWeight == 0) {
-      return items.random;
+      return _randomItem(items);
     }
 
     final interestedThreshold = _config.notInterestedThreshold * totalWeight;
@@ -60,7 +65,7 @@ class FeedUserInterestPicker {
     final interested = items.where((item) => item.value.weight > interestedThreshold).toList();
 
     if (interested.isEmpty) {
-      return items.random;
+      return _randomItem(items);
     }
 
     final interestedTotalWeight = interested.fold<int>(0, (sum, item) => sum + item.value.weight);
@@ -75,15 +80,14 @@ class FeedUserInterestPicker {
       }
     }
 
-    return interested.random;
+    return _randomItem(interested);
   }
-
-  static final _random = Random();
 }
 
 @riverpod
 Future<FeedUserInterestPicker> feedUserInterestPicker(Ref ref, FeedType feedType) async {
   final interests = await ref.watch(feedUserInterestsProvider(feedType).future);
   final config = await ref.watch(feedConfigProvider.future);
-  return FeedUserInterestPicker(config: config, interests: interests);
+  final random = Random();
+  return FeedUserInterestPicker(config: config, interests: interests, random: random);
 }
---
+++ test/feed/feed_user_interests_picker_test.dart
@@ -0,0 +1,88 @@
+import 'dart:math';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ion/app/features/feed/data/models/feed_config.c.dart';
+import 'package:ion/app/features/feed/data/models/feed_interests.c.dart';
+import 'package:ion/app/features/feed/providers/feed_user_interest_picker_provider.c.dart';
+
+import 'package:mocktail/mocktail.dart';
+
+class MockRandom extends Mock implements Random {}
+
+void main() {
+  group('FeedUserInterestPicker', () {
+    late FeedConfig config;
+    late FeedInterests interests;
+    late Random random;
+
+    setUp(() {
+      config = const FeedConfig(
+        notInterestedThreshold: 0.5,
+        notInterestedCategoryChance: 0.5,
+        notInterestedSubcategoryChance: 0.5,
+        followingMaxAge: Duration(days: 1),
+        topMaxAge: Duration(days: 1),
+        trendingMaxAge: Duration(days: 1),
+        exploreMaxAge: Duration(days: 1),
+      );
+      interests = const FeedInterests(
+        categories: {
+          'music': FeedInterestsCategory(
+            weight: 2,
+            children: {
+              'rock': FeedInterestsSubcategory(weight: 10),
+              'pop': FeedInterestsSubcategory(weight: 1),
+            },
+          ),
+          'sports': FeedInterestsCategory(
+            weight: 10,
+            children: {
+              'basketball': FeedInterestsSubcategory(weight: 2),
+              'football': FeedInterestsSubcategory(weight: 5),
+            },
+          ),
+        },
+      );
+      random = MockRandom();
+
+      when(() => random.nextDouble()).thenReturn(Random().nextDouble());
+      when(() => random.nextInt(any())).thenReturn(0);
+    });
+
+    test('rolls a random (first) category and sub-category', () {
+      final picker = FeedUserInterestPicker(
+        config: config.copyWith(notInterestedCategoryChance: 1),
+        interests: interests,
+        random: random,
+      );
+      final result = picker.roll();
+      expect(result, equals('rock'));
+    });
+
+    test('rolls a random (first) subcategory from interested category', () {
+      final picker = FeedUserInterestPicker(
+        config: config.copyWith(
+          notInterestedCategoryChance: 0,
+          notInterestedSubcategoryChance: 1,
+        ),
+        interests: interests,
+        random: random,
+      );
+      final result = picker.roll();
+      expect(result, equals('basketball'));
+    });
+
+    test('rolls an interested subcategory from interested category', () {
+      final picker = FeedUserInterestPicker(
+        config: config.copyWith(
+          notInterestedCategoryChance: 0,
+          notInterestedSubcategoryChance: 0,
+        ),
+        interests: interests,
+        random: random,
+      );
+      final result = picker.roll();
+      expect(result, equals('football'));
+    });
+  });
+}
