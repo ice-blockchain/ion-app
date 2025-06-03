bcb5b80bbb622e9db9d1020e0e60a6acc0975fb2
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-06-03 15:48:11 +0300

    feat: add tap-to-see tooltip for stories with quoted posts

--- lib/app/features/feed/stories/views/components/story_viewer/components/viewers/image_story_viewer.dart
+++ lib/app/features/feed/stories/views/components/story_viewer/components/viewers/image_story_viewer.dart
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/progress_bar/centered_loading_indicator.dart';
 import 'package:ion/app/features/components/ion_connect_network_image/ion_connect_network_image.dart';
 import 'package:ion/app/features/feed/stories/providers/story_image_loading_provider.c.dart';
+import 'package:ion/app/features/feed/stories/providers/story_pause_provider.c.dart';
+import 'package:ion/app/features/feed/stories/views/components/story_viewer/components/viewers/tap_to_see_hint.dart';
 import 'package:ion/app/features/ion_connect/model/quoted_event.c.dart';
 import 'package:ion/app/router/app_routes.c.dart';
 
-class ImageStoryViewer extends ConsumerWidget {
+class ImageStoryViewer extends HookConsumerWidget {
   const ImageStoryViewer({
     required this.imageUrl,
     required this.authorPubkey,
@@ -26,6 +29,7 @@ class ImageStoryViewer extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final cacheManager = ref.watch(storyImageCacheManagerProvider);
     final hasQuotedPost = quotedEvent != null;
+    final showTooltip = useState(false);
 
     final imageWidget = IonConnectNetworkImage(
       imageUrl: imageUrl,
@@ -54,12 +58,28 @@ class ImageStoryViewer extends ConsumerWidget {
     if (hasQuotedPost) {
       return GestureDetector(
         onTap: () {
-          final eventReference = quotedEvent!.eventReference;
-          PostDetailsRoute(
-            eventReference: eventReference.encode(),
-          ).push<void>(context);
+          if (!showTooltip.value) {
+            showTooltip.value = true;
+            ref.read(storyPauseControllerProvider.notifier).paused = true;
+          }
         },
-        child: imageWidget,
+        child: TapToSeeHint(
+          showTooltip: showTooltip.value,
+          onTooltipTap: () {
+            showTooltip.value = false;
+            ref.read(storyPauseControllerProvider.notifier).paused = false;
+
+            final eventReference = quotedEvent!.eventReference;
+            PostDetailsRoute(
+              eventReference: eventReference.encode(),
+            ).push<void>(context);
+          },
+          onHideTooltip: () {
+            showTooltip.value = false;
+            ref.read(storyPauseControllerProvider.notifier).paused = false;
+          },
+          child: imageWidget,
+        ),
       );
     }
 
