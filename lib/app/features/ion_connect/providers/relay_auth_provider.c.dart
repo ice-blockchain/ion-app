77dd569410b15c16574fb288a3616a94abe97070
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-06-03 13:13:40 +0300

    chore: add attestation to auth event (#1295)
    ## Description
    - add attestation for auth event
    - conditionally include delegation based on relay presence in user
    relays
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [x] Chore
    
    ---------
    
    Co-authored-by: ion-endymion <188437551+ice-endymion@users.noreply.github.com>

--- lib/app/features/ion_connect/providers/relay_auth_provider.c.dart
+++ lib/app/features/ion_connect/providers/relay_auth_provider.c.dart
@@ -10,6 +10,8 @@ import 'package:ion/app/features/ion_connect/ion_connect.dart' hide requestEvent
 import 'package:ion/app/features/ion_connect/model/action_source.c.dart';
 import 'package:ion/app/features/ion_connect/model/auth_event.c.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_notifier.c.dart';
+import 'package:ion/app/features/user/providers/user_delegation_provider.c.dart';
+import 'package:ion/app/features/user/providers/user_relays_manager.c.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'relay_auth_provider.c.g.dart';
@@ -24,13 +26,20 @@ class RelayAuth extends _$RelayAuth {
         required String challenge,
         required String relayUrl,
       }) async {
+        // Used cache delegation only because relay not authorized yet
+        final delegationComplete = await ref.read(cacheDelegationCompleteProvider.future);
+        final delegation = await ref.read(currentUserCachedDelegationProvider.future);
+        final userRelays = await ref.read(currentUserRelaysProvider.future);
+
+        final isRelayInUserRelays = userRelays?.urls.contains(relay.url) ?? false;
+
         final authEvent = AuthEvent(
           challenge: challenge,
           relay: relayUrl,
+          userDelegation:
+              (delegationComplete.falseOrValue && !isRelayInUserRelays) ? delegation : null,
         );
 
-        // Used cache delegation only because relay not authorized yet
-        final delegationComplete = await ref.read(cacheDelegationCompleteProvider.future);
         return ref
             .read(ionConnectNotifierProvider.notifier)
             .sign(authEvent, includeMasterPubkey: delegationComplete.falseOrValue);
