3f54d111a62f634d195acfe514217a4c892fdaef
ice-ajax <190340101+ice-ajax@users.noreply.github.com>
2025-06-04 12:26:31 +0200

    fix: support legacy PostEntity in video full screen page

--- lib/app/features/video/views/components/video_post_info.dart
+++ lib/app/features/video/views/components/video_post_info.dart
@@ -6,6 +6,7 @@ import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/core/providers/mute_provider.c.dart';
 import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.c.dart';
 import 'package:ion/app/features/feed/views/components/user_info/user_info.dart';
+import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
 import 'package:ion/app/features/video/views/components/video_button.dart';
 import 'package:ion/app/features/video/views/components/video_post_text.dart';
 import 'package:ion/generated/assets.gen.dart';
@@ -16,10 +17,15 @@ class VideoPostInfo extends StatelessWidget {
     super.key,
   });
 
-  final ModifiablePostEntity videoPost;
+  final IonConnectEntity videoPost;
 
   @override
   Widget build(BuildContext context) {
+    final publishedAt = switch (videoPost) {
+      final ModifiablePostEntity post => post.data.publishedAt.value,
+      _ => videoPost.createdAt,
+    };
+
     return Column(
       children: [
         Container(
@@ -41,7 +47,7 @@ class VideoPostInfo extends StatelessWidget {
             children: [
               UserInfo(
                 pubkey: videoPost.masterPubkey,
-                createdAt: videoPost.data.publishedAt.value,
+                createdAt: publishedAt,
                 trailing: Consumer(
                   builder: (context, ref, child) {
                     final isMuted = ref.watch(globalMuteNotifierProvider);
