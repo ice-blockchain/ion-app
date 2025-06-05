a06f4d236a6e9924e92ef989a9c26e62223628dc
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-06-05 09:14:40 +0300

    fix: import order

--- lib/app/features/feed/providers/delete_entity_provider.c.dart
+++ lib/app/features/feed/providers/delete_entity_provider.c.dart
@@ -16,9 +16,9 @@ import 'package:ion/app/features/feed/providers/counters/replies_count_provider.
 import 'package:ion/app/features/feed/providers/counters/reposted_events_notifier.c.dart';
 import 'package:ion/app/features/feed/providers/counters/reposts_count_provider.c.dart';
 import 'package:ion/app/features/feed/providers/feed_posts_provider.c.dart';
-import 'package:ion/app/features/feed/stories/providers/feed_stories_provider.c.dart';
 import 'package:ion/app/features/feed/providers/user_posts_data_source_provider.c.dart';
 import 'package:ion/app/features/feed/providers/user_videos_data_source_provider.c.dart';
+import 'package:ion/app/features/feed/stories/providers/feed_stories_provider.c.dart';
 import 'package:ion/app/features/ion_connect/model/action_source.c.dart';
 import 'package:ion/app/features/ion_connect/model/deletion_request.c.dart';
 import 'package:ion/app/features/ion_connect/model/entity_data_with_media_content.dart';
--- test/app/features/stories/data/fake_feed_stories_state.dart
+++ test/app/features/stories/data/fake_feed_stories_state.dart
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: ice License 1.0
 
-import 'package:ion/app/features/feed/stories/providers/feed_stories_provider.c.dart';
 import 'package:ion/app/features/feed/stories/data/models/story.c.dart';
+import 'package:ion/app/features/feed/stories/providers/feed_stories_provider.c.dart';
 
 class FakeFeedStories extends FeedStories {
   FakeFeedStories(this.stories);
