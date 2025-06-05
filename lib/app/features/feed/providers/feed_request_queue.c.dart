631c2b71078079cfda6ea437be153c892002335f
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-06-05 09:39:09 +0300

    fix: add licinses

--- lib/app/features/feed/providers/feed_request_queue.c.dart
+++ lib/app/features/feed/providers/feed_request_queue.c.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
 import 'package:ion/app/features/feed/providers/feed_config_provider.c.dart';
