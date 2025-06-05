691716112d7ad09324c64f0a4f6bd4a93a03d340
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-06-05 12:01:10 +0300

    chore: resolve FakeFeedStories linter suppression

--- test/app/features/stories/data/fake_feed_stories_state.dart
+++ test/app/features/stories/data/fake_feed_stories_state.dart
@@ -4,13 +4,12 @@ import 'package:ion/app/features/feed/stories/data/models/story.c.dart';
 import 'package:ion/app/features/feed/stories/providers/feed_stories_provider.c.dart';
 
 class FakeFeedStories extends FeedStories {
-  FakeFeedStories(this.stories);
+  FakeFeedStories(this._stories);
 
-  // ignore: avoid_public_notifier_properties
-  final Iterable<UserStories> stories;
+  final Iterable<UserStories> _stories;
 
   @override
   ({Iterable<UserStories>? items, bool hasMore}) build() {
-    return (items: stories, hasMore: false);
+    return (items: _stories, hasMore: false);
   }
 }
