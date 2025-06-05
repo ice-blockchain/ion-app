0a7ccd0d96590b8be14b90b885811e5a796a3bfa
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-06-05 17:39:51 +0300

    fix: merge conflicts

--- lib/app/features/chat/recent_chats/views/pages/recent_chats_timeline_page/recent_chats_timeline_page.dart
+++ lib/app/features/chat/recent_chats/views/pages/recent_chats_timeline_page/recent_chats_timeline_page.dart
@@ -251,6 +251,11 @@ class E2eeRecentChatTile extends HookConsumerWidget {
       name: isDeleted
           ? context.i18n.common_deleted_account
           : cachedUserMetadata?.data.displayName ?? '',
+      avatarUrl: isDeleted ? null : cachedUserMetadata?.data.picture,
+      eventReference: eventReference,
+      unreadMessagesCount: unreadMessagesCount.valueOrNull ?? 0,
+      lastMessageContent: conversation.latestMessage?.content ?? '',
+      lastMessageAt: (conversation.latestMessage?.createdAt ?? conversation.joinedAt).toDateTime,
       isVerified: isUserVerified,
       onTap: () {
         ConversationRoute(receiverPubKey: receiverPubkey).push<void>(context);
