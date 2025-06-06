eb1015c829bc2c4ebd92f4a7b7ca30e8d8a3cf85
Ice Hades <119406114+ice-hades@users.noreply.github.com>
2025-06-06 12:10:54 +0400

    feat: used search user api for mentions

--- lib/app/features/feed/providers/suggestions/mentions/mention_suggestions_provider.c.dart
+++ lib/app/features/feed/providers/suggestions/mentions/mention_suggestions_provider.c.dart
@@ -1,43 +1,56 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/features/ion_connect/ion_connect.dart';
-import 'package:ion/app/features/ion_connect/model/search_extension.dart';
-import 'package:ion/app/features/ion_connect/providers/ion_connect_notifier.c.dart';
+import 'package:ion/app/features/auth/providers/paginated_users_metadata_provider.c.dart';
 import 'package:ion/app/features/user/model/user_metadata.c.dart';
+import 'package:ion_identity_client/ion_identity.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'mention_suggestions_provider.c.g.dart';
 
+@immutable
+class _SearchUsersByKeyword {
+  const _SearchUsersByKeyword(this.keyword);
+
+  final String keyword;
+
+  Future<List<UserRelaysInfo>> call(
+    int limit,
+    int offset,
+    List<UserMetadataEntity> current,
+    IONIdentityClient ionIdentityClient,
+  ) {
+    return ionIdentityClient.users.searchForUsersByKeyword(
+      limit: limit,
+      offset: offset,
+      keyword: keyword,
+      searchType: SearchUsersSocialProfileType.startsWith,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is _SearchUsersByKeyword && other.keyword == keyword;
+  }
+
+  @override
+  int get hashCode => keyword.hashCode;
+}
+
 @riverpod
 Future<List<String>> mentionSuggestions(Ref ref, String query) async {
-  if (query.isEmpty || !query.startsWith('@')) {
+  final trimmedQuery = query.trim();
+  if (trimmedQuery.isEmpty || !trimmedQuery.startsWith('@')) {
     return [];
   }
 
-  final searchQuery = query.substring(1).toLowerCase();
+  final searchQuery = trimmedQuery.substring(1).toLowerCase();
+  final paginatedUsersMetadataData = await ref.watch(
+    paginatedUsersMetadataProvider(
+      _SearchUsersByKeyword(searchQuery).call,
+    ).future,
+  );
 
-  final requestMessage = RequestMessage()
-    ..addFilter(
-      RequestFilter(
-        kinds: const [UserMetadataEntity.kind],
-        search: SearchExtensions(
-          [
-            QuerySearchExtension(searchQuery: searchQuery),
-            DiscoveryCreatorsSearchExtension(),
-            MentionsSearchExtension(),
-          ],
-        ).toString(),
-        limit: 10,
-      ),
-    );
-  final ionConnect = ref.read(ionConnectNotifierProvider.notifier);
-
-  final pubKeys = <String>{};
-
-  await for (final entity in ionConnect.requestEntities(requestMessage)) {
-    pubKeys.add(entity.pubkey);
-  }
-
-  return pubKeys.toList();
+  return paginatedUsersMetadataData.items.map((metadata) => metadata.masterPubkey).toList();
 }
