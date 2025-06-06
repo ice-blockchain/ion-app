1070a2daeb2dd70b1254305379a601e9748a8278
ice-neoptolemus <214448594+ice-neoptolemus@users.noreply.github.com>
2025-06-06 16:53:47 +0300

    feat: add logic to handle new coin

--- lib/app/features/wallets/model/manage_coins_group.c.dart
+++ lib/app/features/wallets/model/manage_coins_group.c.dart
@@ -10,5 +10,6 @@ class ManageCoinsGroup with _$ManageCoinsGroup {
   const factory ManageCoinsGroup({
     required CoinsGroup coinsGroup,
     required bool isSelected,
+    @Default(false) bool isNewlyAdded,
   }) = _ManageCoinsGroup;
 }
--- lib/app/features/wallets/providers/filtered_assets_provider.c.dart
+++ lib/app/features/wallets/providers/filtered_assets_provider.c.dart
@@ -4,6 +4,7 @@ import 'package:ion/app/extensions/riverpod.dart';
 import 'package:ion/app/features/wallets/model/coins_group.c.dart';
 import 'package:ion/app/features/wallets/model/wallet_data_with_loading_state.c.dart';
 import 'package:ion/app/features/wallets/providers/coins_provider.c.dart';
+import 'package:ion/app/features/wallets/views/pages/manage_coins/providers/manage_coins_provider.c.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'filtered_assets_provider.c.g.dart';
@@ -22,6 +23,9 @@ class WalletSearchQueryController extends _$WalletSearchQueryController {
 class FilteredCoinsNotifier extends _$FilteredCoinsNotifier {
   @override
   Future<List<CoinsGroup>> build() async {
+    final manageCoinsNotifier = ref.watch(manageCoinsNotifierProvider);
+    final selectedCoins = manageCoinsNotifier.value;
+
     final searchQueryListener = ref.listen<String>(
       walletSearchQueryControllerProvider(WalletAssetType.coin),
       (previous, next) => search(next),
@@ -30,13 +34,35 @@ class FilteredCoinsNotifier extends _$FilteredCoinsNotifier {
     ref.onDispose(searchQueryListener.close);
 
     final coinGroups = await ref.watch(coinsInWalletProvider.future);
+
+    if (selectedCoins != null) {
+      final newlyAddedGroups = selectedCoins.values
+          .where((group) => group.isNewlyAdded)
+          .map((group) => group.coinsGroup)
+          .toList();
+
+      if (newlyAddedGroups.isNotEmpty) {
+        return [...coinGroups, ...newlyAddedGroups];
+      }
+    }
+
     return coinGroups;
   }
 
   Future<void> search(String query) async {
     await ref.debounce();
     final coinGroups = await ref.watch(coinsInWalletProvider.future);
-    final filteredCoins = _filterCoins(coinGroups, query);
+    final manageCoinsNotifier = ref.watch(manageCoinsNotifierProvider);
+
+    final newlyAddedGroups = manageCoinsNotifier.value?.values
+            .where((group) => group.isNewlyAdded)
+            .map((group) => group.coinsGroup)
+            .toList() ??
+        [];
+
+    final allCoins = [...coinGroups, ...newlyAddedGroups];
+
+    final filteredCoins = _filterCoins(allCoins, query);
     state = AsyncData(filteredCoins);
   }
 }
