3f28f0d1f5660c21980cd37ee798aab458143591
ice-neoptolemus <214448594+ice-neoptolemus@users.noreply.github.com>
2025-06-09 13:32:40 +0300

    feat: add loading state for adding new coin in the list (#1394)
    ## Description
    Add loading state for new coins
    
    ## Additional Notes
    
    ## Task ID
    ION-2855
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <img width="355" alt="image"
    src="https://github.com/user-attachments/assets/f4396292-68e0-4d81-abfb-5607ee1453e3"
    />

--- lib/app/features/wallets/views/components/coins_list/coin_item.dart
+++ lib/app/features/wallets/views/components/coins_list/coin_item.dart
@@ -4,11 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/coins/coin_icon.dart';
 import 'package:ion/app/components/list_item/list_item.dart';
-import 'package:ion/app/extensions/build_context.dart';
-import 'package:ion/app/extensions/theme_data.dart';
+import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/wallets/model/coins_group.c.dart';
 import 'package:ion/app/features/wallets/providers/wallet_user_preferences/user_preferences_selectors.c.dart';
 import 'package:ion/app/utils/num.dart';
+import 'package:ion/generated/assets.gen.dart';
 
 class CoinsGroupItem extends ConsumerWidget {
   const CoinsGroupItem({
@@ -42,6 +42,64 @@ class CoinsGroupItem extends ConsumerWidget {
             isBalanceVisible ? formatToCurrency(coinsGroup.totalBalanceUSD) : '******',
             style: context.theme.appTextThemes.caption3
                 .copyWith(color: context.theme.appColors.secondaryText),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CoinsGroupItemPlaceholder extends StatelessWidget {
+  const CoinsGroupItemPlaceholder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListItem(
+      title: Container(
+        height: 16.0.s,
+        width: 101.0.s,
+        decoration: BoxDecoration(
+          color: context.theme.appColors.onTerararyFill,
+          borderRadius: BorderRadius.circular(8.0.s),
+        ),
+      ),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 6.0.s),
+          Container(
+            height: 12.0.s,
+            width: 55.0.s,
+            decoration: BoxDecoration(
+              color: context.theme.appColors.onTerararyFill,
+              borderRadius: BorderRadius.circular(8.0.s),
+            ),
+          ),
+        ],
+      ),
+      leading: Assets.svg.walletemptyicon2.icon(size: 36.0.s),
+      trailing: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            height: 16.0.s,
+            width: 40.0.s,
+            decoration: BoxDecoration(
+              color: context.theme.appColors.onTerararyFill,
+              borderRadius: BorderRadius.circular(8.0.s),
+            ),
+          ),
+          SizedBox(height: 6.0.s),
+          Container(
+            height: 12.0.s,
+            width: 30.0.s,
+            decoration: BoxDecoration(
+              color: context.theme.appColors.onTerararyFill,
+              borderRadius: BorderRadius.circular(8.0.s),
+            ),
           ),
         ],
       ),
--- lib/app/features/wallets/views/pages/wallet_page/components/coins/coins_tab.dart
+++ lib/app/features/wallets/views/pages/wallet_page/components/coins/coins_tab.dart
@@ -3,12 +3,13 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
+import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/extensions/num.dart';
 import 'package:ion/app/features/wallets/providers/filtered_assets_provider.c.dart';
 import 'package:ion/app/features/wallets/views/components/coins_list/coin_item.dart';
+import 'package:ion/app/features/wallets/views/pages/manage_coins/providers/manage_coins_provider.c.dart';
 import 'package:ion/app/features/wallets/views/pages/wallet_page/components/coins/coins_tab_footer.dart';
 import 'package:ion/app/features/wallets/views/pages/wallet_page/components/empty_state/empty_state.dart';
-import 'package:ion/app/features/wallets/views/pages/wallet_page/components/loaders/list_loader.dart';
 import 'package:ion/app/features/wallets/views/pages/wallet_page/tab_type.dart';
 import 'package:ion/app/router/app_routes.c.dart';
 
@@ -21,40 +22,45 @@ class CoinsTab extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final selectedCoinsState = ref.watch(filteredCoinsNotifierProvider);
+    final groups = ref.watch(filteredCoinsNotifierProvider.select((state) => state.value));
 
-    return selectedCoinsState.maybeWhen(
-      data: (groups) {
-        if (groups.isEmpty) {
-          return EmptyState(
-            tabType: tabType,
-            onBottomActionTap: () {
-              ManageCoinsRoute().go(context);
-            },
-          );
-        }
+    if (groups == null || groups.isEmpty) {
+      return EmptyState(
+        tabType: tabType,
+        onBottomActionTap: () {
+          ManageCoinsRoute().go(context);
+        },
+      );
+    }
 
-        return SliverMainAxisGroup(
-          slivers: [
-            SliverList.separated(
-              itemCount: groups.length,
-              separatorBuilder: (context, index) => SizedBox(height: 12.0.s),
-              itemBuilder: (context, index) => ScreenSideOffset.small(
-                child: CoinsGroupItem(
-                  coinsGroup: groups[index],
-                  onTap: () =>
-                      CoinsDetailsRoute(symbolGroup: groups[index].symbolGroup).go(context),
+    return SliverMainAxisGroup(
+      slivers: [
+        SliverList.separated(
+          itemCount: groups.length,
+          separatorBuilder: (context, index) => SizedBox(height: 12.0.s),
+          itemBuilder: (context, index) {
+            final group = groups[index];
+            final isNewlyAdded = ref.watch(
+              manageCoinsNotifierProvider.select(
+                (state) => state.maybeWhen(
+                  data: (data) => data[group.symbolGroup]?.isNewlyAdded ?? false,
+                  orElse: () => false,
                 ),
               ),
-            ),
-            const CoinsTabFooter(),
-          ],
-        );
-      },
-      loading: () => const ListLoader(),
-      orElse: () => const SliverToBoxAdapter(
-        child: SizedBox.shrink(),
-      ),
+            );
+
+            return ScreenSideOffset.small(
+              child: isNewlyAdded
+                  ? const CoinsGroupItemPlaceholder()
+                  : CoinsGroupItem(
+                      coinsGroup: group,
+                      onTap: () => CoinsDetailsRoute(symbolGroup: group.symbolGroup).go(context),
+                    ),
+            );
+          },
+        ),
+        const CoinsTabFooter(),
+      ],
     );
   }
 }
