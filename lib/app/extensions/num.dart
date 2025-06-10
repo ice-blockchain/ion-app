61bb350bb730d61e32a021b52507a469555acf4b
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-06-10 12:09:27 +0300

    refactor: reuse toMicroseconds in toDateTime int ext

--- lib/app/extensions/num.dart
+++ lib/app/extensions/num.dart
@@ -39,19 +39,7 @@ extension SizeExtension on num {
 
 extension Timestamp on int {
   DateTime get toDateTime {
-    return switch (toString().length) {
-      // If the timestamp is 10 digits, it's in seconds
-      10 => DateTime.fromMillisecondsSinceEpoch(this * 1000),
-      // If the timestamp is 13 digits, it's in milliseconds
-      13 => DateTime.fromMillisecondsSinceEpoch(this),
-      // If the timestamp is 16 digits, assume it's in microseconds
-      16 => DateTime.fromMicrosecondsSinceEpoch(this),
-      // If the timestamp is 19 digits, assume it's in nanoseconds
-      19 => DateTime.fromMicrosecondsSinceEpoch(this ~/ 1000),
-      _ => throw FormatException(
-          'Invalid timestamp format: ${toString()} length: ${toString().length}',
-        ),
-    };
+    return DateTime.fromMicrosecondsSinceEpoch(toMicroseconds);
   }
 }
 
