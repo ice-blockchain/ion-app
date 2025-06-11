b3b27c9ee1c10f64c5ad0a3eb70af33cd0b41408
ice-orion <102020833+ice-orion@users.noreply.github.com>
2025-06-11 12:02:36 +0300

    refactor: restructire the generated files

--- lib/app/features/feed/data/database/following_feed_database/dao/seen_reposts_dao.c.dart
+++ lib/app/features/feed/data/database/following_feed_database/dao/seen_reposts_dao.c.dart
@@ -1,17 +1,11 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:drift/drift.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/feed/data/database/following_feed_database/following_feed_database.c.dart';
 import 'package:ion/app/features/feed/data/database/following_feed_database/tables/seen_reposts_table.c.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'seen_reposts_dao.c.g.dart';
-
-@Riverpod(keepAlive: true)
-SeenRepostsDao seenRepostsDao(Ref ref) =>
-    SeenRepostsDao(db: ref.watch(followingFeedDatabaseProvider));
 
 @DriftAccessor(tables: [SeenRepostsTable])
 class SeenRepostsDao extends DatabaseAccessor<FollowingFeedDatabase> with _$SeenRepostsDaoMixin {
--- lib/app/features/feed/providers/repository/following_feed_seen_events_repository.c.dart
+++ lib/app/features/feed/providers/repository/following_feed_seen_events_repository.c.dart
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/features/feed/data/database/following_feed_database/dao/providers/seen_events_dao_provider.c.dart';
+import 'package:ion/app/features/feed/data/database/following_feed_database/dao/providers/seen_reposts_dao_provider.c.dart';
 import 'package:ion/app/features/feed/data/database/following_feed_database/dao/seen_events_dao.c.dart';
 import 'package:ion/app/features/feed/data/database/following_feed_database/dao/seen_reposts_dao.c.dart';
 import 'package:ion/app/features/feed/data/database/following_feed_database/following_feed_database.c.dart';
