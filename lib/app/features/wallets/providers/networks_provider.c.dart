a33030a1e27d97f1a796ea5fa5e7dfa14441fc95
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-06-11 15:21:55 +0300

    chore: add alphabetical order for networks in the import token flow

--- lib/app/features/wallets/providers/networks_provider.c.dart
+++ lib/app/features/wallets/providers/networks_provider.c.dart
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:collection/collection.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/wallets/data/repository/networks_repository.c.dart';
 import 'package:ion/app/features/wallets/model/network_data.c.dart';
@@ -9,7 +10,10 @@ part 'networks_provider.c.g.dart';
 
 @riverpod
 Future<List<NetworkData>> networks(Ref ref) {
-  return ref.watch(networksRepositoryProvider).getAll();
+  return ref
+      .watch(networksRepositoryProvider)
+      .getAll()
+      .then((networks) => networks.sortedBy((a) => a.displayName));
 }
 
 @riverpod
