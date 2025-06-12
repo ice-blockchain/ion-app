0ca76c015df0195e921e2290d42a37eabb311e13
ice-tychon <175587874+ice-tychon@users.noreply.github.com>
2025-06-12 20:25:23 +0300

    chore: add logs to SyncTransactionsService

--- lib/app/features/wallets/providers/wallets_initializer_provider.c.dart
+++ lib/app/features/wallets/providers/wallets_initializer_provider.c.dart
@@ -6,6 +6,7 @@ import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
 import 'package:ion/app/features/wallets/domain/coins/coin_initializer.c.dart';
 import 'package:ion/app/features/wallets/domain/networks/networks_initializer.c.dart';
 import 'package:ion/app/features/wallets/domain/transactions/sync_transactions_service.c.dart';
+import 'package:ion/app/services/logger/logger.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'wallets_initializer_provider.c.g.dart';
@@ -31,15 +32,24 @@ class WalletsInitializerNotifier extends _$WalletsInitializerNotifier {
       final networksInitializer = ref.watch(networksInitializerProvider);
       final syncServiceFuture = ref.watch(syncTransactionsServiceProvider.future);
 
-      final (_, _, syncService) = await (
+      final (_, _) = await (
         coinInitializer.initialize(),
         networksInitializer.initialize(),
-        syncServiceFuture,
       ).wait;
 
-      unawaited(
-        syncService.sync(),
-      );
+      try {
+        final syncService = await syncServiceFuture;
+
+        unawaited(
+          syncService.sync(),
+        );
+      } catch (error, stackTrace) {
+        Logger.error(
+          error,
+          stackTrace: stackTrace,
+          message: 'SyncTransactionsService.sync() failed',
+        );
+      }
 
       // Only complete if not already completed
       if (!_completer!.isCompleted) {
