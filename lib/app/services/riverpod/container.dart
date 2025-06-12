3e64e42454c0d668b85717fdd6021a4973237add
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-06-12 15:19:17 +0300

    chore: license

--- lib/app/services/riverpod/container.dart
+++ lib/app/services/riverpod/container.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/services/logger/logger.dart';
 import 'package:ion/app/services/provider_rebuild_counter/provider_rebuild_counter.dart';
