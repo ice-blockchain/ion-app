a64a13b958637aee3738a4ffdc2d7a1a3b7aea60
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-06-15 22:19:22 +0300

    chore: add license text

--- lib/app/features/ion_connect/model/persistent_subscription_encrypted_event_message_handler.dart
+++ lib/app/features/ion_connect/model/persistent_subscription_encrypted_event_message_handler.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:ion/app/features/ion_connect/ion_connect.dart';
 import 'package:ion/app/features/ion_connect/model/ion_connect_gift_wrap.c.dart';
 
