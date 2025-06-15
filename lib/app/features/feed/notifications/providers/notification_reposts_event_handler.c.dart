ccc4c396bda760fb20519fd83ab34f237dd47342
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-06-15 14:44:51 +0300

    feat: add repost global event handler

---
+++ lib/app/features/feed/notifications/providers/notification_reposts_event_handler.c.dart
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ion/app/exceptions/exceptions.dart';
+import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
+import 'package:ion/app/features/feed/data/models/entities/article_data.c.dart';
+import 'package:ion/app/features/feed/data/models/entities/generic_repost.c.dart';
+import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.c.dart';
+import 'package:ion/app/features/feed/notifications/data/repository/comments_repository.c.dart';
+import 'package:ion/app/features/ion_connect/ion_connect.dart';
+import 'package:ion/app/features/ion_connect/providers/ion_connect_persistent_subscription.c.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'notification_reposts_event_handler.c.g.dart';
+
+class NotificationRepostsEventHandler extends PersistentSubscriptionEventHandler {
+  NotificationRepostsEventHandler(this.commentsRepository, this.currentPubkey);
+  final CommentsRepository commentsRepository;
+  final String currentPubkey;
+
+  @override
+  bool canHandle(EventMessage eventMessage) {
+    if (eventMessage.kind == GenericRepostEntity.kind &&
+        eventMessage.masterPubkey != currentPubkey) {
+      final entity = GenericRepostEntity.fromEventMessage(eventMessage);
+      if (entity.data.kind == ModifiablePostEntity.kind || entity.data.kind == ArticleEntity.kind) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @override
+  Future<void> handle(EventMessage eventMessage) async {
+    final entity = GenericRepostEntity.fromEventMessage(eventMessage);
+    await commentsRepository.save(entity);
+  }
+}
+
+@riverpod
+NotificationRepostsEventHandler notificationRepostsEventHandler(Ref ref) {
+  final commentsRepository = ref.watch(commentsRepositoryProvider);
+  final currentPubkey = ref.watch(currentPubkeySelectorProvider);
+
+  if (currentPubkey == null) {
+    throw UserMasterPubkeyNotFoundException();
+  }
+
+  return NotificationRepostsEventHandler(commentsRepository, currentPubkey);
+}
