05957493dbb39187d1a5ee2838feca7f2f987e21
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-06-15 22:29:12 +0300

    refactor: remove unused community messages subscriber

--- lib/app/features/chat/views/pages/chat_main_page/chat_main_page.dart
+++ lib/app/features/chat/views/pages/chat_main_page/chat_main_page.dart
@@ -5,8 +5,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
-import 'package:ion/app/features/chat/community/providers/community_join_requests_provider.c.dart';
-import 'package:ion/app/features/chat/community/providers/community_messages_handler.c.dart';
 import 'package:ion/app/features/chat/providers/conversations_provider.c.dart';
 import 'package:ion/app/features/chat/recent_chats/views/components/recent_chat_skeleton/recent_chat_skeleton.dart';
 import 'package:ion/app/features/chat/recent_chats/views/pages/recent_chats_empty_page/recent_chats_empty_page.dart';
@@ -21,12 +19,6 @@ class ChatMainPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    useOnInit(() {
-      ref
-        ..read(communityJoinRequestsProvider)
-        ..read(communityMessagesSubscriberProvider);
-    });
-
     _useCheckDeviceKeypairDialog(ref);
 
     final conversations = ref.watch(conversationsProvider);
