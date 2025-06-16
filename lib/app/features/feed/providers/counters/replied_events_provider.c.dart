764188c676b781c82bf0d56f1583f8b1c6d3b90c
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-06-16 19:34:06 +0300

    bugfix: feed replies state (#1445)
    ## Description
    - Fixes state update for the feed replies (counter state)
    
    ## Type of Change
    - [x] Bug fix
    - [x] Refactoring

--- lib/app/features/feed/providers/counters/replied_events_provider.c.dart
+++ lib/app/features/feed/providers/counters/replied_events_provider.c.dart
@@ -1,125 +1,87 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'dart:async';
-
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
 import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.c.dart';
 import 'package:ion/app/features/ion_connect/model/event_reference.c.dart';
-import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
 import 'package:ion/app/features/ion_connect/providers/ion_connect_cache.c.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'replied_events_provider.c.g.dart';
 
-@Riverpod(keepAlive: true)
+@riverpod
 class RepliedEvents extends _$RepliedEvents {
-  final _deletedIds = <String>{};
-
   @override
-  Stream<Map<String, List<String>>?> build() async* {
-    final currentPubkey = ref.watch(currentPubkeySelectorProvider);
+  Stream<Map<String, List<String>>> build() async* {
+    keepAliveWhenAuthenticated(ref);
 
-    if (currentPubkey == null) {
-      yield {};
+    final currentMasterPubkey = ref.watch(currentPubkeySelectorProvider);
+
+    if (currentMasterPubkey == null) {
+      yield const {};
       return;
     }
 
-    final cache = ref.read(ionConnectCacheProvider);
-    var repliedMap = _buildInitialMap(cache, currentPubkey);
+    final cache = ref.watch(ionConnectCacheProvider);
+    var repliesForEventsMap = _buildInitialMap(cache, currentMasterPubkey);
 
-    yield repliedMap;
+    yield Map.unmodifiable(repliesForEventsMap);
 
-    final controller = StreamController<Map<String, List<String>>>();
+    await for (final entity in ref.watch(ionConnectCacheStreamProvider)) {
+      if (entity is! ModifiablePostEntity ||
+          entity.data.parentEvent == null ||
+          entity.masterPubkey != currentMasterPubkey) {
+        continue;
+      }
 
-    final subscription = ref.watch(ionConnectCacheStreamProvider).listen((entity) {
-      if (entity case final ModifiablePostEntity post) {
-        final parentId = post.data.parentEvent?.eventReference.toString();
-        if (parentId == null) return;
+      final parentId = entity.data.parentEvent!.eventReference.toString();
+      final updatedList = List<String>.from(repliesForEventsMap[parentId] ?? []);
 
-        final currentUserRepliedIds = _getCurrentUserRepliedIds(post, currentPubkey: currentPubkey);
-
-        if (currentUserRepliedIds != null) {
-          final validIds = currentUserRepliedIds.where((id) => !_deletedIds.contains(id)).toList();
-
-          final updatedMap = Map<String, List<String>>.from(repliedMap);
-
-          if (validIds.isEmpty) {
-            updatedMap.remove(parentId);
-          } else {
-            updatedMap[parentId] = validIds;
-          }
-
-          repliedMap = updatedMap;
-          controller.add(updatedMap);
+      if (entity.isDeleted) {
+        if (updatedList.remove(entity.id)) {
+          repliesForEventsMap = Map<String, List<String>>.from(repliesForEventsMap)
+            ..[parentId] = updatedList;
+          yield Map.unmodifiable(repliesForEventsMap);
         }
+      } else if (!updatedList.contains(entity.id)) {
+        updatedList.add(entity.id);
+        repliesForEventsMap = Map<String, List<String>>.from(repliesForEventsMap)
+          ..[parentId] = updatedList;
+        yield Map.unmodifiable(repliesForEventsMap);
       }
-    });
-
-    ref.onDispose(() async {
-      await subscription.cancel();
-      await controller.close();
-    });
-
-    await for (final update in controller.stream) {
-      yield update;
-    }
-  }
-
-  void removeReply(String parentId, String replyId) {
-    _deletedIds.add(replyId);
-
-    final currentMap = state.valueOrNull;
-    if (currentMap != null && currentMap.containsKey(parentId)) {
-      final updatedReplies = Map<String, List<String>>.from(currentMap);
-      updatedReplies[parentId] = updatedReplies[parentId]!.where((id) => id != replyId).toList();
-
-      if (updatedReplies[parentId]!.isEmpty) {
-        updatedReplies.remove(parentId);
-      }
-
-      state = AsyncData(updatedReplies);
     }
   }
 }
 
 Map<String, List<String>> _buildInitialMap(
   Map<String, CacheEntry> cache,
-  String currentPubkey,
+  String currentUserMasterPubkey,
 ) {
-  return cache.values.fold<Map<String, List<String>>>({}, (result, entry) {
-    if (entry.entity case final ModifiablePostEntity post) {
-      final currentUserRepliedIds = _getCurrentUserRepliedIds(post, currentPubkey: currentPubkey);
-      final parentId = post.data.parentEvent?.eventReference.toString();
-      if (currentUserRepliedIds != null && parentId != null) {
-        if (!result.containsKey(parentId)) {
-          result[parentId] = [];
-        }
-        final newIds = currentUserRepliedIds.where((id) => !result[parentId]!.contains(id));
-        result[parentId] = [...result[parentId]!, ...newIds];
-      }
-    }
-    return result;
-  });
-}
+  final result = <String, List<String>>{};
 
-List<String>? _getCurrentUserRepliedIds(IonConnectEntity entity, {required String currentPubkey}) {
-  if (entity case final ModifiablePostEntity post when post.masterPubkey == currentPubkey) {
-    if (post.data.parentEvent != null) {
-      return [post.id];
+  for (final entry in cache.values) {
+    final entity = entry.entity;
+    if (entity is! ModifiablePostEntity ||
+        entity.isDeleted ||
+        entity.data.parentEvent == null ||
+        entity.masterPubkey != currentUserMasterPubkey) {
+      continue;
+    }
+
+    final parentId = entity.data.parentEvent!.eventReference.toString();
+    result.putIfAbsent(parentId, () => []);
+    if (!result[parentId]!.contains(entity.id)) {
+      result[parentId]!.add(entity.id);
     }
   }
-  return null;
+
+  return result;
 }
 
 @riverpod
 bool isReplied(Ref ref, EventReference eventReference) {
-  final repliedMap = ref.watch(repliedEventsProvider).valueOrNull;
-  final replyIds = repliedMap?[eventReference.toString()];
-
-  final deletedIds = ref.read(repliedEventsProvider.notifier)._deletedIds;
-  final validReplyIds = replyIds?.where((id) => !deletedIds.contains(id)).toList();
-  final hasReply = validReplyIds?.isNotEmpty ?? false;
-
-  return hasReply;
+  final repliedEventsMap = ref.watch(repliedEventsProvider).valueOrNull;
+  final replyIds = repliedEventsMap?[eventReference.toString()];
+  return replyIds?.isNotEmpty ?? false;
 }
