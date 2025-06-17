8470612741a95053719752357312c11f43955ee8
ice-tychon <175587874+ice-tychon@users.noreply.github.com>
2025-06-17 10:02:27 +0300

    fix: compare sets properly (#1453)
    ## Description
    This PR fixes infinite requests loop caused by inappropriate sets
    comparison.
    
    ## Type of Change
    - [x] Bug fix

--- lib/app/features/user/providers/user_file_storage_relays_sync_provider.c.dart
+++ lib/app/features/user/providers/user_file_storage_relays_sync_provider.c.dart
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:collection/collection.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
@@ -39,7 +40,7 @@ Future<void> userFileStorageRelaysSync(Ref ref) async {
       await ref.watch(userFileStorageRelayProvider(pubkey: masterPubkey).future);
   if (userFileStorageRelays != null) {
     final fileStorageRelays = userFileStorageRelays.data.list.map((e) => e.url).toSet();
-    if (fileStorageRelays == relayUrls.toSet()) {
+    if (const SetEquality<String>().equals(fileStorageRelays, relayUrls.toSet())) {
       return;
     }
   }
