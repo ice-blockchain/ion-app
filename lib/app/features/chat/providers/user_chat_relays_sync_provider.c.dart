dae8c749c2d5743d5d8eb37b4e7fc9b60689f862
ice-tychon <175587874+ice-tychon@users.noreply.github.com>
2025-06-17 09:38:34 +0300

    fix: compare sets properly

--- lib/app/features/chat/providers/user_chat_relays_sync_provider.c.dart
+++ lib/app/features/chat/providers/user_chat_relays_sync_provider.c.dart
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'package:collection/collection.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.c.dart';
@@ -37,7 +38,7 @@ Future<void> userChatRelaysSync(Ref ref) async {
   final userChatRelays = await ref.watch(userRelayProvider(masterPubkey).future);
   if (userChatRelays != null) {
     final chatRelays = userChatRelays.data.list.map((e) => e.url).toSet();
-    if (chatRelays == relayUrls.toSet()) {
+    if (const SetEquality<String>().equals(chatRelays, relayUrls.toSet())) {
       return;
     }
   }
