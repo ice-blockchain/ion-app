6c5380cdc0dc244a2ab530488545d1fea97a9e6a
Ice Cerberus <214443964+ice-cerberus@users.noreply.github.com>
2025-06-18 09:50:23 +0300

    code clean up

--- lib/app/extensions/asset_gen_image.dart
+++ lib/app/extensions/asset_gen_image.dart
@@ -5,34 +5,6 @@ import 'dart:math';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ion/app/extensions/num.dart';
-import 'package:ion/generated/assets.gen.dart';
-
-extension IconExtension on AssetGenImage {
-  // Widget icon({Color? color, double? size, BoxFit? fit}) {
-  //   final iconSize = size ?? 24.0.s;
-  //   return Image.asset(
-  //     path,
-  //     width: iconSize,
-  //     height: iconSize,
-  //     excludeFromSemantics: true,
-  //     fit: fit ?? BoxFit.contain,
-  //     color: color,
-  //   );
-  // }
-
-  Widget iconWithDimensions({Color? color, double? width, double? height}) {
-    final iconWidth = width ?? 24.0.s;
-    final iconHeight = height ?? 24.0.s;
-    return Image.asset(
-      path,
-      width: iconWidth,
-      height: iconHeight,
-      excludeFromSemantics: true,
-      fit: BoxFit.contain,
-      color: color,
-    );
-  }
-}
 
 class IconAsset extends StatelessWidget {
   const IconAsset(
