fff2e05e734e3c05738f637146b796d0fee510d7
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-06-20 07:40:49 +0300

    fix: merge conflicts

--- lib/app/features/core/providers/init_provider.c.dart
+++ lib/app/features/core/providers/init_provider.c.dart
@@ -15,6 +15,7 @@ import 'package:ion/app/features/core/providers/window_manager_provider.c.dart';
 import 'package:ion/app/features/core/views/components/widget_error_builder.dart';
 import 'package:ion/app/features/feed/providers/bookmarks_notifier.c.dart';
 import 'package:ion/app/features/feed/providers/feed_bookmarks_notifier.c.dart';
+import 'package:ion/app/features/feed/providers/feed_feature_initializer.dart';
 import 'package:ion/app/features/force_update/providers/force_update_provider.c.dart';
 import 'package:ion/app/features/ion_connect/providers/global_subscription.c.dart';
 import 'package:ion/app/features/push_notifications/background/firebase_messaging_background_service.dart';
