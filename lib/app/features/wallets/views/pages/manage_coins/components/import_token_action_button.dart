2171582dddab2c2cc92b9e3707b3d792e0b1d614
ice-brontes <187490541+ice-brontes@users.noreply.github.com>
2025-06-24 14:44:52 +0300

    chore: add try/catch

--- lib/app/features/wallets/views/pages/manage_coins/components/import_token_action_button.dart
+++ lib/app/features/wallets/views/pages/manage_coins/components/import_token_action_button.dart
@@ -5,6 +5,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/wallets/views/pages/manage_coins/providers/manage_coins_provider.c.dart';
 import 'package:ion/app/router/app_routes.c.dart';
+import 'package:ion/app/services/logger/logger.dart';
 import 'package:ion/generated/assets.gen.dart';
 
 class ImportTokenActionButton extends ConsumerWidget {
@@ -16,9 +17,15 @@ class ImportTokenActionButton extends ConsumerWidget {
 
     return GestureDetector(
       onTap: () async {
-        final notifier = ref.read(manageCoinsNotifierProvider.notifier)..disableSave();
-        await ImportTokenRoute().push<void>(context);
-        notifier.enableSave();
+        final notifier = ref.read(manageCoinsNotifierProvider.notifier);
+        try {
+          notifier.disableSave();
+          await ImportTokenRoute().push<void>(context);
+        } catch (e) {
+          Logger.error('Caught error after ImportTokenRoute pop: $e');
+        } finally {
+          notifier.enableSave();
+        }
       },
       child: Assets.svg.iconPlusCreatechannel.icon(
         color: colors.primaryAccent,
