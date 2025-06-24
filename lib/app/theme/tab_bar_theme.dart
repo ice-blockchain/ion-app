394728b309991e40b7502f276e1c7e9349a356c9
Ice Hades <119406114+ice-hades@users.noreply.github.com>
2025-06-24 20:21:14 +0400

    chore: upgraded flutter to 3.32.4

--- lib/app/theme/tab_bar_theme.dart
+++ lib/app/theme/tab_bar_theme.dart
@@ -2,6 +2,6 @@
 
 import 'package:flutter/material.dart';
 
-TabBarTheme buildTabBarTheme() {
-  return const TabBarTheme(overlayColor: WidgetStatePropertyAll<Color>(Colors.transparent));
+TabBarThemeData buildTabBarTheme() {
+  return const TabBarThemeData(overlayColor: WidgetStatePropertyAll<Color>(Colors.transparent));
 }
