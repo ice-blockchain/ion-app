4fe1b890e34206d51be570d1fdbff1493b3f1973
Ice Cerberus <214443964+ice-cerberus@users.noreply.github.com>
2025-06-25 15:27:47 +0300

    updated attached media preview list

--- lib/app/features/feed/create_post/views/pages/post_form_modal/components/attached_media_preview_list.dart
+++ lib/app/features/feed/create_post/views/pages/post_form_modal/components/attached_media_preview_list.dart
@@ -49,16 +49,19 @@ class AttachedMediaPreview extends ConsumerWidget {
             ),
             child: Stack(
               children: [
-                DecoratedBox(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.0.s),
-                  ),
-                  child: Image(
-                    image: AssetEntityImageProvider(
-                      assetEntity,  
-                      isOriginal: false,
+                AspectRatio(
+                  aspectRatio: aspectRatio.aspectRatio,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.0.s),
+                      image: DecorationImage(
+                        image: AssetEntityImageProvider(
+                          assetEntity,
+                          isOriginal: false,
+                        ),
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                    fit: BoxFit.cover,
                   ),
                 ),
                 PositionedDirectional(
