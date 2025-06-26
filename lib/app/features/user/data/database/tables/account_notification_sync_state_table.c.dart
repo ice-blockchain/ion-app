232dc2e35809d3177a0c266eb56e7ea34869585e
ice-hector <96414297+ice-hector@users.noreply.github.com>
2025-06-26 19:45:18 +0300

    fix: pr comment

--- lib/app/features/user/data/database/tables/account_notification_sync_state_table.c.dart
+++ lib/app/features/user/data/database/tables/account_notification_sync_state_table.c.dart
@@ -2,13 +2,27 @@
 
 import 'package:drift/drift.dart';
 
+enum ContentType {
+  posts(0),
+  stories(1),
+  articles(2),
+  videos(3);
+
+  const ContentType(this.value);
+  final int value;
+
+  static ContentType fromValue(int value) {
+    return ContentType.values.firstWhere((type) => type.value == value);
+  }
+}
+
 @DataClassName('AccountNotificationSyncState')
 class AccountNotificationSyncStateTable extends Table {
   /// Relay URL
   TextColumn get relayUrl => text()();
 
   /// Content type (posts, stories, articles, videos)
-  TextColumn get contentType => text()();
+  IntColumn get contentType => intEnum<ContentType>()();
 
   /// Last sync timestamp in microseconds since epoch
   IntColumn get lastSyncTimestamp => integer()();
