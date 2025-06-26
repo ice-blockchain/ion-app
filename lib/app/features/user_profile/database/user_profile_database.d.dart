475c64734d2f482696150b1a6c34e9ddf7afa3a2
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-06-26 12:39:11 +0300

    test: fix tests

--- lib/app/features/user_profile/database/user_profile_database.d.dart
+++ lib/app/features/user_profile/database/user_profile_database.d.dart
@@ -2,13 +2,12 @@
 
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/exceptions/exceptions.dart';
-import 'package:ion/app/features/auth/providers/auth_provider.m.dart';
+import 'package:ion/app/features/ion_connect/database/converters/event_reference_converter.d.dart';
+import 'package:ion/app/features/ion_connect/database/converters/event_tags_converter.dart';
+import 'package:ion/app/features/ion_connect/model/event_reference.f.dart';
 import 'package:ion/app/features/user_profile/database/tables/user_badge_info_table.d.dart';
 import 'package:ion/app/features/user_profile/database/tables/user_delegation_table.d.dart';
 import 'package:ion/app/features/user_profile/database/tables/user_metadata_table.d.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'user_profile_database.d.g.dart';
 
