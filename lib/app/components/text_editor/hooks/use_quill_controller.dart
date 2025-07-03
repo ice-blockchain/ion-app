6107fe859787736cd491efe10771228df758373b
Ice Hades <119406114+ice-hades@users.noreply.github.com>
2025-07-03 21:07:12 +0400

    feat: rich text input

--- lib/app/components/text_editor/hooks/use_quill_controller.dart
+++ lib/app/components/text_editor/hooks/use_quill_controller.dart
@@ -1,20 +1,46 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_quill/quill_delta.dart';
 
-QuillController useQuillController() {
-  final textEditorController = useRef(
-    QuillController.basic(
-      config: const QuillControllerConfig(
+QuillController useQuillController({String? content}) {
+  final quillController = useMemoized(
+    () {
+      const defaultConfig = QuillControllerConfig(
         clipboardConfig: QuillClipboardConfig(
           enableExternalRichPaste: false,
         ),
-      ),
-    ),
+      );
+      if (content != null) {
+        try {
+          final deltaJson = jsonDecode(content) as List<dynamic>;
+          final doc = Document.fromDelta(Delta.fromJson(deltaJson));
+          return QuillController(
+            document: doc,
+            selection: TextSelection.collapsed(offset: doc.length - 1),
+            config: defaultConfig,
+          );
+        } on FormatException {
+          // Fallback to plain text
+          final doc = Document()..insert(0, content);
+          return QuillController(
+            document: doc,
+            selection: TextSelection.collapsed(offset: content.length),
+            config: defaultConfig,
+          );
+        }
+      } else {
+        return QuillController.basic(config: defaultConfig);
+      }
+    },
+    [content],
   );
 
-  useEffect(() => textEditorController.value.dispose, []);
+  useEffect(() => quillController.dispose, [quillController]);
 
-  return textEditorController.value;
+  return quillController;
 }
