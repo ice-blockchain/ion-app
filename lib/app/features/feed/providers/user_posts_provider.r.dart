92163f09fb45cf2f8baafc39497cfcd7b0f021ce
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-07-04 10:37:30 +0300

    fix: auto-update profile feed when creating posts

---
+++ lib/app/features/feed/providers/user_posts_provider.r.dart
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:async/async.dart';
+import 'package:ion/app/features/feed/create_article/providers/create_article_provider.r.dart';
+import 'package:ion/app/features/feed/create_post/providers/create_post_notifier.m.dart';
+import 'package:ion/app/features/feed/data/models/entities/article_data.f.dart';
+import 'package:ion/app/features/feed/data/models/entities/generic_repost.f.dart';
+import 'package:ion/app/features/feed/data/models/entities/modifiable_post_data.f.dart';
+import 'package:ion/app/features/feed/data/models/entities/post_data.f.dart';
+import 'package:ion/app/features/feed/data/models/entities/repost_data.f.dart';
+import 'package:ion/app/features/feed/providers/repost_notifier.r.dart';
+import 'package:ion/app/features/feed/providers/user_posts_data_source_provider.r.dart';
+import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
+import 'package:ion/app/features/ion_connect/providers/entities_paged_data_provider.m.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'user_posts_provider.r.g.dart';
+
+@riverpod
+class UserPosts extends _$UserPosts with DelegatedPagedNotifier {
+  @override
+  ({Iterable<IonConnectEntity>? items, bool hasMore}) build(String pubkey) {
+    final postsStream = ref.watch(createPostNotifierStreamProvider);
+    final articlesStream = ref.watch(createArticleNotifierStreamProvider);
+    final repostsStream = ref.watch(createRepostNotifierStreamProvider);
+    final subscription = StreamGroup.merge([postsStream, articlesStream, repostsStream])
+        .where((entity) => _filterUserEntities(entity, pubkey))
+        .distinct()
+        .listen(insertEntity);
+    ref.onDispose(subscription.cancel);
+
+    final dataSources = ref.watch(userPostsDataSourceProvider(pubkey));
+    if (dataSources == null) {
+      return (items: null, hasMore: false);
+    }
+
+    final data = ref.watch(entitiesPagedDataProvider(dataSources));
+    if (data == null) {
+      return (items: null, hasMore: false);
+    }
+
+    return (items: data.data.items, hasMore: data.hasMore);
+  }
+
+  @override
+  PagedNotifier getDelegate() {
+    final dataSources = ref.read(userPostsDataSourceProvider(pubkey));
+    if (dataSources == null) {
+      throw StateError('Data sources not available for user posts');
+    }
+    return ref.read(entitiesPagedDataProvider(dataSources).notifier);
+  }
+
+  bool _filterUserEntities(IonConnectEntity entity, String targetPubkey) {
+    if (entity.masterPubkey != targetPubkey) {
+      return false;
+    }
+
+    return (entity is ModifiablePostEntity && entity.data.parentEvent == null) ||
+        entity is GenericRepostEntity ||
+        (entity is PostEntity && entity.data.parentEvent == null) ||
+        entity is RepostEntity ||
+        entity is ArticleEntity;
+  }
+}
--- lib/app/features/user/providers/tab_data_source_provider.r.dart
+++ lib/app/features/user/providers/tab_data_source_provider.r.dart
@@ -3,8 +3,10 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/feed/providers/user_articles_data_source_provider.r.dart';
 import 'package:ion/app/features/feed/providers/user_posts_data_source_provider.r.dart';
+import 'package:ion/app/features/feed/providers/user_posts_provider.r.dart';
 import 'package:ion/app/features/feed/providers/user_replies_data_source_provider.r.dart';
 import 'package:ion/app/features/feed/providers/user_videos_data_source_provider.r.dart';
+import 'package:ion/app/features/ion_connect/model/ion_connect_entity.dart';
 import 'package:ion/app/features/ion_connect/providers/entities_paged_data_provider.m.dart';
 import 'package:ion/app/features/user/model/tab_entity_type.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -26,5 +28,29 @@ List<EntitiesDataSource>? tabDataSource(
       return ref.watch(userRepliesDataSourceProvider(pubkey));
     case TabEntityType.videos:
       return ref.watch(userVideosDataSourceProvider(pubkey));
+  }
+}
+
+@riverpod
+({Iterable<IonConnectEntity>? items, bool hasMore}) tabEntitiesData(
+  Ref ref, {
+  required TabEntityType type,
+  required String pubkey,
+}) {
+  switch (type) {
+    case TabEntityType.posts:
+      return ref.watch(userPostsProvider(pubkey));
+    case TabEntityType.articles:
+    case TabEntityType.replies:
+    case TabEntityType.videos:
+      final dataSource = ref.watch(tabDataSourceProvider(type: type, pubkey: pubkey));
+      if (dataSource == null) {
+        return (items: null, hasMore: false);
+      }
+      final entitiesPagedData = ref.watch(entitiesPagedDataProvider(dataSource));
+      if (entitiesPagedData == null) {
+        return (items: null, hasMore: false);
+      }
+      return (items: entitiesPagedData.data.items, hasMore: entitiesPagedData.hasMore);
   }
 }
