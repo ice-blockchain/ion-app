a59e9d8b1ecce17ed521b66f91eaa9742a647648
ice-hades <119406114+ice-hades@users.noreply.github.com>
2025-07-08 16:17:00 +0400

    feat :profile bio mentions (#1562)
    ## Description
    Profile bio mentions in edit and display mode;
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    
    
    https://github.com/user-attachments/assets/f2089b14-928e-47be-b482-b0df46c88af5

--- lib/app/features/components/user/user_about/user_about.dart
+++ lib/app/features/components/user/user_about/user_about.dart
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: ice License 1.0
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_quill/quill_delta.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/components/text_span_builder/hooks/use_text_span_builder.dart';
-import 'package:ion/app/components/text_span_builder/text_span_builder.dart';
+import 'package:ion/app/components/text_editor/text_editor_preview.dart';
 import 'package:ion/app/features/user/providers/user_metadata_provider.r.dart';
-import 'package:ion/app/services/text_parser/text_parser.dart';
 
 class UserAbout extends HookConsumerWidget {
   const UserAbout({
@@ -20,25 +22,31 @@ class UserAbout extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final text = ref.watch(
+    final about = ref.watch(
       userMetadataProvider(pubkey).select((state) => state.valueOrNull?.data.about),
     );
 
-    if (text == null) {
+    if (about == null) {
       return const SizedBox.shrink();
     }
 
+    final content = useMemoized(
+      () {
+        try {
+          return Delta.fromJson(jsonDecode(about) as List<dynamic>);
+        } on FormatException {
+          // Plain-text fallback: wrap the text in a Delta
+          return Delta()..insert('$about\n');
+        }
+      },
+      [about],
+    );
+
     return Padding(
       padding: padding,
-      child: SizedBox(
-        width: double.infinity,
-        child: Text.rich(
-          useTextSpanBuilder(context).build(
-            TextParser.allMatchers().parse(text),
-            onTap: (match) => TextSpanBuilder.defaultOnTap(context, match: match),
-          ),
-          textScaler: MediaQuery.textScalerOf(context),
-        ),
+      child: TextEditorPreview(
+        scrollable: false,
+        content: content,
       ),
     );
   }
--- lib/app/features/user/pages/profile_edit_page/profile_edit_page.dart
+++ lib/app/features/user/pages/profile_edit_page/profile_edit_page.dart
@@ -4,17 +4,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ion/app/components/controllers/hooks/use_text_editing_with_highlights_controller.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
 import 'package:ion/app/components/screen_offset/screen_top_offset.dart';
 import 'package:ion/app/components/separated/separator.dart';
+import 'package:ion/app/components/text_editor/components/suggestions_container.dart';
+import 'package:ion/app/components/text_editor/text_editor.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/auth/providers/auth_provider.m.dart';
-import 'package:ion/app/features/auth/views/components/user_data_inputs/bio_input.dart';
+import 'package:ion/app/features/auth/views/components/user_data_inputs/bio_rich_text_input.dart';
 import 'package:ion/app/features/auth/views/components/user_data_inputs/location_input.dart';
 import 'package:ion/app/features/auth/views/components/user_data_inputs/name_input.dart';
 import 'package:ion/app/features/auth/views/components/user_data_inputs/nickname_input.dart';
 import 'package:ion/app/features/auth/views/components/user_data_inputs/website_input.dart';
+import 'package:ion/app/features/feed/providers/suggestions/suggestions_notifier_provider.r.dart';
 import 'package:ion/app/features/user/hooks/update_user_metadata_error_message.dart';
 import 'package:ion/app/features/user/hooks/use_verify_nickname_availability_error_message.dart';
 import 'package:ion/app/features/user/pages/components/profile_avatar/profile_avatar.dart';
@@ -64,6 +66,10 @@ class ProfileEditPage extends HookConsumerWidget {
     final verifyNicknameErrorMessage = useVerifyNicknameAvailabilityErrorMessage(ref);
     final updateUserMetadataError = useUpdateUserMetadataErrorMessage(ref);
 
+    final textEditorKey = useMemoized(TextEditorKeys.bioInput);
+    final scrollController = useScrollController();
+    final suggestionsState = ref.watch(suggestionsNotifierProvider);
+
     return Scaffold(
       body: KeyboardDismissOnTap(
         child: Stack(
@@ -73,72 +79,72 @@ class ProfileEditPage extends HookConsumerWidget {
                 Expanded(
                   child: SingleChildScrollView(
                     child: ScreenTopOffset(
-                      child: Column(
-                        children: [
-                          ScreenSideOffset.large(
-                            child: Form(
-                              key: formKey,
-                              child: Column(
-                                children: [
-                                  ProfileAvatar(pubkey: pubkey, showAvatarPicker: true),
-                                  SizedBox(height: paddingValue),
-                                  NameInput(
-                                    initialValue: userMetadata.data.displayName,
-                                    isLive: true,
-                                    onChanged: (text) =>
-                                        update(draftRef.value.copyWith(displayName: text)),
-                                  ),
-                                  SizedBox(height: paddingValue),
-                                  NicknameInput(
-                                    initialValue: userMetadata.data.name,
-                                    isLive: true,
-                                    errorText: updateUserMetadataError.value ??
-                                        verifyNicknameErrorMessage.value,
-                                    onChanged: (text) {
-                                      updateUserMetadataError.value = null;
-                                      verifyNicknameErrorMessage.value = null;
-                                      update(draftRef.value.copyWith(name: text));
-                                      nickname.value = text;
-                                    },
-                                  ),
-                                  SizedBox(height: paddingValue),
-                                  BioInput(
-                                    controller: useTextEditingWithHighlightsController(
-                                      text: userMetadata.data.about,
-                                    ),
-                                    onChanged: (text) => update(
-                                      draftRef.value
-                                          .copyWith(about: text.trim().isEmpty ? null : text),
-                                    ),
-                                  ),
-                                  SizedBox(height: paddingValue),
-                                  CategorySelector(
-                                    selectedCategory: draftRef.value.category,
-                                    onChanged: (category) => update(
-                                      draftRef.value.copyWith(category: category),
-                                    ),
-                                  ),
-                                  SizedBox(height: paddingValue),
-                                  LocationInput(
-                                    initialValue: userMetadata.data.location,
-                                    onChanged: (text) => update(
-                                      draftRef.value.copyWith(location: text.isEmpty ? null : text),
-                                    ),
-                                  ),
-                                  SizedBox(height: paddingValue),
-                                  WebsiteInput(
-                                    initialValue: removeHttpsPrefix(userMetadata.data.website),
-                                    onChanged: (text) => update(
-                                      draftRef.value
-                                          .copyWith(website: text.trim().isEmpty ? null : text),
-                                    ),
-                                  ),
-                                  SizedBox(height: paddingValue),
-                                ],
+                      child: ScreenSideOffset.large(
+                        child: Form(
+                          key: formKey,
+                          child: Column(
+                            children: [
+                              ProfileAvatar(pubkey: pubkey, showAvatarPicker: true),
+                              SizedBox(height: paddingValue),
+                              NameInput(
+                                initialValue: userMetadata.data.displayName,
+                                isLive: true,
+                                onChanged: (text) =>
+                                    update(draftRef.value.copyWith(displayName: text)),
                               ),
-                            ),
+                              SizedBox(height: paddingValue),
+                              NicknameInput(
+                                initialValue: userMetadata.data.name,
+                                isLive: true,
+                                errorText: updateUserMetadataError.value ??
+                                    verifyNicknameErrorMessage.value,
+                                onChanged: (text) {
+                                  updateUserMetadataError.value = null;
+                                  verifyNicknameErrorMessage.value = null;
+                                  update(draftRef.value.copyWith(name: text));
+                                  nickname.value = text;
+                                },
+                              ),
+                              SizedBox(height: paddingValue),
+                              BioRichTextInput(
+                                textEditorKey: textEditorKey,
+                                initialValue: userMetadata.data.about,
+                                onChanged: (text) => update(
+                                  draftRef.value.copyWith(about: text.trim().isEmpty ? null : text),
+                                ),
+                              ),
+                              SuggestionsContainer(
+                                scrollController: scrollController,
+                                editorKey: textEditorKey,
+                              ),
+                              if (!(suggestionsState.isVisible &&
+                                  suggestionsState.suggestions.isNotEmpty))
+                                SizedBox(height: paddingValue),
+                              CategorySelector(
+                                selectedCategory: draftRef.value.category,
+                                onChanged: (category) => update(
+                                  draftRef.value.copyWith(category: category),
+                                ),
+                              ),
+                              SizedBox(height: paddingValue),
+                              LocationInput(
+                                initialValue: userMetadata.data.location,
+                                onChanged: (text) => update(
+                                  draftRef.value.copyWith(location: text.isEmpty ? null : text),
+                                ),
+                              ),
+                              SizedBox(height: paddingValue),
+                              WebsiteInput(
+                                initialValue: removeHttpsPrefix(userMetadata.data.website),
+                                onChanged: (text) => update(
+                                  draftRef.value
+                                      .copyWith(website: text.trim().isEmpty ? null : text),
+                                ),
+                              ),
+                              SizedBox(height: paddingValue),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
                     ),
                   ),
