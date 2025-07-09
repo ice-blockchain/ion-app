ea0906f3532bc2f2286bc9f10363c100d58963d7
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-07-09 11:30:00 +0300

    chore: add SPDX license identifier to scroll_to_bottom_button.dart

--- lib/app/features/chat/views/components/scroll_to_bottom_button.dart
+++ lib/app/features/chat/views/components/scroll_to_bottom_button.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:ion/app/extensions/extensions.dart';
