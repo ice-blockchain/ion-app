07e4c31d93f59552fa22d15444cefa468ddb26a5
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-07-09 11:11:29 +0300

    feat: add volume button unmute functionality for videos
    # Conflicts:
    #	ios/Podfile.lock

--- lib/app/router/components/app_router_builder.dart
+++ lib/app/router/components/app_router_builder.dart
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/global_notification_bar/global_notification_bar_wrapper.dart';
 import 'package:ion/app/extensions/extensions.dart';
+import 'package:ion/app/features/core/providers/mute_provider.r.dart';
+import 'package:ion/app/features/core/providers/volume_stream_provider.r.dart';
 import 'package:ion/app/features/debug/views/debug_rotate_gesture.dart';
 import 'package:ion/app/features/feed/global_notifications/helpers/feed_global_notifications_helper.dart';
 import 'package:ion/app/features/protect_account/secure_account/views/components/two_fa_signature_wrapper.dart';
@@ -16,6 +19,27 @@ class AppRouterBuilder extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final previousVolume = useRef<double>(0);
+
+    ref.listen(volumeStreamProvider, (previous, next) {
+      next.whenData((volume) {
+        final currentMuteState = ref.read(globalMuteNotifierProvider);
+        final prevVol = previousVolume.value;
+
+        // If video is muted and volume increased - unmute
+        if (currentMuteState && volume > prevVol && volume > 0.0) {
+          ref.read(globalMuteNotifierProvider.notifier).toggle();
+        }
+
+        // If device is in silent mode (volume is 0) - mute video
+        if (volume == 0.0 && !currentMuteState) {
+          ref.read(globalMuteNotifierProvider.notifier).toggle();
+        }
+
+        previousVolume.value = volume;
+      });
+    });
+
     return Material(
       color: context.theme.appColors.secondaryBackground,
       child: GlobalNotificationBarWrapper(
--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -9,6 +9,7 @@
 #include <file_saver/file_saver_plugin.h>
 #include <file_selector_linux/file_selector_plugin.h>
 #include <flutter_secure_storage_linux/flutter_secure_storage_linux_plugin.h>
+#include <flutter_volume_controller/flutter_volume_controller_plugin.h>
 #include <screen_retriever_linux/screen_retriever_linux_plugin.h>
 #include <sentry_flutter/sentry_flutter_plugin.h>
 #include <sqlite3_flutter_libs/sqlite3_flutter_libs_plugin.h>
@@ -25,6 +26,9 @@ void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) flutter_secure_storage_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterSecureStorageLinuxPlugin");
   flutter_secure_storage_linux_plugin_register_with_registrar(flutter_secure_storage_linux_registrar);
+  g_autoptr(FlPluginRegistrar) flutter_volume_controller_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterVolumeControllerPlugin");
+  flutter_volume_controller_plugin_register_with_registrar(flutter_volume_controller_registrar);
   g_autoptr(FlPluginRegistrar) screen_retriever_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "ScreenRetrieverLinuxPlugin");
   screen_retriever_linux_plugin_register_with_registrar(screen_retriever_linux_registrar);
