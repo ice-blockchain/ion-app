db3da74eb834760e4b38fda81d0bdae662ad7ec9
ice-hector <96414297+ice-hector@users.noreply.github.com>
2025-07-09 10:02:13 +0300

    fix: license

--- lib/app/services/media_service/media_upload_service.dart
+++ lib/app/services/media_service/media_upload_service.dart
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: ice License 1.0
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/features/core/model/media_type.dart';
 import 'package:ion/app/features/ion_connect/model/file_alt.dart';
