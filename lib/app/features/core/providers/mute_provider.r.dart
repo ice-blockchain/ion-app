347a9b0683760029f3bc80a96d2879e6e13df7bf
ice-alcides <171546305+ice-alcides@users.noreply.github.com>
2025-07-15 11:36:07 +0300

    revert: rollback volume control functionality (#1617)
    ## Description
    This PR reverts the volume control functionality introduced in PRs #1602
    and #1581, while preserving the padding fix for the video preview mute
    button. This functionality causes other bugs, we will re-add it later.
    
    ## Additional Notes
    <!-- Add any extra context or relevant information here. -->
    
    ## Task ID
    <!-- Add corresponding task ID here. -->
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <!-- Include screenshots to demonstrate any UI changes. -->
    <!-- <img width="180" alt="image" src="image_url_here"> -->

--- lib/app/features/core/providers/mute_provider.r.dart
+++ lib/app/features/core/providers/mute_provider.r.dart
@@ -3,9 +3,6 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:flutter_volume_controller/flutter_volume_controller.dart';
-import 'package:ion/app/features/auth/providers/auth_provider.m.dart';
-import 'package:ion/app/features/core/providers/volume_stream_provider.r.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'mute_provider.r.g.dart';
@@ -13,14 +10,11 @@ part 'mute_provider.r.g.dart';
 @Riverpod(keepAlive: true)
 class GlobalMuteNotifier extends _$GlobalMuteNotifier {
   static const _channel = MethodChannel('audio_focus_channel');
-  double _previousVolume = 0;
 
   @override
   bool build() {
     _setupMethodChannelHandler();
     _initializeAudioFocus();
-    _setupVolumeListener();
-    _setupAuthListener();
     return true;
   }
 
@@ -32,42 +26,6 @@ class GlobalMuteNotifier extends _$GlobalMuteNotifier {
 
   Future<void> _initializeAudioFocus() async {
     await _channel.invokeMethod<bool>('initAudioFocus');
-    await _channel.invokeMethod<bool>('abandonAudioFocus');
-  }
-
-  void _setupVolumeListener() {
-    FlutterVolumeController.getVolume().then((volume) {
-      if (volume != null) {
-        _previousVolume = volume;
-      }
-    });
-
-    ref.listen(volumeStreamProvider, (previous, next) {
-      next.whenData((volume) {
-        final currentMuteState = state;
-        final prevVol = _previousVolume;
-
-        // If video is muted and volume increased - unmute
-        if (currentMuteState && volume > prevVol && volume > 0.0) {
-          toggle();
-        }
-
-        // If device is in silent mode (volume is 0) - mute video
-        if (volume == 0.0 && !currentMuteState) {
-          toggle();
-        }
-
-        _previousVolume = volume;
-      });
-    });
-  }
-
-  void _setupAuthListener() {
-    ref.listen(currentIdentityKeyNameSelectorProvider, (previous, next) {
-      if (previous != next && next != null) {
-        reset();
-      }
-    });
   }
 
   Future<void> toggle() async {
@@ -94,11 +52,5 @@ class GlobalMuteNotifier extends _$GlobalMuteNotifier {
     if (success) {
       state = willBeMuted;
     }
-  }
-
-  Future<void> reset() async {
-    state = true;
-    _previousVolume = 0;
-    await _channel.invokeMethod<bool>('abandonAudioFocus');
   }
 }
