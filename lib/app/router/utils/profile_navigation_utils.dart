2cf0129513e41da4352c25375844ec307747190a
ice-hector <96414297+ice-hector@users.noreply.github.com>
2025-07-17 10:07:12 +0300

    fix: same profile push

---
+++ lib/app/router/utils/profile_navigation_utils.dart
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: ice License 1.0
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:ion/app/router/app_routes.gr.dart';
+
+class ProfileNavigationUtils {
+  static Future<T?> navigateToProfile<T extends Object?>(
+    BuildContext context,
+    String pubkey,
+  ) {
+    final currentState = GoRouterState.of(context);
+    final currentPath = currentState.matchedLocation;
+
+    final isCurrentlyOnProfile =
+        currentPath.startsWith('/profile/user/') || currentPath.contains('/user/');
+
+    if (isCurrentlyOnProfile) {
+      final currentPubkey = currentState.pathParameters['pubkey'];
+      if (currentPubkey == pubkey) {
+        return Future.value();
+      }
+    }
+
+    return ProfileRoute(pubkey: pubkey).push<T>(context);
+  }
+}
