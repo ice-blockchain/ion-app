58c7e88634fbf5b4a72c03199e7a9a822bbb0265
ice-neoptolemus <214448594+ice-neoptolemus@users.noreply.github.com>
2025-07-18 09:50:04 +0300

    feat: add verify badge to friends and navigate to profile (#1650)
    ## Description
    Add verify badge to friends and navigate to profile
    
    ## Task ID
    3283
    
    ## Type of Change
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore
    
    ## Screenshots (if applicable)
    <img width="298" height="229" alt="image"
    src="https://github.com/user-attachments/assets/fa873b71-94fe-47f9-aa9d-9c3666fa6fef"
    />

--- lib/app/features/wallets/views/pages/contact_modal_page/components/contact_item.dart
+++ lib/app/features/wallets/views/pages/contact_modal_page/components/contact_item.dart
@@ -2,12 +2,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:ion/app/extensions/build_context.dart';
-import 'package:ion/app/extensions/num.dart';
-import 'package:ion/app/extensions/theme_data.dart';
+import 'package:go_router/go_router.dart';
+import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/user/model/user_metadata.f.dart';
 import 'package:ion/app/features/wallets/views/pages/contact_modal_page/components/contact_item_avatar.dart';
 import 'package:ion/app/features/wallets/views/pages/contact_modal_page/components/contact_item_name.dart';
+import 'package:ion/app/router/app_routes.gr.dart';
 import 'package:ion/app/utils/username.dart';
 
 class ContactItem extends StatelessWidget {
@@ -20,18 +20,28 @@ class ContactItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ContactItemAvatar(pubkey: userMetadata.masterPubkey),
-        SizedBox(height: 8.0.s),
-        ContactItemName(userMetadata: userMetadata.data),
-        SizedBox(height: 4.0.s),
-        Text(
-          prefixUsername(username: userMetadata.data.name, context: context),
-          style: context.theme.appTextThemes.caption
-              .copyWith(color: context.theme.appColors.tertararyText),
-        ),
-      ],
+    return GestureDetector(
+      onTap: () async {
+        context.pop();
+        await ProfileRoute(pubkey: userMetadata.masterPubkey).push<void>(context);
+        final rootContext = rootNavigatorKey.currentContext;
+        if (rootContext != null && rootContext.mounted) {
+          await ContactRoute(pubkey: userMetadata.masterPubkey).push<void>(rootContext);
+        }
+      },
+      child: Column(
+        children: [
+          ContactItemAvatar(pubkey: userMetadata.masterPubkey),
+          SizedBox(height: 8.0.s),
+          ContactItemName(userMetadata: userMetadata),
+          SizedBox(height: 4.0.s),
+          Text(
+            prefixUsername(username: userMetadata.data.name, context: context),
+            style: context.theme.appTextThemes.caption
+                .copyWith(color: context.theme.appColors.tertararyText),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/app/features/wallets/views/pages/contact_modal_page/components/contact_item_name.dart
+++ lib/app/features/wallets/views/pages/contact_modal_page/components/contact_item_name.dart
@@ -1,22 +1,40 @@
 // SPDX-License-Identifier: ice License 1.0
 
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/user/model/user_metadata.f.dart';
+import 'package:ion/app/features/user/providers/badges_notifier.r.dart';
+import 'package:ion/generated/assets.gen.dart';
 
-class ContactItemName extends StatelessWidget {
+class ContactItemName extends ConsumerWidget {
   const ContactItemName({
     required this.userMetadata,
     super.key,
   });
 
-  final UserMetadata userMetadata;
+  final UserMetadataEntity userMetadata;
 
   @override
-  Widget build(BuildContext context) {
-    return Text(
-      userMetadata.displayName,
-      style: context.theme.appTextThemes.title,
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isVerified =
+        ref.watch(isUserVerifiedProvider(userMetadata.masterPubkey)).valueOrNull.falseOrValue;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Flexible(
+          child: Text(
+            userMetadata.data.displayName,
+            style: context.theme.appTextThemes.title,
+          ),
+        ),
+        if (isVerified)
+          Padding(
+            padding: EdgeInsetsDirectional.only(start: 4.0.s),
+            child: Assets.svg.iconBadgeVerify.icon(size: 16.0.s),
+          ),
+      ],
     );
   }
 }
