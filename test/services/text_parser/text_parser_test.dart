c28ad6b069fb9dda68953dead9065041bc15c813
ice-hades <119406114+ice-hades@users.noreply.github.com>
2025-07-21 10:59:55 +0400

    fix: link regex (#1674)
    ## Description
    - Improved and fixed link recognition Regex
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [ ] Chore

--- test/services/text_parser/text_parser_test.dart
+++ test/services/text_parser/text_parser_test.dart
@@ -26,6 +26,20 @@ void main() {
       expect(results.length, 4);
     });
 
+    test('should parse mentions with dots correctly', () {
+      final results = parser.parse('Hello @user.1 and @user.2');
+
+      expect(results.length, equals(4));
+      expect(results[0].text, equals('Hello '));
+      expect(results[1].text, equals('@user.1'));
+      expect(results[1].matcher, isA<MentionMatcher>());
+      expect(results[2].text, equals(' and '));
+      expect(results[3].text, equals('@user.2'));
+      expect(results[3].matcher, isA<MentionMatcher>());
+
+      expect(results.length, 4);
+    });
+
     test('should parse hashtags correctly', () {
       final results = parser.parse('Check out #flutter and #dart');
 
@@ -52,6 +66,54 @@ void main() {
       expect(results[3].matcher, isA<UrlMatcher>());
 
       expect(results.length, 4);
+    });
+
+    test('should not parse as URLs any text with dot', () {
+      final results = parser.parse('Some dummy text.To test links');
+
+      expect(results.length, equals(1));
+      expect(results[0].text, equals('Some dummy text.To test links'));
+    });
+
+    test('should parse www-prefixed URLs correctly', () {
+      final results = parser.parse('Visit www.example.org/page1 for info');
+
+      expect(results.length, equals(3));
+      expect(results[0].text, equals('Visit '));
+      expect(results[1].text, equals('www.example.org/page1'));
+      expect(results[1].matcher, isA<UrlMatcher>());
+      expect(results[2].text, equals(' for info'));
+
+      expect(results.length, 3);
+    });
+
+    test('should parse URLs with port and path correctly', () {
+      final results = parser.parse('Server at http://localhost:8080/path/to');
+
+      expect(results.length, equals(2));
+      expect(results[0].text, equals('Server at '));
+      expect(results[1].text, equals('http://localhost:8080/path/to'));
+      expect(results[1].matcher, isA<UrlMatcher>());
+
+      expect(results.length, 2);
+    });
+
+    test('should parse URLs with credentials correctly', () {
+      final results = parser.parse('Login at http://user:pass@example.com/files');
+
+      expect(results.length, equals(2));
+      expect(results[0].text, equals('Login at '));
+      expect(results[1].text, equals('http://user:pass@example.com/files'));
+      expect(results[1].matcher, isA<UrlMatcher>());
+
+      expect(results.length, 2);
+    });
+
+    test('should not parse URLs without protocol or www prefix', () {
+      final results = parser.parse('Check example.com and foo.bar');
+
+      expect(results.length, equals(1));
+      expect(results[0].text, equals('Check example.com and foo.bar'));
     });
 
     test('should parse mixed content correctly', () {
