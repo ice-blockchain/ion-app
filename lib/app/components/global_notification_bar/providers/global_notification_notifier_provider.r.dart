2ccc9f960ca5c1a3d347271a70ac63f4af20b8ce
ice-kreios <180917405+ice-kreios@users.noreply.github.com>
2025-07-22 16:20:29 +0300

    fix: prevent loading notification from getting stuck on error

--- lib/app/components/global_notification_bar/providers/global_notification_notifier_provider.r.dart
+++ lib/app/components/global_notification_bar/providers/global_notification_notifier_provider.r.dart
@@ -18,12 +18,14 @@ class GlobalNotificationNotifier extends _$GlobalNotificationNotifier {
 
   Timer? _hideTimer;
   CancelableOperation<void>? _cancelableShowOperation;
+  CancelableOperation<void>? _cancelableHideOperation;
 
   static const _notificationDuration = Duration(seconds: 3);
 
   void show(GlobalNotification notification, {bool isPermanent = false}) {
     // Prevent bottom sheet from jumping when showing notification
     _cancelableShowOperation?.cancel();
+    _cancelableHideOperation?.cancel();
     _cancelableShowOperation = CancelableOperation.fromFuture(
       Future.delayed(
         GlobalNotificationBar.animationDuration,
@@ -39,7 +41,15 @@ class GlobalNotificationNotifier extends _$GlobalNotificationNotifier {
   }
 
   void hide() {
-    _hideTimer?.cancel();
-    state = null;
+    _cancelableHideOperation?.cancel();
+    _cancelableHideOperation = CancelableOperation.fromFuture(
+      Future.delayed(
+        GlobalNotificationBar.animationDuration,
+        () {
+          _hideTimer?.cancel();
+          state = null;
+        },
+      ),
+    );
   }
 }
