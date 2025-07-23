c15397f40104f48cd7b3b39c90c4a10e6a60a321
Ice Hades <119406114+ice-hades@users.noreply.github.com>
2025-07-23 13:36:20 +0400

    fix: advanced search people bio ui

--- lib/app/features/search/views/pages/feed_advanced_search_page/components/feed_advanced_search_users/feed_advanced_search_user_list_item.dart
+++ lib/app/features/search/views/pages/feed_advanced_search_page/components/feed_advanced_search_users/feed_advanced_search_user_list_item.dart
@@ -4,13 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ion/app/components/list_item/badges_user_list_item.dart';
 import 'package:ion/app/components/screen_offset/screen_side_offset.dart';
+import 'package:ion/app/components/text_editor/hooks/use_text_delta.dart';
+import 'package:ion/app/components/text_editor/text_editor_preview.dart';
+import 'package:ion/app/components/text_editor/utils/text_editor_styles.dart';
 import 'package:ion/app/extensions/extensions.dart';
 import 'package:ion/app/features/components/user/follow_user_button/follow_user_button.dart';
 import 'package:ion/app/features/user/model/user_metadata.f.dart';
 import 'package:ion/app/router/app_routes.gr.dart';
 import 'package:ion/app/utils/username.dart';
 
-class FeedAdvancedSearchUserListItem extends ConsumerWidget {
+class FeedAdvancedSearchUserListItem extends HookConsumerWidget {
   const FeedAdvancedSearchUserListItem({
     required this.user,
     super.key,
@@ -21,6 +24,7 @@ class FeedAdvancedSearchUserListItem extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final UserMetadata(:about, :displayName, :name, :picture) = user.data;
+    final aboutDelta = useTextDelta(about ?? '');
 
     return GestureDetector(
       onTap: () => ProfileRoute(pubkey: user.masterPubkey).push<void>(context),
@@ -39,9 +43,11 @@ class FeedAdvancedSearchUserListItem extends ConsumerWidget {
             ),
             if (about != null) ...[
               SizedBox(height: 10.0.s),
-              Text(
-                about,
-                style: context.theme.appTextThemes.body2.copyWith(
+              TextEditorPreview(
+                scrollable: false,
+                content: aboutDelta,
+                customStyles: textEditorStyles(
+                  context,
                   color: context.theme.appColors.sharkText,
                 ),
               ),
