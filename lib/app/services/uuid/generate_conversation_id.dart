6a469750ab71e748ce603449a399b82fa3bacc3d
ice-damocles <190337044+ice-damocles@users.noreply.github.com>
2025-07-24 14:25:54 +0300

    fix: switch case

--- lib/app/services/uuid/generate_conversation_id.dart
+++ lib/app/services/uuid/generate_conversation_id.dart
@@ -10,13 +10,11 @@ String generateConversationId({
   final sorted = List<String>.from(receiverMasterPubkeys)..sort();
 
   // For one-to-one conversations, we use v5 UUID to ensure the same conversation ID for the same participants.
-  if (conversationType == ConversationType.oneToOne) {
-    return const Uuid().v5(Namespace.nil.value, sorted.join());
-  } else if (conversationType == ConversationType.group) {
-    return const Uuid().v7();
-  } else if (conversationType == ConversationType.community) {
-    return const Uuid().v7();
-  } else {
-    throw UnsupportedError('Unsupported conversation type: $conversationType');
+  switch (conversationType) {
+    case ConversationType.oneToOne:
+      return const Uuid().v5(Namespace.nil.value, sorted.join());
+    case ConversationType.group:
+    case ConversationType.community:
+      return const Uuid().v7();
   }
 }
