acb937564b6aca9618336aab4540b13f1c818174
ice-hades <119406114+ice-hades@users.noreply.github.com>
2025-08-01 23:37:00 +0400

    fix: passkey platform exception on android (#1778)
    ## Description
    - upgraded passkey lib;
    - added isUserVerifyingPlatformAuthenticatorAvailable check to passkey
    availability check
    
    ## Type of Change
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Refactoring
    - [ ] Documentation
    - [x] Chore

--- packages/ion_identity_client/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/ion_identity_client/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
 import flutter_secure_storage_macos
 import local_auth_darwin
+import package_info_plus
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
